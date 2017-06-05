b3a0e986573e425116ed83c53ca753eaf527c6a5
alex <766f6964+github@gmail.com>
2017-06-05 12:16:57 +0200

    changed methods for returning self object

--- include/Spieldose/DatabaseParam.php
+++ include/Spieldose/DatabaseParam.php
@@ -12,46 +12,51 @@
         /**
         *   set param properties
         */
-        public function set($name, $value, $type) {
+        public function set($name, $value, $type): \Spieldose\DatabaseParam {
 			$this->name = $name;
 			$this->value = $value;
 			$this->type = $type;
+            return($this);
 		}
 
         /**
         *   set NULL param
         */
-		public function null(string $name) {
+		public function null(string $name): \Spieldose\DatabaseParam {
             $this->name = $name;
             $this->value = null;
             $this->type = \PDO::PARAM_NULL;
+            return($this);
 		}
 
         /**
         *   set BOOL param
         */
-		public function bool(string $name, bool $value) {
+		public function bool(string $name, bool $value): \Spieldose\DatabaseParam {
             $this->name = $name;
             $this->value = $value;
             $this->type = \PDO::PARAM_BOOL;
+            return($this);
 		}
 
         /**
         *   set INTEGER param
         */
-		public function int(string $name, int $value) {
+		public function int(string $name, int $value): \Spieldose\DatabaseParam {
             $this->name = $name;
             $this->value = $value;
             $this->type = \PDO::PARAM_INT;
+            return($this);
 		}
 
         /**
         *   set STRING param
         */
-		public function str(string $name, $value) {
+		public function str(string $name, $value): \Spieldose\DatabaseParam {
             $this->name = $name;
             $this->value = $value;
             $this->type = \PDO::PARAM_STR;
+            return($this);
 		}
 
     }
