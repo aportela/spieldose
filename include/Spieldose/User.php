763c008872011560f6046db113ba69368452f0f8
alex <766f6964+github@gmail.com>
2017-06-05 19:05:33 +0200

    added user logout (end session) method

--- include/Spieldose/User.php
+++ include/Spieldose/User.php
@@ -36,6 +36,17 @@
             }
         }
 
+        public static function logout() {
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+            }
+            if (session_status() != PHP_SESSION_NONE) {
+                session_destroy();
+            }
+        }
+
         public function set(\Spieldose\Database $dbh, string $password) {
             if ($dbh == null) {
                 $dbh = new \Spieldose\Database();
