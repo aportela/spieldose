635f1b4a2c4b7bd07577c0d692dfd285b013c415
alex <766f6964+github@gmail.com>
2017-06-05 17:58:33 +0200

    album search changed

--- include/Spieldose/Album.php
+++ include/Spieldose/Album.php
@@ -5,15 +5,22 @@
     class Album
     {
 
-	    public function __construct (string $short, array $long) { }
+	    public function __construct () { }
 
         public function __destruct() { }
 
-        public static function search(\Spieldose\Database $dbh) {
+        public static function search(\Spieldose\Database $dbh, array $filter = array(), string $order = "") {
             if ($dbh == null) {
                 $dbh = new \Spieldose\Database();
             }
-            return($dbh->query("SELECT DISTINCT album as name, artist, albumartist, year, images FROM FILE WHERE album IS NOT NULL GROUP BY COALESCE(albumartist, artist)"));
+            $sqlOrder = "";
+            if (! empty($order)) {
+                $sqlOrder = " ORDER BY RANDOM() ";
+            } else {
+                $sqlOrder = " ORDER BY FILE.album ASC, FILE.albumartist ASC, FILE.artist ASC ";
+            }
+            $query = sprintf(" SELECT DISTINCT album as name, artist, albumartist, year, images FROM FILE WHERE album IS NOT NULL GROUP BY COALESCE(albumartist, artist) %s ", $sqlOrder);
+            return($dbh->query($query));
         }
     }
 
