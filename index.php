d2f300c6450f33c6b679da2a15604c16c7461be3
alex <766f6964+github@gmail.com>
2017-06-05 10:08:58 +0200

    default index script

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace Spieldose;
+
+    ob_start();
+
+    require_once __DIR__ . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR . "configuration.php";
+
+    ob_flush();
+
+?>
