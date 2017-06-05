a61f79b2ec417e72efe6e45603c7de54625ada89
alex <766f6964+github@gmail.com>
2017-06-05 11:38:24 +0200

    database configuration params

--- include/configuration.php
+++ include/configuration.php
@@ -5,6 +5,12 @@
 
     define("DEBUG", true);
 
+    define("PDO_TYPE", "sqlite3");
+    define("SQLITE_DATABASE_PATH", dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "spieldose.sqlite3");
+    define("PDO_USERNAME", "");
+    define("PDO_PASSWORD", "");    
+    define("PDO_CONNECTION_STRING", sprintf("sqlite:%s", SQLITE_DATABASE_PATH));
+
     spl_autoload_register(function ($className) {
         $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
         if (! file_exists($file)) {
