e737def8e9f90948e7323027090c312314bad3e7
alex <766f6964+github@gmail.com>
2017-06-07 20:39:19 +0200

    added track search method

--- include/Spieldose/Track.php
+++ include/Spieldose/Track.php
@@ -35,6 +35,29 @@
                 throw new \Spieldose\Exception\InvalidParamsException("id");
             }
         }
+
+        public static function search(\Spieldose\Database $dbh, int $page = 1, int $resultsPage = 16, array $filter = array(), string $order = "") {
+            if ($dbh == null) {
+                $dbh = new \Spieldose\Database();
+            }
+            $queryCount = "SELECT COUNT(DISTINCT title) AS total FROM FILE";
+            $result = $dbh->query($queryCount);
+            $data = new \stdClass();
+            $data->actualPage = $page;
+            $data->resultsPage = $resultsPage;
+            $data->totalResults = $result[0]->total;
+            $data->totalPages = ceil(($data->totalResults + $resultsPage - 1) / $resultsPage);
+            $sqlOrder = "";
+            if (! empty($order)) {
+                $sqlOrder = " ORDER BY RANDOM() ";
+            } else {
+                $sqlOrder = " ORDER BY FILE.title ASC, FILE.artist ASC, FILE.albumartist ASC ";
+            }
+            $query = sprintf(" SELECT DISTINCT id, title, artist, albumartist, year, images FROM FILE WHERE album IS NOT NULL GROUP BY COALESCE(artist, albumartist) %s LIMIT %d OFFSET %d", $sqlOrder, $resultsPage, $resultsPage * $page);
+            $data->results = $dbh->query($query);
+            return($data);
+        }
+
     }
 
 ?>
