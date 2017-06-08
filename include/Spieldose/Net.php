0f29824c1530e0040001e8067ac8dcf14a41f723
alex <766f6964+github@gmail.com>
2017-06-08 21:27:21 +0200

    networking class

---
+++ include/Spieldose/Net.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace Spieldose;
+
+    class Net
+    {
+
+        /**
+        *   make web request
+        */
+        public static function httpRequest($url) {
+            $ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$referer = "http://" . parse_url($url, PHP_URL_HOST);
+			curl_setopt ($ch, CURLOPT_REFERER, $referer);
+			curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19');
+			$content = curl_exec($ch);
+			curl_close($ch);
+			return($content);
+        }
+
+    }
+
+?>
