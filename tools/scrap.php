27a01c73eef00094fcdf27c3d1e31a7603c35685
alex <766f6964+github@gmail.com>
2017-06-10 14:01:53 +0200

    added musicbrainz album scrapping support

--- tools/scrap.php
+++ tools/scrap.php
@@ -8,7 +8,7 @@
 
     echo "Spieldose scrap utility " . PHP_EOL;
 
-    $cmdLine = new \Spieldose\CmdLine("", array("artists"));
+    $cmdLine = new \Spieldose\CmdLine("", array("artists", "albums"));
     if ($cmdLine->hasParam("artists")) {
         $dbh = new \Spieldose\Database();
         $scrapper = new \Spieldose\Scrapper();
@@ -23,6 +23,28 @@
                 $failed[] = $artists[$i];
             }
             \Spieldose\Utils::showProgressBar($i + 1, $totalArtists, 20);
+        }
+        $totalFailed = count($failed);
+        if ($totalFailed > 0) {
+            echo sprintf("Failed to scrap %d artists:%s", $totalFailed, PHP_EOL);
+            print_r($failed);
+        }
+    }
+    if ($cmdLine->hasParam("albums")) {
+        $dbh = new \Spieldose\Database();
+        $scrapper = new \Spieldose\Scrapper();
+        $albums = $scrapper->getPendingAlbums($dbh);
+        $totalAlbums = count($albums);
+        $failed = array();
+        echo sprintf("Processing %d albums%s", $totalAlbums, PHP_EOL);
+        for ($i = 0; $i < $totalAlbums; $i++) {
+            try {
+                $scrapper->mbAlbumScrap($dbh, $albums[$i]->album, $albums[$i]->artist);
+            } catch (\Throwable $e) {
+                print_r($e);
+                $failed[] = $albums[$i];
+            }
+            \Spieldose\Utils::showProgressBar($i + 1, $totalAlbums, 20);
         }
         $totalFailed = count($failed);
         if ($totalFailed > 0) {
