db18f3b3f9ee9da1c821ea81a2921c40600e4ca6
alex <766f6964+github@gmail.com>
2017-06-10 13:35:19 +0200

    fixed pagination & order on browse albums section

--- include/Spieldose/Album.php
+++ include/Spieldose/Album.php
@@ -13,7 +13,13 @@
             if ($dbh == null) {
                 $dbh = new \Spieldose\Database();
             }
-            $queryCount = "SELECT COUNT(DISTINCT album) AS total FROM FILE";
+            $queryCount = '
+                SELECT
+                    COUNT (DISTINCT(COALESCE(MBC.album, F.album_name))) AS total
+                FROM FILE F
+                LEFT JOIN MB_CACHE_ALBUM MBC ON MBC.mbid = F.album_mbid
+                WHERE COALESCE(MBC.album, F.album_name) IS NOT NULL
+            ';
             $result = $dbh->query($queryCount);
             $data = new \stdClass();
             $data->actualPage = $page;
@@ -21,12 +27,29 @@
             $data->totalResults = $result[0]->total;
             $data->totalPages = ceil(($data->totalResults + $resultsPage - 1) / $resultsPage);
             $sqlOrder = "";
-            if (! empty($order)) {
+            if (! empty($order) && $order == "random") {
                 $sqlOrder = " ORDER BY RANDOM() ";
             } else {
-                $sqlOrder = " ORDER BY FILE.album ASC, FILE.albumartist ASC, FILE.artist ASC ";
+                $sqlOrder = " ORDER BY COALESCE(album, album_name) COLLATE NOCASE ASC ";
             }
-            $query = sprintf(" SELECT DISTINCT album as name, artist, albumartist, year, images FROM FILE WHERE album IS NOT NULL GROUP BY COALESCE(albumartist, artist) %s LIMIT %d OFFSET %d", $sqlOrder, $resultsPage, $resultsPage * $page);
+            $query = sprintf('
+                SELECT DISTINCT
+                    COALESCE(album, album_name) as name,
+                    F.track_artist AS artist,
+                    F.album_artist AS albumartist,
+                    F.year,
+                    MBC.image
+                FROM FILE F
+                LEFT JOIN MB_CACHE_ALBUM MBC ON MBC.mbid = F.album_mbid
+                WHERE F.album_name IS NOT NULL
+                GROUP BY COALESCE(MBC.album, F.album_name)
+                %s
+                LIMIT %d OFFSET %d
+                ',
+                $sqlOrder,
+                $resultsPage,
+                $resultsPage * ($page - 1)
+            );
             $data->results = $dbh->query($query);
             return($data);
         }
