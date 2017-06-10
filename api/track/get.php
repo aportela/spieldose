331b88377ce80ad67696311f0401e4c719f70fe4
alex <766f6964+github@gmail.com>
2017-06-10 13:17:23 +0200

    prevent cache on api json responses

--- api/track/get.php
+++ api/track/get.php
@@ -73,6 +73,9 @@
     } finally {
         if ($errors) {
             header("Content-Type: application/json; charset=utf-8");
+            header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+            header("Cache-Control: post-check=0, pre-check=0", false);
+            header("Pragma: no-cache");
             echo json_encode($response);
         }
     }
