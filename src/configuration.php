2e3501eeef76ff102ea9a372802a966e32511d51
alex <766f6964+github@gmail.com>
2017-10-26 17:29:15 +0200

    old configuration settings (TODO: move to AppSettings.php)

---
+++ src/configuration.php
@@ -0,0 +1,15 @@
+<?php
+
+    declare(strict_types = 1);
+
+    namespace Spieldose;
+
+    define("DEBUG", true);
+
+    define("PDO_TYPE", "sqlite3");
+    define("SQLITE_DATABASE_PATH", dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "spieldose.sqlite3");
+    define("PDO_USERNAME", "");
+    define("PDO_PASSWORD", "");
+    define("PDO_CONNECTION_STRING", sprintf("sqlite:%s", SQLITE_DATABASE_PATH));
+
+?>
