49f4f11f8fd86eccc54fcc56f135e2600a690301
alex <766f6964+github@gmail.com>
2017-10-26 18:01:33 +0200

    added some slim routes

---
+++ src/AppRoutes.php
@@ -0,0 +1,26 @@
+<?php
+    declare(strict_types=1);
+
+    use Slim\Http\Request;
+    use Slim\Http\Response;
+
+    $app->get('/', function (Request $request, Response $response, array $args) {
+        $this->logger->info("Slim-Skeleton GET '/' route");
+        return $response->withRedirect('/login');
+    });
+
+    $app->get('/login', function (Request $request, Response $response, array $args) {
+        $this->logger->info("Slim-Skeleton GET '/login' route");
+        return $this->view->render($response, 'login.html.twig', []);
+    });
+
+    $app->post('/api/user/signin', function (Request $request, Response $response, array $args) {
+        $this->logger->info("Slim-Skeleton POST '/api/user/signin' route");
+        $u = new \Spieldose\User($request->getParam("email"));
+        if ($u->login(new \Spieldose\Database\DB(), $request->getParam("password"))) {
+            return $response->withJson(['logged' => true], 200);
+        } else {
+            return $response->withJson(['logged' => false], 401);
+        }
+    })->add(new \Spieldose\Middleware\APIExceptionCatcher);
+?>
