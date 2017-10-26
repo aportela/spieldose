08b68bf478a94d06f7341cbc44a28c956b00c844
alex <766f6964+github@gmail.com>
2017-10-26 17:26:42 +0200

    exception classes added

---
+++ src/Exception/AccessDeniedException.php
@@ -0,0 +1,20 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace Spieldose\Exception;
+
+    /**
+     * access denied exception
+     */
+    class AccessDeniedException extends \Exception {
+        public function __construct($message = "", $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    }
+
+?>
---
+++ src/Exception/InvalidParamsException.php
@@ -0,0 +1,20 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace Spieldose\Exception;
+
+    /**
+     * invalid parameters exception
+     */
+    class InvalidParamsException extends \Exception {
+        public function __construct($message = "", $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+
+        public function __toString() {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+    }
+
+?>
