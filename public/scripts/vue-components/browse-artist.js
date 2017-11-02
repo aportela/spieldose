b544e6f38b9248f3f0f07792145a9beb9bc3dcd2
alex <766f6964+github@gmail.com>
2017-11-02 21:03:47 +0100

    artist tab changes: enabled full bio & top tracks (not filtering by artist at this time)

--- public/scripts/vue-components/browse-artist.js
+++ public/scripts/vue-components/browse-artist.js
@@ -25,26 +25,35 @@ var vTemplateBrowseArtist = function () {
                             <li v-bind:class="{ 'is-active' : activeTab == 'albums' }"><a href="#" v-on:click.prevent="changeTab('albums');">Albums</a></li>
                         </ul>
                     </div>
-                    <div class="content is-clearfix" id="bio" v-if="artist.bio" v-html="truncatedBio"></div>
-                    <div class="panel" v-if="activeTab =='albums'">
-                    <h2>Albums: <i class="is-unselectable fa fa-2x fa-th-large" v-on:click.prevent="hideAlbumDetails()"></i><i class="is-unselectable fa fa-2x fa-list-ol" v-on:click.prevent="showAlbumDetails()"></i></h2>
-                    <div class="album_item" v-for="album in artist.albums">
-                        <a class="play_album" v-on:click="playAlbum(album.name, artist.name)">
-                            <img class="album_cover" v-if="album.image" v-bind:src="album.image"/>
-                            <img class="album_cover" v-else="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                            <i class="fa fa-play fa-4x"></i>
-                            <img class="vynil no_cover" src="http://fc08.deviantart.net/fs17/f/2007/170/9/8/Vinyl_Disc_Icon_Updated_by_jordygreen.png" />
-                        </a>
-                        <div class="album_info">
-                            <p class="album_name" title="">{{ album.name }} ({{ album.year }})</p>
+                    <div class="panel" v-if="activeTab == 'overview'">
+                        <div class="content is-clearfix" id="bio" v-if="artist.bio" v-html="truncatedBio"></div>
+                        <div class="columns">
+                            <div class="column is-half is-full-mobile">
+                                <spieldose-dashboard-toplist v-if="activeTab == 'overview'" v-bind:type="'topTracks'" v-bind:title="'Top played tracks'" v-bind:listItemCount="10" v-bind:showPlayCount="true"></spieldose-dashboard-toplist>
+                            </div>
                         </div>
-                        <!--
-                        <ul v-if="detailedView">
-                            <li v-for="track in album.tracks"><a>{{ track.title }} ({{ track.playtimeString }})</a></li>
-                        </ul>
-                        -->
                     </div>
-                </div>
+                    <div class="panel" v-if="activeTab == 'bio'" v-html="">
+                        <div class="content is-clearfix" id="bio" v-html="artist.bio"></div>
+                    </div>
+                    <div class="panel" v-if="activeTab == 'albums'">
+                        <div class="album_item" v-for="album in artist.albums">
+                            <a class="play_album" v-on:click="playAlbum(album.name, artist.name)">
+                                <img class="album_cover" v-if="album.image" v-bind:src="album.image"/>
+                                <img class="album_cover" v-else="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/>
+                                <i class="fa fa-play fa-4x"></i>
+                                <img class="vynil no_cover" src="http://fc08.deviantart.net/fs17/f/2007/170/9/8/Vinyl_Disc_Icon_Updated_by_jordygreen.png" />
+                            </a>
+                            <div class="album_info">
+                                <p class="album_name" title="">{{ album.name }} ({{ album.year }})</p>
+                            </div>
+                            <!--
+                            <ul v-if="detailedView">
+                                <li v-for="track in album.tracks"><a>{{ track.title }} ({{ track.playtimeString }})</a></li>
+                            </ul>
+                            -->
+                        </div>
+                    </div>
                 </div>
             </article>
         </div>
@@ -82,6 +91,7 @@ var browseArtist = Vue.component('spieldose-browse-artist', {
         },
         changeTab: function (tab) {
             this.activeTab = tab;
+            console.log(tab);
         },
         playAlbum: function (album, artist) {
             bus.$emit("searchIntoPlayList", 1, DEFAULT_SECTION_RESULTS_PAGE, null, artist, album, null);
