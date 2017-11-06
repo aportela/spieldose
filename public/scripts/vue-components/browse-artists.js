64330b1b0d242077eea2c2a52a42fd570c856f2e
alex <766f6964+github@gmail.com>
2017-11-06 15:37:51 +0100

    moved function to encode name into router

--- public/scripts/vue-components/browse-artists.js
+++ public/scripts/vue-components/browse-artists.js
@@ -6,7 +6,7 @@ var vTemplateBrowseArtists = function () {
         <p class="title is-1 has-text-centered">Browse artists</i></p>
         <spieldose-pagination v-bind:data="pager" v-show="artists.length > 0"></spieldose-pagination>
         <div class="browse-artist-item is-pulled-left" v-for="artist in artists" v-show="! loading">
-            <a v-bind:href="'/#/app/artist/' + safeRouterLink(artist.name)" v-bind:title="'click to open artist section'">
+            <a v-bind:href="'/#/app/artist/' + $router.encodeSafeName(artist.name)" v-bind:title="'click to open artist section'">
                 <img class="album_cover" v-if="artist.image" v-bind:src="artist.image" />
                 <img class="album_cover" v-else src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_festival_512px_GREY.png" />
                 <i class="fa fa-search fa-4x"></i>
@@ -69,13 +69,6 @@ var browseArtists = Vue.component('spieldose-browse-artists', {
                 self.artists = response.artists;
                 self.loading = false;
             });
-        },
-        safeRouterLink: function(name) {
-            if (name && name.indexOf("/") > 0) {
-                return(encodeURIComponent(name));
-            } else {
-                return(name);
-            }
         }
     }
 });
