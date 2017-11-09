0e237e3d4a6b3906c91f9ddb0218ef39ec26bc5b
alex <766f6964+github@gmail.com>
2017-11-10 00:06:01 +0100

    added headers & toggle details

--- public/scripts/vue-components/api-error.js
+++ public/scripts/vue-components/api-error.js
@@ -4,17 +4,26 @@ var vTemplateApiError = function () {
     return `
     <article class="message is-danger">
         <div class="message-header">
-        <p>Error</p>
-        <button class="delete" aria-label="delete"></button>
+        <p><i class="fa fa-bomb" aria-hidden="true"></i> Error</p>
         </div>
         <div class="message-body">
             <div class="content">
-                <h1>The server sent a <strong>invalid response</strong> ({{ apiErrorResponse.code }})</h1>
-                <h2>Api url:</strong> {{ apiErrorResponse.url }}</h2>
-                <h2><strong>Api params:</strong></h2>
-                <pre>{{ apiErrorResponse.params }}</pre>
-                <h2><strong>Api response:</strong></h2>
-                <pre>{{ apiErrorResponse.text }}</pre>
+                <h1 class="has-text-centered">“I'm sorry Dave. I'm afraid I can't do that”</h1>
+                <h2>Uh oh! ...the server sent a <strong>invalid response</strong> ({{ apiErrorResponse.code }})</h2>
+                <p v-if="! visibleDetails"><a href="#" v-on:click.prevent="toggleDetails();">Follow</a> for  the rabbit.</p>
+                <div v-if="visibleDetails">
+                    <hr>
+                    <h2>Api url:</h2>
+                    <pre>{{ apiErrorResponse.url }}</pre>
+                    <h2>Api method:</h2>
+                    <pre>{{ apiErrorResponse.method }}</pre>
+                    <h2>Api params:</h2>
+                    <pre>{{ apiErrorResponse.params }}</pre>
+                    <h2>Api response headers:</h2>
+                    <pre>{{ apiErrorResponse.headers }}</pre>
+                    <h2>Api response body:</h2>
+                    <pre>{{ apiErrorResponse.body }}</pre>
+                </div>
             </div>
         </div>
     </article>
@@ -26,7 +35,12 @@ var apiError = Vue.component('spieldose-api-error-component', {
     template: vTemplateApiError(),
     data: function () {
         return ({
-            details: null
+            visibleDetails: false
         });
-    }, props: ['apiErrorResponse']
+    }, props: ['apiErrorResponse'],
+    methods: {
+        toggleDetails() {
+            this.visibleDetails = ! this.visibleDetails;
+        }
+    }
 });
