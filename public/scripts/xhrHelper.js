6ba0de6189939a5b6acedb6d7bac36149f7a8e9d
alex <766f6964+github@gmail.com>
2017-11-10 21:35:33 +0100

    new http request function (return better response object on callback)

--- public/scripts/xhrHelper.js
+++ public/scripts/xhrHelper.js
@@ -15,7 +15,7 @@ var jsonHttpRequest = function (method, url, jsonData, callback) {
                 console.log(xhr.responseText);
                 console.groupEnd();
             } finally {
-                callback(xhr.status, jsonResult, jsonResult == null ? xhr.responseText: null, xhr.getAllResponseHeaders());
+                callback(xhr.status, jsonResult, jsonResult == null ? xhr.responseText : null, xhr.getAllResponseHeaders());
             }
         }
     }
@@ -23,4 +23,41 @@ var jsonHttpRequest = function (method, url, jsonData, callback) {
         callback(408, null);
     };
     xhr.send(JSON.stringify(jsonData));
+}
+
+var httpRequest = function (request, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(request.method, request.url, true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function (e) {
+        if (this.readyState == 4 && xhr.status !== 0) {
+            var response = {
+                status: xhr.status,
+                statusText: xhr.statusText,
+                headers: xhr.getAllResponseHeaders(),
+                text: xhr.responseText,
+                json: {}
+            };
+            try {
+                if (xhr.getResponseHeader("Content-Type").indexOf("json") != -1) {
+                    response.json = JSON.parse(xhr.responseText);
+                }
+            } catch (e) {
+                response.json = {};
+            } finally {
+                callback(response);
+            }
+        }
+    }
+    xhr.ontimeout = function (e) {
+        var response = {
+            status: 408,
+            statusText: "Request Timeout",
+            headers: [],
+            text: xhr.responseText,
+            json: {}
+        };
+        callback(response);
+    };
+    xhr.send(JSON.stringify(request.params));
 }
