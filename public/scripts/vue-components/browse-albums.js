7ca956b01b67cd3c115c417bcb6d0b49d50fe74e
alex <766f6964+github@gmail.com>
2017-11-11 12:23:52 +0100

    enqueue album tracks via common api object with error checking added

--- public/scripts/vue-components/browse-albums.js
+++ public/scripts/vue-components/browse-albums.js
@@ -4,28 +4,31 @@ var vTemplateBrowseAlbums = function () {
     return `
     <div class="container is-fluid box">
         <p class="title is-1 has-text-centered">Browse albums</i></p>
-        <div class="field">
-            <div class="control has-icons-left" v-bind:class="loading ? 'is-loading': ''">
-                <input class="input " v-model="nameFilter" type="text" placeholder="search album name..." v-on:keyup.esc="abortInstantSearch();" v-on:keyup="instantSearch();">
-                <span class="icon is-small is-left">
-                    <i class="fa fa-search"></i>
-                </span>
+        <div v-if="! errors">
+            <div class="field">
+                <div class="control has-icons-left" v-bind:class="loading ? 'is-loading': ''">
+                    <input class="input " v-model="nameFilter" type="text" placeholder="search album name..." v-on:keyup.esc="abortInstantSearch();" v-on:keyup="instantSearch();">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-search"></i>
+                    </span>
+                </div>
             </div>
-        </div>
-        <spieldose-pagination v-bind:loading="loading" v-bind:data="pager" v-show="albums.length > 0"></spieldose-pagination>
-        <div class="browse-album-item" v-for="album in albums" v-show="! loading">
-            <a class="play-album" v-on:click="enqueueAlbumTracks(album.name, album.artist)" v-bind:title="'click to play album'">
-                <img class="album-thumbnail" v-if="album.image" v-bind:src="album.image"/>
-                <img class="album-thumbnail" v-else="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                <i class="fa fa-play fa-4x"></i>
-                <img class="vinyl no-cover" src="http://fc08.deviantart.net/fs17/f/2007/170/9/8/Vinyl_Disc_Icon_Updated_by_jordygreen.png" />
-            </a>
-            <div class="album-info">
-                <p class="album-name">{{ album.name }}</p>
-                <p class="artist-name"><a v-bind:title="'click to open artist section'" v-bind:href="'/#/app/artist/' + $router.encodeSafeName(album.artist)">by {{ album.albumartist ? album.albumartist: album.artist }}</a><span v-show="album.year"> ({{ album.year }})</span></p>
+            <spieldose-pagination v-bind:loading="loading" v-bind:data="pager" v-show="albums.length > 0"></spieldose-pagination>
+            <div class="browse-album-item" v-for="album in albums" v-show="! loading">
+                <a class="play-album" v-on:click="enqueueAlbumTracks(album.name, album.artist, album.year)" v-bind:title="'click to play album'">
+                    <img class="album-thumbnail" v-if="album.image" v-bind:src="album.image"/>
+                    <img class="album-thumbnail" v-else="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/>
+                    <i class="fa fa-play fa-4x"></i>
+                    <img class="vinyl no-cover" src="http://fc08.deviantart.net/fs17/f/2007/170/9/8/Vinyl_Disc_Icon_Updated_by_jordygreen.png" />
+                </a>
+                <div class="album-info">
+                    <p class="album-name">{{ album.name }}</p>
+                    <p class="artist-name"><a v-bind:title="'click to open artist section'" v-bind:href="'/#/app/artist/' + $router.encodeSafeName(album.artist)">by {{ album.albumartist ? album.albumartist: album.artist }}</a><span v-show="album.year"> ({{ album.year }})</span></p>
+                </div>
             </div>
+            <div class="is-clearfix"></div>
         </div>
-        <div class="is-clearfix"></div>
+        <spieldose-api-error-component v-else v-bind:apiError="apiError"></spieldose-api-error-component>
     </div>
     `;
 }
@@ -36,6 +39,7 @@ var browseAlbums = Vue.component('spieldose-browse-albums', {
         return ({
             loading: false,
             errors: true,
+            apiError: null,
             nameFilter: null,
             timeout: null,
             albums: [],
@@ -85,58 +89,38 @@ var browseAlbums = Vue.component('spieldose-browse-albums', {
             if (self.nameFilter) {
                 d.text = self.nameFilter;
             }
-            jsonHttpRequest("POST", "/api/album/search", d, function (httpStatusCode, response) {
-                if (httpStatusCode == 200) {
-                    for (var i = 0; i < response.albums.length; i++) {
-                        if (response.albums[i].image) {
-                            response.albums[i].albumCoverUrl = response.albums[i].image;
-                        } else {
-                            response.albums[i].albumCoverUrl = null;
-                        }
-                    }
-                    if (response.albums.length > 0) {
-                        self.pager.totalPages = response.totalPages;
+            this.$http.post("/api/album/search", d).then(
+                response => {
+                    self.pager.actualPage = response.body.pagination.actualPage;
+                    self.pager.totalPages = response.body.pagination.totalPages;
+                    self.pager.totalResults = response.body.pagination.totalResults;
+                    if (response.body.albums && response.body.albums.length > 0) {
+                        self.albums = response.body.albums;
                     } else {
-                        self.pager.totalPages = 0;
+                        self.albums = [];
                     }
-                    if (self.pager.actualPage < self.pager.totalPages) {
-                        self.pager.nextPage = self.pager.actualPage + 1;
-                    } else {
-                        self.pager.nextPage = self.pager.totalPages;
-                    }
-                    self.albums = response.albums;
-                } else {
-                    self.errors = true;
-                }
-                self.loading = false;
-            });
-        },
-        getAlbumTracks: function (album, artist, callback) {
-            var d = {
-                artist: artist,
-                album: album
-            };
-            var self = this;
-            self.loading = true;
-            self.errors = false;
-            jsonHttpRequest("POST", "/api/track/search", d, function (httpStatusCode, response) {
-                if (httpStatusCode == 200) {
                     self.loading = false;
-                    callback(response.tracks);
-                } else {
-                    self.loading = false;
+                },
+                response => {
                     self.errors = true;
-                    callback([]);
+                    self.apiError = response.getApiErrorData();
+                    self.loading = false;
                 }
-            });
-
+            );
         },
-        enqueueAlbumTracks: function (album, artist) {
+        enqueueAlbumTracks: function (album, artist, year) {
             var self = this;
-            this.getAlbumTracks(album, artist, function (tracks) {
+            spieldoseAPI.getAlbumTracks(album || null, artist || null , year || null, function(response) {
                 self.playerData.emptyPlayList();
-                self.playerData.tracks = tracks;
-                self.playerData.play();
+                if (response.ok) {
+                    if (response.body.tracks && response.body.tracks.length > 0) {
+                        self.playerData.tracks = response.body.tracks;
+                        self.playerData.play();
+                    }
+                } else {
+                    self.errors = true;
+                    self.apiError = response.getApiErrorData();
+                }
             });
         }
     }
