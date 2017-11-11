cb6924bd8c8d8adbeab9bdeb79d3b8aae9adf89e
alex <766f6964+github@gmail.com>
2017-11-11 14:24:03 +0100

    replaced xhr custom helper by common api object (using vue-resource)

--- public/scripts/vue-components/browse-artist.js
+++ public/scripts/vue-components/browse-artist.js
@@ -2,51 +2,55 @@
 
 var vTemplateBrowseArtist = function () {
     return `
-    <article class="media container is-fluid box" v-show="! loading">
-        <figure class="image media-left">
-            <img v-bind:src="artist.image" alt="Image" class="artist_avatar" v-if="artist.image">
-            <img alt="Image" class="artist_avatar" src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_festival_512px_GREY.png" v-else />
-        </figure>
-        <div class="media-content is-light">
-            <p class="title is-1">{{ artist.name }}</p>
-            <p class="subtitle is-6" v-if="artist.playCount > 0">{{ artist.playCount }} plays</p>
-            <p class="subtitle is-6" v-else>not played yet</p>
-            <div class="tabs is-medium">
-                <ul>
-                    <li v-bind:class="{ 'is-active' : activeTab == 'overview' }"><a href="#" v-on:click.prevent="changeTab('overview');">Overview</a></li>
-                    <li v-bind:class="{ 'is-active' : activeTab == 'bio' }"><a href="#" v-on:click.prevent="changeTab('bio');">Bio</a></li>
-                    <li v-bind:class="{ 'is-active' : activeTab == 'tracks' }"><a href="#" v-on:click.prevent="changeTab('tracks');">Tracks</a></li>
-                    <li v-bind:class="{ 'is-active' : activeTab == 'albums' }"><a href="#" v-on:click.prevent="changeTab('albums');">Albums</a></li>
-                </ul>
-            </div>
-            <div class="panel" v-if="activeTab == 'overview'">
-                <div class="content is-clearfix" id="bio" v-if="artist.bio" v-html="truncatedBio"></div>
-                <div class="columns">
-                    <div class="column is-half is-full-mobile">
-                        <spieldose-dashboard-toplist v-if="activeTab == 'overview'" v-bind:type="'topTracks'" v-bind:title="'Top played tracks'" v-bind:listItemCount="10" v-bind:showPlayCount="true" :key="$route.params.artist" v-bind:artist="$route.params.artist"></spieldose-dashboard-toplist>
+    <div class="container is-fluid box">
+        <p class="title is-1 has-text-centered">Artist details</i></p>
+        <div class="media" v-if="! errors && ! loading">
+            <figure class="image media-left">
+                <img v-bind:src="artist.image" alt="Image" class="artist_avatar" v-if="artist.image">
+                <img alt="Image" class="artist_avatar" src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_festival_512px_GREY.png" v-else />
+            </figure>
+            <div class="media-content is-light">
+                <p class="title is-1">{{ artist.name }}</p>
+                <p class="subtitle is-6" v-if="artist.playCount > 0">{{ artist.playCount }} plays</p>
+                <p class="subtitle is-6" v-else>not played yet</p>
+                <div class="tabs is-medium">
+                    <ul>
+                        <li v-bind:class="{ 'is-active' : activeTab == 'overview' }"><a href="#" v-on:click.prevent="changeTab('overview');">Overview</a></li>
+                        <li v-bind:class="{ 'is-active' : activeTab == 'bio' }"><a href="#" v-on:click.prevent="changeTab('bio');">Bio</a></li>
+                        <li v-bind:class="{ 'is-active' : activeTab == 'tracks' }"><a href="#" v-on:click.prevent="changeTab('tracks');">Tracks</a></li>
+                        <li v-bind:class="{ 'is-active' : activeTab == 'albums' }"><a href="#" v-on:click.prevent="changeTab('albums');">Albums</a></li>
+                    </ul>
+                </div>
+                <div class="panel" v-if="activeTab == 'overview'">
+                    <div class="content is-clearfix" id="bio" v-if="artist.bio" v-html="truncatedBio"></div>
+                    <div class="columns">
+                        <div class="column is-half is-full-mobile">
+                            <spieldose-dashboard-toplist v-if="activeTab == 'overview' && artist.name" v-bind:type="'topTracks'" v-bind:title="'Top played tracks'" v-bind:listItemCount="10" v-bind:showPlayCount="true" :key="$route.params.artist" v-bind:artist="$route.params.artist"></spieldose-dashboard-toplist>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="panel" v-if="activeTab == 'bio'" v-html="">
-                <div class="content is-clearfix" id="bio" v-html="artist.bio"></div>
-            </div>
-            <div class="panel" v-if="activeTab == 'albums'">
-                <div class="browse-album-item" v-for="album in artist.albums" v-show="! loading">
-                    <a class="play-album" v-on:click="enqueueAlbumTracks(album.name, album.artist, album.year)" v-bind:title="'click to play album'">
-                        <img class="album-thumbnail" v-if="album.image" v-bind:src="album.image"/>
-                        <img class="album-thumbnail" v-else="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/>
-                        <i class="fa fa-play fa-4x"></i>
-                        <img class="vinyl no-cover" src="http://fc08.deviantart.net/fs17/f/2007/170/9/8/Vinyl_Disc_Icon_Updated_by_jordygreen.png" />
-                    </a>
-                    <div class="album-info">
-                        <p class="album-name">{{ album.name }}</p>
-                        <p class="album-year" v-show="album.year">({{ album.year }})</p>
-                    </div>
+                <div class="panel" v-if="activeTab == 'bio'" v-html="">
+                    <div class="content is-clearfix" id="bio" v-html="artist.bio"></div>
                 </div>
-                <div class="is-clearfix"></div>
+                <div class="panel" v-if="activeTab == 'albums'">
+                    <div class="browse-album-item" v-for="album in artist.albums" v-show="! loading">
+                        <a class="play-album" v-on:click="enqueueAlbumTracks(album.name, album.artist, album.year)" v-bind:title="'click to play album'">
+                            <img class="album-thumbnail" v-if="album.image" v-bind:src="album.image"/>
+                            <img class="album-thumbnail" v-else="" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="/>
+                            <i class="fa fa-play fa-4x"></i>
+                            <img class="vinyl no-cover" src="http://fc08.deviantart.net/fs17/f/2007/170/9/8/Vinyl_Disc_Icon_Updated_by_jordygreen.png" />
+                        </a>
+                        <div class="album-info">
+                            <p class="album-name">{{ album.name }}</p>
+                            <p class="album-year" v-show="album.year">({{ album.year }})</p>
+                        </div>
+                    </div>
+                    <div class="is-clearfix"></div>
+                </div>
             </div>
         </div>
-    </article>
+        <spieldose-api-error-component v-if="errors" v-bind:apiError="apiError"></spieldose-api-error-component>
+    </div>
     `;
 }
 
@@ -56,6 +60,7 @@ var browseArtist = Vue.component('spieldose-browse-artist', {
         return ({
             loading: false,
             errors: false,
+            apiError: null,
             artist: {},
             activeTab: 'overview',
             truncatedBio: null,
@@ -78,30 +83,24 @@ var browseArtist = Vue.component('spieldose-browse-artist', {
             self.loading = true;
             self.errors = false;
             var d = {};
-            jsonHttpRequest("GET", "/api/artist/" + encodeURIComponent(artist), d, function (httpStatusCode, response) {
-                if (httpStatusCode == 200) {
-                    self.artist = response.artist;
+            spieldoseAPI.getArtist(artist, function(response) {
+                if (response.ok) {
+                    self.artist = response.body.artist;
                     if (self.artist.bio) {
                         self.artist.bio = self.artist.bio.replace(/(?:\r\n|\r|\n)/g, '<br />');
                         self.truncatedBio = self.truncate(self.artist.bio);
                         self.activeTab = "overview";
                     }
+                    self.loading = false;
                 } else {
                     self.errors = true;
+                    self.apiError = response.getApiErrorData();
+                    self.loading = false;
                 }
-                self.loading = false;
             });
         },
         changeTab: function (tab) {
             this.activeTab = tab;
-        },
-        playAlbum: function (album, artist) {
-            bus.$emit("searchIntoPlayList", 1, DEFAULT_SECTION_RESULTS_PAGE, null, artist, album, null);
-        },
-        hideAlbumDetails: function () {
-            this.detailedView = false;
-        }, showAlbumDetails: function () {
-            this.detailedView = true;
         }, truncate: function (text) {
             return (text.substring(0, 500));
         },
