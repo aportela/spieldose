abfc59b0c789bb32406a7bef253ca327633c160d
alex <766f6964+github@gmail.com>
2017-11-11 16:17:53 +0100

    changed component title labels

--- public/scripts/vue-components/dashboard.js
+++ public/scripts/vue-components/dashboard.js
@@ -10,10 +10,10 @@ var vTemplateDashboard = function () {
                 <spieldose-dashboard-toplist v-bind:type="'topTracks'" v-bind:title="'Top played tracks'" v-bind:listItemCount="5" v-bind:showPlayCount="false"></spieldose-dashboard-toplist>
             </div>
             <div class="column is-one-third-desktop is-full-mobile">
-                <spieldose-dashboard-toplist v-bind:type="'topArtists'" v-bind:title="'Top artists'" v-bind:listItemCount="5" v-bind:showPlayCount="false"></spieldose-dashboard-toplist>
+                <spieldose-dashboard-toplist v-bind:type="'topArtists'" v-bind:title="'Top played artists'" v-bind:listItemCount="5" v-bind:showPlayCount="false"></spieldose-dashboard-toplist>
             </div>
             <div class="column is-one-third-desktop is-full-mobile">
-                <spieldose-dashboard-toplist v-bind:type="'topGenres'" v-bind:title="'Top genres'" v-bind:listItemCount="5" v-bind:showPlayCount="false"></spieldose-dashboard-toplist>
+                <spieldose-dashboard-toplist v-bind:type="'topGenres'" v-bind:title="'Top played genres'" v-bind:listItemCount="5" v-bind:showPlayCount="false"></spieldose-dashboard-toplist>
             </div>
         </div>
         <div class="columns is-mobile is-multiline">
