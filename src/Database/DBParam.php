70318b8a7f3ba119e886fa9617319bc7064ae689
alex <766f6964+github@gmail.com>
2017-11-11 12:21:37 +0100

    text indents

--- src/Database/DBParam.php
+++ src/Database/DBParam.php
@@ -1,92 +1,92 @@
 <?php
 
-    declare(strict_types=1);
+      declare(strict_types=1);
 
-    namespace Spieldose\Database;
+      namespace Spieldose\Database;
 
-    /**
-     * Simple PDO Database Param Warapper
-     */
-    class DBParam {
+      /**
+       * Simple PDO Database Param Warapper
+       */
+      class DBParam {
 
-        public $name;
-        public $value;
-        public $type;
+            public $name;
+            public $value;
+            public $type;
 
-        /**
-         * set param properties
-         *
-         * @param $name
-         * @param $value
-         * @param $type
-         *
-         * @return \Spieldose\Database\DBParam
-         */
-        public function set($name, $value, $type): \Spieldose\Database\DBParam {
-			$this->name = $name;
-			$this->value = $value;
-			$this->type = $type;
-            return($this);
-		}
+            /**
+             * set param properties
+             *
+             * @param $name
+             * @param $value
+             * @param $type
+             *
+             * @return \Spieldose\Database\DBParam
+             */
+            public function set($name, $value, $type): \Spieldose\Database\DBParam {
+                  $this->name = $name;
+                  $this->value = $value;
+                  $this->type = $type;
+                  return($this);
+            }
 
-        /**
-         * set NULL param
-         *
-         * @param $name
-         *
-         * @return \Spieldose\Database\DBParam
-         */
-		public function null(string $name): \Spieldose\Database\DBParam {
-            $this->name = $name;
-            $this->value = null;
-            $this->type = \PDO::PARAM_NULL;
-            return($this);
-		}
+            /**
+             * set NULL param
+             *
+             * @param $name
+             *
+             * @return \Spieldose\Database\DBParam
+             */
+            public function null(string $name): \Spieldose\Database\DBParam {
+                  $this->name = $name;
+                  $this->value = null;
+                  $this->type = \PDO::PARAM_NULL;
+                  return($this);
+            }
 
-        /**
-         * set BOOL param
-         *
-         * @param $name string
-         * @param $value boolean
-         *
-         * @return \Spieldose\Database\DBParam
-         */
-		public function bool(string $name, bool $value): \Spieldose\Database\DBParam {
-            $this->name = $name;
-            $this->value = $value;
-            $this->type = \PDO::PARAM_BOOL;
-            return($this);
-		}
+            /**
+             * set BOOL param
+             *
+             * @param $name string
+             * @param $value boolean
+             *
+             * @return \Spieldose\Database\DBParam
+             */
+            public function bool(string $name, bool $value): \Spieldose\Database\DBParam {
+                  $this->name = $name;
+                  $this->value = $value;
+                  $this->type = \PDO::PARAM_BOOL;
+                  return($this);
+            }
 
-        /**
-         * set INTEGER param
-         *
-         * @param $name string
-         * @param $value int
-         *
-         * @return \Spieldose\Database\DBParam
-         */
-		public function int(string $name, int $value): \Spieldose\Database\DBParam {
-            $this->name = $name;
-            $this->value = $value;
-            $this->type = \PDO::PARAM_INT;
-            return($this);
-		}
+            /**
+             * set INTEGER param
+             *
+             * @param $name string
+             * @param $value int
+             *
+             * @return \Spieldose\Database\DBParam
+             */
+            public function int(string $name, int $value): \Spieldose\Database\DBParam {
+                  $this->name = $name;
+                  $this->value = $value;
+                  $this->type = \PDO::PARAM_INT;
+                  return($this);
+            }
 
-        /**
-         * set STRING param
-         *
-         * @param $name string
-         * @param $value int
-         *
-         * @return \Spieldose\Database\DBParam
-         */
-		public function str(string $name, $value): \Spieldose\Database\DBParam {
-            $this->name = $name;
-            $this->value = $value;
-            $this->type = \PDO::PARAM_STR;
-            return($this);
-		}
-    }
+            /**
+             * set STRING param
+             *
+             * @param $name string
+             * @param $value int
+             *
+             * @return \Spieldose\Database\DBParam
+             */
+            public function str(string $name, $value): \Spieldose\Database\DBParam {
+                  $this->name = $name;
+                  $this->value = $value;
+                  $this->type = \PDO::PARAM_STR;
+                  return($this);
+            }
+      }
 
 ?>
