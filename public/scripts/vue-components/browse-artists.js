aac07badd62a69185e071b2b5e612353f6c3ef78
alex <766f6964+github@gmail.com>
2017-11-11 12:26:19 +0100

    check for api errors added

--- public/scripts/vue-components/browse-artists.js
+++ public/scripts/vue-components/browse-artists.js
@@ -4,26 +4,29 @@ var vTemplateBrowseArtists = function () {
     return `
     <div class="container is-fluid box">
         <p class="title is-1 has-text-centered">Browse artists</i></p>
-        <div class="field">
-            <div class="control has-icons-left" v-bind:class="loading ? 'is-loading': ''">
-                <input class="input " v-model="nameFilter" type="text" placeholder="search artist name..." v-on:keyup.esc="abortInstantSearch();" v-on:keyup="instantSearch();">
-                <span class="icon is-small is-left">
-                    <i class="fa fa-search"></i>
-                </span>
+        <div v-if="! errors">
+            <div class="field">
+                <div class="control has-icons-left" v-bind:class="loading ? 'is-loading': ''">
+                    <input class="input " v-model="nameFilter" type="text" placeholder="search artist name..." v-on:keyup.esc="abortInstantSearch();" v-on:keyup="instantSearch();">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-search"></i>
+                    </span>
+                </div>
             </div>
-        </div>
-        <spieldose-pagination v-bind:loading="loading" v-bind:data="pager" v-show="artists.length > 0"></spieldose-pagination>
-        <div class="browse-artist-item is-pulled-left" v-for="artist in artists" v-show="! loading">
-            <a v-bind:href="'/#/app/artist/' + $router.encodeSafeName(artist.name)" v-bind:title="'click to open artist section'">
-                <img v-if="artist.image" v-bind:src="artist.image" />
-                <img v-else src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_festival_512px_GREY.png" />
-                <i class="fa fa-search fa-4x"></i>
-            </a>
-            <div class="artist-info is-clipped">
-                <p class="artist-name has-text-centered">{{ artist.name }}</p>
+            <spieldose-pagination v-bind:loading="loading" v-bind:data="pager" v-show="artists.length > 0"></spieldose-pagination>
+            <div class="browse-artist-item is-pulled-left" v-for="artist in artists" v-show="! loading">
+                <a v-bind:href="'/#/app/artist/' + $router.encodeSafeName(artist.name)" v-bind:title="'click to open artist section'">
+                    <img v-if="artist.image" v-bind:src="artist.image" />
+                    <img v-else src="https://cdn2.iconfinder.com/data/icons/app-types-in-grey/128/app_type_festival_512px_GREY.png" />
+                    <i class="fa fa-search fa-4x"></i>
+                </a>
+                <div class="artist-info is-clipped">
+                    <p class="artist-name has-text-centered">{{ artist.name }}</p>
+                </div>
             </div>
+            <div class="is-clearfix"></div>
         </div>
-        <div class="is-clearfix"></div>
+        <spieldose-api-error-component v-else v-bind:apiError="apiError"></spieldose-api-error-component>
     </div>
     `;
 }
