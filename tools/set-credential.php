49b76bb253345bb9afcdc6f52b9c9d0827117a03
alex <766f6964+github@gmail.com>
2017-11-18 16:46:12 +0100

    new commandline script for manage user accounts

---
+++ tools/set-credential.php
@@ -0,0 +1,37 @@
+<?php
+
+    declare(strict_types=1);
+
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
+    echo "Spieldose acount manager" . PHP_EOL;
+
+    $settings = require dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "AppSettings.php";
+
+    $app = (new \Spieldose\App($settings))->get();
+
+    $cmdLine = new \Spieldose\CmdLine("", array("email:", "password:"));
+    if ($cmdLine->hasParam("email") && $cmdLine->hasParam("password")) {
+        echo "Setting account credentials..." . PHP_EOL;
+        $dbh = new \Spieldose\Database\DB();
+        $found = false;
+        $u = new \Spieldose\User("", $cmdLine->getParamValue("email"), $cmdLine->getParamValue("password"));
+        try {
+            $u->get($dbh);
+            $found = true;
+        } catch (\Spieldose\Exception\NotFoundException $e) { }
+        if ($found) {
+            echo "User found, updating password...";
+            $u->update($dbh);
+            echo "ok!" . PHP_EOL;
+        } else {
+            echo "User not found, creating account...";
+            $u->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+            $u->add($dbh);
+            echo "ok!" . PHP_EOL;
+        }
+    } else {
+        echo "No required params found: --email <email> --password <secret>" . PHP_EOL;
+    }
+
+?>
