19aeae23e305f4183adcd237f51bfe72a895ac91
alex <766f6964+github@gmail.com>
2017-11-23 19:28:36 +0100

    register debug data on logs

--- tools/set-credential.php
+++ tools/set-credential.php
@@ -16,23 +16,29 @@
 
     $cmdLine = new \Spieldose\CmdLine("", array("email:", "password:"));
     if ($cmdLine->hasParam("email") && $cmdLine->hasParam("password")) {
+        $c = $app->getContainer();
+        $c["logger"]->info("Setting account credentials");
         echo "Setting account credentials..." . PHP_EOL;
         $dbh = new \Spieldose\Database\DB();
         if ((new \Spieldose\Database\Version($dbh))->hasUpgradeAvailable()) {
+            $c["logger"]->warning("Process stopped: upgrade database before continue");
             echo "New database version available, an upgrade is required before continue." . PHP_EOL;
             exit;
         }
         $found = false;
         $u = new \Spieldose\User("", $cmdLine->getParamValue("email"), $cmdLine->getParamValue("password"));
+        $c["logger"]->debug("Email: " . $u->email . " / Password: " . $u->password);
         try {
             $u->get($dbh);
             $found = true;
         } catch (\Spieldose\Exception\NotFoundException $e) { }
         if ($found) {
+            $c["logger"]->debug("Account exists -> update credentials");
             echo "User found, updating password...";
             $u->update($dbh);
             echo "ok!" . PHP_EOL;
         } else {
+            $c["logger"]->debug("Account not found -> adding credentials");
             echo "User not found, creating account...";
             $u->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
             $u->add($dbh);
