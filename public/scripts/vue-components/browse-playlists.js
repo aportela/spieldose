f8dd988af21d9923dceb8ab7eefe085c6c537696
alex <766f6964+github@gmail.com>
2017-11-23 16:45:45 +0100

    new action buttons for play/remove playlist

--- public/scripts/vue-components/browse-playlists.js
+++ public/scripts/vue-components/browse-playlists.js
@@ -15,15 +15,34 @@ var browsePlaylists = (function () {
                 </div>
             </div>
             <spieldose-pagination v-bind:data="pager" v-show="playlists.length > 0"></spieldose-pagination>
-            <div class="playlist-item box" v-for="playlist in playlists" v-show="! loading" v-on:click.prevent="loadPlayList(playlist.id);">
-                <p class="playlist-info has-text-centered">
-                    <strong>“{{ playlist.name }}”</strong>
-                    <br>13 tracks
+            <div class="playlist-item box has-text-centered" v-for="playlist in playlists" v-show="! loading">
+                <p class="playlist-item-icon">
+                    <span class="icon has-text-light">
+                        <i class="fa fa-list-alt fa-5x"></i>
+                    </span>
                 </p>
+                <p class="playlist-info">
+                    <strong>“{{ playlist.name }}”</strong>
+                </p>
+                <p class="content is-small">{{ playlist.trackCount }} tracks</p>
+                <div class="field has-addons">
+                    <p class="control">
+                        <a class="button is-small is-link" v-on:click.prevent="loadPlayList(playlist.id);">
+                            <span class="icon is-small"><i class="fa fa-play"></i></span>
+                            <span>play</span>
+                        </a>
+                    </p>
+                    <p class="control">
+                        <a class="button is-small is-danger" v-on:click.prevent="removePlayList(playlist.id);">
+                            <span class="icon is-small"><i class="fa fa-remove"></i></span>
+                            <span>remove</span>
+                        </a>
+                    </p>
+                </div>
             </div>
             <div class="is-clearfix"></div>
         </div>
-        <spieldose-api-error-component v-else v-bind:apiError="apiError"></spieldose-api-error-component>
+        <spieldose-api-error-component v-else v-bind:visible="errors" v-bind:apiError="apiError"></spieldose-api-error-component>
     </div>
     `;
     };
@@ -105,10 +124,25 @@ var browsePlaylists = (function () {
                 var self = this;
                 spieldoseAPI.getPlayList(playListId, function (response) {
                     if (response.ok) {
-                        self.currentPlaylistId = playListId;
-                        self.currentPlaylistName = response.body.playlist.name;
                         self.playerData.replace(response.body.playlist.tracks);
+                        self.playerData.setCurrentPlayList(playListId, response.body.playlist.name);
                         self.$router.push({ name: 'nowPlaying' });
+                    } else {
+                        self.errors = true;
+                        self.apiError = response.getApiErrorData();
+                        self.loading = false;
+                    }
+                });
+            },
+            removePlayList: function(playListId) {
+                var self = this;
+                spieldoseAPI.removePlaylist(playListId, function (response) {
+                    if (response.ok) {
+                        if (self.playerData.currentPlaylistId == playListId) {
+                            self.playerData.unsetCurrentPlayList();
+
+                        }
+                        self.search();
                     } else {
                         self.errors = true;
                         self.apiError = response.getApiErrorData();
