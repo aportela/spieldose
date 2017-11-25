85be6004317b591478caa30efbe2c5f2ceba1782
alex <766f6964+github@gmail.com>
2017-11-25 18:02:18 +0100

    added (optional) user agent param

--- src/Net.php
+++ src/Net.php
@@ -9,13 +9,17 @@
         /**
         *   make web request
         */
-        public static function httpRequest($url) {
+        public static function httpRequest(string $url = "", string $userAgent = "") {
             $ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			$referer = "http://" . parse_url($url, PHP_URL_HOST);
 			curl_setopt ($ch, CURLOPT_REFERER, $referer);
-			curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19');
+            curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, true);
+            if (empty($userAgent)) {
+                curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19');
+            } else {
+                curl_setopt ($ch, CURLOPT_USERAGENT, $userAgent);
+            }
 			$content = curl_exec($ch);
 			curl_close($ch);
 			return($content);
