a6c9beea03426c4abaac981f1eeabeca7917f8b4
alex <766f6964+github@gmail.com>
2017-11-26 18:56:56 +0100

    removed unused methods, added search filters & code cleanups

--- src/Artist.php
+++ src/Artist.php
@@ -1,4 +1,5 @@
 <?php
+
     declare(strict_types=1);
 
     namespace Spieldose;
@@ -17,36 +18,35 @@
 
         public function __destruct() { }
 
-        private function exists(\Spieldose\Database\DB $dbh): bool {
-            return(true);
-        }
-
+        /**
+         * get artist metadata (bio & albums)
+         *
+         * @param \Spieldose\Database\DB $dbh database handler
+         */
         public function get(\Spieldose\Database\DB $dbh) {
-            if (isset($this->name) && ! empty($this->name)) {
-                if ($this->exists($dbh)) {
-                    $params = array();
-                    $params[] = (new \Spieldose\Database\DBParam())->str(":name", $this->name);
-                    $query = sprintf('
-                        SELECT DISTINCT
-                            MBA.mbid,
-                            MBA.bio,
-                            MBA.image,
-                            COUNT(*) AS playCount
-                        FROM FILE F
-                        LEFT JOIN MB_CACHE_ARTIST MBA ON MBA.mbid = F.artist_mbid
-                        LEFT JOIN STATS S ON S.file_id = F.id
-                        WHERE COALESCE(MBA.artist, F.track_artist) = :name
-                        GROUP BY (MBA.mbid)
-                        '
-                    );
-                    $data = $dbh->query($query, $params);
-                    if ($data) {
-                        $this->mbid = $data[0]->mbid;
-                        $this->bio = $data[0]->bio;
-                        $this->image = $data[0]->image;
-                        $this->playCount = $data[0]->playCount;
-                    }
-                    $this->getAlbums($dbh);
+            if (! empty($this->name)) {
+                $params = array(
+                    (new \Spieldose\Database\DBParam())->str(":name", $this->name),
+                    (new \Spieldose\Database\DBParam())->str(":user_id", \Spieldose\User::getUserId())
+                );
+                $query = sprintf('
+                    SELECT DISTINCT
+                        MBA.mbid,
+                        MBA.bio,
+                        MBA.image,
+                        COUNT(*) AS playCount
+                    FROM FILE F
+                    LEFT JOIN MB_CACHE_ARTIST MBA ON MBA.mbid = F.artist_mbid
+                    LEFT JOIN STATS S ON (S.file_id = F.id AND S.user_id = :user_id)
+                    WHERE COALESCE(MBA.artist, F.track_artist) = :name
+                ');
+                $data = $dbh->query($query, $params);
+                if ($data) {
+                    $this->mbid = $data[0]->mbid;
+                    $this->bio = $data[0]->bio;
+                    $this->image = $data[0]->image;
+                    $this->playCount = $data[0]->playCount;
+                    $this->albums = (\Spieldose\Album::search($dbh, 1, 16, array("artist" => $this->name), ""))->results;
                 } else {
                     throw new \Spieldose\Exception\NotFoundException("name: " . $this->name);
                 }
@@ -55,40 +55,26 @@
             }
         }
 
-        private function getAlbums(\Spieldose\Database\DB $dbh) {
-            if (isset($this->name) && ! empty($this->name)) {
-                if ($this->exists($dbh)) {
-                    $params = array();
-                    $params[] = (new \Spieldose\Database\DBParam())->str(":name", $this->name);
-                    $query = sprintf('
-                        SELECT DISTINCT
-                            MBA2.mbid,
-                            COALESCE(MBA2.album, F.album_name) as name,
-                            COALESCE(MBA2.year, F.year) as year,
-                            MBA2.image
-                        FROM FILE F
-                        LEFT JOIN MB_CACHE_ARTIST MBA1 ON MBA1.mbid = F.artist_mbid
-                        LEFT JOIN MB_CACHE_ALBUM MBA2 ON MBA2.mbid = F.album_mbid
-                        WHERE COALESCE(MBA1.artist, F.track_artist) = :name
-                        ORDER BY COALESCE(MBA2.year, F.year)
-                        '
-                    );
-                    $this->albums = $dbh->query($query, $params);
-                } else {
-                    throw new \Spieldose\Exception\NotFoundException("name: " . $this->name);
-                }
-            } else {
-                throw new \Spieldose\Exception\InvalidParamsException("name");
-            }
-        }
-
-        public static function search(\Spieldose\Database\DB $dbh, int $page = 1, int $resultsPage = 16, array $filter = array(), string $order = "") {
+        /**
+         * search artists
+         *
+         * @param \Spieldose\Database\DB $dbh database handler
+         * @param int $page return results from this page
+         * @param int $resultsPage number of results / page
+         * @param array $filter the condition filter
+         * @param string $order results order
+         */
+        public static function search(\Spieldose\Database\DB $dbh, int $page = 1, int $resultsPage = 1024, array $filter = array(), string $order = "") {
             $params = array();
             $whereCondition = "";
             if (isset($filter)) {
-                if (isset($filter["text"]) && ! empty($filter["text"])) {
-                    $whereCondition = " AND COALESCE(MBA.artist, F.track_artist) LIKE :text ";
-                    $params[] = (new \Spieldose\Database\DBParam())->str(":text", "%" . $filter["text"] . "%");
+                if (isset($filter["partialName"]) && ! empty($filter["partialName"])) {
+                    $whereCondition = " AND COALESCE(MBA.artist, F.track_artist) LIKE :partialName ";
+                    $params[] = (new \Spieldose\Database\DBParam())->str(":partialName", "%" . $filter["partialName"] . "%");
+                }
+                if (isset($filter["name"]) && ! empty($filter["name"])) {
+                    $whereCondition = " AND COALESCE(MBA.artist, F.track_artist) LIKE :name ";
+                    $params[] = (new \Spieldose\Database\DBParam())->str(":name", $filter["name"]);
                 }
             }
             $queryCount = '
@@ -105,31 +91,39 @@
             $data->resultsPage = $resultsPage;
             $data->totalResults = $result[0]->total;
             $data->totalPages = ceil($data->totalResults / $resultsPage);
-            $sqlOrder = "";
-            if (! empty($order) && $order == "random") {
-                $sqlOrder = " ORDER BY RANDOM() ";
+            if ($data->totalResults > 0) {
+                $sqlOrder = "";
+                switch($order) {
+                    case "random":
+                        $sqlOrder = " ORDER BY RANDOM() ";
+                    break;
+                    default:
+                        $sqlOrder = " ORDER BY COALESCE(MBA.artist, F.track_artist) COLLATE NOCASE ASC ";
+                    break;
+                }
+                $query = sprintf('
+                    SELECT
+                        DISTINCT COALESCE(MBA.artist, F.track_artist) as name,
+                        MBA.image
+                    FROM FILE F
+                    LEFT JOIN MB_CACHE_ARTIST MBA ON MBA.mbid = F.artist_mbid
+                    WHERE COALESCE(MBA.artist, F.track_artist) IS NOT NULL
+                    %s
+                    %s
+                    LIMIT %d OFFSET %d
+                    ',
+                    $whereCondition,
+                    $sqlOrder,
+                    $resultsPage,
+                    $resultsPage * ($page - 1)
+                );
+                $data->results = $dbh->query($query, $params);
             } else {
-                $sqlOrder = " ORDER BY COALESCE(MBA.artist, F.track_artist) COLLATE NOCASE ASC ";
+                $data->results = array();
             }
-            $query = sprintf('
-                SELECT
-                    DISTINCT COALESCE(MBA.artist, F.track_artist) as name,
-                    MBA.image
-                FROM FILE F
-                LEFT JOIN MB_CACHE_ARTIST MBA ON MBA.mbid = F.artist_mbid
-                WHERE COALESCE(MBA.artist, F.track_artist) IS NOT NULL
-                %s
-                %s
-                LIMIT %d OFFSET %d
-                ',
-                $whereCondition,
-                $sqlOrder,
-                $resultsPage,
-                $resultsPage * ($page - 1)
-            );
-            $data->results = $dbh->query($query, $params);
             return($data);
         }
+
     }
 
 ?>
