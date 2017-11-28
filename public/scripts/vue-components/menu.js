7bf79a73931e152b25cb817277718061d4e05e4c
alex <766f6964+github@gmail.com>
2017-11-28 15:36:28 +0100

    browse by path (todo)

--- public/scripts/vue-components/menu.js
+++ public/scripts/vue-components/menu.js
@@ -11,6 +11,7 @@ var menu = (function () {
             <li><a v-bind:class="{ 'is-active': actualRouteName == 'nowPlaying' }" v-on:click.prevent="$router.push({ name: 'nowPlaying' })"><i class="fa fa-headphones"></i> now playing</a></li>
             <li><a v-bind:class="{ 'is-active': actualRouteName == 'artist' || actualRouteName == 'artists' }" v-on:click.prevent="$router.push({ name: 'artists' })"><i class="fa fa-user"></i> browse artists</a></li>
             <li><a v-bind:class="{ 'is-active': actualRouteName == 'albums' }" v-on:click.prevent="$router.push({ name: 'albums' })"><i class="fa fa-circle"></i> browse albums</a></li>
+            <li><a v-bind:class="{ 'is-active': actualRouteName == 'paths' }" v-on:click.prevent="$router.push({ name: 'paths' })"><i class="fa fa-folder-open"></i> browse paths</a></li>
             <li><a v-bind:class="{ 'is-active': actualRouteName == 'playlists' }" v-on:click.prevent="$router.push({ name: 'playlists' })"><i class="fa fa-list-alt"></i> browse playlists</a></li>
             <li><a v-on:click.prevent="signout();"><i class="fa fa-sign-out"></i> signout</a></li>
         </ul>
