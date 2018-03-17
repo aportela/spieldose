74d418ace078ab784ea20605c845990286f173dc
alex <766f6964+github@gmail.com>
2018-03-17 14:23:38 +0100

    re-enabled tab navigation

--- public/scripts/vue-components/container.js
+++ public/scripts/vue-components/container.js
@@ -3,47 +3,45 @@ var container = (function () {
 
     var template = function () {
         return `
-        <div class="is-light is-bold">
-
             <section class="hero is-fullheight">
                 <div class="hero-body is-paddingless">
                     <div class="container is-fullwidth is-vcentered">
                         <div class="tabs is-centered is-medium is-toggle">
                             <ul>
-                                <li class="is-active">
-                                    <a>
-                                    <span class="icon is-small"><i class="fa fa-headphones"></i></span>
-                                    <span>Now playing</span>
+                                <li v-bind:class="{ 'is-active': $route.name == 'player'}">
+                                    <a v-on:click.prevent="$router.push({ name: 'player' })">
+                                        <span class="icon is-small"><i class="fa fa-headphones"></i></span>
+                                        <span>Now playing</span>
                                     </a>
                                 </li>
-                                <li>
-                                    <a>
-                                    <span class="icon is-small"><i class="fa fa-search"></i></span>
-                                    <span>Search</span>
+                                <li v-bind:class="{ 'is-active': $route.name == 'search'}">
+                                    <a v-on:click.prevent="$router.push({ name: 'search' })">
+                                        <span class="icon is-small"><i class="fa fa-search"></i></span>
+                                        <span>Search</span>
                                     </a>
                                 </li>
-                                <li>
-                                    <a>
-                                    <span class="icon is-small"><i class="fa fa-user"></i></span>
-                                    <span>Browse artists</span>
+                                <li v-bind:class="{ 'is-active': $route.name == 'artists'}">
+                                    <a v-on:click.prevent="$router.push({ name: 'artists' })">
+                                        <span class="icon is-small"><i class="fa fa-user"></i></span>
+                                        <span>Browse artists</span>
                                     </a>
                                 </li>
-                                <li>
-                                    <a>
-                                    <span class="icon is-small"><i class="fa fa-circle"></i></span>
-                                    <span>Browse albums</span>
+                                <li v-bind:class="{ 'is-active': $route.name == 'albums'}">
+                                    <a v-on:click.prevent="$router.push({ name: 'albums' })">
+                                        <span class="icon is-small"><i class="fa fa-circle"></i></span>
+                                        <span>Browse albums</span>
                                     </a>
                                 </li>
-                                <li>
-                                    <a>
-                                    <span class="icon is-small"><i class="fa fa-folder-open"></i></span>
-                                    <span>Browse paths</span>
+                                <li v-bind:class="{ 'is-active': $route.name == 'playlists'}">
+                                    <a v-on:click.prevent="$router.push({ name: 'playlists' })">
+                                        <span class="icon is-small"><i class="fa fa-folder-open"></i></span>
+                                        <span>Browse paths</span>
                                     </a>
                                 </li>
-                                <li>
-                                    <a>
-                                    <span class="icon is-small"><i class="fa fa-list-alt"></i></span>
-                                    <span>Browse paths</span>
+                                <li v-bind:class="{ 'is-active': $route.name == 'paths'}">
+                                    <a v-on:click.prevent="$router.push({ name: 'paths' })">
+                                        <span class="icon is-small"><i class="fa fa-list-alt"></i></span>
+                                        <span>Browse paths</span>
                                     </a>
                                 </li>
                             </ul>
@@ -54,8 +52,6 @@ var container = (function () {
                     </div>
                 </div>
             </section>
-
-        </div>
     `;
     };
 
@@ -63,14 +59,18 @@ var container = (function () {
         template: template(),
         data: function () {
             return ({
+                /*
                 mobileMenu: false,
                 playerData: sharedPlayerData,
+                */
             });
         }, created: function () {
+            /*
             var self = this;
             self.playerData.loadRandomTracks(initialState.defaultResultsPage, function () {
                 self.playerData.play();
             });
+            */
         }
     });
 
