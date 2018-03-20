7907740b9f72b6de31e0cb574ba393bad4b1486a
alex <766f6964+github@gmail.com>
2018-03-20 21:05:33 +0100

    show player section after sign in

--- public/scripts/vue-components/signin.js
+++ public/scripts/vue-components/signin.js
@@ -138,7 +138,7 @@ var signInUp = (function () {
                 self.errors = false;
                 spieldoseAPI.signIn(this.signInEmail, this.signInPassword, function (response) {
                     if (response.ok) {
-                        self.$router.push({ name: 'dashboard' });
+                        self.$router.push({ name: 'player' });
                     } else {
                         switch (response.status) {
                             case 400:
