88d2c93fb76833f8b8beed3f7d8e1bf94cffb5fb
alex <766f6964+github@gmail.com>
2018-05-05 20:09:53 +0200

    use simple validator

--- public/scripts/vue-components/signin.js
+++ public/scripts/vue-components/signin.js
@@ -27,14 +27,14 @@ var signInUp = (function () {
 
                         <div class="tabs is-toggle is-radiusless spieldose-tabs-without-margin-bottom" v-if="allowSignUp">
                             <ul>
-                                <li v-bind:class="tab == 'signin' ? 'is-active': ''">
-                                    <a v-on:click.prevent="tab = 'signin';">
+                                <li v-bind:class="{ 'is-active': isSignInTabActive }">
+                                    <a v-on:click.prevent="changeTab('signin');">
                                         <span class="icon is-small"><i class="fas fa-user"></i></span>
                                         <span>Sign in</span>
                                     </a>
                                 </li>
-                                <li v-bind:class="tab == 'signup' ? 'is-active': ''">
-                                    <a v-on:click.prevent="tab = 'signup';">
+                                <li v-bind:class="{ 'is-active': isSignUpTabActive }">
+                                    <a v-on:click.prevent="changeTab('signup');">
                                         <span class="icon is-small"><i class="fas fa-user-plus"></i></span>
                                         <span>Sign up</span>
                                     </a>
@@ -42,51 +42,50 @@ var signInUp = (function () {
                             </ul>
                         </div>
 
-                        <form v-on:submit.prevent="submitSignIn" v-if="tab == 'signin'">
+                        <form v-on:submit.prevent="submitSignIn" v-show="isSignInTabActive">
                             <div class="box is-radiusless">
                                 <label class="label">Email</label>
-                                <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : invalidSignInUsername }">
-                                    <input class="input" type="email" ref="signInEmail" name="email" maxlength="255" required v-bind:class="{ 'is-danger': invalidSignInUsername }" v-bind:disabled="loading ? true: false" v-model="signInEmail">
+                                <p class="control has-icons-left" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('signInEmail') }">
+                                    <input class="input" type="email" ref="signInEmail" maxlength="255" required v-bind:class="{ 'is-danger': validator.hasInvalidField('signInEmail') }" v-bind:disabled="loading ? true: false" v-model="signInEmail">
                                     <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignInUsername"><i class="fas fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignInUsername">Invalid email</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('signInEmail')"><i class="fas fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('signInEmail')">{{ validator.getInvalidFieldMessage('signInEmail') }}</p>
                                 </p>
                                 <label class="label">Password</label>
-                                <p class="control has-icons-left" id="password-container" v-bind:class="{ 'has-icons-right' : invalidSignInPassword }">
-                                    <input class="input" type="password" name="password" required v-bind:class="{ 'is-danger': invalidSignInPassword }" v-bind:disabled="loading ? true: false" v-model="signInPassword">
+                                <p class="control has-icons-left" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('signInPassword') }">
+                                    <input class="input" type="password" required v-bind:class="{ 'is-danger': validator.hasInvalidField('signInPassword') }" v-bind:disabled="loading ? true: false" v-model="signInPassword">
                                     <span class="icon is-small is-left"><i class="fas fa-key"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignInPassword"><i class="fas fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignInPassword">Invalid password</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('signInPassword')"><i class="fas fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('signInPassword')">{{ validator.getInvalidFieldMessage('signInPassword') }}</p>
                                 </p>
                                 <hr>
                                 <p class="control">
-                                    <button type="submit" class="button is-primary" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading ? true: false">
+                                    <button type="submit" class="button is-black" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading ? true: false">
                                         <span class="icon"><i class="fas fa-lock"></i></span>
                                         <span>Sign in</span>
                                     </button>
                                 </p>
                             </div>
                         </form>
-                        <form v-on:submit.prevent="submitSignUp" v-if="tab == 'signup'">
+                        <form v-on:submit.prevent="submitSignUp" v-show="isSignUpTabActive">
                             <div class="box is-radiusless">
                                 <label class="label">Email</label>
-                                <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : invalidSignUpUsername }">
-                                    <input class="input" type="email" name="email" maxlength="255" required v-bind:class="{ 'is-danger': invalidSignUpUsername }" v-bind:disabled="loading ? true: false" v-model="signUpEmail">
+                                <p class="control has-icons-left" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('signUpEmail') }">
+                                <input class="input" type="email" ref="signUpEmail" maxlength="255" required v-bind:class="{ 'is-danger': validator.hasInvalidField('signUpEmail') }" v-bind:disabled="loading ? true: false" v-model="signUpEmail">
                                     <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignUpUsername"><i class="fas fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignUpUsername">Email already used</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('signUpEmail')"><i class="fas fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('signUpEmail')">{{ validator.getInvalidFieldMessage('signUpEmail') }}</p>
                                 </p>
                                 <label class="label">Password</label>
-                                <p class="control has-icons-left" id="password-container" v-bind:class="{ 'has-icons-right' : invalidSignUpPassword }">
-                                    <input class="input" type="password" name="password" required v-bind:class="{ 'is-danger': invalidSignUpPassword }" v-bind:disabled="loading ? true: false" v-model="signUpPassword">
+                                <p class="control has-icons-left" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('signUpPassword') }">
+                                <input class="input" type="password" required v-bind:class="{ 'is-danger': validator.hasInvalidField('signUpPassword') }" v-bind:disabled="loading ? true: false" v-model="signUpPassword">
                                     <span class="icon is-small is-left"><i class="fas fa-key"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignUpPassword"><i class="fas fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignUpPassword">Invalid password</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('signUpPassword')"><i class="fas fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('signUpPassword')">{{ validator.getInvalidFieldMessage('signUpPassword') }}</p>
                                 </p>
                                 <hr>
-                                <p v-if="errors" class="help is-danger has-text-centered">Error creating account</p>
                                 <p class="control">
-                                    <button type="submit" class="button is-primary" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading ? true: false">
+                                    <button type="submit" class="button is-black" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading ? true: false">
                                         <span class="icon"><i class="fas fa-plus-circle"></i></span>
                                         <span>Sign up</span>
                                     </button>
@@ -115,27 +114,36 @@ var signInUp = (function () {
         },
         data: function () {
             return ({
+                validator: getValidator(),
                 loading: false,
                 signInEmail: null,
                 signInPassword: null,
-                invalidSignInUsername: false,
-                invalidSignInPassword: false,
                 allowSignUp: initialState.allowSignUp,
                 signUpEmail: null,
                 signUpPassword: null,
-                invalidSignUpUsername: false,
-                invalidSignUpPassword: false,
                 errors: false,
                 apiError: null,
                 tab: 'signin'
             });
         },
+        computed: {
+            isSignInTabActive: function () {
+                return (this.tab == 'signin');
+            },
+            isSignUpTabActive: function () {
+                return (this.tab == 'signup');
+            }
+        },
         methods: {
+            changeTab: function (tab) {
+                if (this.tab != tab) {
+                    this.tab = tab;
+                }
+            },
             submitSignIn: function () {
                 var self = this;
-                self.invalidSignInUsername = false;
-                self.invalidSignInPassword = false;
                 self.loading = true;
+                self.validator.clear();
                 self.errors = false;
                 spieldoseAPI.signIn(this.signInEmail, this.signInPassword, function (response) {
                     if (response.ok) {
@@ -143,20 +151,20 @@ var signInUp = (function () {
                     } else {
                         switch (response.status) {
                             case 400:
-                                if (response.body.invalidOrMissingParams.find(function (e) { return (e === "email"); })) {
-                                    self.invalidSignInUsername = true;
-                                } else if (response.body.invalidOrMissingParams.find(function (e) { return (e === "password"); })) {
-                                    self.invalidSignInPassword = true;
+                                if (response.isFieldInvalid("email")) {
+                                    self.validator.setInvalid("signInEmail", "API ERROR: invalid param");
+                                } else if (response.isFieldInvalid("password")) {
+                                    self.validator.setInvalid("signInPassword", "API ERROR: invalid param");
                                 } else {
                                     self.apiError = response.getApiErrorData();
                                     self.errors = true;
                                 }
                                 break;
                             case 404:
-                                self.invalidSignInUsername = true;
+                                self.validator.setInvalid("signInEmail", "No account found with this email");
                                 break;
                             case 401:
-                                self.invalidSignInPassword = true;
+                                self.validator.setInvalid("signInPassword", "Incorrect password");
                                 break;
                             default:
                                 self.apiError = response.getApiErrorData();
@@ -183,17 +191,17 @@ var signInUp = (function () {
                     } else {
                         switch (response.status) {
                             case 400:
-                                if (response.body.invalidOrMissingParams.find(function (e) { return (e === "email"); })) {
-                                    self.invalidSignInUsername = true;
-                                } else if (response.body.invalidOrMissingParams.find(function (e) { return (e === "password"); })) {
-                                    self.invalidSignInPassword = true;
+                                if (response.isFieldInvalid("email")) {
+                                    self.validator.setInvalid("signUpEmail", "API ERROR: invalid param");
+                                } else if (response.isFieldInvalid("password")) {
+                                    self.validator.setInvalid("signUpPassword", "API ERROR: invalid param");
                                 } else {
                                     self.apiError = response.getApiErrorData();
                                     self.errors = true;
                                 }
                                 break;
                             case 409:
-                                self.invalidSignUpUsername = true;
+                                self.validator.setInvalid("signUpEmail", "Email already used");
                                 break;
                             default:
                                 self.apiError = response.getApiErrorData();
