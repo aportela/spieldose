54eafe8d0feb67168f728a9c6a6e202ab41f2c2e
alex <766f6964+github@gmail.com>
2018-05-06 19:51:42 +0200

    code cleanups

--- public/scripts/vue-components/dashboard-recent.js
+++ public/scripts/vue-components/dashboard-recent.js
@@ -1,163 +1,179 @@
-var dashboardRecent = (function () {
+let dashboardRecent = (function () {
     "use strict";
 
-    var template = function () {
+    const template = function () {
         return `
-    <section class="panel chart">
-        <p class="panel-heading">
-            <span class="icon"><i v-if="loading" class="fas fa-cog fa-spin fa-fw"></i><i v-else-if="errors" class="fas fa-exclamation-triangle"></i><i v-else class="far fa-clock"></i></span> {{ title }}
-            <a v-on:click.prevent="load();" title="refresh data" class="icon is-pulled-right"><i class="fas fa-redo fa-fw"></i></a>
-        </p>
-        <p class="panel-tabs">
-            <a v-bind:class="{ 'is-active' : entity == 'tracks' }" v-on:click.prevent="changeEntity('tracks')" href="#">Tracks</a>
-            <a v-bind:class="{ 'is-active' : entity == 'artists' }" v-on:click.prevent="changeEntity('artists')" href="#">Artists</a>
-            <a v-bind:class="{ 'is-active' : entity == 'albums' }" v-on:click.prevent="changeEntity('albums')" href="#">Albums</a>
-        </p>
-        <div class="panel-block cut-text">
-            <ol v-if="items.length > 0">
-                <li class="is-small" v-if="entity == 'tracks'" v-for="item, i in items"><i v-on:click="playTrack(item);" class="cursor-pointer fa fa-play" title="play this track"></i> <i v-on:click="enqueueTrack(item);" class="cursor-pointer fa fa-plus-square" title="enqueue this track"></i> {{ item.title }}<span v-if="item.artist"> / <a v-on:click.prevent="$router.push({ name: 'artist', params: { artist: item.artist } })">{{ item.artist }}</a></span></li>
-                <li class="is-small" v-if="entity == 'artists'" v-for="item, i in items"><a v-on:click.prevent="$router.push({ name: 'artist', params: { artist: item.artist } })">{{ item.artist }}</a></li>
-                <li class="is-small" v-if="entity == 'albums'" v-for="item, i in items"><i v-on:click="playAlbum(item);" class="cursor-pointer fa fa-play" title="play this album"></i> <i v-on:click="enqueueAlbum(item);" class="cursor-pointer fa fa-plus-square" title="enqueue this album"></i> {{ item.album }}<span v-if="item.artist"> / <a v-on:click.prevent="$router.push({ name: 'artist', params: { artist: item.artist } })">{{ item.artist }}</a></span></li>
-            </ol>
-            <p v-else-if="items.length == 0 && ! loading && ! errors">not enough data for the stats</p>
-            <p v-else-if="errors">error loading data (invalid response from server)</p>
-        </div>
-    </section>
-    `;
+            <section class="panel chart">
+                <p class="panel-heading">
+                    <span class="icon">
+                        <i class="fas fa-cog fa-spin fa-fw" v-if="loading"></i>
+                        <i class="fas fa-exclamation-triangle" v-else-if="hasAPIErrors"></i>
+                        <i class="far fa-clock" v-else></i>
+                    </span>
+                    <span>{{ title }}</span>
+                    <a class="icon is-pulled-right" title="refresh data" v-on:click.prevent="load();"><i class="fas fa-redo fa-fw"></i></a>
+                </p>
+                <p class="panel-tabs">
+                    <a v-bind:class="{ 'is-active' : isTrackEntity }" v-on:click.prevent="changeEntity('tracks')">Tracks</a>
+                    <a v-bind:class="{ 'is-active' : isArtistEntity }" v-on:click.prevent="changeEntity('artists')">Artists</a>
+                    <a v-bind:class="{ 'is-active' : isAlbumEntity }" v-on:click.prevent="changeEntity('albums')">Albums</a>
+                </p>
+                <div class="panel-block cut-text">
+                    <ol v-if="hasItems">
+                        <li class="is-small" v-if="isTrackEntity" v-for="item in items" v-bind:key="item.id">
+                            <i class="cursor-pointer fa fa-play" title="play this track" v-on:click="playTrack(item);"></i>
+                            <i class="cursor-pointer fa fa-plus-square" title="enqueue this track" v-on:click="enqueueTrack(item);"></i>
+                            <span>{{ item.title }}</span>
+                            <span v-if="item.artist"> / <a v-on:click.prevent="navigateToArtistPage(item.artist);">{{ item.artist }}</a></span>
+                        </li>
+                        <li class="is-small" v-if="isArtistEntity" v-for="item in items">
+                            <a v-on:click.prevent="navigateToArtistPage(item.artist);">{{ item.artist }}</a>
+                        </li>
+                        <li class="is-small" v-if="isAlbumEntity" v-for="item in items">
+                            <i class="cursor-pointer fa fa-play" title="play this album" v-on:click="playAlbum(item);" ></i>
+                            <i class="cursor-pointer fa fa-plus-square" title="enqueue this album" v-on:click="enqueueAlbum(item);"></i>
+                            <span>{{ item.album }}</span>
+                            <span v-if="item.artist"> / <a v-on:click.prevent="navigateToArtistPage(item.artist);">{{ item.artist }}</a></span>
+                        </li>
+                    </ol>
+                    <p v-else-if="! hasItems && ! loading && ! hasAPIErrors">not enough data for the stats</p>
+                    <p v-else-if="hasAPIErrors">error loading data (invalid response from server)</p>
+                </div>
+            </section>
+        `;
     };
 
-    /* app chart (test) component */
+    /* recent played (track/artist/album) component */
     var module = Vue.component('spieldose-dashboard-recent', {
         template: template(),
+        mixins: [mixinAPIError, mixinPlayer],
         data: function () {
             return ({
                 loading: false,
-                errors: false,
-                entity: 'tracks',
                 items: [],
-                playerData: sharedPlayerData,
+                actualEntity: 'tracks'
             });
         },
+        props: ['type', 'title', 'listItemCount'],
         created: function () {
             this.load();
+        }, computed: {
+            isTrackEntity: function () {
+                return (this.actualEntity == 'tracks');
+            },
+            isArtistEntity: function () {
+                return (this.actualEntity == 'artists');
+            },
+            isAlbumEntity: function () {
+                return (this.actualEntity == 'albums');
+            },
+            hasItems: function () {
+                return (this.items && this.items.length > 0);
+            }
         }, methods: {
+            navigateToArtistPage: function (artist) {
+                if (artist) {
+                    this.$router.push({ name: 'artist', params: { artist: artist } });
+                }
+            },
             loadRecentAddedTracks: function () {
                 var self = this;
-                self.loading = true;
-                self.errors = false;
-                self.items = [];
                 spieldoseAPI.getRecentAddedTracks(this.interval, function (response) {
                     if (response.ok) {
                         if (response.body.metrics && response.body.metrics.length > 0) {
                             self.items = response.body.metrics;
                         }
-                        self.loading = false;
                     } else {
-                        self.loading = false;
-                        self.errors = true;
+                        self.setAPIError(response.getApiErrorData());
                     }
+                    self.loading = false;
                 });
             },
             loadRecentAddedArtists: function () {
                 var self = this;
-                self.loading = true;
-                self.errors = false;
-                self.items = [];
                 spieldoseAPI.getRecentAddedArtists(this.interval, function (response) {
                     if (response.ok) {
                         if (response.body.metrics && response.body.metrics.length > 0) {
                             self.items = response.body.metrics;
                         }
-                        self.loading = false;
                     } else {
-                        self.loading = false;
-                        self.errors = true;
+                        self.setAPIError(response.getApiErrorData());
                     }
+                    self.loading = false;
                 });
             },
             loadRecentAddedAlbums: function () {
                 var self = this;
-                self.loading = true;
-                self.errors = false;
-                self.items = [];
                 spieldoseAPI.getRecentAddedAlbums(this.interval, function (response) {
                     if (response.ok) {
                         if (response.body.metrics && response.body.metrics.length > 0) {
                             self.items = response.body.metrics;
                         }
-                        self.loading = false;
                     } else {
-                        self.loading = false;
-                        self.errors = true;
+                        self.setAPIError(response.getApiErrorData());
                     }
+                    self.loading = false;
                 });
             },
             loadRecentPlayedTracks: function () {
                 var self = this;
-                self.loading = true;
-                self.errors = false;
-                self.items = [];
                 spieldoseAPI.getRecentPlayedTracks(this.interval, function (response) {
                     if (response.ok) {
                         if (response.body.metrics && response.body.metrics.length > 0) {
                             self.items = response.body.metrics;
                         }
-                        self.loading = false;
                     } else {
-                        self.loading = false;
-                        self.errors = true;
+                        self.setAPIError(response.getApiErrorData());
                     }
+                    self.loading = false;
                 });
             },
             loadRecentPlayedArtists: function () {
                 var self = this;
-                self.loading = true;
-                self.errors = false;
-                self.items = [];
                 spieldoseAPI.getRecentPlayedArtists(this.interval, function (response) {
                     if (response.ok) {
                         if (response.body.metrics && response.body.metrics.length > 0) {
                             self.items = response.body.metrics;
                         }
-                        self.loading = false;
                     } else {
-                        self.loading = false;
-                        self.errors = true;
+                        self.setAPIError(response.getApiErrorData());
                     }
+                    self.loading = false;
                 });
             },
             loadRecentPlayedAlbums: function () {
                 var self = this;
-                self.loading = true;
-                self.errors = false;
-                self.items = [];
                 spieldoseAPI.getRecentPlayedAlbums(this.interval, function (response) {
                     if (response.ok) {
                         if (response.body.metrics && response.body.metrics.length > 0) {
                             self.items = response.body.metrics;
                         }
-                        self.loading = false;
                     } else {
-                        self.loading = false;
-                        self.errors = true;
+                        self.setAPIError(response.getApiErrorData());
                     }
+                    self.loading = false;
                 });
             },
             load: function () {
+                this.clearAPIErrors();
+                this.loading = true;
+                this.items = [];
                 switch (this.type) {
                     case "recentlyAdded":
                         this.loadRecentAdded();
                         break;
-                    case "recentlyPlayed": {
+                    case "recentlyPlayed":
                         this.loadRecentPlayed();
-                    }
+                        break;
+                    default:
+                        this.loading = false;
+                        break;
                 }
             },
             loadRecentAdded: function () {
-                switch (this.entity) {
+                switch (this.actualEntity) {
                     case "tracks":
                         this.loadRecentAddedTracks();
-
                         break;
                     case "artists":
                         this.loadRecentAddedArtists();
@@ -165,10 +181,13 @@ var dashboardRecent = (function () {
                     case "albums":
                         this.loadRecentAddedAlbums();
                         break;
+                    default:
+                        this.loading = false;
+                        break;
                 }
             },
             loadRecentPlayed: function () {
-                switch (this.entity) {
+                switch (this.actualEntity) {
                     case "tracks":
                         this.loadRecentPlayedTracks();
                         break;
@@ -178,11 +197,14 @@ var dashboardRecent = (function () {
                     case "albums":
                         this.loadRecentPlayedAlbums();
                         break;
+                    default:
+                        this.loading = false;
+                        break;
                 }
             },
-            changeEntity: function (e) {
-                if (e && e != this.entity) {
-                    this.entity = e;
+            changeEntity: function (entity) {
+                if (entity && entity != this.actualEntity) {
+                    this.actualEntity = entity;
                     this.load();
                 }
             }, playTrack: function (track) {
@@ -190,10 +212,11 @@ var dashboardRecent = (function () {
             }, enqueueTrack: function (track) {
                 this.playerData.enqueue([track]);
             }, playAlbum: function (album) {
+                // TODO
             }, enqueueAlbum: function (album) {
+                // TODO
             }
-        },
-        props: ['type', 'title', 'listItemCount']
+        }
     });
 
     return (module);
