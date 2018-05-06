1e6c2768c3037822ce15d9e7475ddf099e663c58
alex <766f6964+github@gmail.com>
2018-05-06 18:55:43 +0200

    code cleanups

--- public/scripts/vue-components/upgrade.js
+++ public/scripts/vue-components/upgrade.js
@@ -1,48 +1,43 @@
-var upgrade = (function () {
+let upgrade = (function () {
     "use strict";
 
-    var template = function () {
+    const template = function () {
         return `
-    <!-- template credits: daniel (https://github.com/dansup) -->
-    <section class="hero is-fullheight is-light is-bold">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns is-vcentered">
-                    <div class="column is-4 is-offset-4">
-                        <h1 class="title has-text-centered"><span class="icon is-medium"><i class="fas fa-music" aria-hidden="true"></i></span> Spieldose <span class="icon is-medium"><i class="fas fa-music" aria-hidden="true"></i></span></h1>
-                        <h2 class="subtitle is-6 has-text-centered"><cite>...music for the Masses</cite></h2>
-
-                        <div class="notification is-warning" v-if="upgradeAvailable">
-                            <p class="title is-5"><span class="icon"><i class="fas fa-warning"></i></span> New database version available</p>
-                            <hr>
-                            <p class="subtitle is-5">An upgrade is required</p>
-                            <p>Execute this commandline:</p>
-                            <p>php tools/install-upgrade-db.php</p>
+            <!-- template credits: daniel (https://github.com/dansup) -->
+            <section class="hero is-fullheight is-light is-bold">
+                <div class="hero-body">
+                    <div class="container">
+                        <div class="columns is-vcentered">
+                            <div class="column is-4 is-offset-4">
+                                <h1 class="title has-text-centered"><span class="icon is-medium"><i class="fas fa-music" aria-hidden="true"></i></span> Spieldose <span class="icon is-medium"><i class="fas fa-music" aria-hidden="true"></i></span></h1>
+                                <h2 class="subtitle is-6 has-text-centered"><cite>...music for the Masses</cite></h2>
+                                <div class="notification is-warning" v-if="upgradeAvailable">
+                                    <p class="title is-5"><span class="icon"><i class="fas fa-warning"></i></span> New database version available</p>
+                                    <hr>
+                                    <p class="subtitle is-5">An upgrade is required</p>
+                                    <p>Execute this commandline:</p>
+                                    <p>php tools/install-upgrade-db.php</p>
+                                </div>
+                                <div class="notification is-success" v-else>
+                                    <p class="title is-5"><span class="icon"><i class="fas fa-ok"></i></span> Your system is up to date</p>
+                                </div>
+                                <p class="has-text-centered">
+                                    <a href="https://github.com/aportela/spieldose" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span>Project page</a> | <a href="https://github.com/aportela" target="_blank">by alex</a>
+                                </p>
+                            </div>
                         </div>
-                        <div class="notification is-success" v-else>
-                            <p class="title is-5"><span class="icon"><i class="fas fa-ok"></i></span> Your system is up to date</p>
-                        </div>
-
-                        <p class="has-text-centered">
-                            <a href="https://github.com/aportela/spieldose"><span class="icon is-small"><i class="fas fa-github"></i></span>Project page</a> | <a href="mailto:766f6964+github@gmail.com">by alex</a>
-                        </p>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-    `;
+            </section>
+        `;
     };
 
     /* signIn component */
-    var module = Vue.component('spieldose-upgrade-component', {
-        template: template(),
-        data: function () {
-            return ({
-            });
-        }, computed: {
-            upgradeAvailable: function() {
-                return(initialState.upgradeAvailable);
+    let module = Vue.component('spieldose-upgrade-component', {
+        template: template()
+        , computed: {
+            upgradeAvailable: function () {
+                return (initialState.upgradeAvailable);
             }
         }
     });
