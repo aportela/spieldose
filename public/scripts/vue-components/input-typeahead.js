5017fd519496015029a9c67c2ce339e4fe5e56bb
alex <766f6964+github@gmail.com>
2018-05-07 00:28:23 +0200

    new typeahead component for live search (search as you type)

---
+++ public/scripts/vue-components/input-typeahead.js
@@ -0,0 +1,39 @@
+let inputTypeahead = (function () {
+    "use strict";
+
+    const template = function () {
+        return `
+            <input class="input" type="text" v-bind:placeholder="placeholder" v-bind:disabled="loading" v-model.trim="text" v-on:keyup.esc="onClear" v-on:keyup="onChange">
+        `;
+    };
+
+    /* input typeahead component */
+    let module = Vue.component('spieldose-input-typeahead', {
+        template: template(),
+        mixins: [],
+        data: function () {
+            return ({
+                text: null,
+                timeout: null
+            });
+        },
+        props: [
+            'placeholder', 'loading'
+        ],
+        methods: {
+            onClear: function() {
+                this.text = null;
+            },
+            onChange: function() {
+                if (this.timeout) {
+                    clearTimeout(this.timeout);
+                }
+                this.timeout = setTimeout(() => {
+                    this.$emit("on-value-change", this.text);
+                }, 256);
+            }
+        }
+    });
+
+    return (module);
+})();
