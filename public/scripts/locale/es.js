05f87e38985dbc06662eb8c5f2205887e345cb8b
alex <766f6964+github@gmail.com>
2018-06-09 20:19:18 +0200

    added spanish & galician locales

---
+++ public/scripts/locale/es.js
@@ -0,0 +1,296 @@
+const messages = {
+    es: {
+        signIn: {
+            labels: {
+                tabLink: 'Iniciar sesión',
+                email: 'Correo electrónico',
+                password: 'Contraseña'
+            },
+            buttons: {
+                submit: 'Iniciar sesión'
+            },
+            errorMessages: {
+                userNotFound: 'No existe ninguna cuenta con ese correo electrónico asociado',
+                incorrectPassword: 'Clave incorrecta'
+            }
+        },
+        signUp: {
+            labels: {
+                tabLink: 'Crear cuenta',
+                email: 'Correo electrónico',
+                password: 'Contraseña'
+            },
+            buttons: {
+                submit: 'Crear cuenta'
+            },
+            errorMessages: {
+                emailAlreadyUsed: 'El correo electrónico especificado ya está en uso'
+            }
+        },
+        upgrade: {
+            labels: {
+                newDatabaseVersionAvailable: 'Hay disponible una nueva versión de la base de datos',
+                upgradeRequired: 'Se requiere actualizar',
+                executeThisCommandline: 'Ejecute esta línea de comandos:',
+                updateNotRequired: 'Su sistema está actualizado'
+            }
+        },
+        dashboard: {
+            labels: {
+                allTimeInterval: 'Siempre',
+                pastWeekInterval: 'La semana pasada',
+                pastMonthInterval: 'El mes pasado',
+                pastSemesterInterval: 'El semestre pasado',
+                pastYearInterval: 'El año pasado',
+                playCount: 'reproducciones',
+                entityTracks: 'Canciones',
+                entityArtists: 'Artistas',
+                entityAlbums: 'Álbumes',
+                topPlayedTracks: 'Canciones más escuchadas',
+                topPlayedArtists: 'Artistas más escuchados',
+                topPlayedGenres: 'Géneros más escuchados',
+                recentlyAdded: 'Añadido/a recientemente',
+                recentlyPlayed: 'Escuchado/a recientemente',
+                playStatistics: 'Estadísticas de reproducciones',
+                byHour: 'Por hora',
+                playStatsByWeekday: 'Estadísticas de reproducciones semanales',
+                byWeekday: 'Por día de la semana',
+                byMonth: 'Por mes',
+                byYear: 'Por año',
+                monday: 'Lunes',
+                tuesday: 'Martes',
+                wednesday: 'Miércoles',
+                thursday: 'Jueves',
+                friday: 'Viernes',
+                saturday: 'Sábado',
+                sunday: 'Domingo',
+                playStatsByMonth: 'Estadísticas de reproducciones mensuales',
+                january: 'Enero',
+                february: 'Febrero',
+                march: 'Marzo',
+                april: 'Abril',
+                may: 'Mayo',
+                june: 'Junio',
+                july: 'Julio',
+                august: 'Agosto',
+                september: 'Septiembre',
+                october: 'Octubre',
+                november: 'Noviembre',
+                december: 'Diciembre',
+                playStatsByYear: 'Estadísticas de reproducciones anuales'
+            },
+            errors: {
+                notEnoughData: 'No se han recolectado datos suficientes para generar las estadísticas'
+            }
+        },
+        player: {
+            buttons: {
+                shufflePlaylistHint: 'reordenar aleatoriamente la lista actual',
+                toggleRepeatHint: 'cambiar el modo de repetición',
+                previousTrackHint: 'ir a la canción anterior',
+                pauseTrackHint: 'pausar la canción actual',
+                playTrackHint: 'reproducir la canción actual',
+                nextTrackHint: 'ir a la siguiente canción',
+                unloveTrackHint: 'desmarcar la canción actual como favorita',
+                loveTrackHint: 'marcar la canción actual como favorita',
+                downloadTrackHint: 'descargar la canción actual',
+                toggleMuteHint: 'silenciar/restaurar volumen'
+            }
+        },
+        menu: {
+            labels: {
+                header: 'Menu',
+                dashboard: 'Estadísticas',
+                currentPlaylist: 'Lista de reproducción actual',
+                search: 'Búsqueda',
+                browseArtists: 'Artistas',
+                browseAlbums: 'Álbumes',
+                browsePaths: 'Directorios',
+                browsePlaylists: 'Listas de reproducción',
+                signOut: 'Finalizar sesión'
+            }
+        },
+        pagination: {
+            labels: {
+                invalidPageOrNoResults: 'La página especificada es incorrecta o no se han encontrado resultados'
+            },
+            buttons: {
+                previousPage: 'Anterior',
+                nextPage: 'Siguiente'
+            }
+        },
+        deleteConfirmationModal: {
+            labels: {
+                modalTitle: 'Confirmación requerida',
+                modalBody: '¿ Está seguro de que desea eliminar permanentemente este elemento ?'
+            },
+            buttons: {
+                ok: 'Si',
+                cancel: 'Cancelar'
+            }
+        },
+        currentPlaylist: {
+            labels: {
+                sectionName: 'Lista de reproducción actual',
+                tableHeaderTrack: 'Título',
+                tableHeaderArtist: 'Artista',
+                tableHeaderAlbum: 'Álbum',
+                tableHeaderGenre: 'Género',
+                tableHeaderYear: 'Año',
+                tableHeaderActions: 'Acciones',
+                moveElementUpHint: 'subir esta canción en la lista de reproducción',
+                moveElementDownHint: 'bajar esta canción en la lista de reproducción',
+                removeElementHint: 'eliminar esta canción de la lista de reproducción',
+                playThisTrackHint: 'escuchar esta canción',
+                nowPlayingClickToPauseHint: 'reproduciendo, haga click para pausar',
+                pausedClickToResumeHint: 'en pausa, haga click para resumir reproducción'
+            },
+            inputs: {
+                playlistNamePlaceholder: 'teclee el nombre de la lista de reproducción'
+            },
+            buttons: {
+                savePlaylist: 'grabar lista',
+                unsetPlaylist: 'desenlazar lista',
+                loadRandom: 'cargar canciones aleatorias',
+                clearPlaylist: 'vaciar lista',
+                shufflePlaylist: 'reordenar aleatoriamente',
+                previousTrack: 'anterior',
+                nextTrack: 'siguiente',
+                playTrack: 'reproducir',
+                resumeTrack: 'resumir reproducción',
+                pauseTrack: 'pausar',
+                stopTrack: 'detener',
+                loveTrack: 'favorita',
+                unloveTrack: 'favorita',
+                downloadTrack: 'descargar',
+            }
+        },
+        search: {
+            labels: {
+                sectionName: 'Buscar artistas, álbumes, canciones y listas de reproducción'
+            },
+            tabs: {
+                artists: 'Artistas',
+                albums: 'Álbumes',
+                tracks: 'Canciones',
+                playlists: 'Listas de reproducción'
+            },
+            inputs: {
+                searchTextPlaceholder: 'buscar artistas, álbumes, canciones y listas de reproducción'
+            },
+            buttons: {
+                search: 'buscar'
+            }
+        },
+        browseArtists: {
+            labels: {
+                sectionName: 'Artistas',
+            },
+            inputs: {
+                artistNamePlaceholder: 'teclee el nombre del artista...'
+            },
+            buttons: {
+                search: 'buscar'
+            },
+            dropdowns: {
+                filterAllArtists: 'Todos los artistas',
+                filterNotScrapedArtists: 'Artistas sin metadatos'
+            }
+        },
+        browseArtist: {
+            labels: {
+                sectionName: 'Detalles del artista',
+                plays: 'reproducciones',
+                notPlayedYet: 'nunca se ha escuchado',
+                tracksAlbumTableHeader: 'Álbum',
+                tracksSectionYearTableHeader: 'Año',
+                tracksSectionNumberTableHeader: 'Número',
+                tracksSectionTitleTableHeader: 'Canción',
+                tracksSectionActionTableHeader: 'Acciones',
+                musicBrainzSearchArtistName: 'Nombre del artista:',
+                musicBrainzSearchArtistNamePlaceholder: 'buscar artista por nombre...',
+                musicBrainzSearchArtistMBId: 'Identificador Music Brainz:',
+                musicBrainzSearchArtistMBIdPlaceholder: 'establecer el identificador Music Brainz del artista'
+            },
+            tabs: {
+                overview: 'Resumen',
+                bio: 'Biografía',
+                tracks: 'Canciones',
+                albums: 'Álbumes',
+                updateArtist: 'Actualizar metadatos'
+            },
+            buttons: {
+                search: 'buscar',
+                searchOnMusicBrainz: 'buscar en Music Brainz',
+                save: 'grabar',
+                clear: 'eliminar'
+            }
+        },
+        browseAlbums: {
+            labels: {
+                sectionName: 'Álbumes',
+                unknownArtist: 'artista desconocido'
+            },
+            inputs: {
+                albumNamePlaceholder: 'buscar álbum por nombre...',
+                yearPlaceholder: 'año (4 dígitos)',
+                artistNamePlaceholder: 'buscar álbum por nombre de artista...'
+            },
+            buttons: {
+                toggleAdvancedSearch: 'mostrar/ocultar búsqueda avanzada',
+                search: 'buscar'
+            }
+        },
+        browsePaths: {
+            labels: {
+                sectionName: 'Directorios',
+                pathNameTableHeader: 'Ruta',
+                trackCountTableHeader: 'Canciones',
+                actionsTableHeader: 'Acciones',
+                playThisPath: 'Escuchar todas las canciones de esta ruta',
+                enqueueThisPath: 'Añadir todas las canciones de esta ruta a la cola de reproducción actual'
+            },
+            inputs: {
+                pathNamePlaceholder: 'buscar ruta por nombre...'
+            },
+            buttons: {
+                search: 'buscar'
+            }
+        },
+        browsePlaylists: {
+            labels: {
+                sectionName: 'Listas de reproducción'
+            },
+            inputs: {
+                playlistNamePlaceholder: 'buscar lista de reproducción por nombre...'
+            },
+            buttons: {
+                search: 'buscar',
+                play: 'escuchar',
+                remove: 'eliminar'
+            }
+        },
+        commonErrors: {
+            invalidAPIParam: 'API ERROR: parámetro incorrecto',
+            invalidAPIResponse: 'API ERROR: respuesta del servidor desconocida'
+        },
+        commonLabels: {
+            slogan: '...música para todos',
+            projectPageLinkLabel: 'Página del proyecto',
+            authorLinkLabel: 'por alex',
+            playThisTrack: 'Escuchar esta canción',
+            enqueueThisTrack: 'Añadir esta canción a la cola de reproducción actual',
+            downloadThisTrack: 'Descargar esta canción',
+            navigateToArtistPage: 'Visitar la página del artista',
+            playThisAlbum: 'Escuchar este álbum',
+            enqueueThisAlbum: 'Añadir este álbum a la cola de reproducción actual',
+            playThisPlaylist: 'Escuchar esta lista de reproducción',
+            enqueueThisPlaylist: 'Añadir esta lista de reproducción a la cola de reproducción actual',
+            by: 'por',
+            tracksCount: 'canciones'
+        },
+        commonMessages: {
+            refreshData: 'Refrescar datos'
+        }
+    }
+};
---
+++ public/scripts/locale/gl.js
@@ -0,0 +1,296 @@
+const messages = {
+    gl: {
+        signIn: {
+            labels: {
+                tabLink: 'Iniciar sesión',
+                email: 'Correo electrónico',
+                password: 'Contrasinal'
+            },
+            buttons: {
+                submit: 'Iniciar sesión'
+            },
+            errorMessages: {
+                userNotFound: 'Non existe ningunha conta con ese correo electrónico asociado',
+                incorrectPassword: 'Contrasinal incorrecta'
+            }
+        },
+        signUp: {
+            labels: {
+                tabLink: 'Crear conta',
+                email: 'Correo electrónico',
+                password: 'Contrasinal'
+            },
+            buttons: {
+                submit: 'Crear conta'
+            },
+            errorMessages: {
+                emailAlreadyUsed: 'O correo electrónico especificado xa está en uso'
+            }
+        },
+        upgrade: {
+            labels: {
+                newDatabaseVersionAvailable: 'Atopouse disponible unha nova versión da base de datos',
+                upgradeRequired: 'Requírese actualizar',
+                executeThisCommandline: 'Execute esta línea de comandos:',
+                updateNotRequired: 'O seu sistema está actualizado'
+            }
+        },
+        dashboard: {
+            labels: {
+                allTimeInterval: 'Sempre',
+                pastWeekInterval: 'A semana pasada',
+                pastMonthInterval: 'O mes pasado',
+                pastSemesterInterval: 'O semestre pasado',
+                pastYearInterval: 'O ano pasado',
+                playCount: 'reproduccións',
+                entityTracks: 'Cancions',
+                entityArtists: 'Artistas',
+                entityAlbums: 'Álbumes',
+                topPlayedTracks: 'Cancións máis escoitadas',
+                topPlayedArtists: 'Artistas máis escoitados',
+                topPlayedGenres: 'Xéneros máis escoitados',
+                recentlyAdded: 'Engadido/a recentemente',
+                recentlyPlayed: 'Escoitado/a recentemente',
+                playStatistics: 'Estadísticas de reproduccións',
+                byHour: 'Por hora',
+                playStatsByWeekday: 'Estadísticas de reproduccións semanais',
+                byWeekday: 'Por día da semana',
+                byMonth: 'Por mes',
+                byYear: 'Por ano',
+                monday: 'Luns',
+                tuesday: 'Martes',
+                wednesday: 'Mércoles',
+                thursday: 'Xoves',
+                friday: 'Venres',
+                saturday: 'Sábado',
+                sunday: 'Domingo',
+                playStatsByMonth: 'Estadísticas de reproduccións mensuais',
+                january: 'Xaneiro',
+                february: 'Febreiro',
+                march: 'Marzo',
+                april: 'Abril',
+                may: 'Maio',
+                june: 'Xuño',
+                july: 'Xullo',
+                august: 'Agosto',
+                september: 'Setembro',
+                october: 'Outubro',
+                november: 'Novembro',
+                december: 'Decembro',
+                playStatsByYear: 'Estadísticas de reproduccións anuais'
+            },
+            errors: {
+                notEnoughData: 'Non se recolectaron datos suficientes pra xear as estadísticas'
+            }
+        },
+        player: {
+            buttons: {
+                shufflePlaylistHint: 'reordear aleatoriamente a lista actual',
+                toggleRepeatHint: 'cambiar o modo de repetición',
+                previousTrackHint: 'ir á canción anterior',
+                pauseTrackHint: 'pausar a canción actual',
+                playTrackHint: 'reproducir a canción actual',
+                nextTrackHint: 'ir á seguinte canción',
+                unloveTrackHint: 'desmarcar a canción actual como favorita',
+                loveTrackHint: 'marcar a canción actual como favorita',
+                downloadTrackHint: 'descargar a canción actual',
+                toggleMuteHint: 'silenciar/restaurar volumen'
+            }
+        },
+        menu: {
+            labels: {
+                header: 'Menu',
+                dashboard: 'Estadísticas',
+                currentPlaylist: 'Lista de reproducción actual',
+                search: 'Procura',
+                browseArtists: 'Artistas',
+                browseAlbums: 'Álbumes',
+                browsePaths: 'Directorios',
+                browsePlaylists: 'Listas de reproducción',
+                signOut: 'Rematar sesión'
+            }
+        },
+        pagination: {
+            labels: {
+                invalidPageOrNoResults: 'A páxina especificada é incorrecta ou non se atoparon resultados'
+            },
+            buttons: {
+                previousPage: 'Anterior',
+                nextPage: 'Seguinte'
+            }
+        },
+        deleteConfirmationModal: {
+            labels: {
+                modalTitle: 'Confirmación requerida',
+                modalBody: '¿ Está seguro de que desexa eliminar permanentemente este elemento ?'
+            },
+            buttons: {
+                ok: 'Si',
+                cancel: 'Cancelar'
+            }
+        },
+        currentPlaylist: {
+            labels: {
+                sectionName: 'Lista de reproducción actual',
+                tableHeaderTrack: 'Título',
+                tableHeaderArtist: 'Artista',
+                tableHeaderAlbum: 'Álbum',
+                tableHeaderGenre: 'Xénero',
+                tableHeaderYear: 'Ano',
+                tableHeaderActions: 'Accións',
+                moveElementUpHint: 'subir esta canción na lista de reproducción',
+                moveElementDownHint: 'baixar esta canción na lista de reproducción',
+                removeElementHint: 'eliminar esta canción da lista de reproducción',
+                playThisTrackHint: 'escoitar esta canción',
+                nowPlayingClickToPauseHint: 'reproducindo, faga click pra pausar',
+                pausedClickToResumeHint: 'en pausa, faga click pra resumir reproducción'
+            },
+            inputs: {
+                playlistNamePlaceholder: 'teclee o nombre da lista de reproducción'
+            },
+            buttons: {
+                savePlaylist: 'grabar lista',
+                unsetPlaylist: 'desenlazar lista',
+                loadRandom: 'cargar cancións aleatorias',
+                clearPlaylist: 'vaciar lista',
+                shufflePlaylist: 'reordear aleatoriamente',
+                previousTrack: 'anterior',
+                nextTrack: 'seguinte',
+                playTrack: 'reproducir',
+                resumeTrack: 'resumir reproducción',
+                pauseTrack: 'pausar',
+                stopTrack: 'deter',
+                loveTrack: 'favorita',
+                unloveTrack: 'favorita',
+                downloadTrack: 'descargar',
+            }
+        },
+        search: {
+            labels: {
+                sectionName: 'Procurar artistas, álbumes, cancións e listas de reproducción'
+            },
+            tabs: {
+                artists: 'Artistas',
+                albums: 'Álbumes',
+                tracks: 'Cancións',
+                playlists: 'Listas de reproducción'
+            },
+            inputs: {
+                searchTextPlaceholder: 'procurar artistas, álbumes, cancións y listas de reproducción'
+            },
+            buttons: {
+                search: 'procurar'
+            }
+        },
+        browseArtists: {
+            labels: {
+                sectionName: 'Artistas',
+            },
+            inputs: {
+                artistNamePlaceholder: 'procure artista por nome...'
+            },
+            buttons: {
+                search: 'procurar'
+            },
+            dropdowns: {
+                filterAllArtists: 'Tódolos artistas',
+                filterNotScrapedArtists: 'Artistas sen metadatos'
+            }
+        },
+        browseArtist: {
+            labels: {
+                sectionName: 'Detalles do artista',
+                plays: 'reproduccións',
+                notPlayedYet: 'nunca se escoitou',
+                tracksAlbumTableHeader: 'Álbum',
+                tracksSectionYearTableHeader: 'Ano',
+                tracksSectionNumberTableHeader: 'Número',
+                tracksSectionTitleTableHeader: 'Canción',
+                tracksSectionActionTableHeader: 'Accións',
+                musicBrainzSearchArtistName: 'Nome do artista:',
+                musicBrainzSearchArtistNamePlaceholder: 'procurar artista por nome...',
+                musicBrainzSearchArtistMBId: 'Identificador Music Brainz:',
+                musicBrainzSearchArtistMBIdPlaceholder: 'establecer o identificador Music Brainz do artista'
+            },
+            tabs: {
+                overview: 'Resumen',
+                bio: 'Biografía',
+                tracks: 'Cancións',
+                albums: 'Álbumes',
+                updateArtist: 'Actualizar metadatos'
+            },
+            buttons: {
+                search: 'procurar',
+                searchOnMusicBrainz: 'procurar en Music Brainz',
+                save: 'grabar',
+                clear: 'eliminar'
+            }
+        },
+        browseAlbums: {
+            labels: {
+                sectionName: 'Álbumes',
+                unknownArtist: 'artista descoñecido'
+            },
+            inputs: {
+                albumNamePlaceholder: 'procurar álbum por nome...',
+                yearPlaceholder: 'ano (4 díxitos)',
+                artistNamePlaceholder: 'procurar álbum por nome do artista...'
+            },
+            buttons: {
+                toggleAdvancedSearch: 'mostrar/ocultar procura avanzada',
+                search: 'procurar'
+            }
+        },
+        browsePaths: {
+            labels: {
+                sectionName: 'Directorios',
+                pathNameTableHeader: 'Ruta',
+                trackCountTableHeader: 'Cancións',
+                actionsTableHeader: 'Accións',
+                playThisPath: 'Escoitar todas as cancións desta ruta',
+                enqueueThisPath: 'Engadir todas as cancións desta ruta á cola de reproducción actual'
+            },
+            inputs: {
+                pathNamePlaceholder: 'procurar ruta por nome...'
+            },
+            buttons: {
+                search: 'procurar'
+            }
+        },
+        browsePlaylists: {
+            labels: {
+                sectionName: 'Listas de reproducción'
+            },
+            inputs: {
+                playlistNamePlaceholder: 'procurar lista de reproducción por nome...'
+            },
+            buttons: {
+                search: 'procurar',
+                play: 'escoitar',
+                remove: 'eliminar'
+            }
+        },
+        commonErrors: {
+            invalidAPIParam: 'API ERROR: parámetro incorrecto',
+            invalidAPIResponse: 'API ERROR: resposta do servidor descoñecida'
+        },
+        commonLabels: {
+            slogan: '...música pra todos',
+            projectPageLinkLabel: 'Páxina do proxecto',
+            authorLinkLabel: 'por alex',
+            playThisTrack: 'Escoitar esta canción',
+            enqueueThisTrack: 'Engadir esta canción á cola de reproducción actual',
+            downloadThisTrack: 'Descargar esta canción',
+            navigateToArtistPage: 'Visitar a páxina do artista',
+            playThisAlbum: 'Escoitar este álbum',
+            enqueueThisAlbum: 'Engadir este álbum á cola de reproducción actual',
+            playThisPlaylist: 'Escoitar esta lista de reproducción',
+            enqueueThisPlaylist: 'Engadir esta lista de reproducción á cola de reproducción actual',
+            by: 'por',
+            tracksCount: 'cancións'
+        },
+        commonMessages: {
+            refreshData: 'Refrescar datos'
+        }
+    }
+};
