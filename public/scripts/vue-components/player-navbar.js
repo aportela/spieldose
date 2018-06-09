72572f887ea4a3f1e643ae50453f7d1691462e1d
alex <766f6964+github@gmail.com>
2018-06-09 18:39:06 +0200

    added i18n on player navbar

--- public/scripts/vue-components/player-navbar.js
+++ public/scripts/vue-components/player-navbar.js
@@ -9,40 +9,40 @@ let playerNavBar = (function () {
                         <div class="navbar-item has-dropdown has-dropdown-up is-hoverable">
                             <a class="navbar-link">
                                 <span class="icon"><i class="fas fa-bars"></i></span>
-                                <span>Menu</span>
+                                <span>{{ $t('menu.labels.header') }}</span>
                             </a>
                             <div class="navbar-dropdown">
                                 <a v-on:click.prevent="$router.push({ name: 'dashboard' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'dashboard'}">
                                 <span class="panel-icon"><i class="fas fa-chart-line"></i></span>
-                                dashboard
+                                {{ $t('menu.labels.dashboard') }}
                                 </a>
                                 <a v-on:click.prevent="$router.push({ name: 'nowPlaying' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'nowPlaying'}">
                                 <span class="panel-icon"><i class="fas fa-headphones"></i></span>
-                                current playlist
+                                {{ $t('menu.labels.currentPlaylist') }}
                                 </a>
                                 <a v-on:click.prevent="$router.push({ name: 'search' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'search'}">
                                 <span class="panel-icon"><i class="fas fa-search"></i></span>
-                                search
+                                {{ $t('menu.labels.search') }}
                                 </a>
                                 <a v-on:click.prevent="$router.push({ name: 'artists' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'artists'}">
                                 <span class="panel-icon"><i class="fas fa-user"></i></span>
-                                browse artists
+                                {{ $t('menu.labels.browseArtists') }}
                                 </a>
                                 <a v-on:click.prevent="$router.push({ name: 'albums' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'albums'}">
                                 <span class="panel-icon"><i class="fas fa-circle"></i></span>
-                                browse albums
+                                {{ $t('menu.labels.browseAlbums') }}
                                 </a>
                                 <a v-on:click.prevent="$router.push({ name: 'paths' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'paths'}">
                                 <span class="panel-icon"><i class="fas fa-folder-open"></i></span>
-                                browse paths
+                                {{ $t('menu.labels.browsePaths') }}
                                 </a>
                                 <a v-on:click.prevent="$router.push({ name: 'playlists' })" class="navbar-item" v-bind:class="{ 'is-active': $route.name == 'playlists'}">
                                 <span class="panel-icon"><i class="fas fa-list-alt"></i></span>
-                                browse playlists
+                                {{ $t('menu.labels.browsePlaylists') }}
                                 </a>
                                 <a v-on:click.prevent="signout();" class="navbar-item">
                                 <span class="panel-icon"><i class="fas fa-sign-out-alt"></i></span>
-                                signout
+                                {{ $t('menu.labels.signOut') }}
                                 </a>
                             </div>
                         </div>
@@ -50,10 +50,10 @@ let playerNavBar = (function () {
                             <img v-bind:src="coverSrc" style="width: 64px; max-height: 64px;">
                         </div>
                         <div class="navbar-item player-buttons">
-                            <span title="go to previous track" id="btn-previous" v-on:click.prevent="playerData.currentPlaylist.playPrevious();" class="icon"><i class="fas fa-2x fa-step-backward"></i></span>
-                            <span title="pause track" id="btn-pause" v-on:click.prevent="playerData.playback.pause();" v-if="playerData.isPlaying" class="icon"><i class="fas fa-2x fa-pause"></i></span>
-                            <span title="play track" id="btn-play" v-on:click.prevent="playerData.playback.play();" v-else class="icon"><i class="fas fa-2x fa-play"></i></span>
-                            <span title="go to next track" id="btn-next" v-on:click.prevent="playerData.currentPlaylist.playNext();" class="icon"><i class="fas fa-2x fa-step-forward"></i></span>
+                            <span v-bind:title="$t('player.buttons.previousTrack')" id="btn-previous" v-on:click.prevent="playerData.currentPlaylist.playPrevious();" class="icon"><i class="fas fa-2x fa-step-backward"></i></span>
+                            <span v-bind:title="$t('player.buttons.pauseTrackHint')" id="btn-pause" v-on:click.prevent="playerData.playback.pause();" v-if="playerData.isPlaying" class="icon"><i class="fas fa-2x fa-pause"></i></span>
+                            <span v-bind:title="$t('player.buttons.playTrackHint')" id="btn-play" v-on:click.prevent="playerData.playback.play();" v-else class="icon"><i class="fas fa-2x fa-play"></i></span>
+                            <span v-bind:title="$t('player.buttons.nextTrackHint')" id="btn-next" v-on:click.prevent="playerData.currentPlaylist.playNext();" class="icon"><i class="fas fa-2x fa-step-forward"></i></span>
                         </div>
                         <div class="navbar-item">
                             <div>
