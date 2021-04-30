42499a008bb67546754ed652f616d7b51d4ea2ed
aportela <766f6964+github@gmail.com>
2021-04-30 23:30:37 +0200

    minor changes on album cover tiles

--- src-frontend/scripts/vue-components/tiles-album-images.js
+++ src-frontend/scripts/vue-components/tiles-album-images.js
@@ -2,7 +2,7 @@ import { default as spieldoseAPI } from '../api.js';
 
 const template = function () {
     return `
-        <div class="tile is-ancestor">
+        <div class="tile is-ancestor" id="container_tiles">
             <div class="tile is-2 is-vertical" v-for="column in [0,1,2,3,4,5]">
                 <div class="tile" v-for="row in [0,1,2,3,4,5]">
                     <img :src="getImageSource(covers[(5 * column) + row])" style="width:100%" class="blur" @error="event.target.src = '/images/vinyl.png'">
@@ -27,12 +27,14 @@ export default {
     },
     methods: {
         getImageSource: function (img) {
-            if (img.hash) {
-                return ('api/thumbnail?hash=' + img.hash);
-            } else if (img.url) {
-                return ('api/thumbnail?url=' + img.url);
-            } else {
-                return (null);
+            if (img) {
+                if (img.hash) {
+                    return ('api/thumbnail?hash=' + img.hash);
+                } else if (img.url) {
+                    return ('api/thumbnail?url=' + img.url);
+                } else {
+                    return (null);
+                }
             }
         },
         loadRandomAlbumImages: function () {
