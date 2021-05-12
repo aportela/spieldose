a3ca8877288eb6d580412279c503c2a3a72f8320
aportela <766f6964+github@gmail.com>
2021-05-12 20:35:49 +0200

    changed pagination html scheme

--- src-frontend/scripts/vue-components/pagination.js
+++ src-frontend/scripts/vue-components/pagination.js
@@ -1,24 +1,28 @@
 const template = function () {
     return `
         <div>
-            <nav class="pagination is-centered level" v-if="visible">
-                <a class="button is-link pagination-previous" :class="{ 'disabled': loading }" @click.prevent="previous();">
-                    <span class="icon is-small"><i class="fas fa-caret-left" aria-hidden="true"></i></span>
-                    <span>{{ $t("pagination.buttons.previousPage") }}</span>
-                    </a>
-                <a class="button is-link pagination-next" :class="{ 'disabled': loading }" @click.prevent="next();">
-                    <span>{{ $t("pagination.buttons.nextPage") }}</span>
-                    <span class="icon is-small"><i class="fas fa-caret-right" aria-hidden="true"></i></span>
-                </a>
-                <ul class="pagination-list">
-                    <!-- vuejs pagination inspired by Jeff (https://stackoverflow.com/a/35706926) -->
-                    <li v-for="pageNumber in data.totalPages">
-                        <a class="pagination-link" v-if="showIntermediatePage(pageNumber)" :class="{'is-current': isCurrentPage(pageNumber), 'disabled': loading }" @click.prevent="navigateTo(pageNumber);">{{ pageNumber }}</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="notification is-warning" v-if="invalidPage">{{ $t("pagination.labels.invalidPageOrNoResults") }}</div>
-            <div class="is-clearfix" v-else></div>
+            <div class="columns">
+                <div class="column has-text-left">
+                    <button type="button" class="button is-dark pagination-previous" :disabled="loading" @click.prevent="previous();">
+                        <span class="icon is-small"><i class="fas fa-caret-left" aria-hidden="true"></i></span>
+                        <span>{{ $t("pagination.buttons.previousPage") }}</span>
+                    </button>
+                </div>
+                <div class="column has-text-centered">
+                    <div class="field is-grouped">
+                        <p class="control" v-for="pageNumber in data.totalPages" v-show="showIntermediatePage(pageNumber)">
+                            <button class="button is-outline-dark"  :class="{'is-dark': isCurrentPage(pageNumber) }" :disabled="loading" @click.prevent="navigateTo(pageNumber);">{{ pageNumber }}</button>
+                        </p>
+                    </div>
+                </div>
+                <div class="column has-text-right">
+                    <button type="button" class="button is-dark pagination-next" :disabled="loading" @click.prevent="next();">
+                        <span>{{ $t("pagination.buttons.nextPage") }}</span>
+                        <span class="icon is-small"><i class="fas fa-caret-right" aria-hidden="true"></i></span>
+                    </button>
+                </div>
+            </div>
+            <div class="is-clearfix"></div>
         </div>
     `;
 };
