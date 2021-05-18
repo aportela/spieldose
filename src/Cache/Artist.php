7f444cb78be56f227b57682757a91de2ff9e79ba
aportela <766f6964+github@gmail.com>
2021-05-18 22:12:43 +0200

    added last.fm api key

--- src/Cache/Artist.php
+++ src/Cache/Artist.php
@@ -47,7 +47,7 @@
         }
 
         public static function getMusicBrainzDetails(string $mbId) {
-            $json = null;
+            $json = "";
             try {
                 $url = sprintf(self::MUSICBRAINZ_API_GET_ARTIST_DETAILS_FROM_ID, $mbId);
                 $json = \Spieldose\Net::httpRequest($url);
@@ -58,9 +58,9 @@
         }
 
         public static function getLastFMDetailsFromMBId(string $mbId) {
-            $json = null;
+            $json = "";
             try {
-                $url = sprintf(self::LASTFM_API_GET_ARTIST_DETAILS_FROM_MBID, $mbId);
+                $url = sprintf(self::LASTFM_API_GET_ARTIST_DETAILS_FROM_MBID, \Spieldose\LastFM::API_KEY, $mbId);
                 $json = \Spieldose\Net::httpRequest($url);
             } catch (\Throwable $e) {
                 // TODO: log error
@@ -72,21 +72,47 @@
             $jsonMusicBrainz = self::getMusicBrainzDetails($mbId);
             $jsonLastFM = self::getLastFMDetailsFromMBId($mbId);
             $name = null;
+            $image = "";
             if (! empty($jsonMusicBrainz)) {
                 $result = json_decode($jsonMusicBrainz, false);
                 if (isset($result->name) && ! empty($result->name)) {
                     $name = $result->name;
                 }
+                if (isset($result->relations) && count($result->relations) > 0) {
+                    foreach($result->relations as $relation) {
+                        if ($relation->type == "image") {
+                            if (isset($relation->url) && isset($relation->url->resource) && ! empty($relation->url->resource)) {
+                                $url = $relation->url->resource;
+                                if (strpos($url, "https://commons.wikimedia.org/wiki/File:") === 0) {
+                                    // TODO: refresh from wikimedia
+                                    $imageName = str_replace("https://commons.wikimedia.org/wiki/File:", "", $url);
+                                    if (! empty($imageName)) {
+                                        $imageHash = md5($imageName);
+                                    }
+                                    $image = sprintf("https://upload.wikimedia.org/wikipedia/commons/%s/%s/%s", substr($imageHash, 0, 1), substr($imageHash, 0, 2), $imageName);
+                                } else {
+                                    $image = $url;
+                                }
+                            }
+
+                        }
+                    }
+                }
+            } else {
+                throw new \Spieldose\Exception\NotFoundException($mbId);
             }
-            if (empty($name)) {
+            if (empty($name) && ! empty($jsonLastFM)) {
                 $result = json_decode($jsonLastFM, false);
                 if (isset ($result->artist) && isset($result->artist->name) && ! empty($result->artist->name)) {
                     $name = $result->artist->name;
                 }
             }
+            echo $name . PHP_EOL;
+            print_R($jsonLastFM);
+            exit;
+
             if (! empty($name)) {
-                $image = "";
-                self::saveCache($dbh, $mbId, $name, $image, $jsonLastFM, $jsonMusicBrainz);
+                self::saveCache($dbh, $mbId, $name, $image, $jsonLastFM, $jsonMusicBrainz );
             }
         }
 
