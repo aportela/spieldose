12f7fda2e88bb8fc31067071febbeed26eefc355
alex <766f6964+github@gmail.com>
2022-04-03 19:07:46 +0200

    new app bootstrap

---
+++ config/container.php
@@ -0,0 +1,61 @@
+<?php
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
+
+return [
+    'settings' => function () {
+        return require __DIR__ . '/settings.php';
+    },
+
+    App::class => function (ContainerInterface $container) {
+        AppFactory::setContainer($container);
+
+        return AppFactory::create();
+    },
+
+    ResponseFactoryInterface::class => function (ContainerInterface $container) {
+        return $container->get(Psr17Factory::class);
+    },
+
+    ErrorMiddleware::class => function (ContainerInterface $container) {
+        $app = $container->get(App::class);
+        $settings = $container->get('settings')['error'];
+
+        return new ErrorMiddleware(
+            $app->getCallableResolver(),
+            $app->getResponseFactory(),
+            (bool)$settings['display_error_details'],
+            (bool)$settings['log_errors'],
+            (bool)$settings['log_error_details']
+        );
+    },
+
+    PDO::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['db'];
+
+        $host = $settings['host'];
+        $dbname = $settings['database'];
+        $username = $settings['username'];
+        $password = $settings['password'];
+        $charset = $settings['charset'];
+        $flags = $settings['flags'];
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        $dsn = sprintf("sqlite:%s", dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "spieldose.sqlite3");
+
+        return new PDO($dsn, $username, $password, $flags);
+    },
+
+
+    Twig::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['twig'];
+        $twig = \Slim\Views\Twig::create($settings['path'], $settings['options']);
+
+        return $twig;
+    }
+
+];
---
+++ config/middleware.php
@@ -0,0 +1,15 @@
+<?php
+
+use Slim\App;
+use Slim\Middleware\ErrorMiddleware;
+
+return function (App $app) {
+    // Parse json, form data and xml
+    $app->addBodyParsingMiddleware();
+
+    // Add the Slim built-in routing middleware
+    $app->addRoutingMiddleware();
+
+    // Catch exceptions and errors
+    $app->add(ErrorMiddleware::class);
+};
