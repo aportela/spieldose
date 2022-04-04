d2d665f30b8de73d37388d66b0c70fc39cc657c4
aportela <766f6964+github@gmail.com>
2022-04-04 23:32:08 +0200

    changed container logger

--- tools/clean-database.php
+++ tools/clean-database.php
@@ -2,21 +2,23 @@
 
     declare(strict_types=1);
 
-    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+    ob_start();
 
-    echo "Spieldose database cleaner" . PHP_EOL;
+    $app = (require __DIR__ . '/../config/bootstrap.php');
 
-    $app = (new \Spieldose\App())->get();
+    $container = $app->getContainer();
 
-    $missingExtensions = array_diff($app->getContainer()["settings"]["phpRequiredExtensions"], get_loaded_extensions());
+    $settings = $container->get('settings');
+
+    $missingExtensions = array_diff($settings["phpRequiredExtensions"], get_loaded_extensions());
     if (count($missingExtensions) > 0) {
         echo "Error: missing php extension/s: " . implode(", ", $missingExtensions) . PHP_EOL;
         exit;
     }
 
-    $c = $app->getContainer();
-    $c["scrapLogger"]->info("Database cleaner started");
-    $dbh = new \Spieldose\Database\DB($c);
+    $logger = $container->get(\Monolog\Logger::class);
+    $logger->info("Database cleaner started");
+    $dbh = new \Spieldose\Database\DB($container->get(PDO::class));
     $scrapper = new \Spieldose\Scrapper($dbh);
     $results = $scrapper->getAllDatabaseFiles();
     $totalResults = count($results);
@@ -28,10 +30,13 @@
         try {
             for ($i = 0; $i < $totalResults; $i++) {
                 $path = $results[$i]->path . DIRECTORY_SEPARATOR . $results[$i]->filename;
+                $logger->debug(sprintf("Checking path %d/%d", $i + 1, $totalResults), array($path));
                 if (! file_exists($path)) {
-                    $c["scrapLogger"]->debug("Database file not found on filesystem", array($path));
+                    $logger->notice("Path not found");
                     $scrapper->removeDatabaseReferences($results[$i]->id);
                     $totalDeleted++;
+                } else {
+                    $logger->debug("Found path");
                 }
                 \Spieldose\Utils::showProgressBar($i + 1, $totalResults, 20);
             }
@@ -42,7 +47,7 @@
             }
         } catch (\PDOException $e) {
             $errors = true;
-            $c["scrapLogger"]->debug("Error cleaning database", array($e->getMessage()));
+            $logger->error("Error cleaning database", array($e->getMessage()));
         }
         if (! $errors) {
             $dbh->commit();
@@ -52,50 +57,3 @@
     } else {
         echo "No data (database empty)" . PHP_EOL;
     }
-
-
-    /*
-    $cmdLine = new \Spieldose\CmdLine("", array("path:"));
-    if ($cmdLine->hasParam("path")) {
-        $musicPath = $cmdLine->getParamValue("path");
-        if (file_exists($musicPath)) {
-            $c = $app->getContainer();
-            $c["scanLogger"]->info("Scanner started");
-            $dbh = new \Spieldose\Database\DB($c);
-            if ((new \Spieldose\Database\Version($dbh, $c->get("settings")['database']['type']))->hasUpgradeAvailable()) {
-                $c["scanLogger"]->warning("Process stopped: upgrade database before continue");
-                echo "New database version available, an upgrade is required before continue." . PHP_EOL;
-                exit;
-            }
-            $files = \Spieldose\FileSystem::getRecursiveDirectoryFiles($musicPath);
-            $totalFiles = count($files);
-            echo sprintf("Reading %d files from path: %s%s", $totalFiles, $musicPath, PHP_EOL);
-            $scrapper = new \Spieldose\Scrapper($dbh);
-            $failed = array();
-            for ($i = 0; $i < $totalFiles; $i++) {
-                try {
-                    $c["scanLogger"]->debug("Processing " . $files[$i]);
-                    $scrapper->scrapFileTags($files[$i]);
-                } catch (\Throwable $e) {
-                    $c["scanLogger"]->error("Error: " . $e->getMessage(), array('file' => __FILE__, 'line' => __LINE__));
-                    $failed[] = $files[$i];
-                }
-                \Spieldose\Utils::showProgressBar($i + 1, $totalFiles, 20);
-            }
-            $totalFailed = count($failed);
-            if ($totalFailed > 0) {
-                echo sprintf("Failed to scrap %d files:%s", $totalFailed, PHP_EOL);
-                foreach($failed as $f) {
-                    echo " " . $f . PHP_EOL;
-                }
-            }
-            $c["scanLogger"]->info("Scanner finished");
-        } else {
-            echo "Invalid music path" . PHP_EOL;
-        }
-    } else {
-        echo "No required params found: --path <music_path>" . PHP_EOL;
-    }
-    */
-
-?>
