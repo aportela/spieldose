caff8fed9eba60083a8ef6509c1b06473fa5fb34
aportela <766f6964+github@gmail.com>
2022-10-08 01:10:55 +0200

    changed indentation

--- config/settings.php
+++ config/settings.php
@@ -1,86 +1,94 @@
 <?php
 
-    // Should be set to 0 in production
-    error_reporting(E_ALL);
+// Should be set to 0 in production
+error_reporting(E_ALL);
 
-    // Should be set to '0' in production
-    ini_set('display_errors', '1');
+// Should be set to '0' in production
+ini_set('display_errors', '1');
 
-    // Timezone
-    date_default_timezone_set('Europe/Madrid');
+// Timezone
+date_default_timezone_set('Europe/Madrid');
 
-    // Settings
-    $settings = [];
+// Settings
+$settings = [];
 
-    // Path settings
-    $settings['root'] = dirname(__DIR__);
+// Path settings
+$settings['root'] = dirname(__DIR__);
 
-    // Error Handling Middleware settings
-    $settings['error'] = [
+// Error Handling Middleware settings
+$settings['error'] = [
 
-        // Should be set to false in production
-        'display_error_details' => true,
+    // Should be set to false in production
+    'display_error_details' => true,
 
-        // Parameter is passed to the default ErrorHandler
-        // View in rendered output by enabling the "displayErrorDetails" setting.
-        // For the console and unit tests we also disable it
-        'log_errors' => true,
+    // Parameter is passed to the default ErrorHandler
+    // View in rendered output by enabling the "displayErrorDetails" setting.
+    // For the console and unit tests we also disable it
+    'log_errors' => true,
 
-        // Display error details in error log
-        'log_error_details' => true,
-    ];
+    // Display error details in error log
+    'log_error_details' => true,
+];
 
-    $settings['logger'] = [
-        'default'  => [
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/default.log',
-            'name' => 'Spieldose::Default'
-        ],
-        'database' => [
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/database.log',
-            'name' => 'Spieldose::Database'
-        ]
-    ];
+$settings['logger'] = [
+    'default'  => [
+        'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/default.log',
+        'name' => 'Spieldose::Default'
+    ],
+    'installer' => [
+        'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/installer.log',
+        'name' => 'Spieldose::Installer'
+    ],
+    'scanner' => [
+        'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/scanner.log',
+        'name' => 'Spieldose::Scanner'
+    ],
+    'database' => [
+        'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/database.log',
+        'name' => 'Spieldose::Database'
+    ]
+];
 
-    // Database settings
-    $settings['db'] = [
-        'driver' => 'sqlite',
-        'host' => '',
-        'username' => '',
-        'database' => 'spieldose2.sqlite3',
-        'password' => '',
-        'charset' => 'utf8mb4',
-        'collation' => 'utf8mb4_unicode_ci',
-        'flags' => [
-            // Turn off persistent connections
-            PDO::ATTR_PERSISTENT => false,
-            // Enable exceptions
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            // Emulate prepared statements
-            PDO::ATTR_EMULATE_PREPARES => true,
-            // Set default fetch mode to array
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            // Set character set
-            //PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci' // BUG: https://bugs.php.net/bug.php?id=81576
-        ],
-        "upgradeSchemaPath" => __DIR__ . '/../src/Database/schema.sql'
-    ];
+// Database settings
+$settings['db'] = [
+    'driver' => 'sqlite',
+    'host' => '',
+    'username' => '',
+    'database' => 'spieldose2.sqlite3',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'flags' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set character set
+        //PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci' // BUG: https://bugs.php.net/bug.php?id=81576
+    ],
+    "upgradeSchemaPath" => __DIR__ . '/../src/Database/schema.sql'
+];
 
-    $settings['twig'] = [
-        'path' =>  dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates',
-        'options' => ['cache' => false]
-    ];
+$settings['twig'] = [
+    'path' =>  dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates',
+    'options' => ['cache' => false]
+];
 
-    $settings['common'] = [
-        'defaultResultsPage' => 64,
-        'allowSignUp' => true,
-        'liveSearch' => true,
-        'locale' => 'en'
-    ];
+$settings['common'] = [
+    'defaultResultsPage' => 64,
+    'allowSignUp' => true,
+    'liveSearch' => true,
+    'locale' => 'en'
+];
 
-    $settings['albumCoverPathValidFilenames'] = '{cover,Cover,COVER,front,Front,FRONT}.{jpg,Jpg,JPG,jpeg,Jpeg,JPEG,png,Png,PNG}';
+//$settings['albumCoverPathValidFilenames'] = '{cover,Cover,COVER,front,Front,FRONT}.{jpg,Jpg,JPG,jpeg,Jpeg,JPEG,png,Png,PNG}';
 
-    $settings['phpRequiredExtensions'] = array('pdo_sqlite', 'mbstring', 'curl');
+$settings['phpRequiredExtensions'] = array('pdo_sqlite', 'mbstring', 'curl');
 
-    $settings['webpack'] = require_once('webpack.php');
+$settings['webpack'] = require_once('webpack.php');
 
-    return $settings;
+return $settings;
