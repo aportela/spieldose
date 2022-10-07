2cc5bbfbb89d82b891f9ba788fc32308a89e98e2
aportela <766f6964+github@gmail.com>
2022-10-08 01:10:11 +0200

    changed indentation

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,25 +1,25 @@
 <?php
 
-    use DI\ContainerBuilder;
-    use Slim\App;
+use DI\ContainerBuilder;
+use Slim\App;
 
-    require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    $containerBuilder = new ContainerBuilder();
+$containerBuilder = new ContainerBuilder();
 
-    // Set up settings
-    $containerBuilder->addDefinitions(__DIR__ . '/container.php');
+// Set up settings
+$containerBuilder->addDefinitions(__DIR__ . '/container.php');
 
-    // Build PHP-DI Container instance
-    $container = $containerBuilder->build();
+// Build PHP-DI Container instance
+$container = $containerBuilder->build();
 
-    // Create App instance
-    $app = $container->get(App::class);
+// Create App instance
+$app = $container->get(App::class);
 
-    // Register routes
-    (require __DIR__ . '/routes.php')($app);
+// Register routes
+(require __DIR__ . '/routes.php')($app);
 
-    // Register middleware
-    (require __DIR__ . '/middleware.php')($app);
+// Register middleware
+(require __DIR__ . '/middleware.php')($app);
 
-    return $app;
+return $app;
