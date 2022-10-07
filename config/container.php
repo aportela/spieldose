8f396b92f6516ca31c950134a216e73e8c87d77f
aportela <766f6964+github@gmail.com>
2022-10-08 01:10:31 +0200

    new classes

--- config/container.php
+++ config/container.php
@@ -1,107 +1,130 @@
 <?php
 
-    use Nyholm\Psr7\Factory\Psr17Factory;
-    use Psr\Container\ContainerInterface;
-    use Psr\Http\Message\ResponseFactoryInterface;
-    use Slim\App;
-    use Slim\Factory\AppFactory;
-    use Slim\Middleware\ErrorMiddleware;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
 
-    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
-    return [
-        'settings' => function () {
-            return require __DIR__ . '/settings.php';
-        },
+return [
+    'settings' => function () {
+        return require __DIR__ . '/settings.php';
+    },
 
-        App::class => function (ContainerInterface $container) {
-            AppFactory::setContainer($container);
+    App::class => function (ContainerInterface $container) {
+        AppFactory::setContainer($container);
 
-            return AppFactory::create();
-        },
+        return AppFactory::create();
+    },
 
-        ResponseFactoryInterface::class => function (ContainerInterface $container) {
-            return $container->get(Psr17Factory::class);
-        },
+    ResponseFactoryInterface::class => function (ContainerInterface $container) {
+        return $container->get(Psr17Factory::class);
+    },
 
-        ErrorMiddleware::class => function (ContainerInterface $container) {
-            $app = $container->get(App::class);
-            $settings = $container->get('settings')['error'];
+    ErrorMiddleware::class => function (ContainerInterface $container) {
+        $app = $container->get(App::class);
+        $settings = $container->get('settings')['error'];
 
-            return new ErrorMiddleware(
-                $app->getCallableResolver(),
-                $app->getResponseFactory(),
-                (bool)$settings['display_error_details'],
-                (bool)$settings['log_errors'],
-                (bool)$settings['log_error_details']
-            );
-        },
+        return new ErrorMiddleware(
+            $app->getCallableResolver(),
+            $app->getResponseFactory(),
+            (bool)$settings['display_error_details'],
+            (bool)$settings['log_errors'],
+            (bool)$settings['log_error_details']
+        );
+    },
 
-        Twig::class => function (ContainerInterface $container) {
-            $settings = $container->get('settings')['twig'];
-            $twig = \Slim\Views\Twig::create($settings['path'], $settings['options']);
+    Twig::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['twig'];
+        $twig = \Slim\Views\Twig::create($settings['path'], $settings['options']);
 
-            return $twig;
-        },
+        return $twig;
+    },
 
-        PDO::class => function (ContainerInterface $container) {
-            $settings = $container->get('settings')['db'];
+    PDO::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['db'];
 
-            $host = $settings['host'];
-            $dbname = $settings['database'];
-            $username = $settings['username'];
-            $password = $settings['password'];
-            $charset = $settings['charset'];
-            $flags = $settings['flags'];
-            $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-            $dsn = sprintf("sqlite:%s", dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "spieldose.sqlite3");
+        $host = $settings['host'];
+        $dbname = $settings['database'];
+        $username = $settings['username'];
+        $password = $settings['password'];
+        $charset = $settings['charset'];
+        $flags = $settings['flags'];
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        $dsn = sprintf("sqlite:%s", dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "spieldose.sqlite3");
 
-            return new PDO($dsn, $username, $password, $flags);
-        },
+        return new PDO($dsn, $username, $password, $flags);
+    },
 
-        DB::class => function (ContainerInterface $container) {
-            $settings = $container->get('settings')['db'];
-            $adapter = new \aportela\DatabaseWrapper\Adapter\PDOSQLiteAdapter(
-                dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . $settings["database"],
-                // READ upgrade SQL schema file definition on next block of this README.md
-                $settings["upgradeSchemaPath"]
-            );
-            $logger = $container->get(DBLogger::class);
-            // main object
-            $db = new \aportela\DatabaseWrapper\DB
-            (
-                $adapter,
-                $logger
-            );
-            return($db);
-        },
+    DB::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['db'];
+        $adapter = new \aportela\DatabaseWrapper\Adapter\PDOSQLiteAdapter(
+            dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . $settings["database"],
+            // READ upgrade SQL schema file definition on next block of this README.md
+            $settings["upgradeSchemaPath"]
+        );
+        $logger = $container->get(DBLogger::class);
+        // main object
+        $db = new \aportela\DatabaseWrapper\DB(
+            $adapter,
+            $logger
+        );
+        return ($db);
+    },
 
-        \Monolog\Logger::class => function(ContainerInterface $container) {
-            $settings = $container->get('settings')['logger'];
-            $logger = new \Monolog\Logger('spieldose-default');
-            $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-            $handler = new \Monolog\Handler\RotatingFileHandler($settings['default']['path'], 0, \Monolog\Level::Debug);
-            $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
-            $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
-            $handler->setFormatter($formatter);
-            $logger->pushHandler($handler);
-            return ($logger);
-        },
+    \Monolog\Logger::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['logger'];
+        $logger = new \Monolog\Logger('spieldose-default');
+        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+        $handler = new \Monolog\Handler\RotatingFileHandler($settings['default']['path'], 0, \Monolog\Level::Debug);
+        $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
+        $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        return ($logger);
+    },
 
-        DBLogger::class => function(ContainerInterface $container) {
-            $settings = $container->get('settings')['logger'];
-            $logger = new \Monolog\Logger('spieldose-db');
-            $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-            $handler = new \Monolog\Handler\RotatingFileHandler($settings['database']['path'], 0, \Monolog\Level::Debug);
-            $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
-            $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
-            $handler->setFormatter($formatter);
-            $logger->pushHandler($handler);
-            return ($logger);
-        },
+    DBLogger::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['logger'];
+        $logger = new \Monolog\Logger($settings['database']['name']);
+        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+        $handler = new \Monolog\Handler\RotatingFileHandler($settings['database']['path'], 0, \Monolog\Level::Debug);
+        $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
+        $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        return ($logger);
+    },
 
-        \Spieldose\Middleware\APIExceptionCatcher::class => function (ContainerInterface $container) {
-            return(new \Spieldose\Middleware\APIExceptionCatcher($container->get(\Monolog\Logger::class)));
-        }
+    InstallerLogger::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['logger'];
+        $logger = new \Monolog\Logger($settings['installer']['name']);
+        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+        $handler = new \Monolog\Handler\RotatingFileHandler($settings['installer']['path'], 0, \Monolog\Level::Debug);
+        $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
+        $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        return ($logger);
+    },
 
-    ];
+    ScannerLogger::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['logger'];
+        $logger = new \Monolog\Logger($settings['scanner']['name']);
+        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+        $handler = new \Monolog\Handler\RotatingFileHandler($settings['scanner']['path'], 0, \Monolog\Level::Debug);
+        $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
+        $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        return ($logger);
+    },
+
+    \Spieldose\Middleware\APIExceptionCatcher::class => function (ContainerInterface $container) {
+        return (new \Spieldose\Middleware\APIExceptionCatcher($container->get(\Monolog\Logger::class)));
+    }
+
+];
