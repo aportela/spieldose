a296f7c1797c5fe98bc71cc89fdf387935c9f778
aportela <766f6964+github@gmail.com>
2022-10-08 01:12:21 +0200

    changed indentation

--- src/Utils.php
+++ src/Utils.php
@@ -1,70 +1,62 @@
 <?php
-    declare(strict_types=1);
 
-    namespace Spieldose;
+declare(strict_types=1);
 
-    class Utils {
+namespace Spieldose;
 
-        public static function setAppDefaults() {
-            session_start();
-            if (DEBUG) {
-                error_reporting(E_ALL);
-                ini_set("display_errors", "1");
-            }
+class Utils
+{
+
+    /**
+     * show console progress bar (// http://snipplr.com/view/29548/)
+     *
+     * @param $done int Done steps
+     * @param $total int Total number of steps
+     * @param $size int width (chars)
+     */
+    public static function showProgressBar(int $done = 0, int $total = 0, int $size = 30)
+    {
+
+        static $start_time;
+
+        // if we go over our bound, just ignore it
+        if ($done > $total) return;
+
+        if (empty($start_time)) $start_time = time();
+        $now = time();
+
+        $perc = (float)($done / $total);
+
+        $bar = floor($perc * $size);
+
+        $status_bar = "\r[";
+        $status_bar .= str_repeat("=", intval($bar));
+        if ($bar < $size) {
+            $status_bar .= ">";
+            $status_bar .= str_repeat(" ", intval($size - $bar));
+        } else {
+            $status_bar .= "=";
         }
 
+        $disp = number_format($perc * 100, 0);
 
-        /**
-         * show console progress bar (// http://snipplr.com/view/29548/)
-         *
-         * @params $done
-         * @params $total
-         * @params $size
-         */
-        public static function showProgressBar($done, $total, $size=30) {
+        $status_bar .= "] $disp%  $done/$total";
 
-            static $start_time;
+        $rate = ($now - $start_time) / $done;
+        $left = $total - $done;
+        $eta = round($rate * $left, 2);
 
-            // if we go over our bound, just ignore it
-            if($done > $total) return;
+        $elapsed = $now - $start_time;
 
-            if(empty($start_time)) $start_time=time();
-            $now = time();
+        $status_bar .= " remaining: " . number_format($eta) . " sec.  elapsed: " . number_format($elapsed) . " sec.";
 
-            $perc=(double)($done/$total);
+        echo "$status_bar  ";
 
-            $bar=floor($perc*$size);
+        flush();
 
-            $status_bar="\r[";
-            $status_bar .= str_repeat("=", intval($bar));
-            if($bar<$size){
-                $status_bar.=">";
-                $status_bar.=str_repeat(" ", intval($size-$bar));
-            } else {
-                $status_bar.="=";
-            }
-
-            $disp=number_format($perc*100, 0);
-
-            $status_bar.="] $disp%  $done/$total";
-
-            $rate = ($now-$start_time)/$done;
-            $left = $total - $done;
-            $eta = round($rate * $left, 2);
-
-            $elapsed = $now - $start_time;
-
-            $status_bar.= " remaining: ".number_format($eta)." sec.  elapsed: ".number_format($elapsed)." sec.";
-
-            echo "$status_bar  ";
-
-            flush();
-
-            // when done, send a newline
-            if($done == $total) {
-                echo "\n";
-            }
-
+        // when done, send a newline
+        if ($done == $total) {
+            echo PHP_EOL;
         }
     }
-?>
+}
