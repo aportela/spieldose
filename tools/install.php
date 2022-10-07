9d352859c8a3fc56a11a7c48811bb2aba34f1a6d
aportela <766f6964+github@gmail.com>
2022-10-08 01:13:49 +0200

    logger changed

--- tools/install.php
+++ tools/install.php
@@ -1,61 +1,61 @@
 <?php
 
 use DI\ContainerBuilder;
-use Slim\App;
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
-
 
 $containerBuilder = new ContainerBuilder();
 
 // Set up settings
-
 $containerBuilder->addDefinitions(__DIR__ . '../../config/container.php');
 
 // Build PHP-DI Container instance
 $container = $containerBuilder->build();
 
-// Create App instance
-$app = $container->get(App::class);
-
 echo "Spieldose installer" . PHP_EOL;
 
-$logger = $container->get(\Monolog\Logger::class);
-$logger->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Level::Debug));
+$logger = $container->get(InstallerLogger::class);
 
-$logger->debug("Installation started");
-
-//$app = (new \Spieldose\App())->get();
+$logger->info("Install started");
 
 $settings = $container->get('settings');
 
 $missingExtensions = array_diff($settings["phpRequiredExtensions"], get_loaded_extensions());
 if (count($missingExtensions) > 0) {
-    $logger->critical("Error: missing php extension/s: ", [implode(", ", $missingExtensions)]);
-    echo "Error: missing php extension/s: " . implode(", ", $missingExtensions) . PHP_EOL;
-    exit;
+    $missingExtensionsStr = implode(", ", $missingExtensions);
+    echo "Error: missing php extension/s: " . $missingExtensionsStr . PHP_EOL;
+    $logger->critical("Error: missing php extension/s: ", [$missingExtensionsStr]);
 } else {
     $db = $container->get(DB::class);
-
     $success = true;
     // check if the database is already installed (install scheme with version table already exists)
     if (!$db->isSchemaInstalled()) {
+        $logger->info("Schema not found, creating database");
         if ($db->installSchema()) {
             echo "Database install success" . PHP_EOL;
+            $logger->info("Database install success");
         } else {
-            echo sprintf("Database install error, check logs (at %s)%s", $settings["logger"]["path"], PHP_EOL);
+            echo "Install error, verify logs";
+            $logger->critical("Install error, verify logs");
             $success = false;
         }
     } else {
         echo "Database already installed" . PHP_EOL;
+        $logger->info("Database already installed");
     }
 
     if ($success) {
-        $results = $db->query(" SELECT release_number, release_date FROM VERSION; ");
+        $results = [];
+        try {
+            $results = $db->query(" SELECT release_number, release_date FROM VERSION; ");
+        } catch (\Exception $e) {
+            $logger->critical("SQL Error retrieving current schema version: " . $e->getMessage());
+        }
         if (is_array($results) && count($results) == 1) {
             echo sprintf("Current version: %s (installed on: %s)%s", $results[0]->release_number, $results[0]->release_date, PHP_EOL);
+            $logger->debug(sprintf("Current version: %s (installed on: %s)", $results[0]->release_number, $results[0]->release_date));
         } else {
-            echo "SQL error" . PHP_EOL;
+            echo "SQL Error retrieving current schema version" . PHP_EOL;
         }
     }
 }
--- tools/upgrade.php
+++ tools/upgrade.php
@@ -1,48 +1,39 @@
 <?php
 
 use DI\ContainerBuilder;
-use Slim\App;
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
-
 
 $containerBuilder = new ContainerBuilder();
 
 // Set up settings
-
 $containerBuilder->addDefinitions(__DIR__ . '../../config/container.php');
 
 // Build PHP-DI Container instance
 $container = $containerBuilder->build();
 
-// Create App instance
-$app = $container->get(App::class);
-
 echo "Spieldose upgrade" . PHP_EOL;
 
-$logger = $container->get(\Monolog\Logger::class);
-$logger->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Level::Debug));
+$logger = $container->get(InstallerLogger::class);
 
-$logger->debug("Upgrade started");
-
-//$app = (new \Spieldose\App())->get();
+$logger->info("Upgrade started");
 
 $settings = $container->get('settings');
 
 $missingExtensions = array_diff($settings["phpRequiredExtensions"], get_loaded_extensions());
 if (count($missingExtensions) > 0) {
-    $logger->critical("Error: missing php extension/s: ", [implode(", ", $missingExtensions)]);
-    echo "Error: missing php extension/s: " . implode(", ", $missingExtensions) . PHP_EOL;
-    exit;
+    $missingExtensionsStr = implode(", ", $missingExtensions);
+    echo "Error: missing php extension/s: " . $missingExtensionsStr . PHP_EOL;
+    $logger->critical("Error: missing php extension/s: ", [$missingExtensionsStr]);
 } else {
     $db = $container->get(DB::class);
-
-    $success = true;
     // try to upgrade SQL schema to last version
     $currentVersion = $db->upgradeSchema();
     if ($currentVersion !== -1) {
-        echo "Upgrade ok", PHP_EOL;
+        echo "Database upgraded with success" . PHP_EOL;
+        $logger->info("Database upgraded with success");
     } else {
-        echo sprintf("Database upgrade error, check logs (at %s)%s", $settings["logger"]["path"], PHP_EOL);
+        echo "Upgrade error, verify logs";
+        $logger->critical("Upgrade error, verify logs");
     }
 }
