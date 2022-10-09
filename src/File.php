bef24227657937df31334363afc6e947c3a49e7e
aportela <766f6964+github@gmail.com>
2022-10-09 09:59:18 +0200

    new File class

---
+++ src/File.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spieldose;
+
+class File
+{
+
+    private $dbh;
+    public $id;
+    public $path;
+    public $mime;
+    public $length;
+
+    public function __construct($container, string $id = "")
+    {
+        if (!$container) {
+            throw new \Spieldose\Exception\InvalidParamsException("container");
+        } else {
+            $this->dbh = $container->get(DB::class);
+            $this->id = $id;
+        }
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public function get()
+    {
+        $results = $this->dbh->query(
+            "
+                SELECT
+                    (DIRECTORIES.PATH || :directory_separator || FILES.NAME) AS path,
+                    FILES.MIME AS mime
+                FROM FILES
+                INNER JOIN DIRECTORIES ON DIRECTORIES.ID = FILES.DIRECTORY_ID
+                WHERE FILES.ID = :id
+             ",
+            array(
+                new \aportela\DatabaseWrapper\Param\StringParam(":directory_separator", DIRECTORY_SEPARATOR),
+                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id)
+            )
+        );
+        if (count($results) == 1) {
+            $this->path = $results[0]->path;
+            $this->mime = $results[0]->mime;
+            $this->length = filesize($this->path);
+        } else {
+            throw new \Spieldose\Exception\NotFoundException($this->id);
+        }
+    }
+
+    public function getData(int $offset, int $length)
+    {
+        if (!empty($this->path)) {
+            return (null);
+            $file = fopen($this->path, 'r');
+            fseek($file, $offset);
+            $data = fread($file, $length);
+            fclose($file);
+            return ($data);
+        } else {
+            throw new \Spieldose\Exception\InvalidParamsException("path");
+        }
+    }
+}
