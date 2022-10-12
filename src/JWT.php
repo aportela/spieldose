6432285c99929e78a171e0427f7b0b37f2345cc3
alex <766f6964+github@gmail.com>
2022-10-12 14:09:16 +0200

    more jwt class & middleware fixes

--- src/JWT.php
+++ src/JWT.php
@@ -37,11 +37,12 @@ class JWT
                 ),
                 $this->passphrase,
                 self::ALGORITHM
-            );
+            );           
         } catch (\Throwable $e) {
             $this->logger->error("JWT encoding error", [$e->getMessage()]);
-        }
-        return ($jwt);
+        } finally {
+            return ($jwt);
+        }        
     }
 
     public function decode(string $jwt)
@@ -50,9 +51,35 @@ class JWT
         try {
             $this->logger->notice("JWT decoding", [$jwt]);
             $data = \Firebase\JWT\JWT::decode($jwt, new \Firebase\JWT\Key($this->passphrase, self::ALGORITHM));
+        } catch (\InvalidArgumentException $e) {
+            // provided key/key-array is empty or malformed.
+            $this->logger->error("JWT decoding error", [$e->getMessage()]);
+        } catch (\DomainException $e) {
+            // provided algorithm is unsupported OR
+            // provided key is invalid OR
+            // unknown error thrown in openSSL or libsodium OR
+            // libsodium is required but not available.
+            $this->logger->error("JWT decoding error", [$e->getMessage()]);
+        } catch (\Firebase\JWT\SignatureInvalidException $e) {
+            // provided JWT signature verification failed.
+            $this->logger->error("JWT decoding error", [$e->getMessage()]);
+        } catch (\Firebase\JWT\BeforeValidException $e) {
+            // provided JWT is trying to be used before "nbf" claim OR
+            // provided JWT is trying to be used before "iat" claim.
+            $this->logger->error("JWT decoding error", [$e->getMessage()]);
+        } catch (\Firebase\JWT\ExpiredException $e) {
+            // provided JWT is trying to be used after "exp" claim.
+            $this->logger->error("JWT decoding error", [$e->getMessage()]);
+        } catch (\UnexpectedValueException $e) {
+            // provided JWT is malformed OR
+            // provided JWT is missing an algorithm / using an unsupported algorithm OR
+            // provided JWT algorithm does not match provided key OR
+            // provided key ID in key/key-array is empty or invalid.
+            $this->logger->error("JWT decoding error", [$e->getMessage()]);
         } catch (\Throwable $e) {
             $this->logger->error("JWT decoding error", [$e->getMessage()]);
+        } finally {
+            return ($data);
         }
-        return ($data);
     }
 }
--- src/Middleware/JWT.php
+++ src/Middleware/JWT.php
@@ -17,7 +17,7 @@ class JWT
     }
 
     /**
-     * middleware to check api methods with auth required
+     * middleware to manage JWT authentication HTTP header
      *
      * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
      * @param  \Psr\Http\Server\RequestHandlerInterface $handler  PSR7 request handler object
@@ -27,65 +27,31 @@ class JWT
     public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Server\RequestHandlerInterface $handler): \Psr\Http\Message\ResponseInterface
     {
         $clientHeaderJWT = $request->hasHeader("SPIELDOSE-JWT") ? $request->getHeader("SPIELDOSE-JWT")[0] : null;
-        if (!\Spieldose\User::isLogged()) {
-            if (!empty($clientHeaderJWT)) {
-                try {
-                    $jwt = new \Spieldose\JWT($this->logger, $this->passphrase);
-                    $decoded = $jwt->decode($clientHeaderJWT);
-
-                    if (isset($decoded) && isset($decoded->data) && isset($decoded->data->userId) && isset($decoded->data->email)) {
-                        $this->logger->debug("JWT decoded", $decoded->data);
-                        $_SESSION["userId"] = $decoded->data->userId;
-                        $_SESSION["email"] = $decoded->data->email;
-                    } else {
-                        throw new \Spieldose\Exception\InvalidParamsException("jwt");
-                    }
-                } catch (\InvalidArgumentException $e) {
-                    // provided key/key-array is empty or malformed.
-                    $this->logger->error("Error decoding JWT", [$e->getMessage()]);
-                } catch (\DomainException $e) {
-                    // provided algorithm is unsupported OR
-                    // provided key is invalid OR
-                    // unknown error thrown in openSSL or libsodium OR
-                    // libsodium is required but not available.
-                    $this->logger->error("Error decoding JWT", [$e->getMessage()]);
-                } catch (\Firebase\JWT\SignatureInvalidException $e) {
-                    // provided JWT signature verification failed.
-                    $this->logger->error("Error decoding JWT", [$e->getMessage()]);
-                } catch (\Firebase\JWT\BeforeValidException $e) {
-                    // provided JWT is trying to be used before "nbf" claim OR
-                    // provided JWT is trying to be used before "iat" claim.
-                    $this->logger->error("Error decoding JWT", [$e->getMessage()]);
-                } catch (\Firebase\JWT\ExpiredException $e) {
-                    // provided JWT is trying to be used after "exp" claim.
-                    $this->logger->error("Error decoding JWT", [$e->getMessage()]);
-                } catch (\UnexpectedValueException $e) {
-                    // provided JWT is malformed OR
-                    // provided JWT is missing an algorithm / using an unsupported algorithm OR
-                    // provided JWT algorithm does not match provided key OR
-                    // provided key ID in key/key-array is empty or invalid.
-                    $this->logger->error("Error decoding JWT", [$e->getMessage()]);
-                }
-            }
+        // user not logged (or session lost) && jwt auth header found => re-auth with jwt
+        if (!\Spieldose\User::isLogged() && !empty($clientHeaderJWT)) {
+            // try decoding jwt data
+            $jwt = new \Spieldose\JWT($this->logger, $this->passphrase);
+            $decoded = $jwt->decode($clientHeaderJWT);
+            if (isset($decoded) && isset($decoded->data) && isset($decoded->data->userId) && isset($decoded->data->email)) {
+                $this->logger->notice("JWT valid data decoded", $decoded->data);
+                \Spieldose\User::setSessionVars($decoded->data->userId, $decoded->data->email);
+            } else {
+                throw new \Spieldose\Exception\InvalidParamsException("jwt");
+            }          
             $response = $handler->handle($request);
             if (!empty($clientHeaderJWT)) {
-                return $response->withHeader("SPIELDOSE-JWT", "BB"); //$clientHeaderJWT);
+                return $response->withHeader("SPIELDOSE-JWT", $clientHeaderJWT);
             } else {
                 return ($response);
-            }
+            }        
         } else {
-            $this->logger->debug("User logged");
             if (empty($clientHeaderJWT)) {
                 $payload = array(
                     "id" => isset($_SESSION["userId"]) ? $_SESSION["userId"] : null,
                     "email" => isset($_SESSION["email"]) ? $_SESSION["email"] : null
                 );
-                try {
-                    $jwt = new \Spieldose\JWT($this->logger, $this->passphrase);
-                    $clientHeaderJWT = $jwt->encode($payload);
-                } catch (\Throwable $e) {
-                    $this->logger->error("Error encoding JWT payload", [$payload]);
-                }
+                $jwt = new \Spieldose\JWT($this->logger, $this->passphrase);
+                $clientHeaderJWT = $jwt->encode($payload);
             }
             $response = $handler->handle($request);
             if ($clientHeaderJWT) {
