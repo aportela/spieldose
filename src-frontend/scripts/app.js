df8df2f6fc061593e7d7f122387d3fbd52906540
alex <766f6964+github@gmail.com>
2022-10-12 20:51:09 +0200

    v2 migration started

--- src-frontend/scripts/app.js
+++ src-frontend/scripts/app.js
@@ -2,6 +2,7 @@
 import { createApp } from 'vue';
 import { default as router } from './routes.js';
 import axios from 'axios';
+import { default as Basil } from 'Basil';
 import { default as i18n } from './i18n.js';
 import { default as spieldoseAPI } from './api.js';
 import { bus } from './bus.js';
@@ -9,6 +10,8 @@ import { bus } from './bus.js';
 import { mixinAPIError } from './mixins.js';
 
 import { default as audioplayer } from './plugins/audioplayer.js';
+
+window.spieldose = window.spieldose || {};
 
 const getApiErrorDataFromAxiosResponse = function (r) {
     var data = {
@@ -37,7 +40,6 @@ const getApiErrorDataFromAxiosResponse = function (r) {
     */
     return (data);
 };
-
 
 axios.interceptors.response.use(function (response) {
     response.getApiErrorData = function () {
@@ -73,13 +75,51 @@ const spieldoseApp = {
         return ({
             logged: false,
             errors: false,
-            apiError: null
+            apiError: null,
+            localStorage: null,
+            axios: null
         });
     },
     created: function () {
+        this.localStorage = new Basil({
+            namespace: 'spieldose',
+            storages: ['local', 'cookie', 'session', 'memory'],
+            storage: 'local',
+            expireDays: 3650
+        });
+
+        this.axios = axios.create({});
+
+        const jwt = this.localStorage.get('jwt');
+        if (jwt) {
+            window.spieldose.jwt = jwt;            
+            if (this.jwt) {
+                this.axios.interceptors.request.use((config) => {
+                    config.headers["SPIELDOSE-JWT"] = this.jwt;
+                    return (config);
+                }, (error) => {
+                    return Promise.reject(error);
+                });
+            }
+        }
+
+        this.axios.interceptors.response.use((response) => {
+            // warning: axios lowercase received header names
+            const apiResponseJWT = response.headers["spieldose-jwt"] || null;
+            if (apiResponseJWT) {
+                if (apiResponseJWT && apiResponseJWT != this.jwt) {
+                    this.localStorage.set("jwt", apiResponseJWT);
+                    window.spieldose.jwt = apiResponseJWT;
+                }
+            }
+            return response;
+        }, (error) => {
+            return Promise.reject(error.message);
+        });
         bus.on("signOut", () => {
             this.signOut();
         });
+        /*
         if (!initialState.upgradeAvailable) {
             if (!initialState.logged) {
                 if (this.$route.name != 'signin') {
@@ -93,6 +133,8 @@ const spieldoseApp = {
         } else {
             this.$router.push({ name: 'upgrade' });
         }
+        */
+        this.$router.push({ name: 'signin' });
     },
     methods: {
         signOut: function () {
