59c58f4e054996a9e5a3d0bfd2509db7bbc4d723
aportela <766f6964+github@gmail.com>
2022-10-14 01:08:18 +0200

    indents

--- src-frontend/scripts/vue-components/tiles-album-images.js
+++ src-frontend/scripts/vue-components/tiles-album-images.js
@@ -5,7 +5,7 @@ const template = function () {
         <div class="tile is-ancestor" id="container_tiles">
             <div class="tile is-2 is-vertical" v-for="column in [0,1,2,3,4,5]">
                 <div class="tile" v-for="row in [0,1,2,3,4,5]">
-                    <img v-if="coverHashes.length > 0" :src="getImgSource((5 * column) + row)" style="width:100%" class="blur" @error="onImageError($event)">
+                    <img v-if="coverHashes.length > 0" :src="getImgSource((5 * column) + row)" style="width: 100%" class="blur" @error="onImageError($event)">
                 </div>
             </div>
         </div>
@@ -26,11 +26,11 @@ export default {
     mounted: function () {
     },
     methods: {
-        getImgSource: function(index) {
+        getImgSource: function (index) {
             if (index < this.coverHashes.length) {
-                return('api2/thumbnail/400/400/' + this.coverHashes[index]);
+                return ('api2/thumbnail/400/400/' + this.coverHashes[index]);
             } else {
-                return('/images/vinyl.png');
+                return ('/images/vinyl.png');
             }
         },
         loadRandomAlbumImages: function () {
