6bf5c724058d8ec2f88ca4df69a2774baa2c3a0d
aportela <766f6964+github@gmail.com>
2022-10-14 01:05:46 +0200

    indents

--- src-frontend/scripts/validator.js
+++ src-frontend/scripts/validator.js
@@ -6,7 +6,6 @@ module.clear = function () {
     module.invalidFields = [];
 }
 
-
 module.setInvalid = function (field, message) {
     module.invalidFields.push({ field: field, message: message });
 }
@@ -16,9 +15,9 @@ module.hasInvalidFields = function () {
 }
 
 module.hasInvalidField = function (field) {
-
     return (module.invalidFields.findIndex(e => e.field == field) > -1);
 }
+
 module.getInvalidFieldMessage = function (field) {
     let idx = module.invalidFields.findIndex(e => e.field == field);
     if (idx > -1) {
