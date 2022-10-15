deae0a672bdf4fca0bb2495be37be30bb3413147
aportela <766f6964+github@gmail.com>
2022-10-16 00:53:22 +0200

    main layout changes

--- src-frontend/scripts/layouts/app.js
+++ src-frontend/scripts/layouts/app.js
@@ -1,4 +1,4 @@
-import { default as playerComponent } from '../vue-components/player.js';
+import { default as player } from '../vue-components/player.js';
 import { default as menu } from '../vue-components/menu.js';
 import { default as playerNavBar } from '../vue-components/player-navbar.js';
 
@@ -8,8 +8,15 @@ const template = function () {
             <section class="section is-fullheight">
                 <div class="columns is-desktop">
                     <!-- TODO: test class "is-hidden-touch" for mobile -->
-                    <div class="is-narrow column">
-                        <spieldose-player :track="track"></spieldose-player>
+                    <div class="is-narrow column" id="sidebar">
+                        <spieldose-component-player :track="currentTrack" @previous="onPreviousTrack" @next="onNextTrack">
+                            <template slot="top-left-icon">
+                                <a class="list__link" href="#" @click.prevent="loadTracks('')"><i class="fa fa-navicon"></i></a>
+                            </template>
+                            <template slot="top-right-icon">
+                                <a class="list__link" href="#" @click.prevent><i class="fa fa-search"></i></a>
+                            </template>
+                        </spieldose-component-player>
                         <spieldose-menu-component></spieldose-menu-component>
                     </div>
                     <div class="column">
@@ -17,7 +24,9 @@ const template = function () {
                     </div>
                 </div>
             </section>
+            <!--
             <player-navbar></player-navbar>
+            -->
         </div>
     `;
 };
@@ -27,15 +36,23 @@ export default {
     template: template(),
     data: function () {
         return ({
-            track: {}
+            currentTrack: {}
         });
     },
     components: {
-        'spieldose-player': playerComponent,
-        'spieldose-menu-component': menu,
+        'spieldose-component-player': player,
+        'spieldose-menu-component': menu
         //'player-navbar': playerNavBar
     },
     created: function () {
-        this.$bus.on('onTrackChanged', (data) => { this.track = data.track;});
+        this.$bus.on('onTrackChanged', (data) => { this.currentTrack = data.track; });
+    },
+    methods: {
+        onPreviousTrack: function () {
+
+        },
+        onNextTrack: function () {
+
+        }
     }
 }
