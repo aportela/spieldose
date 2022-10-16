a3649354ab78fbcf2d56fa5888897fd6a2511fab
aportela <766f6964+github@gmail.com>
2022-10-16 11:45:28 +0200

    new methods

--- src-frontend/scripts/plugins/audio.js
+++ src-frontend/scripts/plugins/audio.js
@@ -1,5 +1,21 @@
+let audioInterface = {
+    element: null
+};
+
+audioInterface.getElement = () => {
+    if (!audioInterface.element) {
+        audioInterface.element = document.getElementById('audio');
+    }
+    return (audioInterface.element);
+}
+audioInterface.setVolume = (volume) => {
+    if (audioInterface) {
+        audioInterface.getElement().volume = volume;
+    }
+}
+
 export default {
     install: (app, options) => {
-        app.config.globalProperties.$audio = document.getElementById('audio')
+        app.config.globalProperties.$audio = audioInterface;
     }
 }
