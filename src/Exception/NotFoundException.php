bf3ff506b57729919b8bd2495e6999da8f772096
aportela <766f6964+github@gmail.com>
2022-10-19 22:32:32 +0200

    code indentations

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -1,20 +1,21 @@
 <?php
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    namespace Spieldose\Exception;
+namespace Spieldose\Exception;
 
-    /**
-     * not found exception
-     */
-    class NotFoundException extends \Exception {
-        public function __construct($message = "", $code = 0, \Exception $previous = null) {
-            parent::__construct($message, $code, $previous);
-        }
-
-        public function __toString() {
-            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-        }
+/**
+ * not found exception
+ */
+class NotFoundException extends \Exception
+{
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
     }
 
-?>
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
