40aa968349d8e905c3dde5081fd374b8969565fe
alex <766f6964+github@gmail.com>
2022-10-28 16:20:41 +0200

    show random backgrounds if we don't have at least 32 album covers cached

--- src-frontend/scripts/vue-components/tiles-album-images.js
+++ src-frontend/scripts/vue-components/tiles-album-images.js
@@ -2,7 +2,7 @@ const template = function () {
     return `
         <div class="tile is-ancestor" id="container_tiles">
             <div class="tile is-2 is-vertical" v-for="column in [0,1,2,3,4,5]">
-                <div class="tile" v-for="row in [0,1,2,3,4,5]">
+                <div class="tile" v-for="row in [0,1,2,3,4,5]" :style="'background-color: ' + getRandomColor() + ';'">
                     <img v-if="imageURLs.length > 0" :src="getImgSource((5 * column) + row)" style="width: 100%" class="blur" @error="onImageError($event)">
                     <img src="/images/vinyl.png" v-else>
                 </div>
@@ -25,6 +25,15 @@ export default {
     mounted: function () {
     },
     methods: {
+        // https://stackoverflow.com/q/10014271
+        getRandomColor: function () {
+            const allowed = "ABCDEF0123456789";
+            let S = "#";
+            while (S.length < 7) {
+                S += allowed.charAt(Math.floor((Math.random() * 16) + 1));
+            }
+            return (S);
+        },
         getImgSource: function (index) {
             if (index < this.imageURLs.length) {
                 return (this.imageURLs[index]);
@@ -34,7 +43,9 @@ export default {
         },
         loadRandomAlbumImages: function () {
             this.$api.album.getRandomAlbumCoverThumbnails().then(response => {
-                this.imageURLs = response.data.coverURLs;
+                if (response.data.coverURLs.length >= 32) {
+                    this.imageURLs = response.data.coverURLs;
+                }
             }).catch(error => {
             });
         },
