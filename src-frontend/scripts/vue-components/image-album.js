0f0d563d5f0e3cbda3dae656a367f39625b3a52a
aportela <766f6964+github@gmail.com>
2022-10-30 10:41:54 +0100

    new components

---
+++ src-frontend/scripts/vue-components/image-album.js
@@ -0,0 +1,48 @@
+const template = function () {
+    return `
+        <div v-if="url">
+            <img class="album-thumbnail" :src="url" @error="unsetImage()">
+        </div>
+    `;
+};
+
+export default {
+    name: 'spieldose-image-album',
+    template: template(),
+    data: function () {
+        return ({
+            url: null
+        });
+    },
+    props: ['src'],
+    created: function () {
+        //this.url = this.getAlbumImageUrl(this.src);
+        this.url = this.src;
+    },
+    watch: {
+        src: function (newValue, oldValue) {
+            //this.url = this.getAlbumImageUrl(newValue);
+            this.url = newValue;
+        }
+    },
+    methods: {
+        getAlbumImageUrl: function (value) {
+            if (value) {
+                if (value.indexOf("http") == 0) {
+                    return ("api/thumbnail?url=" + encodeURIComponent(value));
+                } else {
+                    return ("api/thumbnail?hash=" + value);
+                }
+            } else {
+                /**
+                 * Vinyl disc icon credits: Jordan Green (http://www.jordangreenphoto.com/)
+                 * https://jordygreen.deviantart.com/art/Vinyl-Disc-Icon-Updated-57968239
+                 */
+                return ("images/image-album-not-set.png");
+            }
+        },
+        unsetImage: function () {
+            this.url = 'images/image-album-not-set.png';
+        }
+    }
+}
---
+++ src-frontend/scripts/vue-components/image-artist.js
@@ -0,0 +1,37 @@
+const template = function () {
+    return `
+        <div v-if="url">
+            <img :src="url" :class="extraClass" @error="unsetImage()">
+        </div>
+    `;
+};
+
+export default {
+    name: 'spieldose-image-artist',
+    template: template(),
+    data: function () {
+        return ({
+            url: null
+        });
+    },
+    props: ['src', 'extraClass'],
+    created: function () {
+        this.url = this.getArtistImageUrl(this.src);
+    },
+    methods: {
+        getArtistImageUrl: function (value) {
+            if (value) {
+                return ("api/thumbnail?url=" + value);
+            } else {
+                /**
+                 * Music band icon credits: adiante apps (http://www.adianteapps.com/)
+                 * https://www.iconfinder.com/icons/339940/band_festival_music_rock_stage_icon
+                 */
+                return ('images/image-artist-not-set.png');
+            }
+        },
+        unsetImage: function () {
+            this.url = 'images/image-artist-not-set.png';
+        }
+    }
+}
