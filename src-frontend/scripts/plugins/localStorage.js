d1b177d0fef78f92cd8220ffa862245b12c6dd69
aportela <766f6964+github@gmail.com>
2022-11-03 00:10:50 +0100

    plugin refactored

--- src-frontend/scripts/plugins/localStorage.js
+++ src-frontend/scripts/plugins/localStorage.js
@@ -2,6 +2,6 @@ import { default as Basil } from 'basil';
 
 export default {
     install: (app, options) => {
-        app.config.globalProperties.$spieldoseLocalStorage = new Basil(options);
+        app.config.globalProperties.$localStorage = new Basil(options);
     }
 }
