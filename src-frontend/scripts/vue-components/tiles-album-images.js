af6c991e9ace5bbc0be1ed3a13fcbf874d215cab
alex <766f6964+github@gmail.com>
2022-11-08 18:48:16 +0100

    class definitions refactored (spieldose own classes with "sp-" prefix)

--- src-frontend/scripts/vue-components/tiles-album-images.js
+++ src-frontend/scripts/vue-components/tiles-album-images.js
@@ -1,10 +1,10 @@
 const template = function () {
     return `
-        <div class="tile is-ancestor mt-0" id="container_tiles">
+        <div class="tile is-ancestor mt-0" id="sp-album-cover-tiles-container">
             <div class="tile is-2 is-vertical" v-for="column in [0,1,2,3,4,5]">
                 <div class="tile" v-for="row in [0,1,2,3,4,5]" :style="'background-color: ' + getRandomColor() + ';'">
-                    <img v-if="imageURLs.length > 0" :src="getImgSource((5 * column) + row)" @error="onImageError($event)">
-                    <img :src="defaultImage" v-else>
+                    <img class="album-cover-tile" v-if="imageURLs.length > 0" :src="getImgSource((5 * column) + row)" @error="onImageError($event)">
+                    <img class="album-cover-tile" :src="defaultImage" v-else>
                 </div>
             </div>
         </div>
