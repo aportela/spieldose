d398ca788b0d7115214f0f1ccc5bb88fccfc6719
alex <766f6964+github@gmail.com>
2022-11-09 17:18:21 +0100

    replaced link with span

--- src-frontend/scripts/vue-components/dashboard-block.js
+++ src-frontend/scripts/vue-components/dashboard-block.js
@@ -4,11 +4,11 @@ const template = function () {
             <p class="panel-heading">
                 <span class="icon mr-1"><slot name="icon"></slot></span>
                 <span><slot name="title"></slot></span>
-                <a class="icon is-pulled-right" v-bind:title="$t('commonMessages.refreshData')" v-on:click.prevent="onReload" v-else-if="reloadFunction">
+                <span class="icon is-clickable is-pulled-right" v-bind:title="$t('commonMessages.refreshData')" v-on:click.prevent="onReload" v-else-if="reloadFunction">
                     <i class="fa-fw fas fa-cog fa-spin fa-fw" v-if="loading"></i>
                     <i class="fa-fw fas fa-exclamation-triangle" v-else-if="errors"></i>
                     <i class="fas fa-redo fa-fw" v-else></i>
-                </a>
+                </span>
             </p>
             <slot name="body"></slot>
             <p class="panel-block title is-6 has-text-centered has-text-weight-bold is-block" v-if="errors">{{ $t("commonErrors.invalidAPIResponse") }}</p>
