8df78fe7cd8186d47bf2c49a5822b5ae81f5b2d6
alex <766f6964+github@gmail.com>
2022-11-14 17:05:04 +0100

    replaced manual-links with router-links

--- src-frontend/scripts/vue-components/menu.js
+++ src-frontend/scripts/vue-components/menu.js
@@ -2,23 +2,25 @@ const template = function () {
     return `
         <nav id="menu" class="panel is-unselectable">
             <p class="panel-heading">{{ $t("menu.labels.header") }}</p>
-            <a class="panel-block" v-bind:class="{ 'is-active': isSectionActive('dashboard') }" v-on:click.prevent="changeSection('dashboard');">
+            <router-link :to="{ name: 'dashboard' }" :class="{'panel-block': true, 'is-active': isSectionActive('dashboard') }">
                 <span class="panel-icon"><i class="fas fa-chart-line"></i></span>
                 <span>{{ $t("menu.labels.dashboard") }}</span>
-            </a>
-            <a class="panel-block" v-bind:class="{ 'is-active': isSectionActive('nowPlaying') }" v-on:click.prevent="changeSection('nowPlaying');">
+            </router-link>
+            <router-link :to="{ name: 'nowPlaying' }" :class="{'panel-block': true, 'is-active': isSectionActive('nowPlaying') }">
                 <span class="panel-icon"><i class="fas fa-headphones"></i></span>
                 <span>{{ $t("menu.labels.currentPlaylist") }}</span>
-            </a>
+            </router-link>
             <!--
             <a class="panel-block" v-bind:class="{ 'is-active': isSectionActive('search') }" v-on:click.prevent="changeSection('search');">
                 <span class="panel-icon"><i class="fas fa-search"></i></span>
                 <span>{{ $t("menu.labels.search") }}</span>
             </a>
+            -->
             <a class="panel-block" v-bind:class="{ 'is-active': isSectionActive('artists') }" v-on:click.prevent="changeSection('artists');">
                 <span class="panel-icon"><i class="fas fa-user"></i></span>
                 <span>{{ $t("menu.labels.browseArtists") }}</span>
             </a>
+            <!--
             <a class="panel-block" v-bind:class="{ 'is-active': isSectionActive('albums') }" v-on:click.prevent="changeSection('albums');">
                 <span class="panel-icon"><i class="fas fa-circle"></i></span>
                 <span>{{ $t("menu.labels.browseAlbums") }}</span>
@@ -40,11 +42,10 @@ const template = function () {
                 <span>Toggle dark mode</span>
             </a>
             -->
-            <a class="panel-block" v-bind:class="{ 'is-active': isSectionActive('profile') }" v-on:click.prevent="changeSection('profile');">
+            <router-link :to="{ name: 'profile' }" :class="{'panel-block': true, 'is-active': isSectionActive('profile') }">
                 <span class="panel-icon"><i class="fas fa-user"></i></span>
                 <span>{{ $t("menu.labels.profile") }}</span>
-            </a>
-
+            </router-link>
             <a class="panel-block" v-on:click.prevent="signOut();">
                 <span class="panel-icon"><i class="fas fa-sign-out-alt"></i></span>
                 <span>{{ $t("menu.labels.signOut") }}</span>
