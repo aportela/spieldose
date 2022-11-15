493076324d31a6187d140b16ac8a84e08f16b4d3
alex <766f6964+github@gmail.com>
2022-11-15 17:26:40 +0100

    new methods for setting totalResults & get SQL Query pagination limit

--- src/Helper/Pager.php
+++ src/Helper/Pager.php
@@ -7,12 +7,26 @@ namespace Spieldose\Helper;
 class Pager
 {
     public $currentPage;
-    public $totalPages;
     public $resultsPage;
+    public $totalPages;
+    public $totalResults;
 
     public function __construct(int $currentPage = 1, int $resultsPage = 32)
     {
         $this->currentPage = $currentPage;
         $this->resultsPage = $resultsPage;
+    }
+
+    public function setTotalResults(int $totalResults) {
+        $this->totalResults = $totalResults;
+        if ($this->resultsPage > 0) {
+            $this->totalPages = ceil($this->totalResults / $this->resultsPage);
+        } else {
+            $this->totalPages = 0;
+        }
+    }
+
+    public function getSQLQueryLimitFrom() {
+        return(($this->currentPage - 1) * $this->resultsPage);
     }
 }
