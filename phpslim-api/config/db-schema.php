80e12a7422d52cb25df4690cf9a0ce9448209a7a
alex <766f6964+github@gmail.com>
2023-07-26 12:05:16 +0200

    added user table

--- phpslim-api/config/db-schema.php
+++ phpslim-api/config/db-schema.php
@@ -1,7 +1,14 @@
 <?php
 
 return (array(
-    /*
-    2 => array(
+    1 => array(
+        '
+            CREATE TABLE `USER` (
+                `id` VARCHAR(36) NOT NULL,
+                `email` VARCHAR(255) NOT NULL UNIQUE,
+                `password_hash` VARCHAR(60) NOT NULL,
+                PRIMARY KEY (`id`)
+            );
+        '
     )
-    */));
+));
