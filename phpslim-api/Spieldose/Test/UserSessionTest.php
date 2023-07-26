491f0d7520b66720768e8492db9c9dab380ff2cb
alex <766f6964+github@gmail.com>
2023-07-26 12:06:40 +0200

    new UserSession tests

---
+++ phpslim-api/Spieldose/Test/UserSessionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spieldose\Test;
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
+final class UserSessionTest extends \Spieldose\Test\BaseTest
+{
+    /**
+     * Clean up the whole test class
+     */
+    public static function tearDownAfterClass(): void
+    {
+        self::$dbh = null;
+        self::$container = null;
+        self::$app = null;
+    }
+
+    public function testIsLoggedWithoutSession(): void
+    {
+        \Spieldose\User::signOut();
+        $this->assertFalse(\Spieldose\UserSession::isLogged());
+    }
+
+    public function testIsLogged(): void
+    {
+        $id = \Spieldose\Utils::uuidv4();
+        $u = new \Spieldose\User($id, $id . "@server.com", "secret");
+        $u->add(self::$dbh);
+        $u->signIn(self::$dbh);
+        $this->assertTrue(\Spieldose\UserSession::isLogged());
+    }
+
+    public function testGetUserIdWithoutSession(): void
+    {
+        \Spieldose\User::signOut();
+        $this->assertEmpty(\Spieldose\UserSession::getUserId());
+    }
+
+    public function testGetUserId(): void
+    {
+        \Spieldose\User::signOut();
+        $id = \Spieldose\Utils::uuidv4();
+        $u = new \Spieldose\User($id, $id . "@server.com", "secret");
+        $u->add(self::$dbh);
+        $u->signIn(self::$dbh);
+        $this->assertEquals($u->id, \Spieldose\UserSession::getUserId());
+    }
+
+    public function testGetEmailWithoutSession(): void
+    {
+        \Spieldose\User::signOut();
+        $this->assertEmpty(\Spieldose\UserSession::getEmail());
+    }
+
+    public function testGetEmail(): void
+    {
+        \Spieldose\User::signOut();
+        $id = \Spieldose\Utils::uuidv4();
+        $u = new \Spieldose\User($id, $id . "@server.com", "secret");
+        $u->add(self::$dbh);
+        $u->signIn(self::$dbh);
+        $this->assertEquals($u->email, \Spieldose\UserSession::getEmail());
+    }
+}
