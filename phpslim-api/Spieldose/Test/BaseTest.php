3b591e4aff727b3960ccc2dccaabad73dd3995dd
alex <766f6964+github@gmail.com>
2023-07-26 12:06:06 +0200

    new base test

---
+++ phpslim-api/Spieldose/Test/BaseTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spieldose\Test;
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
+class BaseTest extends \PHPUnit\Framework\TestCase
+{
+    static public $app = null;
+    static public $container = null;
+    static public $settings = null;
+    static public $dbh = null;
+
+    /**
+     * Called once just like normal constructor
+     */
+    public static function setUpBeforeClass(): void
+    {
+        $containerBuilder = new \DI\ContainerBuilder();
+
+        // Set up settings
+        $containerBuilder->addDefinitions(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'container.php');
+
+        // Build PHP-DI Container instance
+        self::$container = $containerBuilder->build();
+
+        self::$settings = self::$container->get('settings');
+
+        // Create App instance
+        self::$app = self::$container->get(\Slim\App::class);
+
+        self::$dbh = self::$container->get(\aportela\DatabaseWrapper\DB::class);
+    }
+
+    /**
+     * Initialize the test case
+     * Called for every defined test
+     */
+    public function setUp(): void
+    {
+        self::$dbh->beginTransaction();
+    }
+
+    /**
+     * Clean up the test case, called for every defined test
+     */
+    public function tearDown(): void
+    {
+        self::$dbh->rollBack();
+    }
+
+    /**
+     * Clean up the whole test class
+     */
+    public static function tearDownAfterClass(): void
+    {
+        self::$dbh = null;
+        self::$container = null;
+        self::$app = null;
+    }
+
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
