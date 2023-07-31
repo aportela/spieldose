ce6578c00a1879f9859a91e80a2fea4c5b45ca64
alex <766f6964+github@gmail.com>
2023-07-31 23:05:36 +0200

    fixed commandline params (clean removed)

--- phpslim-api/tools/scrap.php
+++ phpslim-api/tools/scrap.php
@@ -32,89 +32,86 @@ if (count($missingExtensions) > 0) {
             echo "New database version available, an upgrade is required before continue." . PHP_EOL;
             exit;
         }
-        $cmdLine = new \Spieldose\CmdLine("", array("all", "artists", "albums", "cleanup"));
-        if ($cmdLine->hasParam("all") || $cmdLine->hasParam("artists") || $cmdLine->hasParam("albums") || $cmdLine->hasParam("cleanup")) {
-            $scrapArtists = $cmdLine->hasParam("artists") || $cmdLine->hasParam("all");
-            $scrapAlbums = $cmdLine->hasParam("albums") || $cmdLine->hasParam("all");
-            if ($scrapArtists || $scrapAlbums) {
-                $scraper = new \Spieldose\Scraper($db, $logger);
-                if ($scrapArtists) {
-                    echo "Artist scraping...." . PHP_EOL;
-                    $logger->info("Scraping artists");
-                    $artistNames = $scraper->getArtistNamesWithoutMusicBrainzId();
-                    $totalArtistNames = count($artistNames);
-                    if ($totalArtistNames > 0) {
-                        echo sprintf("Processing %d artist name/s without MusicBrainzId%s", $totalArtistNames, PHP_EOL);
-                        for ($i = 0; $i < $totalArtistNames; $i++) {
-                            $mbId = $scraper->searchArtistMusicBrainzIdByName($artistNames[$i]);
-                            if (!empty($mbId)) {
-                                $scraper->saveArtistMusicBrainzId($mbId, $artistNames[$i]);
-                            }
-                            sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
-                            \Spieldose\Utils::showProgressBar($i + 1, $totalArtistNames, 20, "Name: " . $artistNames[$i]);
+        $cmdLine = new \Spieldose\CmdLine("", array("all", "artists", "albums"));
+        $scrapArtists = $cmdLine->hasParam("artists") || $cmdLine->hasParam("all");
+        $scrapAlbums = $cmdLine->hasParam("albums") || $cmdLine->hasParam("all");
+        if ($scrapArtists || $scrapAlbums) {
+            $scraper = new \Spieldose\Scraper($db, $logger);
+            if ($scrapArtists) {
+                echo "Artist scraping...." . PHP_EOL;
+                $logger->info("Scraping artists");
+                $artistNames = $scraper->getArtistNamesWithoutMusicBrainzId();
+                $totalArtistNames = count($artistNames);
+                if ($totalArtistNames > 0) {
+                    echo sprintf("Processing %d artist name/s without MusicBrainzId%s", $totalArtistNames, PHP_EOL);
+                    for ($i = 0; $i < $totalArtistNames; $i++) {
+                        $mbId = $scraper->searchArtistMusicBrainzIdByName($artistNames[$i]);
+                        if (!empty($mbId)) {
+                            $scraper->saveArtistMusicBrainzId($mbId, $artistNames[$i]);
                         }
-                    } else {
-                        echo sprintf("No artist names without MusicBrainzId found%s", PHP_EOL);
+                        sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
+                        \Spieldose\Utils::showProgressBar($i + 1, $totalArtistNames, 20, "Name: " . $artistNames[$i]);
                     }
-                    $artistMBIds = $scraper->getArtistMusicBrainzIdsWithoutCachedMetadata();
-                    $totalArtistMBIds = count($artistMBIds);
-                    if ($totalArtistMBIds > 0) {
-                        echo sprintf("Processing %d artist without MusicBrainz cached metadata%s", $totalArtistMBIds, PHP_EOL);
-                        for ($i = 0; $i < $totalArtistMBIds; $i++) {
-                            $mbArtist = $scraper->getArtistMusicBrainzMetadata($artistMBIds[$i]);
-                            if (!empty($mbArtist->mbId) && !empty($mbArtist->name)) {
-                                $scraper->saveArtistMusicBrainzCachedMetadata($mbArtist);
-                            }
-                            sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
-                            \Spieldose\Utils::showProgressBar($i + 1, $totalArtistMBIds, 20, "MusicBrainzId: " . $artistMBIds[$i]);
-                        }
-                    } else {
-                        echo sprintf("All Artist MusicBrainz metadata is cached%s", PHP_EOL);
-                    }
+                } else {
+                    echo sprintf("No artist names without MusicBrainzId found%s", PHP_EOL);
                 }
-                if ($scrapAlbums) {
-                    echo "Album scraping...." . PHP_EOL;
-                    $logger->info("Scraping albums");
-                    $albums = $scraper->getAlbumsWithoutMusicBrainzId();
-                    $totalAlbums = count($albums);
-                    if (false && $totalAlbums > 0) {
-                        echo sprintf("Processing %d album/s without MusicBrainzId%s", $totalAlbums, PHP_EOL);
-                        for ($i = 0; $i < $totalAlbums; $i++) {
-                            $mbId = $scraper->searchAlbumMusicBrainzId($albums[$i]->album, $albums[$i]->artist, $albums[$i]->year);
-                            if (!empty($mbId)) {
-                                $scraper->saveAlbumMusicBrainzId($mbId, $albums[$i]->album, $albums[$i]->artist, $albums[$i]->year);
-                            }
-                            sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
-                            \Spieldose\Utils::showProgressBar($i + 1, $totalAlbums, 20, "Name: " . $albums[$i]->album . " / " . $albums[$i]->artist);
+                $artistMBIds = $scraper->getArtistMusicBrainzIdsWithoutCachedMetadata();
+                $totalArtistMBIds = count($artistMBIds);
+                if ($totalArtistMBIds > 0) {
+                    echo sprintf("Processing %d artist without MusicBrainz cached metadata%s", $totalArtistMBIds, PHP_EOL);
+                    for ($i = 0; $i < $totalArtistMBIds; $i++) {
+                        $mbArtist = $scraper->getArtistMusicBrainzMetadata($artistMBIds[$i]);
+                        if (!empty($mbArtist->mbId) && !empty($mbArtist->name)) {
+                            $scraper->saveArtistMusicBrainzCachedMetadata($mbArtist);
                         }
-                    } else {
-                        echo sprintf("No albums without MusicBrainzId found%s", PHP_EOL);
+                        sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
+                        \Spieldose\Utils::showProgressBar($i + 1, $totalArtistMBIds, 20, "MusicBrainzId: " . $artistMBIds[$i]);
                     }
-
-                    $albumMBIds = $scraper->getAlbumMusicBrainzIdsWithoutCachedMetadata();
-                    $totalAlbumMBIds = count($albumMBIds);
-                    if ($totalAlbumMBIds > 0) {
-                        echo sprintf("Processing %d albums without MusicBrainz cached metadata%s", $totalAlbumMBIds, PHP_EOL);
-                        for ($i = 0; $i < $totalAlbumMBIds; $i++) {
-                            $mbAlbum = $scraper->getAlbumMusicBrainzMetadata($albumMBIds[$i]);
-                            if (!empty($mbAlbum->mbId) && !empty($mbAlbum->title)) {
-                                $scraper->saveAlbumMusicBrainzCachedMetadata($mbAlbum);
-                            }
-                            sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
-                            \Spieldose\Utils::showProgressBar($i + 1, $totalAlbumMBIds, 20, "MusicBrainzId: " . $albumMBIds[$i]);
-                        }
-                    } else {
-                        echo sprintf("All Album MusicBrainz metadata is cached%s", PHP_EOL);
-                    }
+                } else {
+                    echo sprintf("All Artist MusicBrainz metadata is cached%s", PHP_EOL);
                 }
             }
-            if ($cmdLine->hasParam(("cleanup"))) {
-                echo "Cleaning database" . PHP_EOL;
-                $logger->debug("Cleaning database");
-                $scraper->cleanUp();
+            if ($scrapAlbums) {
+                echo "Album scraping...." . PHP_EOL;
+                $logger->info("Scraping albums");
+                $albums = $scraper->getAlbumsWithoutMusicBrainzId();
+                $totalAlbums = count($albums);
+                if (false && $totalAlbums > 0) {
+                    echo sprintf("Processing %d album/s without MusicBrainzId%s", $totalAlbums, PHP_EOL);
+                    for ($i = 0; $i < $totalAlbums; $i++) {
+                        $mbId = $scraper->searchAlbumMusicBrainzId($albums[$i]->album, $albums[$i]->artist, $albums[$i]->year);
+                        if (!empty($mbId)) {
+                            $scraper->saveAlbumMusicBrainzId($mbId, $albums[$i]->album, $albums[$i]->artist, $albums[$i]->year);
+                        }
+                        sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
+                        \Spieldose\Utils::showProgressBar($i + 1, $totalAlbums, 20, "Name: " . $albums[$i]->album . " / " . $albums[$i]->artist);
+                    }
+                } else {
+                    echo sprintf("No albums without MusicBrainzId found%s", PHP_EOL);
+                }
+
+                $albumMBIds = $scraper->getAlbumMusicBrainzIdsWithoutCachedMetadata();
+                $totalAlbumMBIds = count($albumMBIds);
+                if ($totalAlbumMBIds > 0) {
+                    echo sprintf("Processing %d albums without MusicBrainz cached metadata%s", $totalAlbumMBIds, PHP_EOL);
+                    for ($i = 0; $i < $totalAlbumMBIds; $i++) {
+                        $mbAlbum = $scraper->getAlbumMusicBrainzMetadata($albumMBIds[$i]);
+                        if (!empty($mbAlbum->mbId) && !empty($mbAlbum->title)) {
+                            $scraper->saveAlbumMusicBrainzCachedMetadata($mbAlbum);
+                        }
+                        sleep(1); // wait 1 second between queries for prevent too much remote api requests in small amount of time and get banned
+                        \Spieldose\Utils::showProgressBar($i + 1, $totalAlbumMBIds, 20, "MusicBrainzId: " . $albumMBIds[$i]);
+                    }
+                } else {
+                    echo sprintf("All Album MusicBrainz metadata is cached%s", PHP_EOL);
+                }
             }
         } else {
-            echo "No required params found: --all or --artists or --albums or --cleanup" . PHP_EOL;
+            echo "No required params found." . PHP_EOL;
+            echo "Scrap & cache pending/missing artists:" . PHP_EOL;
+            echo "\tphp " . $argv[0] . " --artists" . PHP_EOL;
+            echo "Scrap & cache pending/missing albums:" . PHP_EOL;
+            echo "\tphp " . $argv[0] . " --albums" . PHP_EOL;
         }
     } catch (\Exception $e) {
         echo "Uncaught exception: " . $e->getMessage() . PHP_EOL;
