d10f6dec0fb613326439695856f2d7bfc5c22d72
alex <766f6964+github@gmail.com>
2023-08-16 16:04:13 +0200

    moved audio equalizer css animation to separated component

---
+++ quasar-frontend/src/components/CSSAnimatedAudioEqualizer.vue
@@ -0,0 +1,156 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" class="equilizer" viewBox="0 0 128 128">
+    <g>
+      <title>Audio Equalizer</title>
+      <rect class="bar" transform="translate(0,0)" y="15"></rect>
+      <rect class="bar" transform="translate(25,0)" y="15"></rect>
+      <rect class="bar" transform="translate(50,0)" y="15"></rect>
+      <rect class="bar" transform="translate(75,0)" y="15"></rect>
+      <rect class="bar" transform="translate(100,0)" y="15"></rect>
+    </g>
+  </svg>
+</template>
+
+<style>
+.equilizer {
+  height: 100px;
+  width: 100%;
+  transform: rotate(180deg);
+}
+
+.bar {
+  fill: #dfd8d9;
+  width: 18px;
+  animation: equalize 1.25s steps(25, end) 0s infinite;
+}
+
+.bar:nth-child(1) {
+  animation-duration: 1.9s;
+}
+
+.bar:nth-child(2) {
+  animation-duration: 2s;
+}
+
+.bar:nth-child(3) {
+  animation-duration: 2.3s;
+}
+
+.bar:nth-child(4) {
+  animation-duration: 2.4s;
+}
+
+.bar:nth-child(5) {
+  animation-duration: 2.1s;
+}
+
+@keyframes equalize {
+  0% {
+    height: 60px;
+  }
+
+  4% {
+    height: 50px;
+  }
+
+  8% {
+    height: 40px;
+  }
+
+  12% {
+    height: 30px;
+  }
+
+  16% {
+    height: 20px;
+  }
+
+  20% {
+    height: 30px;
+  }
+
+  24% {
+    height: 40px;
+  }
+
+  28% {
+    height: 10px;
+  }
+
+  32% {
+    height: 40px;
+  }
+
+  36% {
+    height: 60px;
+  }
+
+  40% {
+    height: 20px;
+  }
+
+  44% {
+    height: 40px;
+  }
+
+  48% {
+    height: 70px;
+  }
+
+  52% {
+    height: 30px;
+  }
+
+  56% {
+    height: 10px;
+  }
+
+  60% {
+    height: 30px;
+  }
+
+  64% {
+    height: 50px;
+  }
+
+  68% {
+    height: 60px;
+  }
+
+  72% {
+    height: 70px;
+  }
+
+  76% {
+    height: 80px;
+  }
+
+  80% {
+    height: 70px;
+  }
+
+  84% {
+    height: 60px;
+  }
+
+  88% {
+    height: 50px;
+  }
+
+  92% {
+    height: 60px;
+  }
+
+  96% {
+    height: 70px;
+  }
+
+  100% {
+    height: 80px;
+  }
+}
+
+</style>
+
+<script setup>
+</script>
--- quasar-frontend/src/pages/SignInPage.vue
+++ quasar-frontend/src/pages/SignInPage.vue
@@ -2,16 +2,7 @@
   <q-page>
     <div class="row items-center">
       <div class="col-xs-10 offset-xs-1 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-0 col-xl-3 offset-xl-0 desktop-only justify-center q-pa-xl">
-        <svg xmlns="http://www.w3.org/2000/svg" class="equilizer" viewBox="0 0 128 128">
-          <g>
-            <title>Audio Equalizer</title>
-            <rect class="bar" transform="translate(0,0)" y="15"></rect>
-            <rect class="bar" transform="translate(25,0)" y="15"></rect>
-            <rect class="bar" transform="translate(50,0)" y="15"></rect>
-            <rect class="bar" transform="translate(75,0)" y="15"></rect>
-            <rect class="bar" transform="translate(100,0)" y="15"></rect>
-          </g>
-        </svg>
+        <CSSAnimatedAudioEqualizer></CSSAnimatedAudioEqualizer>
         <q-card class="q-pa-md my_card">
           <form @submit.prevent.stop="onValidateForm" autocorrect="off" autocapitalize="off" autocomplete="off"
             spellcheck="false" style="padding: 1.25rem;">
@@ -64,151 +55,6 @@
   </q-page>
 </template>
 
-
-<style>
-/* signin vu-meter effect */
-
-.equilizer {
-  height: 100px;
-  width: 100%;
-  transform: rotate(180deg);
-}
-
-.bar {
-  fill: #dfd8d9;
-  width: 18px;
-  animation: equalize 1.25s steps(25, end) 0s infinite;
-}
-
-.bar:nth-child(1) {
-  animation-duration: 1.9s;
-}
-
-.bar:nth-child(2) {
-  animation-duration: 2s;
-}
-
-.bar:nth-child(3) {
-  animation-duration: 2.3s;
-}
-
-.bar:nth-child(4) {
-  animation-duration: 2.4s;
-}
-
-.bar:nth-child(5) {
-  animation-duration: 2.1s;
-}
-
-@keyframes equalize {
-  0% {
-    height: 60px;
-  }
-
-  4% {
-    height: 50px;
-  }
-
-  8% {
-    height: 40px;
-  }
-
-  12% {
-    height: 30px;
-  }
-
-  16% {
-    height: 20px;
-  }
-
-  20% {
-    height: 30px;
-  }
-
-  24% {
-    height: 40px;
-  }
-
-  28% {
-    height: 10px;
-  }
-
-  32% {
-    height: 40px;
-  }
-
-  36% {
-    height: 60px;
-  }
-
-  40% {
-    height: 20px;
-  }
-
-  44% {
-    height: 40px;
-  }
-
-  48% {
-    height: 70px;
-  }
-
-  52% {
-    height: 30px;
-  }
-
-  56% {
-    height: 10px;
-  }
-
-  60% {
-    height: 30px;
-  }
-
-  64% {
-    height: 50px;
-  }
-
-  68% {
-    height: 60px;
-  }
-
-  72% {
-    height: 70px;
-  }
-
-  76% {
-    height: 80px;
-  }
-
-  80% {
-    height: 70px;
-  }
-
-  84% {
-    height: 60px;
-  }
-
-  88% {
-    height: 50px;
-  }
-
-  92% {
-    height: 60px;
-  }
-
-  96% {
-    height: 70px;
-  }
-
-  100% {
-    height: 80px;
-  }
-}
-
-/* signin vu-meter effect */
-</style>
-
 <script setup>
 
 import { ref, nextTick } from "vue";
@@ -216,6 +62,7 @@ import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
 import { useI18n } from 'vue-i18n'
 import { api } from 'boot/axios'
+import { default as CSSAnimatedAudioEqualizer } from "components/CSSAnimatedAudioEqualizer.vue";
 import { default as TileAlbumImages } from "components/TileAlbumImages.vue";
 
 const { t } = useI18n();
