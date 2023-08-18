05b8fe60d4db70d715a23027499c23baa3e73ea8
alex <766f6964+github@gmail.com>
2023-08-18 12:47:29 +0200

    splitted sidebar player current track info component

--- quasar-frontend/src/components/SidebarPlayer.vue
+++ quasar-frontend/src/components/SidebarPlayer.vue
@@ -2,13 +2,7 @@
   <div>
     <SidebarPlayerAlbumCover></SidebarPlayerAlbumCover>
     <SidebarPlayerVolumeControl></SidebarPlayerVolumeControl>
-
-    <div id="song_info">
-      <p class="song_info_track text-center text-weight-bolder" style="color: #d30320;">Won’t Get Fooled Again</p>
-      <p class="song_info_album text-center">Who’s Next</p>
-      <p class="song_info_artist text-center"><a href="#/app/artist/The%20Who" style="text-decoration: none;">The Who</a>
-      </p>
-    </div>
+    <SidebarPlayerTrackInfo></SidebarPlayerTrackInfo>
     <SidebarPlayerMainControls></SidebarPlayerMainControls>
     <SidebarPlayerSeekControl></SidebarPlayerSeekControl>
     <SidebarPlayerTrackActions></SidebarPlayerTrackActions>
@@ -18,6 +12,7 @@
 <script setup>
 import { default as SidebarPlayerAlbumCover } from "components/SidebarPlayerAlbumCover.vue";
 import { default as SidebarPlayerVolumeControl } from "components/SidebarPlayerVolumeControl.vue";
+import { default as SidebarPlayerTrackInfo } from "components/SidebarPlayerTrackInfo.vue";
 import { default as SidebarPlayerMainControls } from "components/SidebarPlayerMainControls.vue";
 import { default as SidebarPlayerSeekControl } from "components/SidebarPlayerSeekControl.vue";
 import { default as SidebarPlayerTrackActions } from "components/SidebarPlayerTrackActions.vue";
---
+++ quasar-frontend/src/components/SidebarPlayerTrackInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="song_info">
+    <p class="song_info_track text-center text-weight-bolder" style="color: #d30320;">Won’t Get Fooled Again</p>
+    <p class="song_info_album text-center">Who’s Next</p>
+    <p class="song_info_artist text-center"><a href="#/app/artist/The%20Who" style="text-decoration: none;">The Who</a>
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
