35d11122a141f9b0366a82b9a5a2d4dd619c875f
alex <766f6964+github@gmail.com>
2023-08-18 12:47:07 +0200

    added missing styles

--- quasar-frontend/src/components/SidebarPlayerTrackActions.vue
+++ quasar-frontend/src/components/SidebarPlayerTrackActions.vue
@@ -7,10 +7,23 @@
       <li class="text-h5"><q-icon name="shuffle" class="cursor-pointer" title="Toggle random sort"></q-icon></li>
       <li class="text-h5"><q-icon name="replay" class="cursor-pointer" title="Toggle repeat mode"></q-icon></li>
       <li class="text-h5"><q-icon name="file_download" class="cursor-pointer" title="Download track"></q-icon></li>
-      <li class="text-h5"><q-icon name="screenshot_monitor" class="cursor-pointer" title="Toggle section details"></q-icon></li>
+      <li class="text-h5"><q-icon name="screenshot_monitor" class="cursor-pointer"
+          title="Toggle section details"></q-icon></li>
     </ul>
   </div>
 </template>
+<style>
+div#current_track_actions {
+  padding: 1rem 4rem;
+}
 
+div#current_track_actions ul {
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  justify-content: space-between;
+}
+</style>
 <script setup>
 </script>
