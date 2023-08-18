8d29d800eacfe3103d7958ae156551767276ae12
alex <766f6964+github@gmail.com>
2023-08-18 13:50:08 +0200

    added chartist tests

--- quasar-frontend/src/pages/DashboardPage.vue
+++ quasar-frontend/src/pages/DashboardPage.vue
@@ -28,22 +28,49 @@
   </div>
   <DashboardBaseBlock :icon="'analytics'" :title="'Play statistics'">
     <template v-slot:body>
-      GRAPH
+      <q-tabs v-model="tab" no-caps class="text-pink-7">
+        <q-tab name="hour" label="By hour" />
+        <q-tab name="weekday" label="By weekday" />
+        <q-tab name="month" label="By month" />
+        <q-tab name="year" label="By year" />
+      </q-tabs>
+      <div class="ct-chart"></div>
     </template>
   </DashboardBaseBlock>
 </template>
+
+<style lang="scss">
+@import "~chartist/dist/index.css";
+</style>
 
 <script>
 import { defineComponent } from 'vue'
 import { default as leftSidebar } from 'components/AppLeftSidebar.vue';
 import { default as dashboardBaseBlock } from 'components/DashboardBaseBlock.vue';
-
+import { BarChart } from 'chartist';
 
 export default defineComponent({
   name: 'DashboardPage',
   components: {
     'leftSidebar': leftSidebar,
     'DashboardBaseBlock': dashboardBaseBlock
+  },
+  setup() {
+    const tab = 'weekday';
+    return( { tab });
+  },
+  mounted() {
+    new BarChart('.ct-chart', {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      series: [
+        [12, 9, 7, 8, 5, 4, 3]
+      ]
+    }, {
+      fullWidth: true,
+      chartPadding: {
+        right: 40
+      }
+    });
   }
 })
 </script>
