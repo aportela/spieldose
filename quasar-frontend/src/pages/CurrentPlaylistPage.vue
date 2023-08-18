6737c94a1ced6bb90b74d1ce5805bba5231e4c09
alex <766f6964+github@gmail.com>
2023-08-18 23:16:12 +0200

    added dummy track playlist

--- quasar-frontend/src/pages/CurrentPlaylistPage.vue
+++ quasar-frontend/src/pages/CurrentPlaylistPage.vue
@@ -1,16 +1,45 @@
 <template>
   <leftSidebar></leftSidebar>
-  <div>Current playlist</div>
+  <div class="q-pa-md">
+    <q-markup-table flat bordered>
+      <thead>
+        <tr>
+          <th class="text-right">Index</th>
+          <th class="text-left">Title</th>
+          <th class="text-left">Artist</th>
+          <th class="text-left">Album Artist</th>
+          <th class="text-left">Album</th>
+          <th class="text-right">Album Track nº</th>
+          <th class="text-right">Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="track, index in tracks" :key="track.id">
+          <td class="text-right">{{ index + 1 }}/32</td>
+          <td class="text-left">{{ track.title }}</td>
+          <td class="text-left">{{ track.artist }} <a href="#/app/artist/Texas" class=""><i class="fas fa-link ml-1"></i></a></td>
+          <td class="text-left">{{ track.albumArtist }} <span class="is-clickable"><i class="fas fa-link ml-1"></i></span></td>
+          <td class="text-left">{{ track.album }}<span class="is-clickable"><i class="fas fa-link ml-1"></i></span></td>
+          <td class="text-right">{{  track.trackNumber }}</td>
+          <td class="text-right">{{ track.year }}</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from "vue";
 import { default as leftSidebar } from 'components/AppLeftSidebar.vue';
+import { api } from 'boot/axios'
 
-export default defineComponent({
-  name: 'CurrentPlaylistPage',
-  components: {
-    'leftSidebar': leftSidebar
-  }
-})
+const tracks = ref([]);
+function search() {
+  api.track.search().then((success) => {
+    tracks.value = success.data.tracks;
+  }).catch((error) => {
+  });
+}
+search();
+
 </script>
