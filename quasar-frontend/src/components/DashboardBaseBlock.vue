f6304a8c4e065b508206ac7ba227836cda31c6b1
alex <766f6964+github@gmail.com>
2023-08-18 12:22:56 +0200

    added dashboard block templates

---
+++ quasar-frontend/src/components/DashboardBaseBlock.vue
@@ -0,0 +1,31 @@
+<template>
+<q-card class="my-card">
+  <q-card-section class="bg-grey-3 text-black">
+    <div class="text-h6"><q-icon :name="icon" class="cursor-pointer q-mr-sm"></q-icon>{{ title }}<q-icon name="refresh" class="cursor-pointer"></q-icon></div>
+  </q-card-section>
+
+  <q-card-section class="bg-white text-black">
+    <slot name="body">
+      <p>There is not enough data to calculate the statistics</p>
+    </slot>
+  </q-card-section>
+</q-card>
+</template>
+
+<script setup>
+
+import { defineProps } from "vue";
+
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
--- quasar-frontend/src/pages/DashboardPage.vue
+++ quasar-frontend/src/pages/DashboardPage.vue
@@ -1,16 +1,49 @@
 <template>
   <leftSidebar></leftSidebar>
   <div>Dashboard</div>
+
+  <div class="row q-col-gutter-xl">
+    <div class="col-4">
+      <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played tracks'"></DashboardBaseBlock>
+    </div>
+    <div class="col-4">
+      <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played albums'"></DashboardBaseBlock>
+    </div>
+    <div class="col-4">
+      <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played artists'"></DashboardBaseBlock>
+    </div>
+    <q-separator spaced />
+  </div>
+  <div class="row q-col-gutter-xl">
+    <div class="col-4">
+      <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played genres'"></DashboardBaseBlock>
+    </div>
+    <div class="col-4">
+      <DashboardBaseBlock :icon="'schedule'" :title="'Recently added'"></DashboardBaseBlock>
+    </div>
+    <div class="col-4">
+      <DashboardBaseBlock :icon="'schedule'" :title="'Recently added'"></DashboardBaseBlock>
+    </div>
+    <q-separator spaced />
+  </div>
+  <DashboardBaseBlock :icon="'analytics'" :title="'Play statistics'">
+    <template v-slot:body>
+      GRAPH
+    </template>
+  </DashboardBaseBlock>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import { default as leftSidebar } from 'components/AppLeftSidebar.vue';
+import { default as dashboardBaseBlock } from 'components/DashboardBaseBlock.vue';
+
 
 export default defineComponent({
   name: 'DashboardPage',
   components: {
-    'leftSidebar': leftSidebar
+    'leftSidebar': leftSidebar,
+    'DashboardBaseBlock': dashboardBaseBlock
   }
 })
 </script>
