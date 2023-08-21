65d4c53ff3927a3ed213b6c0d112a6c0b295acb4
alex <766f6964+github@gmail.com>
2023-08-21 13:15:39 +0200

    return album musicbrainz id

--- phpslim-api/Spieldose/Entities/Track.php
+++ phpslim-api/Spieldose/Entities/Track.php
@@ -9,7 +9,7 @@ class Track extends \Spieldose\Entities\Entity
     public static function search(\aportela\DatabaseWrapper\DB $dbh): array
     {
         $query = "
-            SELECT FIT.id, FIT.title, FIT.artist, FIT.album, FIT.album_artist AS albumArtist, FIT.year, FIT.track_number as trackNumber
+            SELECT FIT.id, FIT.title, FIT.artist, FIT.album, FIT.album_artist AS albumArtist, FIT.year, FIT.track_number as trackNumber, FIT.mb_album_id AS musicBrainzAlbumId
             FROM FILE_ID3_TAG FIT INNER JOIN FILE F ON F.ID = FIT.id
             ORDER BY RANDOM()
             LIMIT 32
