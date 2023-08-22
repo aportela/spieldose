612354bda2fdf234e97d27f523af799b346720de
alex <766f6964+github@gmail.com>
2023-08-22 11:18:49 +0200

    structure changed

--- quasar-frontend/src/pages/DashboardPage.vue
+++ quasar-frontend/src/pages/DashboardPage.vue
@@ -1,40 +1,45 @@
 <template>
   <q-page>
-    <div class="row q-col-gutter-xl">
-      <div class="col-4">
-        <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played tracks'"></DashboardBaseBlock>
+    <q-card class="q-pa-lg">
+      <q-breadcrumbs class="q-mb-lg">
+        <q-breadcrumbs-el icon="home" label="Spieldose" />
+        <q-breadcrumbs-el icon="analytics" label="Dashboard" />
+      </q-breadcrumbs>
+      <div class="row q-mb-lg">
+        <div class="col-xl-4 col-lg-4 col-12">
+          <component :is="dashboardBaseBlock" :icon="'format_list_numbered'" :title="'Top played tracks'"></component>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-12">
+          <component class="q-mx-lg" :is="dashboardBaseBlock" :icon="'format_list_numbered'" :title="'Top played albums'">
+          </component>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-12">
+          <component :is="dashboardBaseBlock" :icon="'format_list_numbered'" :title="'Top played artists'"></component>
+        </div>
       </div>
-      <div class="col-4">
-        <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played albums'"></DashboardBaseBlock>
+      <div class="row q-mb-lg">
+        <div class="col-xl-4 col-lg-4 col-12">
+          <component :is="dashboardBaseBlock" :icon="'format_list_numbered'" :title="'Top played genres'"></component>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-12">
+          <component class="q-mx-lg" :is="dashboardBaseBlock" :icon="'schedule'" :title="'Recently added'"></component>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-12">
+          <component :is="dashboardBaseBlock" :icon="'schedule'" :title="'Recently added'"></component>
+        </div>
       </div>
-      <div class="col-4">
-        <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played artists'"></DashboardBaseBlock>
-      </div>
-      <q-separator spaced />
-    </div>
-    <div class="row q-col-gutter-xl">
-      <div class="col-4">
-        <DashboardBaseBlock :icon="'format_list_numbered'" :title="'Top played genres'"></DashboardBaseBlock>
-      </div>
-      <div class="col-4">
-        <DashboardBaseBlock :icon="'schedule'" :title="'Recently added'"></DashboardBaseBlock>
-      </div>
-      <div class="col-4">
-        <DashboardBaseBlock :icon="'schedule'" :title="'Recently added'"></DashboardBaseBlock>
-      </div>
-      <q-separator spaced />
-    </div>
-    <DashboardBaseBlock :icon="'analytics'" :title="'Play statistics'">
-      <template v-slot:body>
-        <q-tabs v-model="tab" no-caps class="text-pink-7 q-mb-md">
-          <q-tab name="hour" label="By hour" />
-          <q-tab name="weekday" label="By weekday" />
-          <q-tab name="month" label="By month" />
-          <q-tab name="year" label="By year" />
-        </q-tabs>
-        <div class="ct-chart"></div>
-      </template>
-    </DashboardBaseBlock>
+      <component :is="dashboardBaseBlock" :icon="'analytics'" :title="'Play statistics'">
+        <template v-slot:body>
+          <q-tabs v-model="tab" no-caps class="text-pink-7 q-mb-md">
+            <q-tab name="hour" label="By hour" />
+            <q-tab name="weekday" label="By weekday" />
+            <q-tab name="month" label="By month" />
+            <q-tab name="year" label="By year" />
+          </q-tabs>
+          <div class="ct-chart"></div>
+        </template>
+      </component>
+    </q-card>
   </q-page>
 </template>
 
@@ -42,32 +47,24 @@
 @import "~chartist/dist/index.css";
 </style>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref, onMounted } from "vue";
 import { default as dashboardBaseBlock } from 'components/DashboardBaseBlock.vue';
 import { BarChart } from 'chartist';
 
-export default defineComponent({
-  name: 'DashboardPage',
-  components: {
-    'DashboardBaseBlock': dashboardBaseBlock
-  },
-  setup() {
-    const tab = 'weekday';
-    return ({ tab });
-  },
-  mounted() {
-    new BarChart('.ct-chart', {
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      series: [
-        [12, 9, 7, 8, 5, 4, 3]
-      ]
-    }, {
-      fullWidth: true,
-      chartPadding: {
-        right: 40
-      }
-    });
-  }
-})
+const tab = ref('weekday');
+
+onMounted(() => {
+  new BarChart('.ct-chart', {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    series: [
+      [12, 9, 7, 8, 5, 4, 3]
+    ]
+  }, {
+    fullWidth: true,
+    chartPadding: {
+      right: 40
+    }
+  });
+});
 </script>
