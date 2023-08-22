11323b3cf057dfb250820aadc326c3119a327b15
alex <766f6964+github@gmail.com>
2023-08-22 11:19:01 +0200

    removed empty space

--- quasar-frontend/src/components/DashboardBaseBlock.vue
+++ quasar-frontend/src/components/DashboardBaseBlock.vue
@@ -3,7 +3,6 @@
   <q-card-section class="bg-grey-3 text-black">
     <div class="text-h6"><q-icon :name="icon" class="cursor-pointer q-mr-sm"></q-icon>{{ title }}<q-icon name="refresh" class="cursor-pointer"></q-icon></div>
   </q-card-section>
-
   <q-card-section class="bg-white text-black">
     <slot name="body">
       <p>There is not enough data to calculate the statistics</p>
