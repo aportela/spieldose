fdf164a2004ec93c4d410811cee915652cf0682a
alex <766f6964+github@gmail.com>
2023-08-22 11:19:52 +0200

    enable artist navigation

--- quasar-frontend/src/components/SidebarPlayerTrackInfo.vue
+++ quasar-frontend/src/components/SidebarPlayerTrackInfo.vue
@@ -2,8 +2,10 @@
   <div id="song_info">
     <p class="song_info_track text-center text-weight-bolder" style="color: #d30320;">{{ currentTrack ? currentTrack.title: "No title" }}</p>
     <p class="song_info_album text-center">{{ currentTrack ? currentTrack.album: "No album" }}</p>
-    <p class="song_info_artist text-center"><a href="#/app/artist/The%20Who" style="text-decoration: none;">{{ currentTrack ? currentTrack.artist: "No artist" }}</a>
-    </p>
+    <p class="song_info_artist text-center">
+        <router-link v-if="currentTrack && currentTrack.artist" style="text-decoration: none;" :to="{ name: 'artist', params: { name: currentTrack.artist }}" >{{ currentTrack.artist }}</router-link>
+        <span v-else>No artist</span>
+      </p>
   </div>
 </template>
 
