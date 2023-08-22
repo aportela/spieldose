cbd13d5a7f3d067963850e21cd932d6572a83655
alex <766f6964+github@gmail.com>
2023-08-22 11:12:41 +0200

    testing some header toolbars

--- quasar-frontend/src/layouts/MainLayout.vue
+++ quasar-frontend/src/layouts/MainLayout.vue
@@ -1,6 +1,38 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
+      <!--
+      <q-header elevated>
+        <q-toolbar class="bg-pink text-white">
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <q-toolbar-title>Spieldose</q-toolbar-title>
+          <q-btn flat round dense icon="whatshot" />
+        </q-toolbar>
+        <q-toolbar class="bg-grey-10 text-white">
+          <q-img src="images/vinyl.png" width="90px" height="90px" />
+          <q-toolbar-title>
+            <div class="row">
+              <div class="col">
+                <h5>Title<br>Artist</h5>
+              </div>
+              <div class="col"></div>
+              <div class="q-pa-md q-gutter-sm text-center">
+                <q-btn round dense size="md" :disable="disabled || !allowSkipPrevious" @click="onSkipPrevious"><q-icon
+                    name="skip_previous" title="Toggle navigation menu"></q-icon></q-btn>
+                <q-btn round dense size="lg" :disable="disabled || !allowPlay" @click="onPlay" class="q-mx-md"><q-icon
+                    :name="isPlaying ? 'pause' : 'play_arrow'" title="Toggle navigation menu"
+                    :class="{ 'text-pink-6': isPlaying }"></q-icon></q-btn>
+                <q-btn round dense size="md" :disable="disabled || !allowSkipNext" @click="onSkipNext"><q-icon
+                    name="skip_next" title="Toggle navigation menu"></q-icon></q-btn>
+              </div>
+            </div>
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+      -->
       <div class="row q-gutter-lg q-pa-lg">
         <div class="col" style="width: 400px; max-width: 400px;">
           <leftSidebar></leftSidebar>
