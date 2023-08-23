8a0d593bc5d2816b37a73f182d0828db11f67227
alex <766f6964+github@gmail.com>
2023-08-23 11:21:47 +0200

    new artists page

--- quasar-frontend/src/pages/BrowseArtistsPage.vue
+++ quasar-frontend/src/pages/BrowseArtistsPage.vue
@@ -1,13 +1,51 @@
 <template>
   <q-page>
-    <div>Artists</div>
+    <q-card class="q-pa-lg">
+      <q-breadcrumbs class="q-mb-lg">
+        <q-breadcrumbs-el icon="home" label="Spieldose" />
+        <q-breadcrumbs-el icon="person" label="Browse artists" />
+      </q-breadcrumbs>
+
+      <q-card-section>
+        <div class="q-gutter-sm row items-start" v-if="artists">
+            <router-link :to="{ name: 'artist', params: { name: artist.name }}" v-for="artist in artists" :key="artist">
+            <q-img :src="artist.image || '#'" width="300px" height="300px" fit="cover">
+              <div class="absolute-bottom text-subtitle1 text-center">
+              {{ artist.name }}
+              </div>
+              <template v-slot:error>
+                <div class="absolute-full flex flex-center bg-grey-3 text-dark">
+                  <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
+                    {{ artist.name }}
+                  </div>
+                </div>
+              </template>
+            </q-img>
+            </router-link>
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
 
-export default defineComponent({
-  name: 'BrowseArtistsPage',
-})
+import { ref, watch, computed } from "vue";
+import { api } from 'boot/axios'
+
+const loading = ref(false);
+const artists = ref([]);
+
+function search() {
+  loading.value = true;
+  api.artist.search(0, 32, { }).then((success) => {
+    artists.value = success.data.artists;
+    loading.value = false;
+  }).catch((error) => {
+    loading.value = false;
+  });
+}
+
+search();
+
 </script>
