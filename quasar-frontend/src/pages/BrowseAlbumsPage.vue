c311a7d10c62b83f2b2443690bf65d53e75b473a
alex <766f6964+github@gmail.com>
2023-08-23 11:21:41 +0200

    new albums page

--- quasar-frontend/src/pages/BrowseAlbumsPage.vue
+++ quasar-frontend/src/pages/BrowseAlbumsPage.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page>
-    <div>Albums</div>
+    <q-card class="q-pa-lg">
+      <q-breadcrumbs class="q-mb-lg">
+        <q-breadcrumbs-el icon="home" label="Spieldose" />
+        <q-breadcrumbs-el icon="album" label="Browse albums" />
+      </q-breadcrumbs>
+    </q-card>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'BrowseAlbumsPage',
-})
+<script setup>
 </script>
