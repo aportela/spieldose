21559dad94a210e39c470efa30914a953a00d014
alex <766f6964+github@gmail.com>
2023-08-23 11:34:06 +0200

    removed (for now) sidebar (drawer) menu

--- quasar-frontend/src/components/AppLeftSidebar.vue
+++ quasar-frontend/src/components/AppLeftSidebar.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-card>
+  <q-card class="q-ma-lg">
     <leftSidebarPlayer></leftSidebarPlayer>
+    <!--
     <leftSidebarMenu></leftSidebarMenu>
+    -->
   </q-card>
 </template>
 
 <script setup>
 
 import { default as leftSidebarPlayer } from 'components/SidebarPlayer.vue';
-import { default as leftSidebarMenu } from 'components/SidebarMenu.vue';
+//import { default as leftSidebarMenu } from 'components/SidebarMenu.vue';
 
 </script>
