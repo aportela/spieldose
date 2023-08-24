907668b8bee262c8e6b6a7be6eec7ef2c0f8623d
alex <766f6964+github@gmail.com>
2023-08-25 00:10:24 +0200

    removed whitespace

--- quasar-frontend/src/pages/BrowseArtistsPage.vue
+++ quasar-frontend/src/pages/BrowseArtistsPage.vue
@@ -17,7 +17,6 @@
             <q-icon name="search" class="cursor-pointer" @click="search" />
           </template>
         </q-input>
-
         <div class="q-pa-lg flex flex-center" v-if="totalPages > 1">
           <q-pagination v-model="currentPageIndex" color="dark" :max="totalPages" :max-pages="5" boundary-numbers
             direction-links boundary-links @update:model-value="onPaginationChanged" :disable="loading" />
