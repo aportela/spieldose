c846ffdd80e1708d57e23bf5c68810f506803f41
alex <766f6964+github@gmail.com>
2023-08-25 12:16:20 +0200

    create thumbnail basePath if not exists

--- phpslim-api/tools/scrap.php
+++ phpslim-api/tools/scrap.php
@@ -138,6 +138,16 @@ if (count($missingExtensions) > 0) {
                 }
             }
             if ($scrapArtistsThumbnails) {
+                if (!file_exists($settings['thumbnails']['artists']['basePath'])) {
+                    if (!mkdir($settings['thumbnails']['artists']['basePath'], 0750, true)) {
+                        $logger->critical("Error creating artist thumbnail basePath: " . $settings['thumbnails']['artists']['basePath']);
+                    }
+                }
+                if (!file_exists($settings['thumbnails']['albums']['basePath'])) {
+                    if (!mkdir($settings['thumbnails']['albums']['basePath'], 0750, true)) {
+                        $logger->critical("Error creating album thumbnail basePath: " . $settings['thumbnails']['albums']['basePath']);
+                    }
+                }
                 $artistImageURLs = $scraper->getArtistsImageURLs();
                 $totalArtistsImageURLs = count($artistImageURLs);
                 echo sprintf("Processing %d artist images%s", $totalArtistsImageURLs, PHP_EOL);
