966fa9447827e9c9e1b1c10c602e27d21f73c5c8
alex <766f6964+github@gmail.com>
2023-09-09 01:09:32 +0200

    temporal fix (white not done on server) for missing metrics gaps

--- quasar-frontend/src/components/DashboardBaseBlockChart.vue
+++ quasar-frontend/src/components/DashboardBaseBlockChart.vue
@@ -83,21 +83,38 @@ const chartOptions = {
 function drawChart() {
   // TODO: labels are not reactive on i18n changes
   let labels = [];
-  const values = items.value.map((item) => { return (item.total); });
+  let values = [];
+  //  items.value.map((item) => { return (item.total); });
   switch (tab.value) {
     case 'hour':
-      labels = items.value.map((item) => { return (item.hour); });
+      labels = [ '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20','21','22', '23'];
+      values = new Array(24).fill(0);
+      items.value.forEach((item) => {
+        values[new Number(item.hour) ] = item.total;
+      });
       break;
     case 'weekday':
       labels = [t('Sunday'), t('Monday'), t('Tuesday'), t('Wednesday'), t('Thursday'), t('Friday'), t('Saturday')];
+      values = new Array(7).fill(0);
+      // TODO: check correct index starting on sunday
+      items.value.forEach((item) => {
+        values[new Number(item.weekday) ] = item.total;
+      });
       break;
     case 'month':
       labels = [t('January'), t('February'), t('March'), t('April'), t('May'), t('June'), t('July'), t('August'), t('September'), t('October'), t('November'), t('December')]
+      values = new Array(12).fill(0);
+      items.value.forEach((item) => {
+        values[new Number(item.month) ] = item.total;
+      });
       break;
     case 'year':
       labels = items.value.map((item) => { return (item.year); });
+      values = items.value.map((item) => { return (item.total); });
       break;
   }
+  console.log(labels);
+  console.log(values);
   if (labels.length > 1) {
     new LineChart('.ct-chart', {
       labels: labels,
