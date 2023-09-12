d45f5a7340362b509574a141f9ee4a39790b1092
alex <766f6964+github@gmail.com>
2023-09-12 13:31:10 +0200

    moved q-table to table (works better with hunded & thousand elements)

--- quasar-frontend/src/pages/CurrentPlaylistPage.vue
+++ quasar-frontend/src/pages/CurrentPlaylistPage.vue
@@ -5,59 +5,72 @@
       <q-breadcrumbs-el icon="list_alt" :label="t('Current playlist')" />
     </q-breadcrumbs>
     <q-btn-group spread class="q-mb-md">
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Clear') : ''" icon="clear"
-        @click="onClear" :disable="loading || !(elements && elements.length > 0)">
+      <q-btn outline color="dark" :label="t('Clear')" icon="clear" @click="onClear"
+        :disable="loading || !(elements && elements.length > 0)">
       </q-btn>
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Randomize') : ''" icon="bolt"
-        @click="onRandom" :disable="loading">
+      <q-btn outline color="dark" :label="t('Randomize')" icon="bolt" @click="onRandom" :disable="loading">
       </q-btn>
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Previous') : ''"
-        icon="skip_previous" @click="onPreviusPlaylist" :disable="loading || !currentPlaylist.allowSkipPrevious" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Play') : ''" icon="play_arrow"
-        @click="onPlay" :disable="loading || !currentPlaylist.hasElements" v-if="playerStatus.isStopped" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Pause') : ''" icon="pause"
-        @click="onPause" :disable="loading || !(elements && elements.length > 0)" v-else-if="playerStatus.isPlaying" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Resume') : ''" icon="play_arrow"
-        @click="onResume" :disable="loading || !(elements && elements.length > 0)" v-else-if="playerStatus.isPaused" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Stop') : ''" icon="stop"
-        @click="onStop" :disable="loading || playerStatus.isStopped || !(elements && elements.length > 0)" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Next') : ''" icon="skip_next"
-        @click="onNextPlaylist" :disable="loading || !currentPlaylist.allowSkipNext" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Download') : ''" icon="save_alt"
+      <q-btn outline color="dark" :label="t('Previous')" icon="skip_previous" @click="onPreviusPlaylist"
+        :disable="loading || !currentPlaylist.allowSkipPrevious" />
+      <q-btn outline color="dark" :label="t('Play')" icon="play_arrow" @click="onPlay"
+        :disable="loading || !currentPlaylist.hasElements" v-if="playerStatus.isStopped" />
+      <q-btn outline color="dark" :label="t('Pause')" icon="pause" @click="onPause"
+        :disable="loading || !(elements && elements.length > 0)" v-else-if="playerStatus.isPlaying" />
+      <q-btn outline color="dark" :label="t('Resume')" icon="play_arrow" @click="onResume"
+        :disable="loading || !(elements && elements.length > 0)" v-else-if="playerStatus.isPaused" />
+      <q-btn outline color="dark" :label="t('Stop')" icon="stop" @click="onStop"
+        :disable="loading || playerStatus.isStopped || !(elements && elements.length > 0)" />
+      <q-btn outline color="dark" :label="t('Next')" icon="skip_next" @click="onNextPlaylist"
+        :disable="loading || !currentPlaylist.allowSkipNext" />
+      <q-btn outline color="dark" :label="t('Download')" icon="save_alt"
         :disable="loading || !currentPlaylist.getCurrentElementURL" :href="currentPlaylist.getCurrentElementURL" />
-      <q-btn :stack="$q.screen.lt.sm" outline color="dark" :label="$q.screen.gt.md ? t('Save as') : ''" icon="save"
+      <q-btn outline color="dark" :label="t('Save as')" icon="save_alt"
         :disable="loading || !(elements && elements.length > 0)" @click="onSavePlaylist" />
     </q-btn-group>
-    <q-markup-table flat bordered>
-      <thead>
-        <tr class="bg-grey-2 text-grey-10">
-          <th class="text-right">{{ t('Index') }}</th>
-          <th class="text-left">{{ t('Title') }}</th>
-          <th class="text-left">{{ t('Artist') }}</th>
-          <th class="text-left gt-lg">{{ t('Album Artist') }}</th>
-          <th class="text-left">{{ t('Album') }}</th>
-          <th class="text-right gt-lg">{{ t('Album Track nº') }}</th>
-          <th class="text-right">{{ t('Year') }}</th>
-          <th class="text-center">{{ t('Actions') }}</th>
-        </tr>
-      </thead>
-      <tbody v-if="!loading">
-        <CurrentPlaylistTableRow v-for="track, index in elements" :key="track.id" :element="track" :index="index"
-          :lastIndex="elements.length" :selected="currentTrackIndex == index"
-          @setcurrentIndex="setCurrentTrackIndex(index)" @up="onMoveUpTrackAtIndex(index)"
-          @down="onMoveDownTrackAtIndex(index)" @toggleFavorite="onToggleFavoriteAtIndex(index)"
-          @remove="onRemoveElementAtIndex(index)" :disabled="loading" :isPlaying="playerStatus.isPlaying"
-          :isPaused="playerStatus.isPaused" :isStopped="playerStatus.isStopped">
-        </CurrentPlaylistTableRow>
-      </tbody>
-      <tbody v-else>
-        <tr>
-          <td colspan="8" class="text-center">
-            <q-spinner v-show="loading" color="pink" size="xl" class="q-ml-sm" :thickness="10" />
-          </td>
-        </tr>
-      </tbody>
-    </q-markup-table>
+    <q-table class="my-sticky-header-table" style="height: 46.8em" title="Current playlist" :rows="rows"
+      :columns="columns" row-key="id" virtual-scroll :rows-per-page-options="[0]" :visible-columns="visibleColumns"
+      @row-click="onRowClick">
+      <template v-slot:top>
+        <q-space></q-space>
+        <q-select v-model="visibleColumns" multiple outlined dense options-dense :display-value="$q.lang.table.columns"
+          emit-value map-options :options="columns" option-value="name" options-cover style="min-width: 150px" />
+      </template>
+      <template v-slot:body-cell-index="props">
+        <q-td :props="props">
+          <q-icon :name="rowIcon" color="pink" size="sm" class="q-mr-sm" v-if="currentTrackIndex + 1 == props.value"></q-icon>
+          {{ props.value }} / {{ rows.length }}
+        </q-td>
+      </template>
+      <template v-slot:body-cell-artist="props">
+        <q-td :props="props">
+          <router-link v-if="props.value" :class="{ 'text-white text-bold': false }"
+            :to="{ name: 'artist', params: { name: props.value } }"><q-icon name="link" class="q-mr-sm"></q-icon>{{
+              props.value }}</router-link>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-albumArtist="props">
+        <q-td :props="props">
+          <router-link v-if="props.value" :class="{ 'text-white text-bold': false }"
+            :to="{ name: 'artist', params: { name: props.value } }"><q-icon name="link" class="q-mr-sm"></q-icon>{{
+              props.value }}</router-link>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          {{ props.value }}
+          <q-btn-group outline>
+            <q-btn size="sm" color="white" text-color="grey-5" icon="north" :title="t('Up')" disable
+              @click="onMoveUpTrackAtIndex" />
+            <q-btn size="sm" color="white" text-color="grey-5" icon="south" :title="t('Down')" disable
+              @click="onMoveDownTrackAtIndex" />
+            <q-btn size="sm" color="white" text-color="grey-5" icon="favorite" :title="t('Toggle favorite')" disable />
+            <q-btn size="sm" color="white" text-color="grey-5" icon="delete" :title="t('Remove')" disable
+              @click="onRemoveElementAtIndex" />
+          </q-btn-group>
+        </q-td>
+      </template>
+    </q-table>
+
   </q-card>
   <q-dialog v-model="showSavePlaylistDialog">
     <q-card style="min-width: 350px">
@@ -82,6 +95,34 @@
   </q-dialog>
 </template>
 
+<style lang="sass">
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 310px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #eeeeee
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+
+  /* prevent scrolling behind sticky top row on focus */
+  tbody
+    /* height of all previous header rows */
+    scroll-margin-top: 48px
+</style>
+
 <script setup>
 import { ref, watch, computed } from "vue";
 import { useQuasar, uid } from "quasar";
@@ -101,6 +142,97 @@ const currentPlaylist = useCurrentPlaylistStore();
 
 const currentPlayListElementsLastChanges = computed(() => {
   return (currentPlaylist.getElementsLastChangeTimestamp);
+});
+
+const rows = ref([]);
+const columns = [
+  {
+    name: 'index',
+    required: true,
+    label: 'Índice',
+    align: 'right',
+    field: row => row.index,
+    sortable: false
+  },
+  {
+    name: 'title',
+    required: true,
+    label: 'Title',
+    align: 'left',
+    field: row => row.title,
+    sortable: true
+  },
+  {
+    name: 'artist',
+    required: true,
+    label: 'Artist',
+    align: 'left',
+    field: row => row.artist.name,
+    sortable: true
+  },
+  {
+    name: 'albumArtist',
+    required: true,
+    label: 'Album artist',
+    align: 'left',
+    field: row => row.album.artist.name,
+    sortable: true
+  },
+  {
+    name: 'albumTitle',
+    required: true,
+    label: 'Album',
+    align: 'left',
+    field: row => row.album.title,
+    sortable: true
+  },
+  {
+    name: 'albumTrackIndex',
+    required: true,
+    label: 'Album Track nº',
+    align: 'right',
+    field: row => row.trackNumber,
+    sortable: true
+  },
+  {
+    name: 'year',
+    required: true,
+    label: 'Year',
+    align: 'right',
+    field: row => row.album.year,
+    sortable: true
+  },
+  {
+    name: 'actions',
+    required: false,
+    label: 'Actions',
+    align: 'center',
+    field: row => {
+      index: row.index
+    }
+  },
+];
+
+const visibleColumns = ref(columns.map((column) => { return (column.name); }));
+
+const initialPagination = ref({
+  //sortBy: 'desc',
+  descending: false,
+  page: 1,
+  rowsPerPage: 64
+  // rowsNumber: xx if getting data from a server
+});
+
+const rowIcon = computed(() => {
+  if (playerStatus.isPlaying) {
+    return ('play_arrow');
+  } else if (playerStatus.isPaused) {
+    return ('pause');
+  } else if (playerStatus.isStopped) {
+    return ('stop');
+  } else {
+    return ('play_arrow');
+  }
 });
 
 watch(currentPlayListElementsLastChanges, (newValue) => {
@@ -126,6 +258,7 @@ const newPlaylistPublic = ref(false);
 function onClear() {
   player.stop();
   elements.value = [];
+  rows.value = [];
   currentPlaylist.saveElements([]);
 }
 
@@ -179,9 +312,18 @@ function onToggleFavoriteAtIndex(index) {
   }
 }
 
+
 function onRemoveElementAtIndex(index) {
   elements.value.splice(index, 1);
   currentPlaylist.saveElements(elements.value);
+}
+
+function onRowClick(evt, row, index) {
+  player.interact();
+  currentPlaylist.saveCurrentTrackIndex(index);
+  if (!playerStatus.isPlaying) {
+    player.play();
+  }
 }
 
 function search() {
@@ -190,6 +332,7 @@ function search() {
   currentTrackIndex.value = 0;
   api.track.search({}, 1, 32, true, null, null).then((success) => {
     elements.value = success.data.data.items.map((item) => { return ({ track: item }); });
+    rows.value = elements.value.map((element, index) => { element.track.index = index + 1; return (element.track) });
     currentPlaylist.saveElements(elements.value);
     loading.value = false;
   }).catch((error) => {
@@ -271,6 +414,7 @@ function onSavePlaylistElements() {
 }
 
 elements.value = currentPlaylist.getElements;
+rows.value = elements.value.map((element, index) => { element.track.index = index + 1; return (element.track) });
 currentTrackIndex.value = currentPlaylist.getCurrentIndex;
 
 </script>
