cd4bc6a728bd1b86781ae685ae8c5b06427aeb8e
alex <766f6964+github@gmail.com>
2023-09-24 00:04:32 +0200

    fixed v-memo directive and use new element (server) hash as key (v-for)

--- quasar-frontend/src/pages/BrowseArtistsPage.vue
+++ quasar-frontend/src/pages/BrowseArtistsPage.vue
@@ -4,12 +4,12 @@
       <q-breadcrumbs-el icon="home" label="Spieldose" />
       <q-breadcrumbs-el icon="person" label="Browse artists" />
     </q-breadcrumbs>
-    <q-card-section v-if="artists">
-      <div class="row q-gutter-xs">
+    <q-card-section>
+      <div class="row q-gutter-xs q-mb-md">
         <div class="col">
           <q-input v-model="artistName" clearable type="search" outlined dense placeholder="Text condition"
             hint="Search artists with name" :loading="loading" @keydown.enter.prevent="search(true)"
-            @clear="noArtistsFound = false; search(true)" :error="noArtistsFound"
+            @clear="artistsNotFound = false; search(true)" :error="artistsNotFound"
             :errorMessage="'No artists found with specified condition'" :disable="loading" ref="artistNameRef">
             <template v-slot:prepend>
               <q-icon name="filter_alt" />
@@ -22,20 +22,14 @@
         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-4" v-if="genresValues && genresValues.length > 0">
           <q-select outlined dense v-model="genre" :options="filteredGenres" options-dense label="Genre"
             :disable="loading || route.params.genre != null" emit-value filled clearable=""
-            :hint="! genre ? 'Minimum 3 characters to trigger filtering': 'Filtering by genre'" use-input hide-selected input-debounce="0" @filter="onFilterGenres" @update:model-value="search(true)">
+            :hint="!genre ? 'Minimum 3 characters to trigger filtering' : 'Filtering by genre'" use-input hide-selected
+            input-debounce="0" @filter="onFilterGenres" @update:model-value="search(true)">
             <template v-slot:selected>
-          <q-chip
-            v-if="genre"
-            dense
-            square
-            color="white"
-            text-color="primary"
-            class="q-my-none q-ml-xs q-mr-none"
-          >
-            {{ genre.name }}
-          </q-chip>
-          <q-badge v-else>*none*</q-badge>
-        </template>
+              <q-chip v-if="genre" dense square color="white" text-color="primary" class="q-my-none q-ml-xs q-mr-none">
+                {{ genre.name }}
+              </q-chip>
+              <q-badge v-else>*none*</q-badge>
+            </template>
             <template v-slot:no-option>
               <q-item>
                 <q-item-section class="text-grey">
@@ -62,40 +56,42 @@
           </q-select>
         </div>
       </div>
-      <!-- TODO margins when no pager -->
-      <div class="q-pa-lg flex flex-center" v-if="totalPages > 1">
-        <q-pagination v-model="currentPageIndex" color="dark" :max="totalPages" :max-pages="5" boundary-numbers
-          direction-links boundary-links @update:model-value="onPaginationChanged" :disable="loading" />
-      </div>
-      <div class="q-gutter-md row items-start" v-memo="artists">
-        <router-link :to="{ name: 'artist', params: { name: artist.name } }" v-for="artist in artists" :key="artist">
-          <q-img img-class="artist_image" :src="artist.image || '#'" width="250px" height="250px" fit="cover">
-            <div class="absolute-bottom text-subtitle1 text-center">
-              {{ artist.name }}
-              <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
-                'track') }}</p>
-            </div>
-            <template v-slot:loading>
-              <div class="absolute-full flex flex-center bg-grey-3 text-dark">
-                <q-spinner color="pink" size="xl" />
-                <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
-                  {{ artist.name }}
-                  <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
-                    'track') }}</p>
-                </div>
+      <div v-if="artists && artists.length > 0">
+        <div class="flex flex-center" v-if="totalPages > 1">
+          <q-pagination v-model="currentPageIndex" color="dark" :max="totalPages" :max-pages="5" boundary-numbers
+            direction-links boundary-links @update:model-value="onPaginationChanged" :disable="loading" />
+        </div>
+        <div class="q-mt-md q-gutter-md row items-start">
+          <router-link :to="{ name: 'artist', params: { name: artist.name } }" v-for="artist in artists"
+            :key="artist.hash" v-memo="[lastChangesTimestamp]">
+            <q-img img-class="artist_image" :src="artist.image || '#'" width="250px" height="250px" fit="cover">
+              <div class="absolute-bottom text-subtitle1 text-center">
+                {{ artist.name }}
+                <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
+                  'track') }}</p>
               </div>
-            </template>
-            <template v-slot:error>
-              <div class="absolute-full flex flex-center bg-grey-3 text-dark">
-                <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
-                  {{ artist.name }}
-                  <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
-                    'track') }}</p>
+              <template v-slot:loading>
+                <div class="absolute-full flex flex-center bg-grey-3 text-dark">
+                  <q-spinner color="pink" size="xl" />
+                  <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
+                    {{ artist.name }}
+                    <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
+                      'track') }}</p>
+                  </div>
                 </div>
-              </div>
-            </template>
-          </q-img>
-        </router-link>
+              </template>
+              <template v-slot:error>
+                <div class="absolute-full flex flex-center bg-grey-3 text-dark">
+                  <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
+                    {{ artist.name }}
+                    <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
+                      'track') }}</p>
+                  </div>
+                </div>
+              </template>
+            </q-img>
+          </router-link>
+        </div>
       </div>
     </q-card-section>
   </q-card>
@@ -129,9 +125,12 @@ import { useQuasar } from "quasar";
 
 const $q = useQuasar();
 const artistName = ref(null);
-const noArtistsFound = ref(false);
+const artistsNotFound = ref(false);
 const loading = ref(false);
-let artists = [];
+
+const artists = ref([]);
+
+const lastChangesTimestamp = ref(0);
 
 const route = useRoute();
 
@@ -177,25 +176,29 @@ function search(resetPager) {
   if (resetPager) {
     currentPageIndex.value = 1;
   }
-  noArtistsFound.value = false;
+  artistsNotFound.value = false;
   loading.value = true;
-  api.artist.search( { genre: genre.value || null, name: artistName.value || null }, currentPageIndex.value, 32, sortField.value.value, sortOrder.value.value).then((success) => {
-    artists = success.data.data.items;
+
+  api.artist.search({ genre: genre.value || null, name: artistName.value || null }, currentPageIndex.value, 32, sortField.value.value, sortOrder.value.value).then((success) => {
+    artists.value = success.data.data.items;
     totalPages.value = success.data.data.pager.totalPages;
-    if (artistName.value && success.data.data.pager.totalResults < 1) {
-      noArtistsFound.value = true;
+    if (success.data.data.pager.totalResults < 1) {
+      artistsNotFound.value = true;
     }
+    loading.value = false;
+    lastChangesTimestamp.value = Date.now();
     nextTick(() => {
       artistNameRef.value.$el.focus();
     });
-    loading.value = false;
   }).catch((error) => {
+    artists.value = [];
     $q.notify({
       type: "negative",
       message: "API Error: error loading artists",
       caption: "API Error: fatal error details: HTTP {" + error.response.status + "} ({" + error.response.statusText + "})"
     });
     loading.value = false;
+    lastChangesTimestamp.value = Date.now();
   });
 }
 
