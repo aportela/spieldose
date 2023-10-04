23df60c8339390a0cb4146a5281e6c8fdefe3f67
alex <766f6964+github@gmail.com>
2023-10-04 02:05:09 +0200

    fixed route

--- quasar-frontend/src/components/ArtistGenreChip.vue
+++ quasar-frontend/src/components/ArtistGenreChip.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="{ name: 'artistsByGenre', params: { genre: name }}">
+  <router-link :to="{ name: 'artistsPaged', params: { page: 1 }, query: { genre: name }}">
     <q-chip size="md" icon="tag" truncate-chip-labels>{{ name }}</q-chip>
   </router-link>
 </template>
