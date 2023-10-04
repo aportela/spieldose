8fac9fc4af7e10adef88d98e3a7fb40df4020b79
alex <766f6964+github@gmail.com>
2023-10-04 02:05:16 +0200

    removed unused route

--- quasar-frontend/src/router/routes.js
+++ quasar-frontend/src/router/routes.js
@@ -40,11 +40,6 @@ const routes = [
         component: () => import("pages/BrowseArtistsPage.vue"),
       },
       {
-        name: "artistsByGenre",
-        path: "artists/genre/:genre",
-        component: () => import("pages/BrowseArtistsPage.vue"),
-      },
-      {
         name: "artist",
         path: "artist/:name/:tab",
         component: () => import("pages/ArtistPage.vue"),
