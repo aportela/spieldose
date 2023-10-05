914517298f19133e059826e6f076b62bc9837212
alex <766f6964+github@gmail.com>
2023-10-05 12:00:37 +0200

    use new browse component

--- quasar-frontend/src/pages/BrowseArtistsPage.vue
+++ quasar-frontend/src/pages/BrowseArtistsPage.vue
@@ -1,85 +1,83 @@
 <template>
-  <q-card class="q-pa-lg">
-    <q-breadcrumbs class="q-mb-lg">
-      <q-breadcrumbs-el icon="home" label="Spieldose" />
+  <BrowserBase :disable="loading" :currentPageIndex="currentPageIndex" :totalPages="totalPages"
+    :totalResults="totalResults" @paginationChanged="onPaginationChanged">
+    <template #breacrumb>
       <q-breadcrumbs-el icon="person" :label="t('Browse artists')" />
-    </q-breadcrumbs>
-    <q-card-section>
+    </template>
+    <template #filter>
       <div class="row q-gutter-xs q-mb-md">
         <div class="col">
-          <q-input v-model="artistName" clearable type="search" outlined dense :placeholder="t('Type text condition')"
-            :hint="t('Search by artist name')" :loading="loading" @keydown.enter.prevent="onChangeName" @clear="search"
-            :error="noItemsFound" :errorMessage="t('No results found with the specified condition filter')"
-            :disable="loading" ref="artistNameRef">
+          <q-input v-model="name" clearable type="search" outlined dense :placeholder="t('Type text condition')"
+            :hint="t('Search by artist name')" :loading="loading" @keydown.enter.prevent="onNameChanged" @clear="search"
+            :error="warningNoItems" :errorMessage="t('No results found with the specified condition filter')"
+            :disable="loading" ref="nameRef">
             <template v-slot:prepend>
               <q-icon name="filter_alt" />
             </template>
             <template v-slot:append>
-              <q-icon name="search" class="cursor-pointer" @click="onChangeName" />
+              <q-icon name="search" class="cursor-pointer" @click="onNameChanged" />
             </template>
           </q-input>
         </div>
         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-4">
-          <ArtistsGenreSelector :disable="loading" :defaultGenre="filterByGenre" @change="onChangeGenre"></ArtistsGenreSelector>
+          <ArtistsGenreSelector :disable="loading" :defaultGenre="filterByGenre" @change="onGenreChanged">
+          </ArtistsGenreSelector>
         </div>
         <div class="col-xl-1 col-lg-2 col-md-3 col-sm-4 col-xs-4">
-          <SortFieldSelector :disable="loading" :options="sortFieldOptions" :field="sortField" @change="onChangeSortField">
+          <SortFieldSelector :disable="loading" :options="sortFieldOptions" :field="sortField"
+            @change="onSortFieldChanged">
           </SortFieldSelector>
         </div>
         <div class="col-xl-1 col-lg-2 col-md-3 col-sm-4 col-xs-4">
-          <SortOrderSelector :disable="loading" :order="sortOrder" @change="onChangeSortOrder"></SortOrderSelector>
+          <SortOrderSelector :disable="loading" :order="sortOrder" @change="onSortOrderChanged"></SortOrderSelector>
         </div>
       </div>
-      <div v-if="artists && artists.length > 0">
-        <div class="flex flex-center" v-if="totalPages > 1">
-          <q-pagination v-model="currentPageIndex" color="dark" :max="totalPages" :max-pages="5" boundary-numbers
-            direction-links boundary-links @update:model-value="onPaginationChanged" :disable="loading" />
-        </div>
-        <div class="q-mt-md q-gutter-md row items-start">
-          <router-link
-            :to="{ name: artist.mbId && artist.name ? 'mbArtist' : 'artist', params: { mbid: artist.mbId, name: artist.name, tab: 'overview' } }"
-            v-for="artist in artists" :key="artist.hash" v-memo="[lastChangesTimestamp]">
-            <q-img img-class="artist_image" :src="artist.image || '#'" width="250px" height="250px" fit="cover">
-              <div class="absolute-bottom text-subtitle1 text-center">
+    </template>
+    <template #items>
+      <router-link
+        :to="{ name: artist.mbId && artist.name ? 'mbArtist' : 'artist', params: { mbid: artist.mbId, name: artist.name, tab: 'overview' } }"
+        v-for="artist in artists" :key="artist.hash" v-memo="[lastChangesTimestamp]">
+        <q-img img-class="sp-artist-image-filter" :src="artist.image || '#'" width="250px" height="250px" fit="cover">
+          <div class="absolute-bottom text-subtitle1 text-center">
+            {{ artist.name }}
+            <p class="text-caption q-mb-none">{{ artist.totalTracks }} {{ t(artist.totalTracks > 1 ? "tracks" : "track")
+            }}
+            </p>
+          </div>
+          <template v-slot:loading>
+            <div class="absolute-full flex flex-center bg-grey-3 text-dark">
+              <q-spinner color="pink" size="xl" />
+              <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
                 {{ artist.name }}
-                <p class="text-caption q-mb-none">{{ artist.totalTracks }} {{ t(artist.totalTracks > 1 ? "tracks": "track") }}</p>
+                <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
+                  'track') }}</p>
               </div>
-              <template v-slot:loading>
-                <div class="absolute-full flex flex-center bg-grey-3 text-dark">
-                  <q-spinner color="pink" size="xl" />
-                  <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
-                    {{ artist.name }}
-                    <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
-                      'track') }}</p>
-                  </div>
-                </div>
-              </template>
-              <template v-slot:error>
-                <div class="absolute-full flex flex-center bg-grey-3 text-dark">
-                  <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
-                    {{ artist.name }}
-                    <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
-                      'track') }}</p>
-                  </div>
-                </div>
-              </template>
-            </q-img>
-          </router-link>
-        </div>
-      </div>
-    </q-card-section>
-  </q-card>
+            </div>
+          </template>
+          <template v-slot:error>
+            <div class="absolute-full flex flex-center bg-grey-3 text-dark">
+              <div class="absolute-bottom text-subtitle1 text-center bg-grey-5 q-py-md">
+                {{ artist.name }}
+                <p class="text-caption q-mb-none">{{ artist.totalTracks + " " + (artist.totalTracks > 1 ? 'tracks' :
+                  'track') }}</p>
+              </div>
+            </div>
+          </template>
+        </q-img>
+      </router-link>
+    </template>
+  </BrowserBase>
 </template>
 
 <style>
-img.artist_image {
+img.sp-artist-image-filter {
   -webkit-filter: grayscale(100%) blur(4px) opacity(0.5);
   /* Safari 6.0 - 9.0 */
   filter: grayscale(100%) blur(4px) opacity(0.5);
   transition: filter 0.2s ease-in;
 }
 
-img.artist_image:hover {
+img.sp-artist-image-filter:hover {
   -webkit-filter: none;
   /* Safari 6.0 - 9.0 */
   filter: none;
@@ -95,18 +93,19 @@ import { useRoute, useRouter } from "vue-router";
 import { api } from "boot/axios";
 import { useQuasar } from "quasar";
 import { useI18n } from "vue-i18n";
+import { default as BrowserBase } from "components/BrowserBase.vue";
 import { default as ArtistsGenreSelector } from "components/ArtistsGenreSelector.vue";
 import { default as SortFieldSelector } from "components/SortFieldSelector.vue";
 import { default as SortOrderSelector } from "components/SortOrderSelector.vue";
 
-const { t } = useI18n();
 const $q = useQuasar();
+const { t } = useI18n();
 
 const route = useRoute();
 const router = useRouter();
 
-const artistNameRef = ref(null);
-const artistName = ref(route.query.q || null);
+const nameRef = ref(null);
+const name = ref(route.query.q || null);
 const filterByGenre = ref(route.query.genre || null);
 const sortFieldOptions = computed(() => [
   {
@@ -120,24 +119,25 @@ const sortFieldOptions = computed(() => [
 ]);
 
 const sortField = ref(sortFieldOptions.value[0].value);
-const sortOrder = ref(null);
-const noItemsFound = ref(false);
+const sortOrder = ref(route.query.sortOrder == "DESC" ? "DESC": "ASC");
+const warningNoItems = ref(false);
 const loading = ref(false);
 const artists = ref([]);
 const lastChangesTimestamp = ref(0);
 
 const totalPages = ref(0);
+const totalResults = ref(0);
 const currentPageIndex = ref(parseInt(route.query.page || 1));
 
 router.beforeEach(async (to, from) => {
-  if (from.name == "artists" || from.name == "artistsPaged") {
+  if (from.name == "artists") {
     currentPageIndex.value = parseInt(to.query.page || 1);
     filterByGenre.value = to.query.genre || null;
-    artistName.value = to.query.q || null;
-    sortOrder.value = to.query.sortOrder == "DESC" ? to.query.sortOrder : "ASC";
+    name.value = to.query.q || null;
+    sortOrder.value = to.query.sortOrder == "DESC" ? "DESC" : "ASC";
     sortField.value = sortFieldOptions.value[to.query.sortField == "totalTracks" ? 1 : 0].value;
     if (
-      (to.name == "artists" || to.name == "artistsPaged") &&
+      (to.name == "artists") &&
       (
         to.query.page != from.query.page ||
         to.query != from.query ||
@@ -153,10 +153,10 @@ router.beforeEach(async (to, from) => {
   }
 });
 
-function refreshURL(pageIndex, artistName, selectedGenre, sortField, sortOrder) {
+function refreshURL(pageIndex, name, selectedGenre, sortField, sortOrder) {
   const query = Object.assign({}, route.query || {});
   query.page = pageIndex || 1;
-  query.q = artistName || null;
+  query.q = name || null;
   query.genre = selectedGenre || null;
   query.sortField = sortField || "name";
   query.sortOrder = sortOrder || "ASC";
@@ -167,42 +167,45 @@ function refreshURL(pageIndex, artistName, selectedGenre, sortField, sortOrder) 
 }
 
 function onPaginationChanged(pageIndex) {
-  refreshURL(pageIndex, artistName.value, filterByGenre.value, sortField.value, sortOrder.value);
+  refreshURL(pageIndex, name.value, filterByGenre.value, sortField.value, sortOrder.value);
 }
 
-function onChangeName() {
-  refreshURL(1, artistName.value, filterByGenre.value, sortField.value, sortOrder.value);
+function onNameChanged() {
+  refreshURL(1, name.value, filterByGenre.value, sortField.value, sortOrder.value);
 }
 
-function onChangeGenre(selectedGenre) {
-  refreshURL(1, artistName.value, selectedGenre, sortField.value, sortOrder.value);
+function onGenreChanged(selectedGenre) {
+  refreshURL(1, name.value, selectedGenre, sortField.value, sortOrder.value);
 }
 
-function onChangeSortField(selectedSortField) {
-  refreshURL(currentPageIndex.value, artistName.value, filterByGenre.value, selectedSortField, sortOrder.value);
+function onSortFieldChanged(selectedSortField) {
+  refreshURL(currentPageIndex.value, name.value, filterByGenre.value, selectedSortField, sortOrder.value);
 }
 
-function onChangeSortOrder(selectedSortOrder) {
-  refreshURL(currentPageIndex.value, artistName.value, filterByGenre.value, sortField.value, selectedSortOrder);
+function onSortOrderChanged(selectedSortOrder) {
+  refreshURL(currentPageIndex.value, name.value, filterByGenre.value, sortField.value, selectedSortOrder);
 }
 
 function search() {
-  noItemsFound.value = false;
+  warningNoItems.value = false;
   loading.value = true;
-  api.artist.search({ genre: filterByGenre.value || null, name: artistName.value || null }, currentPageIndex.value, 32, sortField.value, sortOrder.value).then((success) => {
+  api.artist.search({ genre: filterByGenre.value || null, name: name.value || null }, currentPageIndex.value, 32, sortField.value, sortOrder.value).then((success) => {
     artists.value = success.data.data.items;
     totalPages.value = success.data.data.pager.totalPages;
-    noItemsFound.value = success.data.data.pager.totalResults < 1;
+    totalResults.value = success.data.data.pager.totalResults;
+    warningNoItems.value = success.data.data.pager.totalResults < 1;
     loading.value = false;
     lastChangesTimestamp.value = Date.now();
     nextTick(() => {
-      artistNameRef.value.focus();
+      nameRef.value.focus();
     });
   }).catch((error) => {
     artists.value = [];
+    totalPages.value = 0;
+    totalResults.value = 0;
     $q.notify({
       type: "negative",
-      message: "API Error: error loading artists",
+      message: t("API Error: error loading artists"),
       caption: t("API Error: fatal error details", { status: error.response.status, statusText: error.response.statusText })
     });
     loading.value = false;
