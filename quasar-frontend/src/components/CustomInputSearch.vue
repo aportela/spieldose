10a45340d49c66fd1c79d8334f71f7e778a741cd
alex <766f6964+github@gmail.com>
2023-10-13 11:18:52 +0200

    added loading property

--- quasar-frontend/src/components/CustomInputSearch.vue
+++ quasar-frontend/src/components/CustomInputSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <q-input v-model="value" clearable type="search" outlined dense :placeholder="t(placeholder)" :hint="t(hint)"
-    :loading="disable && value?.length > 0" :disable="disable" @keydown.enter.prevent="onSubmit" @clear="value = null"
-    :error="error && value?.length > 0" :errorMessage="t(errorMessage)" ref="inputRef">
+    :loading="loading" :disable="disable" @keydown.enter.prevent="onSubmit" @clear="value = null"
+    :error="error" :errorMessage="t(errorMessage)" ref="inputRef">
     <template v-slot:prepend>
       <q-icon name="filter_alt" />
     </template>
@@ -16,7 +16,7 @@
 import { ref, computed } from "vue";
 import { useI18n } from "vue-i18n";
 
-const props = defineProps(['hint', 'placeholder', 'error', 'errorMessage', 'modelValue', 'disable']);
+const props = defineProps(['hint', 'placeholder', 'error', 'errorMessage', 'modelValue', 'disable', 'loading']);
 const emit = defineEmits(['update:modelValue', 'submit']);
 
 const { t } = useI18n();
