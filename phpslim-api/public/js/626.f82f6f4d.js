a32b22282c7a610ff81f8c69979722bceb6e03df
alex <766f6964+github@gmail.com>
2024-01-31 13:13:32 +0100

    new build

--- phpslim-api/public/js/626.f82f6f4d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis["webpackChunkspieldose"]=globalThis["webpackChunkspieldose"]||[]).push([[626],{626:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});var a=r(9835);function i(e,s){return(0,a.wg)(),(0,a.iD)("p")}var l=r(1639);const n={},u=(0,l.Z)(n,[["render",i]]),o=u}}]);
