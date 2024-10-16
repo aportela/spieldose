3977a80a27e7365160d56822507c86e760ee8ef2
alex <766f6964+github@gmail.com>
2024-10-16 16:50:02 +0200

    fixed session lost with valid jwt

--- phpslim-api/Spieldose/Middleware/JWT.php
+++ phpslim-api/Spieldose/Middleware/JWT.php
@@ -36,7 +36,7 @@ class JWT
             if (isset($decoded) && isset($decoded->data) && isset($decoded->data->userId) && isset($decoded->data->email)) {
                 $this->logger->notice("JWT valid data decoded", [print_r($decoded->data, true)]);
                 $user = new \Spieldose\User($decoded->data->userId);
-                if (!$user->exists($this->dbh)) {
+                if ($user->exists($this->dbh)) {
                     \Spieldose\UserSession::set($decoded->data->userId, $decoded->data->email);
                 } else {
                     throw new \Spieldose\Exception\NotFoundException("userId");
