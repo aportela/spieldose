e3422bead47c89e41ee5af509e61bbdc268e0335
alex <766f6964+github@gmail.com>
2025-10-20 12:04:21 +0200

    php-stan warnings fixed (missing param types)

--- phpslim-api/Spieldose/Logger/BaseLogger.php
+++ phpslim-api/Spieldose/Logger/BaseLogger.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Spieldose\Logger;
 
+use Stringable;
+
 class BaseLogger implements \Psr\Log\LoggerInterface
 {
     private \Psr\Log\LoggerInterface $logger;
@@ -13,47 +15,47 @@ class BaseLogger implements \Psr\Log\LoggerInterface
         $this->logger = $logger;
     }
 
-    public function emergency($message, array $context = []): void
+    public function emergency(string|Stringable $message, array $context = []): void
     {
         $this->logger->emergency($message, $context);
     }
 
-    public function alert($message, array $context = []): void
+    public function alert(string|Stringable $message, array $context = []): void
     {
         $this->logger->alert($message, $context);
     }
 
-    public function critical($message, array $context = []): void
+    public function critical(string|Stringable $message, array $context = []): void
     {
         $this->logger->critical($message, $context);
     }
 
-    public function error($message, array $context = []): void
+    public function error(string|Stringable $message, array $context = []): void
     {
         $this->logger->error($message, $context);
     }
 
-    public function warning($message, array $context = []): void
+    public function warning(string|Stringable $message, array $context = []): void
     {
         $this->logger->warning($message, $context);
     }
 
-    public function notice($message, array $context = []): void
+    public function notice(string|Stringable $message, array $context = []): void
     {
         $this->logger->notice($message, $context);
     }
 
-    public function info($message, array $context = []): void
+    public function info(string|Stringable $message, array $context = []): void
     {
         $this->logger->info($message, $context);
     }
 
-    public function debug($message, array $context = []): void
+    public function debug(string|Stringable $message, array $context = []): void
     {
         $this->logger->debug($message, $context);
     }
 
-    public function log($level, $message, array $context = []): void
+    public function log(mixed $level, string|Stringable $message, array $context = []): void
     {
         $this->logger->log($level, $message, $context);
     }
