e24e2e35530f8a4b7b9d3af7a19bec342548714a
alex <766f6964+github@gmail.com>
2025-10-20 11:34:05 +0200

    new exception for JSON serializer errors

---
+++ phpslim-api/Spieldose/Exception/JSONSerializerException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spieldose\Exception;
+
+/**
+ * json serializer exception
+ */
+class JSONSerializerException extends \Exception
+{
+    public function __construct(string $message = "", int $code = 0, ?\Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
