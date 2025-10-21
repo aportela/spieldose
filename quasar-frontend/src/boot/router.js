cf90befb5d85feae9435ac1c11a8f26d79805b47
alex <766f6964+github@gmail.com>
2025-10-21 17:08:12 +0200

    fixed not found route

--- quasar-frontend/src/boot/router.js
+++ quasar-frontend/src/boot/router.js
@@ -1,46 +1,30 @@
 import { boot } from "quasar/wrappers";
 import { useSessionStore } from "stores/session";
 
+const session = useSessionStore();
+
 export default boot(({ app, router, store }) => {
   router.beforeEach((to, from, next) => {
-    const session = useSessionStore(store);
+    if (!to.matched.length) {
+      next({ name: "notFound" });
+      return;
+    }
+
     if (!session.isLoaded) {
       session.load();
     }
 
-    const isLogged = session.isLogged;
-
-    // TODO: not found page
-    if (isLogged) {
-      if (!to.name) {
-        // no route => dashboard
-        next({
-          name: "dashboard",
-        });
+    if (session.isLogged) {
+      if (to.name === "signIn" || to.name === "signUp") {
+        next({ name: "dashboard" });
       } else {
-        if (to.name !== "signIn" && to.name != "signUp") {
-          // go to specified route
-          next();
-        } else {
-          next({
-            name: "dashboard",
-          });
-        }
+        next();
       }
     } else {
-      if (!to.name) {
-        // no route => signIn
-        next({
-          name: "signIn",
-        });
-      } else if (to.name !== "signIn" && to.name != "signUp") {
-        // not allowed route  (! signin && !signup)
-        next({
-          name: "signIn",
-        });
-      } else {
-        // allowed route (signin || signup)
+      if (to.name === "signIn" || to.name === "signUp") {
         next();
+      } else {
+        next({ name: "signIn" });
       }
     }
   });
