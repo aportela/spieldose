c384ad5fa9a1c3480d13890e00b4465ea88dc8d8
alex <766f6964+github@gmail.com>
2025-10-22 00:08:33 +0200

    custom banner components cloned from homedocs

---
+++ quasar-frontend/src/components/Banners/CustomBanner.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-banner dense rounded class="theme-default-q-banner"
+    :class="{ 'theme-default-q-banner-success': success, 'theme-default-q-banner-error': error, 'theme-default-q-banner-warning': warning }">
+    <template v-slot:avatar v-if="success || error || warning">
+      <q-icon size="sm" v-if="success" name="check" class="text-green" />
+      <q-icon size="sm" v-else-if="error" name="error" class="text-red" />
+      <q-icon size="sm" v-else-if="warning" name="warning" class="text-warning" />
+    </template>
+    <slot name="text">
+      {{ t(text) }}
+    </slot>
+    <slot name="details"></slot>
+  </q-banner>
+</template>
+
+<script setup>
+
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: false,
+  },
+  success: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  error: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  warning: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+});
+
+</script>
---
+++ quasar-frontend/src/components/Banners/CustomErrorBanner.vue
@@ -0,0 +1,30 @@
+<template>
+  <CustomBanner :text="text" error>
+    <template v-slot:details v-if="initialState.isDevEnvironment && apiError">
+      <APIErrorDetails class="q-mt-md" :api-error="apiError"></APIErrorDetails>
+    </template>
+  </CustomBanner>
+</template>
+
+<script setup>
+
+import { useInitialStateStore } from "src/stores/initialState";
+
+import { default as CustomBanner } from "src/components/Banners/CustomBanner.vue";
+import { default as APIErrorDetails } from "src/components/APIErrorDetails.vue";
+
+const initialState = useInitialStateStore();
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  apiError: {
+    type: Object,
+    required: false,
+    default: null
+  }
+});
+
+</script>
