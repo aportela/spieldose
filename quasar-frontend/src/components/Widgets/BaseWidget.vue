fd5d7d477b4ffddc7d0a69e13cbe015743e14e04
alex <766f6964+github@gmail.com>
2025-10-22 18:09:34 +0200

    new custom base widget component cloned from homedocs :-)

---
+++ quasar-frontend/src/components/Widgets/BaseWidget.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-card class="full-width">
+    <q-item class="theme-default-q-card-section-header">
+      <q-item-section avatar>
+        <q-icon v-if="loading" name="settings" class="animation-spin"></q-icon>
+        <q-icon v-else-if="error" name="error" color="red" @click.stop="onHeaderIconClicked" :class="iconClass">
+          <DesktopToolTip v-if="iconToolTip">{{ t(iconToolTip) }}</DesktopToolTip>
+        </q-icon>
+        <q-icon v-else :name="icon" @click.stop="onHeaderIconClicked" :class="iconClass">
+          <DesktopToolTip v-if="iconToolTip">{{ t(iconToolTip) }}</DesktopToolTip>
+        </q-icon>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ t(title) }}</q-item-label>
+        <q-item-label caption>{{ t(caption) }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-card-section>
+      <slot name="content"></slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+
+import { computed } from "vue";
+
+import { useI18n } from "vue-i18n";
+
+import { default as DesktopToolTip } from "src/components/DesktopToolTip.vue";
+
+const { t } = useI18n();
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  caption: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  iconToolTip: {
+    type: String,
+    required: false
+  },
+  onHeaderIconClick: {
+    type: Function,
+    default: null
+  },
+  loading: {
+    type: Boolean,
+    required: false
+  },
+  error: {
+    type: Boolean,
+    required: false
+  }
+});
+
+const iconClass = computed(() => !!props.onHeaderIconClick ? "cursor-pointer" : "cursor-default");
+
+const onHeaderIconClicked = () => {
+  if (props.onHeaderIconClick && typeof props.onHeaderIconClick === 'function') {
+    props.onHeaderIconClick();
+  }
+}
+
+</script>
