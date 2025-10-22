cbd6267d09ba80b4293793dbdd161b3f84888a2f
alex <766f6964+github@gmail.com>
2025-10-22 11:26:32 +0200

    new layout/pages for signin/signup

---
+++ quasar-frontend/src/layouts/AuthRegisterLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout>
+    <div class="row">
+      <div
+        class="col-xs-10 offset-xs-1 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-0 col-xl-3 offset-xl-0 desktop-only justify-center q-pa-xl">
+        <q-page-container>
+          <router-view />
+        </q-page-container>
+      </div>
+      <div class="gt-md col-lg-8 col-xl-9">
+        <TileAlbumImages></TileAlbumImages>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+import { default as TileAlbumImages } from "components/TileAlbumImages.vue";
+
+const { t } = useI18n();
+
+</script>
--- quasar-frontend/src/pages/SignInPage.vue
+++ quasar-frontend/src/pages/SignInPage.vue
@@ -1,183 +1,23 @@
 <template>
-  <q-card class="q-pa-md my_card">
-    <form @submit.prevent.stop="onValidateForm" autocorrect="off" autocapitalize="off" autocomplete="off"
-      spellcheck="false" style="padding: 1.25rem;">
-      <q-card-section class="text-center">
-        <h3>{{ $t('Spieldose') }}</h3>
-        <div class="text-grey-8">{{ $t('Sign in below to access your account') }}</div>
-      </q-card-section>
-      <q-card-section>
-        <q-input dense outlined ref="emailRef" v-model="email" type="email" name="email" :label="t('Email')"
-          :disable="loading" :autofocus="true" :rules="requiredFieldRules" lazy-rules
-          :error="remoteValidation.email.hasErrors" :errorMessage="remoteValidation.email.message">
-          <template v-slot:prepend>
-            <q-icon name="alternate_email" />
-          </template>
-        </q-input>
-        <q-input dense outlined class="q-mt-md" ref="passwordRef" v-model="password" name="password" type="password"
-          :label="t('Password')" :disable="loading" :rules="requiredFieldRules" lazy-rules
-          :error="remoteValidation.password.hasErrors" :errorMessage="remoteValidation.password.message">
-          <template v-slot:prepend>
-            <q-icon name="key" />
-          </template>
-        </q-input>
-      </q-card-section>
-      <q-card-section>
-        <q-btn color="dark" size="md" :label="$t('Sign in')" no-caps class="full-width" icon="account_circle"
-          :disable="loading || (!(email && password))" :loading="loading" type="submit">
-          <template v-slot:loading>
-            <q-spinner-hourglass class="on-left" />
-            {{ t("Loading...") }}
-          </template>
-        </q-btn>
-      </q-card-section>
-      <q-card-section class="text-center q-pt-none">
-        <div class="text-grey-8">
-          {{ t("Don't have an account yet ?") }}
-          <router-link :to="{ name: 'signUp' }">
-            <span class="text-dark text-weight-bold" style="text-decoration: none">{{
-              t("Click here to sign up") }}</span>
-          </router-link>
-        </div>
-      </q-card-section>
-    </form>
-  </q-card>
+  <q-page>
+    <q-card class="q-pa-xl" flat>
+      <CSSAnimatedAudioEqualizer></CSSAnimatedAudioEqualizer>
+      <SignInForm @success="onSuccessSignIn"></SignInForm>
+    </q-card>
+  </q-page>
 </template>
 
 <script setup>
-
-import { ref, nextTick, inject } from "vue";
-import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
-import { useI18n } from "vue-i18n";
-import { useAPI } from "src/composables/useAPI";
-import { useSpieldoseStore } from "stores/spieldose";
-
-const { t } = useI18n();
-
-const $q = useQuasar();
+import { default as SignInForm } from "src/components/Forms/SignInForm.vue";
+import { default as CSSAnimatedAudioEqualizer } from "components/CSSAnimatedAudioEqualizer.vue";
 
 const router = useRouter();
 
-const spieldoseStore = useSpieldoseStore();
-
-const { api } = useAPI();
-
-const loading = ref(false);
-
-const remoteValidation = ref({
-  email: {
-    hasErrors: false,
-    message: null
-  },
-  password: {
-    hasErrors: false,
-    message: null
-  }
-});
-
-const requiredFieldRules = [
-  val => !!val || t('Field is required')
-];
-
-const email = ref(null);
-const emailRef = ref(null);
-
-const password = ref(null);
-const passwordRef = ref(null);
-
-function onResetForm() {
-  remoteValidation.value.email.hasErrors = false;
-  remoteValidation.value.email.message = null;
-  remoteValidation.value.password.hasErrors = false;
-  remoteValidation.value.password.message = null;
-  emailRef.value.resetValidation();
-  passwordRef.value.resetValidation();
-}
-
-function onValidateForm() {
-  onResetForm();
-  emailRef.value.validate();
-  passwordRef.value.validate();
-  nextTick(() => {
-    if (!(emailRef.value.hasError || passwordRef.value.hasError)) {
-      onSubmitForm();
-    }
+const onSuccessSignIn = (apiResponseData) => {
+  router.push({
+    name: "dashboard",
   });
-}
-
-function onSubmitForm() {
-  loading.value = true;
-  api.user
-    .signIn(email.value, password.value)
-    .then((success) => {
-      spieldoseStore.interact();
-      router.push({
-        name: "dashboard",
-      });
-      loading.value = false;
-    })
-    .catch((error) => {
-      loading.value = false;
-      switch (error.response.status) {
-        case 400:
-          if (
-            error.response.data.invalidOrMissingParams.find(function (e) {
-              return e === "email";
-            })
-          ) {
-            $q.notify({
-              type: "negative",
-              message: t("API Error: missing email param"),
-            });
-            emailRef.value.focus();
-          } else if (
-            error.response.data.invalidOrMissingParams.find(function (e) {
-              return e === "password";
-            })
-          ) {
-            $q.notify({
-              type: "negative",
-              message: t("API Error: missing password param"),
-            });
-            passwordRef.value.focus();
-          } else {
-            $q.notify({
-              type: "negative",
-              message: t("API Error: invalid/missing param"),
-            });
-          }
-          break;
-        case 404:
-          remoteValidation.value.email.hasErrors = true;
-          remoteValidation.value.email.message = t("Email not registered");
-          nextTick(() => {
-            emailRef.value.focus();
-          });
-          break;
-        case 401:
-          remoteValidation.value.password.hasErrors = true;
-          remoteValidation.value.password.message = t("Invalid password");
-          nextTick(() => {
-            passwordRef.value.focus();
-          });
-          break;
-        case 410:
-          remoteValidation.value.email.hasErrors = true;
-          remoteValidation.value.email.message = t("Account has been deleted");
-          nextTick(() => {
-            emailRef.value.focus();
-          });
-          break;
-        default:
-          $q.notify({
-            type: "negative",
-            message: t("API Error: fatal error"),
-            caption: t("API Error: fatal error details", { status: error.response.status, statusText: error.response.statusText })
-          });
-          break;
-      }
-    });
-}
+};
 
 </script>
