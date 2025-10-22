d3efdda0aeb454482cd747cd49c103a30b971263
alex <766f6964+github@gmail.com>
2025-10-22 11:27:05 +0200

    new layout for 404

---
+++ quasar-frontend/src/layouts/ErrorNotFoundLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout>
+  </q-layout>
+</template>
+
+<script setup>
+
+import { useI18n } from "vue-i18n";
+
+import { default as DarkModeButton } from "src/components/Buttons/DarkModeButton.vue"
+import { default as SwitchLanguageButton } from "src/components/Buttons/SwitchLanguageButton.vue"
+import { default as GitHubButton } from "src/components/Buttons/GitHubButton.vue"
+import { GITHUB_PROJECT_URL } from "src/constants"
+
+const { t } = useI18n();
+
+</script>
+
+<style lang="css" scoped></style>
