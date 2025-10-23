39d73811e7eb750f3d056a60e1d55560f344ac82
alex <766f6964+github@gmail.com>
2025-10-24 00:49:22 +0200

    new enum for id3 tag types

---
+++ phpslim-api/Spieldose/Library/ID3TAGType.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spieldose\Library;
+
+enum ID3TAGType
+{
+    case TITLE;
+    case TRACK_ARTIST_NAME;
+    case ALBUM_ARTIST_NAME;
+    case ALBUM;
+    case GENRE;
+    case TRACK_NUMBER;
+    case DISC_NUMBER;
+    case YEAR;
+    case PLAYTIME_SECONDS;
+    case PLAYTIME_STRING;
+    case BITRATE;
+    case MIME_TYPE;
+    case MB_ARTIST_ID;
+    case MB_ALBUM_ID;
+    case MB_ALBUM_ARTIST_ID;
+    case MB_RELEASE_GROUP_ID;
+    case MB_RELEASE_TRACK_ID;
+}
