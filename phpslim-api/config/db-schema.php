60e2570afd179a8764e256743c2bb0ed34fd39f6
alex <766f6964+github@gmail.com>
2025-10-24 00:08:15 +0200

    table field renamed

--- phpslim-api/config/db-schema.php
+++ phpslim-api/config/db-schema.php
@@ -65,7 +65,7 @@ return (array(
 
             CREATE TABLE QUEUE_FILE_ID3_SCAN (
                 file_id CHAR(36) NOT NULL,
-                ctime INTEGER NOT NULL,
+                created_on_timestamp INTEGER NOT NULL,
                 PRIMARY KEY (file_id),
                 FOREIGN KEY(file_id) REFERENCES FILE(id) ON DELETE CASCADE
             );
