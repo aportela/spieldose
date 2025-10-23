acd437d77d382fce2706656711008da10ee04315
alex <766f6964+github@gmail.com>
2025-10-23 23:47:32 +0200

    new table for id3 scan file queue

--- phpslim-api/config/db-schema.php
+++ phpslim-api/config/db-schema.php
@@ -62,6 +62,13 @@ return (array(
                 PRIMARY KEY (file_id),
                 FOREIGN KEY(file_id) REFERENCES FILE(id) ON DELETE CASCADE
             );
+
+            CREATE TABLE QUEUE_FILE_ID3_SCAN (
+                file_id CHAR(36) NOT NULL,
+                ctime INTEGER NOT NULL,
+                PRIMARY KEY (file_id),
+                FOREIGN KEY(file_id) REFERENCES FILE(id) ON DELETE CASCADE
+            );
         '
     )
 ));
