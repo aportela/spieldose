a888bc506f76d1cb142118241441821ed231bc4f
alex <766f6964+github@gmail.com>
2025-11-13 15:01:38 +0100

    __toString method removed, added param type

--- phpslim-api/Spieldose/Exception/JSONSerializerException.php
+++ phpslim-api/Spieldose/Exception/JSONSerializerException.php
@@ -13,9 +13,4 @@ class JSONSerializerException extends \Exception
     {
         parent::__construct($message, $code, $previous);
     }
-
-    public function __toString()
-    {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-    }
 }
--- phpslim-api/Spieldose/Exception/UnauthorizedException.php
+++ phpslim-api/Spieldose/Exception/UnauthorizedException.php
@@ -9,13 +9,8 @@ namespace Spieldose\Exception;
  */
 class UnauthorizedException extends \Exception
 {
-    public function __construct($message = "", $code = 0, ?\Exception $previous = null)
+    public function __construct(string $message = "", int $code = 0, ?\Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
-    }
-
-    public function __toString()
-    {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 }
