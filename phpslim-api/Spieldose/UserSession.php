96d6ea19910d92795d68199d5b4c97a5f36b8076
alex <766f6964+github@gmail.com>
2025-11-13 15:30:01 +0100

    warnings fixed (php-stan)

--- phpslim-api/Spieldose/UserSession.php
+++ phpslim-api/Spieldose/UserSession.php
@@ -11,6 +11,7 @@ class UserSession
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
+
         $_SESSION["userId"] = $userId;
         $_SESSION["email"] = $email;
     }
@@ -20,26 +21,27 @@ class UserSession
         if (session_status() !== PHP_SESSION_NONE) {
             $_SESSION = [];
             session_unset();
-            if (ini_get("session.use_cookies") && PHP_SAPI != 'cli') {
+            if (ini_get("session.use_cookies") && PHP_SAPI !== 'cli' && is_string(session_name())) {
                 $params = session_get_cookie_params();
-                setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+                setcookie(session_name(), '', ['expires' => time() - 42000, 'path' => $params["path"], 'domain' => $params["domain"], 'secure' => $params["secure"], 'httponly' => $params["httponly"]]);
             }
+
             session_destroy();
         }
     }
 
     public static function isLogged(): bool
     {
-        return isset($_SESSION["userId"]) && !empty($_SESSION["userId"]);
+        return array_key_exists("userId", $_SESSION) && is_string($_SESSION["userId"]);
     }
 
     public static function getUserId(): string
     {
-        return $_SESSION["userId"] ?? '';
+        return array_key_exists("userId", $_SESSION) && is_string($_SESSION["userId"]) ? $_SESSION["userId"] : '';
     }
 
     public static function getEmail(): string
     {
-        return $_SESSION["email"] ?? '';
+        return array_key_exists("email", $_SESSION) && is_string($_SESSION["email"]) ? $_SESSION["email"] : '';
     }
 }
