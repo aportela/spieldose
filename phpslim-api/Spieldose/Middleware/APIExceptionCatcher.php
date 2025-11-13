ad13e999ca0d3a0a95dd33ffabf30dcb0ef61861
alex <766f6964+github@gmail.com>
2025-11-13 15:04:23 +0100

    applied rector withPreparedSets

--- phpslim-api/Spieldose/Middleware/APIExceptionCatcher.php
+++ phpslim-api/Spieldose/Middleware/APIExceptionCatcher.php
@@ -6,66 +6,79 @@ namespace Spieldose\Middleware;
 
 class APIExceptionCatcher
 {
-    protected $logger;
-
-    public function __construct(\Spieldose\Logger\HTTPRequestLogger $logger)
-    {
-        $this->logger = $logger;
-    }
+    public function __construct(protected \Psr\Log\LoggerInterface $logger) {}
 
     /**
      * @param array<mixed> $payload
      */
-    private function handleException(\Exception $e, int $statusCode, array $payload): \Psr\Http\Message\ResponseInterface
+    private function handleException(\Exception $exception, int $statusCode, array $payload): \Psr\Http\Message\ResponseInterface
     {
-        $this->logger->debug(sprintf("Exception caught (%s) - Message: %s", get_class($e), $e->getMessage()));
+        $this->logger->error(sprintf("Exception caught (%s) - Message: %s", $exception::class, $exception->getMessage()));
         $response = new \Slim\Psr7\Response();
-        $response->getBody()->write(json_encode($payload));
+        $json = json_encode($payload);
+        if (is_string($json)) {
+            $response->getBody()->write($json);
+        } else {
+            $this->logger->error("Error serializing payload", $payload);
+            $response->getBody()->write("{}");
+        }
+
         return $response->withStatus($statusCode);
     }
 
-    private function handleGenericException(\Throwable $e): \Psr\Http\Message\ResponseInterface
+    private function handleGenericException(\Throwable $throwable): \Psr\Http\Message\ResponseInterface
     {
-        $this->logger->error(sprintf("Unhandled exception caught (%s) - Message: %s", get_class($e), $e->getMessage()));
-        $this->logger->debug($e->getTraceAsString());
+        $this->logger->error(sprintf("Unhandled exception caught (%s) - Message: %s", $throwable::class, $throwable->getMessage()));
+        $this->logger->debug($throwable->getTraceAsString());
+
         $response = new \Slim\Psr7\Response();
         $exception = [
-            'type' => get_class($e),
-            'message' => $e->getMessage(),
-            'file' => $e->getFile(),
-            'line' => $e->getLine()
+            'type' => $throwable::class,
+            'message' => $throwable->getMessage(),
+            'file' => $throwable->getFile(),
+            'line' => $throwable->getLine()
         ];
-        $parent = $e->getPrevious();
-        if ($parent) {
+        $parent = $throwable->getPrevious();
+        if ($parent instanceof \Throwable) {
             $exception['parent'] = [
-                'type' => get_class($parent),
+                'type' => $parent::class,
                 'message' => $parent->getMessage(),
                 'file' => $parent->getFile(),
                 'line' => $parent->getLine()
             ];
         }
+
         $payload = json_encode([
             'exception' => $exception,
             'status' => 500
         ]);
-        $response->getBody()->write($payload);
+        if (is_string($payload)) {
+            $response->getBody()->write($payload);
+        } else {
+            $this->logger->error("Error serializing payload", [$exception]);
+            $response->getBody()->write("{}");
+        }
+
         return $response->withStatus(500);
     }
 
     /**
      * APIExceptionCatcher middleware invokable class
      *
-     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
-     * @param  \Psr\Http\Server\RequestHandlerInterface $handler  PSR7 request handler object
-     *
-     * @return \Psr\Http\Message\ResponseInterface
+     * @param \Psr\Http\Message\ServerRequestInterface $serverRequest PSR7 request
+     * @param \Psr\Http\Server\RequestHandlerInterface $requestHandler PSR7 request handler object
      */
-    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Server\RequestHandlerInterface $handler): \Psr\Http\Message\ResponseInterface
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $serverRequest, \Psr\Http\Server\RequestHandlerInterface $requestHandler): \Psr\Http\Message\ResponseInterface
     {
         try {
-            $this->logger->debug($request->getMethod() . " " . $request->getUri()->getPath());
-            $this->logger->debug($request->getBody());
-            return $handler->handle($request);
+            $this->logger->debug($serverRequest->getMethod() . " " . $serverRequest->getUri()->getPath());
+            $contentType = $serverRequest->getHeaderLine('Content-Type');
+            if (str_contains($contentType, 'application/json')) {
+                $data = json_decode($serverRequest->getBody()->getContents(), true);
+                $this->logger->debug("JSON", [$data]);
+            }
+
+            return $requestHandler->handle($serverRequest);
         } catch (\Spieldose\Exception\InvalidParamsException $e) {
             return $this->handleException($e, 400, ['invalidOrMissingParams' => explode(",", $e->getMessage())]);
         } catch (\Spieldose\Exception\AlreadyExistsException $e) {
