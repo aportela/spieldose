c9cefe5eefb51c14ee7252537f3863f6694f9182
alex <766f6964+github@gmail.com>
2025-11-13 15:05:20 +0100

    applied rector withPreparedSets

--- phpslim-api/Spieldose/Middleware/CheckAuth.php
+++ phpslim-api/Spieldose/Middleware/CheckAuth.php
@@ -6,25 +6,18 @@ namespace Spieldose\Middleware;
 
 class CheckAuth
 {
-    public function __construct(\Psr\Container\ContainerInterface $container)
-    {
-    }
-
     /**
      * middleware to check api methods with auth required
      *
-     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
-     * @param  \Psr\Http\Server\RequestHandlerInterface $handler  PSR7 request handler object
-     *
-     * @return \Psr\Http\Message\ResponseInterface
+     * @param \Psr\Http\Message\ServerRequestInterface $serverRequest PSR7 request
+     * @param \Psr\Http\Server\RequestHandlerInterface $requestHandler PSR7 request handler object
      */
-    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Server\RequestHandlerInterface $handler): \Psr\Http\Message\ResponseInterface
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $serverRequest, \Psr\Http\Server\RequestHandlerInterface $requestHandler): \Psr\Http\Message\ResponseInterface
     {
         if (\Spieldose\UserSession::isLogged()) {
-            $response = $handler->handle($request);
-            return $response;
+            return $requestHandler->handle($serverRequest);
         } else {
-            throw new \Spieldose\Exception\UnauthorizedException($request->getMethod() . " " . $request->getUri()->getPath());
+            throw new \Spieldose\Exception\UnauthorizedException($serverRequest->getMethod() . " " . $serverRequest->getUri()->getPath());
         }
     }
 }
