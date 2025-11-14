cdda3f52753f7a024a731ac857b9256478d99a96
alex <766f6964+github@gmail.com>
2025-11-14 10:14:40 +0100

    moved session start to bootstrap file

--- phpslim-api/config/bootstrap.php
+++ phpslim-api/config/bootstrap.php
@@ -5,6 +5,8 @@ use Slim\App;
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+\Spieldose\UserSession::start();
+
 $containerBuilder = new ContainerBuilder();
 
 // Set up settings
--- quasar-frontend/public/index.php
+++ quasar-frontend/public/index.php
@@ -4,15 +4,6 @@ declare(strict_types=1);
 
 ob_start();
 
-if (session_status() !== PHP_SESSION_ACTIVE) {
-  session_set_cookie_params(["SameSite" => "Strict"]);
-  session_set_cookie_params(["Secure" => "true"]);
-  session_set_cookie_params(["HttpOnly" => "true"]);
-  session_name('SPIELDOSE');
-  session_cache_limiter("nocache");
-  session_start();
-}
-
 $app = (require dirname(__DIR__) . DIRECTORY_SEPARATOR .  'config' . DIRECTORY_SEPARATOR . 'bootstrap.php');
 if (! $app instanceof Slim\App) {
   throw new \RuntimeException("Failed to create App from bootstrap");
