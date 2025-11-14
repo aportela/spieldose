40321eb10899e07ead1baa86cbb9f85206df0b9c
alex <766f6964+github@gmail.com>
2025-11-14 21:58:02 +0100

    added dummy columns

--- quasar-frontend/src/pages/IndexPage.vue
+++ quasar-frontend/src/pages/IndexPage.vue
@@ -1,3 +1,16 @@
 <template>
-  test
+  <q-page>
+    <div class="row q-col-gutter-sm">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        Column one
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        Column two
+      </div>
+    </div>
+  </q-page>
 </template>
+
+<script setup>
+
+</script>
