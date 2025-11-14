be4b55dbd231ac4943b4848a7307679546f6d6ea
alex <766f6964+github@gmail.com>
2025-11-14 10:17:01 +0100

    old comment removed

--- quasar-frontend/public/cliserver.php
+++ quasar-frontend/public/cliserver.php
@@ -2,8 +2,6 @@
 
 // https://gist.github.com/ckressibucher/e37520cf2f1d08ec56d250c54d96ed72
 
-// this is required for debug (interal php cli server) with some custom API routes with "special chars" (like /api/2/artist/R.E.M. for example)
-
 // public/cliserver.php (router script)
 
 if (PHP_SAPI !== 'cli-server') {
