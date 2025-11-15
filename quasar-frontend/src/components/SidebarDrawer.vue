a415463449fba34eb9094238b335bb7a6f849017
alex <766f6964+github@gmail.com>
2025-11-15 15:41:26 +0100

    old sidebar links added

--- quasar-frontend/src/components/SidebarDrawer.vue
+++ quasar-frontend/src/components/SidebarDrawer.vue
@@ -66,9 +66,56 @@ if (!session.isLoaded) {
 const mini = computed(() => props.mini);
 
 const menuItems = [
-  { icon: 'home', text: "Dashboard", routeName: 'index' },
+  { icon: 'home', text: "Index", routeName: 'index' },
+
+  {
+    icon: 'analytics',
+    text: 'Dashboard',
+    routeName: 'dashboard'
+  },
+
+  {
+    icon: 'list_alt',
+    text: 'Current playlist',
+    routeName: 'currentPlaylist'
+  },
+  {
+    icon: 'person',
+    text: 'Browse artists',
+    routeName: 'artists'
+  },
+  /*
+  {
+    icon: 'search',
+    text: 'Search',
+    routeName: 'search'
+  },
+  */
+  {
+    icon: 'album',
+    text: 'Browse albums',
+    routeName: 'albums'
+  },
+  {
+    icon: 'folder_open',
+    text: 'Browse paths',
+    routeName: 'paths'
+  },
+
+  {
+    icon: 'list',
+    text: 'Browse playlists',
+    routeName: 'playlists'
+  },
+  {
+    icon: 'radio',
+    text: 'Browse radio stations',
+    routeName: 'radioStations'
+  },
   { icon: 'account_circle', text: "My profile", routeName: 'profile' },
 ];
+
+
 
 function onDrawerClick(e) {
   if (mini.value) {
