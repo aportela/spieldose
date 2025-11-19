f98641e22ccdb4d0fe71155ebd3b6cc84e6c40a1
alex <766f6964+github@gmail.com>
2025-11-19 14:30:03 +0100

    added browse path method

--- quasar-frontend/src/composables/useAPI.js
+++ quasar-frontend/src/composables/useAPI.js
@@ -112,6 +112,30 @@ export function useAPI() {
         }
         return axios.post(basePath + "/browse/album", params);
       },
+      path: function (
+        filter,
+        currentPageIndex,
+        resultsPage,
+        sortField,
+        sortOrder,
+        skipCount,
+      ) {
+        let params = {
+          filter: filter || {},
+          pager: {
+            currentPageIndex: currentPageIndex,
+            resultsPage: resultsPage,
+          },
+          sort: {
+            field: sortField,
+            order: sortOrder,
+          },
+        };
+        if (skipCount) {
+          params.skipCount = true;
+        }
+        return axios.post(basePath + "/browse/path", params);
+      },
     },
     cloud: {
       getMusicBrainzArtistGenreCloud: function () {
