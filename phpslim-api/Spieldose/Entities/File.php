343ce6faa78c2047b5f9544803b3046418543b64
alex <766f6964+github@gmail.com>
2025-11-20 12:22:02 +0100

    added default mime

--- phpslim-api/Spieldose/Entities/File.php
+++ phpslim-api/Spieldose/Entities/File.php
@@ -26,13 +26,14 @@ class File
         $results = $dbh->query(
             "
                 SELECT
-                    FILE.name, FILE.size, FILE_ID3_TAG.mime, FILE_ID3_TAG.title, FILE_ID3_TAG.playtime_seconds, FILE_ID3_TAG.release_track_mbid, FILE_ID3_TAG.artist, FILE_ID3_TAG.album, COALESCE(FILE_ID3_TAG.original_year, FILE_ID3_TAG.year) AS year
+                    FILE.name, FILE.size, COALESCE(FILE_ID3_TAG.mime, :default_mime) AS mime, FILE_ID3_TAG.title, FILE_ID3_TAG.playtime_seconds, FILE_ID3_TAG.release_track_mbid, FILE_ID3_TAG.artist, FILE_ID3_TAG.album, COALESCE(FILE_ID3_TAG.original_year, FILE_ID3_TAG.year) AS year
                 FROM FILE
                 LEFT JOIN FILE_ID3_TAG ON FILE_ID3_TAG.file_id = FILE.id
                 WHERE FILE.id = :id
             ",
             [
-                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id)
+                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id),
+                new \aportela\DatabaseWrapper\Param\StringParam(":default_mime", "application/octet-stream")
             ]
         );
         if (count($results) == 1) {
