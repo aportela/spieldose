ea9887cf8e1701c0ac0e18810bf3702394f243c3
alex <766f6964+github@gmail.com>
2025-11-20 16:23:04 +0100

    changed temporal struct

--- phpslim-api/Spieldose/Entities/File.php
+++ phpslim-api/Spieldose/Entities/File.php
@@ -43,10 +43,15 @@ class File
             $this->trackInfo = new \stdClass();
             $this->trackInfo->playTimeSeconds = $results[0]->playtime_seconds;
             $this->trackInfo->title = $results[0]->title;
-            $this->trackInfo->artist = $results[0]->artist;
+            $this->trackInfo->artist = new \stdClass();
+            $this->trackInfo->artist->mbId = null;
+            $this->trackInfo->artist->name = $results[0]->artist;
             $this->trackInfo->album = new \stdClass();
             $this->trackInfo->album->mbId = $results[0]->release_mbid;
             $this->trackInfo->album->title = $results[0]->album;
+            $this->trackInfo->album->artist = new \stdClass();
+            $this->trackInfo->album->artist->mbId = null;
+            $this->trackInfo->album->artist->name = $results[0]->artist;
             $this->trackInfo->year = intval($results[0]->year);
         } else {
             throw new \Spieldose\Exception\NotFoundException("id");
