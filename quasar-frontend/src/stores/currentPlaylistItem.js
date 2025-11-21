c8fda92b705c3dae45f75cc143cefa6a0c0c7cfb
alex <766f6964+github@gmail.com>
2025-11-21 21:30:16 +0100

    added new store for currentplaylistItem

---
+++ quasar-frontend/src/stores/currentPlaylistItem.js
@@ -0,0 +1,68 @@
+import { defineStore } from "pinia";
+
+export const useCurrentPlaylistItemStore = defineStore("currentPlaylistItem", {
+  state: () => ({
+    file: null,
+    stream: null,
+  }),
+  getters: {
+    isTrack: (state) => state.file !== null,
+    isRadioStation: (state) => state.file !== null,
+    track: (state) => state.file,
+    radioStation: (state) => state.stream,
+  },
+  actions: {
+    setTrack(
+      fileId = null,
+      fileName = null,
+      fileSize = 0,
+      mime = null,
+      playTimeSeconds = 0,
+      title = null,
+      artistName = null,
+      artistMBId = null,
+      albumTitle = null,
+      albumMBId = null,
+      albumYear = null,
+      albumArtistName = null,
+      albumArtistMBId = null,
+      image,
+    ) {
+      this.stream = null;
+      this.file = {
+        id: fileId,
+        name: fileName,
+        size: fileSize,
+        mime: mime,
+        trackInfo: {
+          playTimeSeconds: playTimeSeconds,
+          title: title,
+          artist: {
+            name: artistName,
+            mbId: artistMBId,
+          },
+          album: {
+            title: albumTitle,
+            mbId: albumMBId,
+            year: albumYear,
+            artist: {
+              name: albumArtistName,
+              mbId: albumArtistMBId,
+            },
+          },
+          image: image,
+        },
+      };
+    },
+    // TODO
+    setRadioStation(id = null, name = null, url = null, image = null) {
+      this.file = null;
+      this.stream = {
+        id: id,
+        name: name,
+        url: url,
+        image: image,
+      };
+    },
+  },
+});
