d018c79d8cd9f42c521cb363915eacb72ca93c5f
alex <766f6964+github@gmail.com>
2025-11-22 01:08:40 +0100

    get data from stores & code cleanups

--- quasar-frontend/src/components/Widgets/SidebarPlayer/SidebarPlayerAlbumCover.vue
+++ quasar-frontend/src/components/Widgets/SidebarPlayer/SidebarPlayerAlbumCover.vue
@@ -1,74 +1,67 @@
 <template>
-  <div id="spieldose-sidebar-vinyl-container" class="cursor-pointer"
-    :class="{ 'spieldose-sidebar-animation-rotation-infinite': animated }" :style="style" v-if="animation"
-    @click="toggleMode" :title="t('Toggle art animation')">
-    <q-img v-if="images.small" :src="images.small" @error="images.small = null" img-class="vinyl_mini_cover"
+  <div v-if="playerStore.currentVinylAnimation == 'rotate'" @click="toggleAnimation"
+    class="cursor-pointer overflow-hidden relative-position"
+    style="background: url(images/vinyl.png) no-repeat; background-size: cover;"
+    :class="{ 'spieldose-sidebar-animation-rotation-infinite': playerStore.isPlaying }"
+    :title="t('Toggle art animation')">
+    <q-img v-if="images.small" :src="images.small" @error="images.small = null" :ratio="1" img-class="vinyl_mini_cover"
       no-spinner></q-img>
   </div>
-  <div id="spieldose-sidebar-fullcover-container" class="cursor-pointer" v-else @click="toggleMode"
-    :title="t('Toggle art animation')">
+  <div v-else @click="toggleAnimation" class="cursor-pointer" :title="t('Toggle art animation')">
     <q-img v-if="images.normal" :src="images.normal" @error="images.normal = null" alt="Album cover" :ratio="1"
       width="100%" spinner-color="pink" />
-    <q-img v-else src="images/vinyl.png" alt="Vinyl" width="400px" height="400px" spinner-color="pink" />
+    <q-img v-else src="images/vinyl.png" alt="Vinyl" :ratio="1" width="100%" spinner-color="pink" />
   </div>
 </template>
 
 <script setup>
-import { ref, computed, watch } from "vue";
+import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
+import { usePlayerStore } from "src/stores/player";
+import { useCurrentPlaylistItemStore } from "src/stores/currentPlaylistItem";
 
-
-const props = defineProps({
-  animation: Boolean,
-  animated: Boolean,
-  normalImage: String,
-  smallImage: String
-});
-
-const emit = defineEmits(['change']);
+const playerStore = usePlayerStore();
+const currentPlaylistItemStore = useCurrentPlaylistItemStore();
 
 const { t } = useI18n();
 
-// custom style for avoiding "images/vinyl.png" asset loading error if we put this on the <style> block
-const style = "background: url(images/vinyl.png) no-repeat; background-size: cover;";
 const images = ref({
-  normal: props.normalImage,
-  small: props.smallImage
+  normal: currentPlaylistItemStore.trackImageNormal,
+  small: currentPlaylistItemStore.trackImageSmall,
 });
-const normalImage = computed(() => props.normalImage || null);
-const smallImage = computed(() => props.smallImage || null);
 
-watch(smallImage, (newValue) => {
+watch(() => currentPlaylistItemStore.trackImageSmall, (newValue) => {
   images.value.small = newValue;
 });
 
-watch(normalImage, (newValue) => {
+watch(() => currentPlaylistItemStore.trackImageNormal, (newValue) => {
   images.value.normal = newValue;
 });
 
-function toggleMode() {
-  emit('change');
-}
+const toggleAnimation = () => {
+  if (playerStore.currentVinylAnimation == 'rotate') {
+    playerStore.setCurrentVinylAnimation(null);
+  } else {
+    playerStore.setCurrentVinylAnimation('rotate');
+  }
+};
+
 </script>
 
 <style lang="css">
-div#spieldose-sidebar-vinyl-container {
-  width: 400px;
-  height: 400px;
-  overflow: hidden;
+div#spieldose-sidebar-vinyl-container2 {
+  aspect-ratio: 1;
 }
 
-div#spieldose-sidebar-vinyl-container img {
-  width: 130px;
-  height: 130px;
-  top: 136px;
-  position: relative;
-  margin-left: 137px;
+img.vinyl_mini_cover {
+  width: 30%;
+  height: 30%;
+  position: absolute;
+  left: 50.5%;
+  top: 50.5%;
+  transform: translateX(-50%) translateY(-50%);
+  border: 1px solid #454545;
   border-radius: 100%;
-}
-
-div#spieldose-sidebar-fullcover-container {
-  overflow: hidden;
 }
 
 div.spieldose-sidebar-animation-rotation-infinite {
