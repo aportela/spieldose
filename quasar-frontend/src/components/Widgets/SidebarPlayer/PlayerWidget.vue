3a75612253b416a5ead9ec27361d6a2c8fb4a6c1
alex <766f6964+github@gmail.com>
2025-11-22 01:18:46 +0100

    old code removed

--- quasar-frontend/src/components/Widgets/SidebarPlayer/PlayerWidget.vue
+++ quasar-frontend/src/components/Widgets/SidebarPlayer/PlayerWidget.vue
@@ -1,9 +1,5 @@
 <template>
-
   <q-card class="overflow-hidden">
-    <!--
-    <div class="contenedor"></div>
-    -->
     <SidebarPlayerAlbumCover />
     <!--
     <div style="width: 100%; height: 200px;">
@@ -13,14 +9,12 @@
     <SidebarPlayerSpectrumAnalyzer v-if="miniSpectrumAnalyzerSettings.isVisible" />
     <SidebarPlayerVolumeControl />
     <SidebarPlayerTrackInfo />
-    <SidebarPlayerMainControls @changeTrack="refresh" />
+    <SidebarPlayerMainControls />
     <SidebarPlayerSeekControl />
   </q-card>
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from "vue";
-
 import { default as SidebarPlayerAlbumCover } from "./SidebarPlayerAlbumCover.vue";
 import { default as SidebarPlayerSpectrumAnalyzer } from "./SidebarPlayerSpectrumAnalyzer.vue";
 import { default as SidebarPlayerAnalogVuMeter } from "./SidebarPlayerAnalogVuMeter.vue";
@@ -29,76 +23,8 @@ import { default as SidebarPlayerTrackInfo } from './SidebarPlayerTrackInfo.vue'
 import { default as SidebarPlayerMainControls } from './SidebarPlayerMainControls.vue';
 import { default as SidebarPlayerSeekControl } from './SidebarPlayerSeekControl.vue';
 
-import { useAPI } from 'src/composables/useAPI';
-import { usePlayerStore } from 'src/stores/player';
-import { useCurrentPlaylistItemStore } from 'src/stores/currentPlaylistItem';
 import { useMiniSpectrumAnalyzerSettingsStore } from "src/stores/miniSpectrumAnalyzerSettings";
 
-import { useThumbnail } from "src/composables/useThumbnail";
-
-const playerStore = usePlayerStore();
-const currentPlaylistItemStore = useCurrentPlaylistItemStore();
 const miniSpectrumAnalyzerSettings = useMiniSpectrumAnalyzerSettingsStore();
 
-const { api } = useAPI();
-
-const { getMediumURL } = useThumbnail();
-
-const imageUrl = ref("images/vinyl.png");
-
-const refresh = () => {
-  imageUrl.value = null;
-  api.file.getRandom().then((successResponse) => {
-    currentPlaylistItemStore.setTrack(
-      successResponse.data.file.id,
-      successResponse.data.file.filename,
-      successResponse.data.file.filesize,
-      successResponse.data.file.mime,
-      successResponse.data.file.trackInfo.playTimeSeconds,
-      successResponse.data.file.trackInfo.title,
-      successResponse.data.file.trackInfo.artist.name,
-      successResponse.data.file.trackInfo.artist.mbId,
-      successResponse.data.file.trackInfo.album.title,
-      successResponse.data.file.trackInfo.album.mbId,
-      successResponse.data.file.trackInfo.album.year,
-      successResponse.data.file.trackInfo.album.artist.name,
-      successResponse.data.file.trackInfo.album.artist.mbId,
-      successResponse.data.file.trackInfo.imageURL.small,
-      successResponse.data.file.trackInfo.imageURL.normal,
-    );
-
-  })
-    .catch((errorResponse) => {
-      console.error(errorResponse);
-    });
-};
-
-onMounted(() => {
-  refresh();
-});
-
 </script>
-
-<style>
-.contenedor {
-  z-index: 200;
-  width: 300px;
-  height: 20px;
-  background: linear-gradient(135deg, #dcdcdc 20%, #a9a9a9 50%, #dcdcdc 80%);
-  position: relative;
-  left: 390px;
-  top: 20px;
-  animation: rotar 5s infinite;
-  transform-origin: left center;
-}
-
-@keyframes rotar {
-  0% {
-    transform: rotate(90deg);
-  }
-
-  100% {
-    transform: rotate(120deg);
-  }
-}
-</style>
