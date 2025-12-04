0a2400db620677fafd31c72943b2f373085da452
alex <766f6964+github@gmail.com>
2025-12-05 00:34:07 +0100

    added album cover widget

---
+++ quasar-frontend-ts/src/components/Widgets/Player/AlbumCover.vue
@@ -0,0 +1,105 @@
+<template>
+  <!--
+  vinyl svg credits:
+  https://commons.wikimedia.org/wiki/File:Vinyl_record.svg
+  -->
+  <div v-if="playerStore.currentVinylAnimation == 'rotate'" @click="toggleAnimation"
+    id="spieldose-sidebar-vinyl-container" class="cursor-pointer overflow-hidden relative-position full-width"
+    style="background: url(images/Vinyl_record.svg) no-repeat; background-size: cover;"
+    :class="{ 'spieldose-sidebar-animation-rotation-infinite': playerStore.isPlaying }"
+    :title="t('Toggle art animation')">
+    <q-img v-if="images.small" :src="images.small" @error="images.small = null" :ratio="1" img-class="vinyl_mini_cover"
+      spinner-color="pink"></q-img>
+  </div>
+  <div v-else @click="toggleAnimation" class="cursor-pointer" :title="t('Toggle art animation')">
+    <q-img v-if="images.normal" :src="images.normal" @error="images.normal = null" alt="Album cover" :ratio="1"
+      width="100%" spinner-color="pink" />
+    <q-img v-else src="images/Vinyl_record.svg" alt="Vinyl" :ratio="1" width="100%" spinner-color="pink" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { nextTick, ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
+import { usePlayerStore } from "src/stores/player";
+import { useCurrentPlaylistItemStore } from "src/stores/currentPlaylistItem";
+
+const playerStore = usePlayerStore();
+const currentPlaylistItemStore = useCurrentPlaylistItemStore();
+
+const { t } = useI18n();
+
+const images = ref({
+  normal: currentPlaylistItemStore.trackImageNormal,
+  small: currentPlaylistItemStore.trackImageSmall,
+});
+
+watch(() => currentPlaylistItemStore.trackImageSmall, (newValue) => {
+  images.value.small = null;
+  if (newValue) {
+    (() => {
+      images.value.small = newValue
+    });
+  }
+});
+
+watch(() => currentPlaylistItemStore.trackImageNormal, (newValue) => {
+  images.value.normal = null;
+  if (newValue) {
+    nextTick(() => {
+      images.value.normal = newValue
+    });
+  }
+});
+
+const toggleAnimation = () => {
+  if (playerStore.currentVinylAnimation == 'rotate') {
+    playerStore.setCurrentVinylAnimation(null);
+  } else {
+    playerStore.setCurrentVinylAnimation('rotate');
+  }
+};
+
+</script>
+
+<style lang="css">
+div#spieldose-sidebar-vinyl-container {
+  aspect-ratio: 1;
+}
+
+img.vinyl_mini_cover {
+  width: 27%;
+  height: 27%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  border: 0px solid #454545;
+  border-radius: 100%;
+}
+
+div.spieldose-sidebar-animation-rotation-infinite {
+  animation: rotation 8s linear infinite;
+  z-index: 1;
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(359deg);
+  }
+}
+
+@-webkit-keyframes rotate {
+  from {
+    -webkit-transform: rotate(0deg);
+  }
+
+  to {
+    -webkit-transform: rotate(359deg);
+  }
+}
+</style>
