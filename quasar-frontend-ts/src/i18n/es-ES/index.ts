e0acf726cf4e7135b13e14b99655186c3a6746a4
alex <766f6964+github@gmail.com>
2025-12-04 14:25:01 +0100

    new locale for spanish language added

---
+++ quasar-frontend-ts/src/i18n/es-ES/index.ts
@@ -0,0 +1,7 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  failed: 'La operación ha fallado',
+  success: 'La operación se realizó con éxito',
+};
