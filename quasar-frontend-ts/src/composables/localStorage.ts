db882a22706ad642ca0dfcf14da1aaf10c57af23
alex <766f6964+github@gmail.com>
2025-12-04 23:39:59 +0100

    new storage keys added

--- quasar-frontend-ts/src/composables/localStorage.ts
+++ quasar-frontend-ts/src/composables/localStorage.ts
@@ -41,10 +41,38 @@ const email = createStorageEntry<string | null>("email", null);
 const darkMode = createStorageEntry<boolean>("darkMode", false);
 const locale = createStorageEntry<string | null>("locale", null);
 const showToolTips = createStorageEntry<boolean>("showToolTips", false);
+const playerVolume = createStorageEntry<number>("playerVolume", 1);
+const playerMuted = createStorageEntry<boolean>("playerMuted", false);
+const showMiniSpectrumAnalyzer = createStorageEntry<boolean>("showMiniSpectrumAnalyzer", true);
+const playerMiniAnalyzerMode = createStorageEntry<number>("playerMiniAnalyzerMode", 7);
+const playerMiniAnalyzerFPS = createStorageEntry<number>("playerMiniAnalyzerFPS", 30);
+const playerMiniAnalyzerBarSpace = createStorageEntry<number>("playerMiniAnalyzerBarSpace", 0.2);
+const playerMiniAnalyzerHeight = createStorageEntry<number>("playerMiniAnalyzerHeight", 40);
+const playerMiniAnalyzerChannelLayout = createStorageEntry<string>("playerMiniAnalyzerChannelLayout", "single");
+const playerMiniAnalyzerShowPeaks = createStorageEntry<boolean>("playerMiniAnalyzerShowPeaks", true);
+const playerMiniAnalyzerLedBars = createStorageEntry<boolean>("playerMiniAnalyzerLedBars", true);
+const playerMiniAnalyzerTrueLeds = createStorageEntry<boolean>("playerMiniAnalyzerTrueLeds", true);
+const playerMiniAnalyzerGradient = createStorageEntry<string>("playerMiniAnalyzerTrueLeds", "spieldose");
+const playerMiniAnalyzerLoRes = createStorageEntry<boolean>("playerMiniAnalyzerLoRes", false);
+const playerVinylAnimation = createStorageEntry<string | null>("playerVinylAnimation", null);
 
 export {
   email,
   darkMode,
   locale,
   showToolTips,
+  playerVolume,
+  playerMuted,
+  showMiniSpectrumAnalyzer,
+  playerMiniAnalyzerMode,
+  playerMiniAnalyzerFPS,
+  playerMiniAnalyzerBarSpace,
+  playerMiniAnalyzerHeight,
+  playerMiniAnalyzerChannelLayout,
+  playerMiniAnalyzerShowPeaks,
+  playerMiniAnalyzerLedBars,
+  playerMiniAnalyzerTrueLeds,
+  playerMiniAnalyzerGradient,
+  playerMiniAnalyzerLoRes,
+  playerVinylAnimation,
 };
