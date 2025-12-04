69f0ef3cbe34c1b0b38c38bf19a812c7eaa1674f
alex <766f6964+github@gmail.com>
2025-12-04 15:25:07 +0100

    added server environment boot file

---
+++ quasar-frontend-ts/src/boot/serverEnvironment.ts
@@ -0,0 +1,19 @@
+import { defineBoot } from '#q-app/wrappers'
+
+import { api } from "src/composables/api";
+import { useServerEnvironmentStore } from "src/stores/serverEnvironment";
+import { type getServerEnvironmentResponseData } from "src/types/api-responses";
+
+const serverEnvironment = useServerEnvironmentStore();
+
+export default defineBoot(async (/* { app, router, ... } */) => {
+  try {
+    const response: getServerEnvironmentResponseData = await api.common.getServerEnvironment();
+    serverEnvironment.set(
+      response.data.serverEnvironment.allowSignUp,
+      response.data.serverEnvironment.environment,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})
