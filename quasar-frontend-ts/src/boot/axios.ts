0e0d80a9435f1766adffe3686d95327b840b5069
alex <766f6964+github@gmail.com>
2025-12-04 15:04:48 +0100

    removed old example code

--- quasar-frontend-ts/src/boot/axios.ts
+++ quasar-frontend-ts/src/boot/axios.ts
@@ -14,7 +14,6 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
@@ -23,9 +22,4 @@ export default defineBoot(({ app }) => {
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
 });
-
-export { api };
