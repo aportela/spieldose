468c5e80b0a00b9e060f6bf8debb2b65fd08c433
alex <766f6964+github@gmail.com>
2025-12-04 21:18:35 +0100

    disabled ES/GL languages (for now)

--- quasar-frontend-ts/src/i18n/index.ts
+++ quasar-frontend-ts/src/i18n/index.ts
@@ -1,9 +1,9 @@
 import enUS from "./en-US";
-import esES from "./es-ES";
-import glGL from "./gl-GL";
+//import esES from "./es-ES";
+//import glGL from "./gl-GL";
 
 export default {
   "en-US": enUS,
-  "es-ES": esES,
-  "gl-GL": glGL,
+  //"es-ES": esES,
+  //"gl-GL": glGL,
 };
