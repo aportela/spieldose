8c730d480193f13a93cf8df9c586ec5d95c85820
alex <766f6964+github@gmail.com>
2025-12-04 16:33:41 +0100

    new galician i18n translations language file

---
+++ quasar-frontend-ts/src/i18n/gl-GL/index.ts
@@ -0,0 +1,7 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  failed: 'A operación fallou',
+  success: 'A operación realizouse con éxito',
+};
