c3669a0b578ae1c0c487157406cb5e0782db50f0
alex <766f6964+github@gmail.com>
2025-12-04 15:56:50 +0100

    new login/register routes

--- quasar-frontend-ts/src/router/routes.ts
+++ quasar-frontend-ts/src/router/routes.ts
@@ -2,16 +2,27 @@ import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    path: "/auth",
+    component: () => import("layouts/LoginRegisterLayout.vue"),
+    children: [
+      {
+        name: "login",
+        path: "login",
+        component: () => import("pages/LoginPage.vue"),
+      },
+      {
+        name: "register",
+        path: "register",
+        component: () => import("pages/RegisterPage.vue"),
+      },
+    ],
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: "/:catchAll(.*)*",
+    name: "notFound",
+    component: () => import("layouts/ErrorNotFoundLayout.vue"),
   },
 ];
 
