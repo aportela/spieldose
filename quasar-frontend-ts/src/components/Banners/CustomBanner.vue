311279d5ffe9ddfbd3bc7913fdd50c35aa13011b
alex <766f6964+github@gmail.com>
2025-12-04 16:19:08 +0100

    new components added (copied from homedocs project)

---
+++ quasar-frontend-ts/src/components/Banners/CustomBanner.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-banner dense rounded class="theme-default-q-banner"
+    :class="{ 'theme-default-q-banner-success': success, 'theme-default-q-banner-error': error, 'theme-default-q-banner-warning': warning }">
+    <template v-slot:avatar v-if="success || error || warning">
+      <q-icon size="sm" v-if="success" name="check" class="text-green" />
+      <q-icon size="sm" v-else-if="error" name="error" class="text-red" />
+      <q-icon size="sm" v-else-if="warning" name="warning" class="text-warning" />
+    </template>
+    <slot name="text">
+      {{ t(text) }}
+    </slot>
+    <slot name="details"></slot>
+  </q-banner>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
+interface CustomBannerProps {
+  text?: string,
+  success?: boolean,
+  error?: boolean,
+  warning?: boolean
+};
+
+withDefaults(defineProps<CustomBannerProps>(), {
+  text: "",
+  success: false,
+  error: false,
+  warning: false,
+});
+</script>
---
+++ quasar-frontend-ts/src/components/Banners/CustomErrorBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <CustomBanner :text="text" error>
+    <template v-slot:details v-if="serverEnvironment.isCurrentEnvironmentDevelopment && apiError">
+      <APIErrorDetails class="q-mt-md" :api-error="apiError"></APIErrorDetails>
+    </template>
+  </CustomBanner>
+</template>
+
+<script setup lang="ts">
+import { useServerEnvironmentStore } from "src/stores/serverEnvironment";
+
+import { default as CustomBanner } from "src/components/Banners/CustomBanner.vue";
+import { default as APIErrorDetails } from "src/components/APIErrorDetails.vue";
+
+import type { APIErrorDetails as APIErrorDetailsInterface } from "src/types/api-error-details";
+
+const serverEnvironment = useServerEnvironmentStore();
+
+interface CustomErrorBannerProps {
+  text: string,
+  apiError?: APIErrorDetailsInterface | null;
+};
+
+defineProps<CustomErrorBannerProps>();
+</script>
