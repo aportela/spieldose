afea5fb66e3343e25b9643450f730253851a669e
alex <766f6964+github@gmail.com>
2025-12-04 21:17:28 +0100

    added missing translation keys for login/register forms

--- quasar-frontend-ts/src/i18n/en-US/index.ts
+++ quasar-frontend-ts/src/i18n/en-US/index.ts
@@ -2,6 +2,19 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful',
+  "Welcome aboard!": "Welcome aboard!",
+  "Glad to see you again!": "Glad to see you again!",
+  "Unlock the music you’ve been looking for.": "Unlock the music you’ve been looking for.",
+  "The music never ends—just keep listening.": "The music never ends—just keep listening.",
+
+  "Email": "Email",
+  "Password": "Password",
+  "Sign in": "Sign in",
+  "Don't have an account yet ?": "Don't have an account yet ?",
+  "Click here to sign up": "Click here to sign up",
+  "Sign up now and take control.": "Sign up now and take control.",
+  "The first track to your musical adventure begins here!": "The first track to your musical adventure begins here!",
+  "Sign up": "Sign up",
+  "Already have an account ?": "Already have an account ?",
+  "Click here to sign in": "Click here to sign in",
 };
