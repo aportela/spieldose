9f2d5b25d0cee7780e0281f082aaf53fb0fa478d
alex <766f6964+github@gmail.com>
2025-12-04 15:59:56 +0100

    added implementation for i18n store

--- quasar-frontend-ts/src/stores/i18n.ts
+++ quasar-frontend-ts/src/stores/i18n.ts
@@ -1,11 +1,34 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
+import { locale as localStorageLocale } from 'src/composables/localStorage';
+import { autodetectLocale, getMatchedLocale } from 'src/composables/i18n';
 
-export const useMyStore = defineStore('myStore', {
-  state: () => ({}),
-  getters: {},
-  actions: {}
+interface State {
+  locale: string;
+};
+
+export const useI18nStore = defineStore('i18nStore', {
+  state: (): State => ({
+    locale: autodetectLocale()
+  }),
+  getters: {
+    currentLocale(state): string {
+      return state.locale;
+    },
+  },
+  actions: {
+    setLocale(locale: string): boolean {
+      const matched = getMatchedLocale(locale);
+      if (matched !== null) {
+        this.locale = matched;
+        localStorageLocale.set(this.locale);
+        return (true);
+      } else {
+        return (false);
+      }
+    },
+  }
 });
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useMyStore, import.meta.hot));
+  import.meta.hot.accept(acceptHMRUpdate(useI18nStore, import.meta.hot));
 }
