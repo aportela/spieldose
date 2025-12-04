9d00fbaed4c949351f1d201c26b52766e4010bbf
alex <766f6964+github@gmail.com>
2025-12-04 15:56:27 +0100

    new login/register pages (copied from homedocs)

---
+++ quasar-frontend-ts/src/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page>
+    <q-card class="q-pa-xl" flat>
+      <LoginForm @success="onSuccessLogin" />
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { default as LoginForm } from "src/components/Forms/LoginForm.vue";
+
+const router = useRouter();
+
+const onSuccessLogin = () => {
+  router.push(
+    { name: "index" }
+  ).catch((e) => {
+    console.error(e);
+  });;
+};
+
+</script>
---
+++ quasar-frontend-ts/src/pages/RegisterPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page>
+    <q-card class="q-pa-xl" flat>
+      <RegisterForm />
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { default as RegisterForm } from "src/components/Forms/RegisterForm.vue";
+</script>
