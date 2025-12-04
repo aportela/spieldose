162041155a366e27aae8c6ea5ad520f18f6bf744
alex <766f6964+github@gmail.com>
2025-12-04 23:39:46 +0100

    added new composable for track actions

---
+++ quasar-frontend-ts/src/composables/trackActions.ts
@@ -0,0 +1,23 @@
+import { api } from "src/composables/api";
+
+const setFavoriteTrack = async (id: string) => {
+  try {
+    const response = await api.track.setFavorite(id);
+    return response.data?.favorited;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const unsetFavoriteTrack = async (id: string) => {
+  try {
+    const response = await api.track.unSetFavorite(id);
+    return response.data?.favorited;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { setFavoriteTrack, unsetFavoriteTrack };
