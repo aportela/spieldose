88c0883dd749ce357efb9b2dbd65d0998e8e05b6
alex <766f6964+github@gmail.com>
2025-12-04 21:14:00 +0100

    added better error control

--- quasar-frontend-ts/src/boot/router.ts
+++ quasar-frontend-ts/src/boot/router.ts
@@ -1,20 +1,28 @@
 import { defineBoot } from '#q-app/wrappers'
-
 import { type GetNewAccessTokenResponse as GetNewAccessTokenResponseInterface } from "src/types/api-responses";
 import { useSessionStore } from "src/stores/session";
 import { api } from "src/composables/api";
+import { AxiosError } from "axios";
 
 const sessionStore = useSessionStore();
 
 export default defineBoot(async ({ router }) => {
+
   if (!sessionStore.hasAccessToken) {
     try {
       const successResponse: GetNewAccessTokenResponseInterface = await api.auth.renewAccessToken();
       sessionStore.setAccessToken(successResponse.data.accessToken);
-    } catch (e) {
-      console.error(e);
+    } catch (e: unknown) {
+      if (e instanceof AxiosError) {
+        if (e.status !== 401) { // 401 is the "normal" error response if we do not have a previous refresh token
+          console.error("Invalid error http response code", e.status);
+        }
+      } else {
+        console.error("Invalid error response", e);
+      }
     }
   }
+
   router.beforeEach((to, _from, next) => {
     if (!to.matched.length) {
       next({ name: "notFound" });
