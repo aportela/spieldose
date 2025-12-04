d48f35b60d544fd8af51e4a8eaf767429636e767
alex <766f6964+github@gmail.com>
2025-12-04 14:19:50 +0100

    added implementation for dark mode store

--- quasar-frontend-ts/src/stores/darkMode.ts
+++ quasar-frontend-ts/src/stores/darkMode.ts
@@ -1,11 +1,42 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
+import { Dark } from "quasar";
+import { darkMode as localStorageDarkMode } from "src/composables/localStorage";
 
-export const useMyStore = defineStore('myStore', {
-  state: () => ({}),
-  getters: {},
-  actions: {}
+const savedMode = localStorageDarkMode.get();
+
+if (savedMode === true) {
+  Dark.set(true);
+} else if (savedMode === false) {
+  Dark.set(false);
+} else {
+  Dark.set("auto");
+}
+
+interface State {
+  active: boolean;
+};
+
+export const useDarkModeStore = defineStore('darkModeStore', {
+  state: (): State => ({
+    active: Dark.isActive
+  }),
+  getters: {
+    isActive(state): boolean {
+      return state.active
+    },
+  },
+  actions: {
+    set(active: boolean): void {
+      this.active = active;
+      Dark.set(this.active);
+      localStorageDarkMode.set(this.active);
+    },
+    toggle(): void {
+      this.set(!this.active);
+    }
+  }
 });
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useMyStore, import.meta.hot));
+  import.meta.hot.accept(acceptHMRUpdate(useDarkModeStore, import.meta.hot));
 }
