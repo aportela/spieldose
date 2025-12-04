162bc0c3c5661dd0d1f6578a5eed09cb15a84280
alex <766f6964+github@gmail.com>
2025-12-04 14:21:14 +0100

    added implementation for session store

--- quasar-frontend-ts/src/stores/session.ts
+++ quasar-frontend-ts/src/stores/session.ts
@@ -1,11 +1,35 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
 
-export const useMyStore = defineStore('myStore', {
-  state: () => ({}),
-  getters: {},
-  actions: {}
+interface State {
+  tokens: {
+    access: string | null;
+  }
+};
+
+export const useSessionStore = defineStore("session", {
+  state: (): State => ({
+    tokens: {
+      access: null,
+    }
+  }),
+  getters: {
+    hasAccessToken(state): boolean {
+      return state.tokens.access !== null;
+    },
+    accessToken(state): string | null {
+      return state.tokens.access
+    },
+  },
+  actions: {
+    setAccessToken(token: string): void {
+      this.tokens.access = token;
+    },
+    removeAccessToken(): void {
+      this.tokens.access = null;
+    },
+  },
 });
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useMyStore, import.meta.hot));
+  import.meta.hot.accept(acceptHMRUpdate(useSessionStore, import.meta.hot));
 }
