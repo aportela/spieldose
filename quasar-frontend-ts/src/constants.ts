60427882c5782631fabfd0eac0e4556675ef4204
alex <766f6964+github@gmail.com>
2025-12-04 14:15:36 +0100

    added constant definitions

---
+++ quasar-frontend-ts/src/constants.ts
@@ -0,0 +1,7 @@
+export const GITHUB_PROJECT_URL = "http://github.com/aportela/spieldose";
+// this (prefix/namespace) is for preventing sharing keys on web sites hosted on same url
+export const LOCAL_STORAGE_NAMESPACE = "spieldose#";
+
+export const SERVER_API_BASE_PATH = "api2";
+
+export const DEFAULT_LOCALE = "en-US";
