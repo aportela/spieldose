ff95beaccfa1de9b38b36c4570f2f30e425e0e41
alex <766f6964+github@gmail.com>
2025-12-04 15:08:08 +0100

    added implementation for server environment store

--- quasar-frontend-ts/src/stores/serverEnvironment.ts
+++ quasar-frontend-ts/src/stores/serverEnvironment.ts
@@ -1,11 +1,36 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
 
-export const useMyStore = defineStore('myStore', {
-  state: () => ({}),
-  getters: {},
-  actions: {}
+import { type EnvironmentType } from "src/types/common";
+
+interface State {
+  allowSignUp: boolean;
+  environment: EnvironmentType;
+};
+
+export const useServerEnvironmentStore = defineStore('serverEnvironment', {
+  state: (): State => ({
+    allowSignUp: true,
+    environment: "production",
+  }),
+  getters: {
+    isSignUpAllowed(state): boolean {
+      return state.allowSignUp
+    },
+    isCurrentEnvironmentDevelopment(state): boolean {
+      return state.environment == "development"
+    },
+  },
+  actions: {
+    set(
+      allowSignUp: boolean = false,
+      environment: EnvironmentType = "production",
+    ): void {
+      this.allowSignUp = !!allowSignUp;
+      this.environment = environment;
+    },
+  },
 });
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useMyStore, import.meta.hot));
+  import.meta.hot.accept(acceptHMRUpdate(useServerEnvironmentStore, import.meta.hot));
 }
