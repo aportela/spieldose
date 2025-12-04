b988eb991146849d1b549f4e467719226abdb323
alex <766f6964+github@gmail.com>
2025-12-04 23:58:05 +0100

    new type for spectrum analyzer channel layout

--- quasar-frontend-ts/src/types/common.ts
+++ quasar-frontend-ts/src/types/common.ts
@@ -4,6 +4,8 @@ type ValidAuthTypes = "Bearer";
 
 type VinylAnimation = "rotate" | null;
 
+type SpectrumAnalyzerChannelLayout = "single" | "dual-combined" | "dual-horizontal" | "dual-vertical";
+
 export {
-  type EnvironmentType, type ValidAuthTypes, type VinylAnimation,
+  type EnvironmentType, type ValidAuthTypes, type VinylAnimation, type SpectrumAnalyzerChannelLayout
 };
