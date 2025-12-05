f56e8eec1c11a152ee1217dbc0d69921078cbd57
alex <766f6964+github@gmail.com>
2025-12-06 00:33:59 +0100

    added new sort components

---
+++ quasar-frontend-ts/src/components/Forms/Fields/SortFieldSelector.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-select v-bind="attrs" :options="options" v-model="model" :label="label">
+  </q-select>
+</template>
+
+<script setup lang="ts">
+import { useAttrs, computed } from 'vue';
+
+interface OptionInterface {
+  label: string;
+  value: string;
+}
+
+interface SortFieldSelector {
+  modelValue: OptionInterface,
+  label?: string;
+  options: OptionInterface[];
+}
+
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps<SortFieldSelector>();
+
+const attrs = useAttrs();
+
+const model = computed({
+  get() {
+    return (props.modelValue);
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  }
+});
+
+</script>
---
+++ quasar-frontend-ts/src/components/Forms/Fields/SortOrderSelector.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-select v-bind="attrs" :options="sortOrderSelectorOptions" v-model="model" :label="label">
+  </q-select>
+</template>
+
+<script setup lang="ts">
+import { useAttrs, computed } from 'vue';
+import { type SelectorOption, sortOrderSelectorOptions } from 'src/types/common';
+
+interface SortSelectorProps {
+  modelValue: SelectorOption,
+  label?: string
+}
+
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps<SortSelectorProps>();
+
+const attrs = useAttrs();
+
+const model = computed({
+  get() {
+    return (props.modelValue);
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  }
+});
+
+</script>
