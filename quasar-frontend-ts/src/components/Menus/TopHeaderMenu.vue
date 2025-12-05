e72cdfede62d9f18677ada7ef645bf871fd49700
alex <766f6964+github@gmail.com>
2025-12-05 14:35:50 +0100

    added logout action button

--- quasar-frontend-ts/src/components/Menus/TopHeaderMenu.vue
+++ quasar-frontend-ts/src/components/Menus/TopHeaderMenu.vue
@@ -1,14 +1,19 @@
 <template>
   <q-btn-group flat class="q-ml-md" v-if="visible">
-    <q-btn stack v-for="item in topHeaderMenuItems" size="md" no-caps :icon="item.icon" :key="item.text"
-      :to="item.routeName">{{
-        item.text
-      }}</q-btn>
+    <q-btn stack size="md" no-caps icon="search" :label="t('Search')" />
+    <q-btn stack v-for="item in sidebarMenuItems" size="md" no-caps :icon="item.icon" :key="item.text"
+      :to="item.routeName" :label="t(item.text)" />
+    <q-btn stack size="md" no-caps icon="logout" @click="logout" :label="t('Sign out')" />
   </q-btn-group>
 </template>
 
 <script setup lang="ts">
-import { topHeaderMenuItems } from "src/types/menu-item";
+import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
+import { api } from "src/composables/api";
+import { useSessionStore } from "src/stores/session";
+import { sidebarMenuItems } from "src/types/menu-item";
+import { default as DesktopToolTip } from "../DesktopToolTip.vue";
 
 interface TopHeaderMenuProps {
   visible: boolean;
@@ -16,4 +21,29 @@ interface TopHeaderMenuProps {
 
 defineProps<TopHeaderMenuProps>();
 
+const { t } = useI18n();
+const router = useRouter();
+const sessionStore = useSessionStore();
+
+const logout = () => {
+  api.auth
+    .logout()
+    .then(() => {
+      sessionStore.removeAccessToken();
+      router.push({
+        name: "login",
+      }).catch((e) => {
+        console.error(e);
+      });
+    })
+    .catch((errorResponse) => {
+      console.error(errorResponse);
+      sessionStore.removeAccessToken();
+      router.push({
+        name: "login",
+      }).catch((e) => {
+        console.error(e);
+      });
+    });
+};
 </script>
