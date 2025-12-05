5ce4e173ee3fa3fab1d62be26f370b62c516bff1
alex <766f6964+github@gmail.com>
2025-12-06 00:33:49 +0100

    new types added

--- quasar-frontend-ts/src/types/common.ts
+++ quasar-frontend-ts/src/types/common.ts
@@ -8,6 +8,24 @@ type SpectrumAnalyzerChannelLayout = "single" | "dual-combined" | "dual-horizont
 
 type PlayerStatus = "stopped" | "playing" | "paused";
 
+type SortOrder = "ASC" | "DESC";
+
+interface SelectorOption {
+  label: string;
+  value: SortOrder;
+}
+
+const sortOrderSelectorOptions: SelectorOption[] = [
+  {
+    label: "Ascending",
+    value: "ASC",
+  },
+  {
+    label: "Descending",
+    value: "DESC",
+  }
+];
+
 export {
-  type EnvironmentType, type ValidAuthTypes, type VinylAnimation, type SpectrumAnalyzerChannelLayout, type PlayerStatus
+  type EnvironmentType, type ValidAuthTypes, type VinylAnimation, type SpectrumAnalyzerChannelLayout, type PlayerStatus, type SortOrder, type SelectorOption, sortOrderSelectorOptions
 };
