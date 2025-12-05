d0b19f5b577d74a8894ae2cd03351d18c3602afe
alex <766f6964+github@gmail.com>
2025-12-05 12:04:47 +0100

    moved logout item & q-list to sidebar menu component

--- quasar-frontend-ts/src/components/SidebarDrawer.vue
+++ quasar-frontend-ts/src/components/SidebarDrawer.vue
@@ -1,41 +1,16 @@
 <template>
   <q-drawer v-bind="attrs" show-if-above bordered :width="430" :mini="mini" class=" fit theme-default-q-drawer">
     <SidebarPlayer v-if="!mini" />
-    <q-list v-if="mini">
-      <q-item class="cursor-pointer non-selectable no-pointer-events rounded-borders q-ma-sm theme-default-q-item">
-        <q-item-section avatar>
-          <q-avatar square size="24px">
-            <img src="icons/favicon-128x128.png" />
-          </q-avatar>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label class="text-weight-bold text-uppercase">Spieldose</q-item-label>
-        </q-item-section>
-      </q-item>
-      <SideBarMenu />
-      <q-item v-ripple clickable @click="logout" class="rounded-borders q-ma-sm theme-default-q-item">
-        <q-item-section avatar>
-          <q-icon name="logout" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ t("Sign out") }}</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
+    <SideBarMenu v-if="mini" />
   </q-drawer>
 </template>
 
 <script setup lang="ts">
 
 import { computed, useAttrs } from "vue";
-import { useRouter } from "vue-router";
-import { useI18n } from "vue-i18n";
-import { api } from "src/composables/api";
-import { useSessionStore } from "src/stores/session";
 import { default as SidebarPlayer } from "./Widgets/SidebarPlayer.vue";
-import SideBarMenu from "./Menus/SideBarMenu.vue";
+import { default as SideBarMenu } from "./Menus/SideBarMenu.vue";
 
-//import { default as PlayerWidget } from "./Player/PlayerWidget.vue";
 
 const props = defineProps({
   mini: {
@@ -46,35 +21,6 @@ const props = defineProps({
 });
 
 const attrs = useAttrs();
-
-const { t } = useI18n();
-const router = useRouter();
-
-
-const sessionStore = useSessionStore();
-
 const mini = computed(() => props.mini);
-
-const logout = () => {
-  api.auth
-    .logout()
-    .then(() => {
-      sessionStore.removeAccessToken();
-      router.push({
-        name: "login",
-      }).catch((e) => {
-        console.error(e);
-      });
-    })
-    .catch((errorResponse) => {
-      console.error(errorResponse);
-      sessionStore.removeAccessToken();
-      router.push({
-        name: "login",
-      }).catch((e) => {
-        console.error(e);
-      });
-    });
-};
 
 </script>
