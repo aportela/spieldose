4d942b7dcf4ddd5705d1d15159f3fc3b1182fe89
alex <766f6964+github@gmail.com>
2025-12-05 01:00:27 +0100

    added audiomotion analyzer store implementation

--- quasar-frontend-ts/src/stores/audioMotionAnalyzer.ts
+++ quasar-frontend-ts/src/stores/audioMotionAnalyzer.ts
@@ -1,9 +1,39 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
+import { usePlayerStore } from "./player";
+
+const playerStore = usePlayerStore();
+
+interface State {
+  audioInstance: HTMLAudioElement | null,
+  options: {
+    instanced: boolean;
+    connectSpeakers: boolean;
+  };
+};
 
 export const useAudioMotionAnalyzerStore = defineStore('audioMotionAnalyzerStore', {
-  state: () => ({}),
-  getters: {},
-  actions: {}
+  state: (): State => ({
+    audioInstance: null,
+    options: {
+      instanced: false,
+      connectSpeakers: true,
+    },
+  }),
+  getters: {
+    audioInstance: () => playerStore.audioInstance,
+    hasOtherRuningInstances: (state) => state.options.instanced,
+    connectSpeakers: (state) => state.options.connectSpeakers,
+  },
+  actions: {
+    setAudio(audio: HTMLAudioElement) {
+      this.audioInstance = audio;
+    },
+    instance() {
+      this.options.instanced = true;
+      // disable connected Speakers for next instances
+      this.options.connectSpeakers = false;
+    },
+  }
 });
 
 if (import.meta.hot) {
