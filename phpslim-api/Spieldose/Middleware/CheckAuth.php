056931c6846614e3f2144a73e0026f89dab783d5
alex <766f6964+github@gmail.com>
2025-12-05 13:31:39 +0100

    restored deleted invoke method contents removed by rector

--- phpslim-api/Spieldose/Middleware/CheckAuth.php
+++ phpslim-api/Spieldose/Middleware/CheckAuth.php
@@ -8,6 +8,8 @@ class CheckAuth
 {
     protected \Psr\Log\LoggerInterface $logger;
 
+    private readonly string $passphrase;
+
     protected \aportela\DatabaseWrapper\DB $dbh;
 
     public function __construct(\Psr\Container\ContainerInterface $container)
@@ -18,6 +20,7 @@ class CheckAuth
         }
 
         $this->logger = $logger;
+        $this->passphrase = new \Spieldose\Settings()->getJWTPassphrase();
         $dbh = $container->get(\aportela\DatabaseWrapper\DB::class);
         if (! $dbh instanceof \aportela\DatabaseWrapper\DB) {
             throw new \RuntimeException("Failed to create database handler from container");
@@ -34,6 +37,58 @@ class CheckAuth
      */
     public function __invoke(\Psr\Http\Message\ServerRequestInterface $serverRequest, \Psr\Http\Server\RequestHandlerInterface $requestHandler): \Psr\Http\Message\ResponseInterface
     {
-        return ($requestHandler->handle($serverRequest));
+        // check for valid (non empty/not expired) access token on current session (requires cookies sent by client)
+        if (\Spieldose\UserSession::hasValidAccessToken()) {
+            return ($requestHandler->handle($serverRequest));
+        } else {
+            \Spieldose\UserSession::unsetAccessTokenData();
+            // check for valid Authorization Bearer header with (JWT) access token
+            $authorizationHeader = $serverRequest->getHeader('Authorization');
+            if ($authorizationHeader === []) {
+                throw new \Spieldose\Exception\UnauthorizedException('Authorization header is missing');
+            }
+
+            $bearerToken = $authorizationHeader[0];
+            if (!str_starts_with($bearerToken, 'Bearer ')) {
+                throw new \Spieldose\Exception\UnauthorizedException('Invalid Authorization header format');
+            }
+
+            $bearerToken = trim(substr($bearerToken, 7));
+            if ($bearerToken === '' || $bearerToken === '0') {
+                throw new \Spieldose\Exception\UnauthorizedException('Bearer token is missing');
+            } else {
+                $decoded = null;
+                try {
+                    $decoded = new \Spieldose\JWT($this->logger, $this->passphrase)->decode($bearerToken);
+                } catch (\Firebase\JWT\ExpiredException $e) {
+                    $this->logger->notice("JWT expired", [$e->getMessage()]);
+                    throw new \Spieldose\Exception\UnauthorizedException("JWT expired");
+                } catch (\Throwable $e) {
+                    $this->logger->notice("JWT decode error", [$e->getMessage()]);
+                    throw new \Spieldose\Exception\UnauthorizedException("JWT decode error");
+                }
+
+                if (property_exists($decoded, "sub") && is_string($decoded->sub) && ($decoded->sub !== '' && $decoded->sub !== '0')) {
+                    $user = new \Spieldose\User($decoded->sub);
+                    try {
+                        $user->get($this->dbh);
+                    } catch (\Spieldose\Exception\NotFoundException) {
+                        throw new \Spieldose\Exception\NotFoundException("userId");
+                    }
+
+                    // access token from Authorization Bearer header is good, set new valid session & continue
+                    \Spieldose\UserSession::init(strval($user->id), strval($user->email));
+                    if (is_numeric($decoded->exp)) {
+                        \Spieldose\UserSession::setAccessTokenData($bearerToken, intval($decoded->exp));
+                    } else {
+                        throw new \Spieldose\Exception\UnauthorizedException('Invalid token expiration');
+                    }
+                } else {
+                    throw new \Spieldose\Exception\UnauthorizedException('Bearer token is missing');
+                }
+
+                return ($requestHandler->handle($serverRequest));
+            }
+        }
     }
 }
