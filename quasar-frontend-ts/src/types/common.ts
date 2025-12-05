4ba2825780dfc8706876b13da903f0b07d33a428
alex <766f6964+github@gmail.com>
2025-12-05 01:20:40 +0100

    added player status type

--- quasar-frontend-ts/src/types/common.ts
+++ quasar-frontend-ts/src/types/common.ts
@@ -6,6 +6,8 @@ type VinylAnimation = "rotate" | null;
 
 type SpectrumAnalyzerChannelLayout = "single" | "dual-combined" | "dual-horizontal" | "dual-vertical";
 
+type PlayerStatus = "stopped" | "playing" | "paused";
+
 export {
-  type EnvironmentType, type ValidAuthTypes, type VinylAnimation, type SpectrumAnalyzerChannelLayout
+  type EnvironmentType, type ValidAuthTypes, type VinylAnimation, type SpectrumAnalyzerChannelLayout, type PlayerStatus
 };
