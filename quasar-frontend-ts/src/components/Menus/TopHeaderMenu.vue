f7ae596d95f4187bad5b7fd64c978792efe065d3
alex <766f6964+github@gmail.com>
2025-12-05 11:44:07 +0100

    added default top header menu items

--- quasar-frontend-ts/src/components/Menus/TopHeaderMenu.vue
+++ quasar-frontend-ts/src/components/Menus/TopHeaderMenu.vue
@@ -1,13 +1,14 @@
 <template>
   <q-btn-group flat class="q-ml-md" v-if="visible">
-    <q-btn stack v-for="item in menuItems" size="md" no-caps :icon="item.icon" :key="item.text" :to="item.routeName">{{
-      item.text
-    }}</q-btn>
+    <q-btn stack v-for="item in topHeaderMenuItems" size="md" no-caps :icon="item.icon" :key="item.text"
+      :to="item.routeName">{{
+        item.text
+      }}</q-btn>
   </q-btn-group>
 </template>
 
 <script setup lang="ts">
-import { type MenuItem } from "src/types/menu-item";
+import { type MenuItem, topHeaderMenuItems } from "src/types/menu-item";
 
 interface TopHeaderMenuProps {
   visible: boolean;
@@ -15,54 +16,4 @@ interface TopHeaderMenuProps {
 
 defineProps<TopHeaderMenuProps>();
 
-const menuItems: MenuItem[] = [
-  { icon: 'home', text: "Index", routeName: 'index' },
-  { icon: 'search', text: "Search", routeName: 'search' },
-
-  {
-    icon: 'analytics',
-    text: 'Dashboard',
-    routeName: 'dashboard'
-  },
-
-  {
-    icon: 'list_alt',
-    text: 'Current playlist',
-    routeName: 'currentPlaylist'
-  },
-  {
-    icon: 'person',
-    text: 'Browse artists',
-    routeName: 'artists'
-  },
-  /*
-  {
-    icon: 'search',
-    text: 'Search',
-    routeName: 'search'
-  },
-  */
-  {
-    icon: 'album',
-    text: 'Browse albums',
-    routeName: 'albums'
-  },
-  {
-    icon: 'folder_open',
-    text: 'Browse paths',
-    routeName: 'paths'
-  },
-
-  {
-    icon: 'list',
-    text: 'Browse playlists',
-    routeName: 'playlists'
-  },
-  {
-    icon: 'radio',
-    text: 'Browse radio stations',
-    routeName: 'radioStations'
-  },
-  { icon: 'account_circle', text: "My profile", routeName: 'profile' },
-];
 </script>
--- quasar-frontend-ts/src/types/menu-item.ts
+++ quasar-frontend-ts/src/types/menu-item.ts
@@ -6,4 +6,55 @@ interface MenuItem {
   action?: () => void;
 };
 
-export { type MenuItem };
+const topHeaderMenuItems: MenuItem[] = [
+  { icon: 'home', text: "Index", routeName: 'index' },
+  { icon: 'search', text: "Search", routeName: 'search' },
+
+  {
+    icon: 'analytics',
+    text: 'Dashboard',
+    routeName: 'dashboard'
+  },
+
+  {
+    icon: 'list_alt',
+    text: 'Current playlist',
+    routeName: 'currentPlaylist'
+  },
+  {
+    icon: 'person',
+    text: 'Browse artists',
+    routeName: 'artists'
+  },
+  /*
+  {
+    icon: 'search',
+    text: 'Search',
+    routeName: 'search'
+  },
+  */
+  {
+    icon: 'album',
+    text: 'Browse albums',
+    routeName: 'albums'
+  },
+  {
+    icon: 'folder_open',
+    text: 'Browse paths',
+    routeName: 'paths'
+  },
+
+  {
+    icon: 'list',
+    text: 'Browse playlists',
+    routeName: 'playlists'
+  },
+  {
+    icon: 'radio',
+    text: 'Browse radio stations',
+    routeName: 'radioStations'
+  },
+  { icon: 'account_circle', text: "My profile", routeName: 'profile' },
+];
+
+export { type MenuItem, topHeaderMenuItems };
