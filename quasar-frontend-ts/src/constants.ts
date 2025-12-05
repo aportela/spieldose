7358a67bd5966d3832aaaf5ae570d56e70d58f59
alex <766f6964+github@gmail.com>
2025-12-05 16:08:15 +0100

    new constants added

--- quasar-frontend-ts/src/constants.ts
+++ quasar-frontend-ts/src/constants.ts
@@ -5,3 +5,13 @@ export const LOCAL_STORAGE_NAMESPACE = "spieldose#";
 export const SERVER_API_BASE_PATH = "api2";
 
 export const DEFAULT_LOCALE = "en-US";
+
+export const THUMBNAIL_API_URL = `/${SERVER_API_BASE_PATH}/remote_thumbnail`;
+
+export const SMALL_THUMBNAIL_WIDTH = 200;
+export const SMALL_THUMBNAIL_HEIGHT = 200;
+export const SMALL_THUMBNAIL_QUALITY = 90;
+
+export const MEDIUM_THUMBNAIL_WIDTH = 400;
+export const MEDIUM_THUMBNAIL_HEIGHT = 400;
+export const MEDIUM_THUMBNAIL_QUALITY = 90;
