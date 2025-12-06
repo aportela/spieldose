c4355a9f5e5e63811462e8dbd7951926cd4b50f1
alex <766f6964+github@gmail.com>
2025-12-06 16:05:49 +0100

    code cleanups

--- quasar-frontend-ts/src/components/Widgets/Player/SpectrumAnalyzer.vue
+++ quasar-frontend-ts/src/components/Widgets/Player/SpectrumAnalyzer.vue
@@ -7,7 +7,7 @@
 <script setup lang="ts">
 import { ref, watch, onMounted, onBeforeUnmount } from "vue";
 import { useI18n } from "vue-i18n";
-import AudioMotionAnalyzer from "audiomotion-analyzer";
+import { AudioMotionAnalyzer, type ConstructorOptions as AudioMotionAnalyzerConstructorOptionsInterface, type GradientOptions as GradientOptionsInterface } from "audiomotion-analyzer";
 import { usePlayerStore } from "src/stores/player";
 import { useAudioMotionAnalyzerStore } from "src/stores/audioMotionAnalyzer";
 import { useMiniSpectrumAnalyzerSettingsStore } from "src/stores/miniSpectrumAnalyzerSettings";
@@ -18,7 +18,13 @@ const playerStore = usePlayerStore();
 const audioMotionAnalyzerStore = useAudioMotionAnalyzerStore();
 const miniSpectrumAnalyzerSettingsStore = useMiniSpectrumAnalyzerSettingsStore();
 
-const analyzer = ref(null);
+const analyzerInstance = ref<AudioMotionAnalyzer | null>(null);
+
+const defaultAnalyzerConstructorOptions: AudioMotionAnalyzerConstructorOptionsInterface = {
+  source: audioMotionAnalyzerStore.audioInstance,
+  connectSpeakers: audioMotionAnalyzerStore.connectSpeakers,
+  start: false,
+};
 
 const defaultAnalyzerOptions = {
   showCanvas: true,
@@ -35,7 +41,7 @@ const defaultAnalyzerOptions = {
   showScaleX: false,
   showScaleY: false,
   channelLayout: miniSpectrumAnalyzerSettingsStore.currentChannelLayout,
-  colorcurrentMode: 'gradient',
+  colorMode: 'gradient',
   splitGradient: false,
   bgAlpha: 1,
   overlay: true,
@@ -43,74 +49,78 @@ const defaultAnalyzerOptions = {
 };
 
 watch(() => playerStore.hasPreviousUserInteractions, (newValue) => {
-  if (!analyzer.value) {
-    if (newValue) {
-      createAudioMotionAnalyzer(defaultAnalyzerOptions, true);
-    }
-  } else {
-    if (newValue) {
-      analyzer.value.start();
+  if (newValue) {
+    if (analyzerInstance.value === null) {
+      createAudioMotionAnalyzerInstance(defaultAnalyzerConstructorOptions, defaultAnalyzerOptions, true);
+    } else {
+      startAnalyzer();
     }
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.currentMode, (newValue) => {
-  if (analyzer.value && (newValue == 10 || (newValue >= 0 && newValue <= 8))) {
-    analyzer.value.setOptions({ mode: newValue, barSpace: miniSpectrumAnalyzerSettingsStore.currentBarSpace });
+  if (analyzerInstance.value && (newValue == 10 || (newValue >= 0 && newValue <= 8))) {
+    analyzerInstance.value.setOptions({ mode: newValue, barSpace: miniSpectrumAnalyzerSettingsStore.currentBarSpace });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.currentBarSpace, (newValue) => {
-  if (analyzer.value && newValue >= 0 && newValue <= 1) {
-    analyzer.value.setOptions({ mode: miniSpectrumAnalyzerSettingsStore.currentMode, barSpace: newValue });
+  if (analyzerInstance.value && newValue >= 0 && newValue <= 1) {
+    analyzerInstance.value.setOptions({ mode: miniSpectrumAnalyzerSettingsStore.currentMode, barSpace: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.currentFPS, (newValue) => {
-  if (analyzer.value && newValue >= 0 && newValue <= 144) {
-    analyzer.value.setOptions({ maxFPS: newValue });
+  if (analyzerInstance.value && newValue >= 0 && newValue <= 144) {
+    analyzerInstance.value.setOptions({ maxFPS: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.currentHeight, (newValue) => {
-  if (analyzer.value && newValue > 0) {
-    analyzer.value.setOptions({ height: newValue });
+  if (analyzerInstance.value && newValue > 0) {
+    analyzerInstance.value.setOptions({ height: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.currentChannelLayout, (newValue) => {
-  if (analyzer.value) {
-    analyzer.value.setOptions({ channelLayout: newValue });
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ channelLayout: newValue });
+  }
+});
+
+watch(() => miniSpectrumAnalyzerSettingsStore.currentColorMode, (newValue) => {
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ colorMode: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.currentGradient, (newValue) => {
-  if (analyzer.value) {
-    analyzer.value.setOptions({ gradient: newValue });
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ gradient: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.showPeaks, (newValue) => {
-  if (analyzer.value) {
-    analyzer.value.setOptions({ showPeaks: newValue });
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ showPeaks: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.ledBarsActive, (newValue) => {
-  if (analyzer.value) {
-    analyzer.value.setOptions({ ledBars: newValue });
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ ledBars: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.trueLedsActive, (newValue) => {
-  if (analyzer.value) {
-    analyzer.value.setOptions({ trueLeds: newValue });
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ trueLeds: newValue });
   }
 });
 
 watch(() => miniSpectrumAnalyzerSettingsStore.isLoResActive, (newValue) => {
-  if (analyzer.value) {
-    analyzer.value.setOptions({ loRes: newValue });
+  if (analyzerInstance.value) {
+    analyzerInstance.value.setOptions({ loRes: newValue });
   }
 });
 
@@ -122,50 +132,63 @@ const onToggleCurrentMode = () => {
   miniSpectrumAnalyzerSettingsStore.setMode(mode);
 };
 
-const createAudioMotionAnalyzer = (defaultOptions, start) => {
-  if (!analyzer.value) {
-    analyzer.value = new AudioMotionAnalyzer(
-      document.getElementById('spieldose-sidebar-analyzer-container'),
-      defaultOptions
-    );
-    const gradientOptions = {
-      bgColor: '#fff',
-      dir: 'v',
-      colorStops: [
-        { color: '#d30320', level: 0.9 },
-        { color: '#d72c43', level: 0.8 },
-        { color: '#db5063', level: 0.6 },
-        { color: '#de6b7b', level: 0.4 },
-        { color: '#e399a3', level: 0.2 }
-      ]
+const startAnalyzer = (): void => {
+  if (analyzerInstance.value !== null) {
+    if (!analyzerInstance.value.isOn) {
+      analyzerInstance.value.start();
     }
-    analyzer.value.registerGradient('spieldose', gradientOptions);
-    analyzer.value.gradient = miniSpectrumAnalyzerSettingsStore.currentGradient;
-    if (!audioMotionAnalyzerStore.hasOtherRuningInstances) {
-      audioMotionAnalyzerStore.instance();
-    }
-    if (start) {
-      analyzer.value.start();
+  } else {
+    console.error("AudioMotion analyzer instance is null");
+  }
+};
+
+const createAudioMotionAnalyzerInstance = (constructorOptions: AudioMotionAnalyzerConstructorOptionsInterface, defaultOptions: AudioMotionAnalyzerConstructorOptionsInterface, start: boolean) => {
+  if (analyzerInstance.value === null) {
+    const constainerElement = document.getElementById('spieldose-sidebar-analyzer-container');
+    if (constainerElement !== null) {
+      analyzerInstance.value = new AudioMotionAnalyzer(
+        constainerElement,
+        constructorOptions
+      );
+      if (!audioMotionAnalyzerStore.hasOtherRuningInstances) {
+        audioMotionAnalyzerStore.instance();
+      }
+      analyzerInstance.value.setOptions(defaultOptions);
+      const gradientOptions: GradientOptionsInterface = {
+        bgColor: '#fff',
+        //dir: 'v',
+        colorStops: [
+          { color: '#d30320', level: 0.9 },
+          { color: '#d72c43', level: 0.8 },
+          { color: '#db5063', level: 0.6 },
+          { color: '#de6b7b', level: 0.4 },
+          { color: '#e399a3', level: 0.2 }
+        ]
+      }
+      analyzerInstance.value.registerGradient('spieldose', gradientOptions);
+      analyzerInstance.value.gradient = miniSpectrumAnalyzerSettingsStore.currentGradient;
+      if (start) {
+        startAnalyzer();
+      }
     }
   }
 };
 
-const destroyAudioMotionAnalyzer = () => {
-  if (analyzer.value) {
-    analyzer.value.stop();
-    // TODO: stops audio
-    //analyzer.value.destroy();
+const destroyAudioMotionAnalyzerInstance = () => {
+  if (analyzerInstance.value !== null) {
+    analyzerInstance.value.stop();
+    // TODO: WARNING: possible leak, next line stops audio (check disconnectInput method before destroy)
+    //analyzerInstance.value.destroy();
   }
 };
-
 
 onMounted(() => {
   // TODO: WARNING: on empty playlists js console show warning about AudioContext auto start denied
-  createAudioMotionAnalyzer(defaultAnalyzerOptions, playerStore.hasPreviousUserInteractions);
+  createAudioMotionAnalyzerInstance(defaultAnalyzerConstructorOptions, defaultAnalyzerOptions, playerStore.hasPreviousUserInteractions);
 });
 
 onBeforeUnmount(() => {
-  destroyAudioMotionAnalyzer();
+  destroyAudioMotionAnalyzerInstance();
 });
 
 </script>
