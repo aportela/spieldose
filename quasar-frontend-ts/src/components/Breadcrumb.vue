33396b7a76c6f1011083bc8cfbd9a7ec0afb9d6d
alex <766f6964+github@gmail.com>
2025-12-08 00:02:43 +0100

    new Breadcrumb component

---
+++ quasar-frontend-ts/src/components/Breadcrumb.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-breadcrumbs class="q-mb-lg">
+    <q-breadcrumbs-el icon="home" label="Spieldose" />
+    <slot name="current-breadcrumb" v-if="icon && label">
+      <q-breadcrumbs-el :icon="icon" :label="t(label)" />
+    </slot>
+  </q-breadcrumbs>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+interface BreadcrumbProps {
+  icon?: string;
+  label?: string;
+};
+
+defineProps<BreadcrumbProps>();
+
+</script>
