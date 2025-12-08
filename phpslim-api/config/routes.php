736326b9c0d073f2ee4b6e342bc904b4353a696c
alex <766f6964+github@gmail.com>
2025-12-08 13:00:37 +0100

    moved common functions to top level

--- phpslim-api/config/routes.php
+++ phpslim-api/config/routes.php
@@ -91,6 +91,52 @@ return function (App $app): void {
             $settings = new \Spieldose\Settings();
             $serverEnvironment = \Spieldose\Utils::GetServerEnvironment($settings);
 
+            function getPagerFromParams(array $params = []): \aportela\DatabaseBrowserWrapper\Pager
+            {
+                $currentPageIndex = 1;
+                $resultsPage = \Spieldose\Browse\Base::DEFAULT_RESULTS_PAGE;
+                if (array_key_exists("pager", $params)) {
+                    if (array_key_exists("currentPageIndex", $params["pager"]) && is_numeric($params["pager"]["currentPageIndex"])) {
+                        $currentPageIndex = intval($params["pager"]["currentPageIndex"]);
+                    }
+
+                    if (array_key_exists("resultsPage", $params["pager"]) && is_numeric($params["pager"]["resultsPage"])) {
+                        $resultsPage = intval($params["pager"]["resultsPage"]);
+                    }
+                }
+                return (new \aportela\DatabaseBrowserWrapper\Pager(true, $currentPageIndex, $resultsPage));
+            };
+
+            function getSortFromParams(array $params = [], string $defaultSortField = '', ?\aportela\DatabaseBrowserWrapper\Order $defaultSortOrder = null, bool $caseInsensitive = false): \aportela\DatabaseBrowserWrapper\Sort
+            {
+                $sortItem = null;
+                if (array_key_exists("sort", $params)) {
+                    $sortItem = new \aportela\DatabaseBrowserWrapper\SortItem(
+                        array_key_exists("field", $params["sort"]) && is_string($params["sort"]["field"]) ? $params["sort"]["field"] : $defaultSortField,
+                        array_key_exists("order", $params["sort"]) && is_string($params["sort"]["order"]) && in_array($params["sort"]["order"], ["ASC", "DESC"]) ? \aportela\DatabaseBrowserWrapper\Order::from($params["sort"]["order"]) : $defaultSortOrder,
+                        $caseInsensitive
+                    );
+                } else {
+                    $sortItem = new \aportela\DatabaseBrowserWrapper\SortItem(
+                        $defaultSortField,
+                        $defaultSortOrder,
+                        $caseInsensitive
+                    );
+                }
+
+                return (new \aportela\DatabaseBrowserWrapper\Sort([$sortItem]));
+            };
+
+            function getFilterFromParams(array $params = []): \aportela\DatabaseBrowserWrapper\Filter
+            {
+                return (new \aportela\DatabaseBrowserWrapper\Filter(array_key_exists("filter", $params) && is_array($params["filter"]) ? $params["filter"] : []));
+            }
+
+            function skipCountTrueParamFound(array $params): bool
+            {
+                return (array_key_exists("skipCount", $params) && is_bool($params["skipCount"]) && $params["skipCount"] === true);
+            }
+
             $group->get('/server_environment', function (Request $request, Response $response, array $args) use ($serverEnvironment): \Psr\Http\Message\MessageInterface {
                 $payload = \Spieldose\Utils::getJSONPayload(
                     [
@@ -437,52 +483,6 @@ return function (App $app): void {
                 $dbh = $container->get(\aportela\DatabaseWrapper\DB::class);
                 if (! $dbh instanceof \aportela\DatabaseWrapper\DB) {
                     throw new \RuntimeException("Failed to create database handler from container");
-                }
-
-                function getPagerFromParams(array $params = []): \aportela\DatabaseBrowserWrapper\Pager
-                {
-                    $currentPageIndex = 1;
-                    $resultsPage = \Spieldose\Browse\Base::DEFAULT_RESULTS_PAGE;
-                    if (array_key_exists("pager", $params)) {
-                        if (array_key_exists("currentPageIndex", $params["pager"]) && is_numeric($params["pager"]["currentPageIndex"])) {
-                            $currentPageIndex = intval($params["pager"]["currentPageIndex"]);
-                        }
-
-                        if (array_key_exists("resultsPage", $params["pager"]) && is_numeric($params["pager"]["resultsPage"])) {
-                            $resultsPage = intval($params["pager"]["resultsPage"]);
-                        }
-                    }
-                    return (new \aportela\DatabaseBrowserWrapper\Pager(true, $currentPageIndex, $resultsPage));
-                };
-
-                function getSortFromParams(array $params = [], string $defaultSortField = '', ?\aportela\DatabaseBrowserWrapper\Order $defaultSortOrder = null, bool $caseInsensitive = false): \aportela\DatabaseBrowserWrapper\Sort
-                {
-                    $sortItem = null;
-                    if (array_key_exists("sort", $params)) {
-                        $sortItem = new \aportela\DatabaseBrowserWrapper\SortItem(
-                            array_key_exists("field", $params["sort"]) && is_string($params["sort"]["field"]) ? $params["sort"]["field"] : $defaultSortField,
-                            array_key_exists("order", $params["sort"]) && is_string($params["sort"]["order"]) && in_array($params["sort"]["order"], ["ASC", "DESC"]) ? \aportela\DatabaseBrowserWrapper\Order::from($params["sort"]["order"]) : $defaultSortOrder,
-                            $caseInsensitive
-                        );
-                    } else {
-                        $sortItem = new \aportela\DatabaseBrowserWrapper\SortItem(
-                            $defaultSortField,
-                            $defaultSortOrder,
-                            $caseInsensitive
-                        );
-                    }
-
-                    return (new \aportela\DatabaseBrowserWrapper\Sort([$sortItem]));
-                };
-
-                function getFilterFromParams(array $params = []): \aportela\DatabaseBrowserWrapper\Filter
-                {
-                    return (new \aportela\DatabaseBrowserWrapper\Filter(array_key_exists("filter", $params) && is_array($params["filter"]) ? $params["filter"] : []));
-                }
-
-                function skipCountTrueParamFound(array $params): bool
-                {
-                    return (array_key_exists("skipCount", $params) && is_bool($params["skipCount"]) && $params["skipCount"] === true);
                 }
 
                 $routeCollectorProxy->post('/artists', function (Request $request, Response $response, array $args) use ($dbh) {
