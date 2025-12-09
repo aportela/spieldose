9d8aec672e86e2c84e0182b4d8900859fb681d5a
alex <766f6964+github@gmail.com>
2025-12-09 09:30:23 +0100

    new component for show track images

---
+++ quasar-frontend-ts/src/components/TrackImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-img :src="url" ratio="1" @error="error = true" placeholder-src="#" />
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+interface TrackImageProps {
+  src: string | null;
+};
+
+const props = defineProps<TrackImageProps>();
+const defaultImage = "vectors/Vinyl_record.svg";
+const error = ref<boolean>(false);
+const url = computed(() => props.src ? (!error.value ? props.src : defaultImage) : defaultImage);
+
+</script>
