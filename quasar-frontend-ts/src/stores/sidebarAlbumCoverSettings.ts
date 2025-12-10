25966c59571678064d85478b08fb4cc9eacf363c
alex <766f6964+github@gmail.com>
2025-12-10 18:26:11 +0100

    new sidebar album cover settings store

---
+++ quasar-frontend-ts/src/stores/sidebarAlbumCoverSettings.ts
@@ -0,0 +1,60 @@
+import { defineStore, acceptHMRUpdate } from 'pinia';
+import { createStorageEntry } from 'src/composables/localStorage';
+
+type AlbumCoverMode = "staticImage" | "vinyl" | "cassetteTape";
+
+const localStorageVisible = createStorageEntry<boolean>("visualizations.sidebar.albumCover.visible", true);
+const localStorageMode = createStorageEntry<AlbumCoverMode>("visualizations.sidebar.albumCover.mode", "staticImage");
+const localStoragePlaybackAnimation = createStorageEntry<boolean>("visualizations.sidebar.albumCover.playbackAnimation", true);
+
+interface State {
+  settings: {
+    visible: boolean;
+    mode: AlbumCoverMode;
+    playBackAnimation: boolean;
+  }
+};
+
+export const useSidebarAlbumCoverSettingsStore = defineStore('sidebarAlbumCoverSettingsStore', {
+  state: (): State => ({
+    settings: {
+      visible: localStorageVisible.get(),
+      mode: localStorageMode.get(),
+      playBackAnimation: localStoragePlaybackAnimation.get(),
+    },
+  }),
+  getters: {
+    hasStaticImageMode: (state): boolean => state.settings.mode === "staticImage",
+    hasVinilMode: (state): boolean => state.settings.mode === "vinyl",
+    hasCassetteTapeMode: (state): boolean => state.settings.mode === "cassetteTape",
+    hasPlayBackAnimation: (state): boolean => state.settings.playBackAnimation,
+  },
+  actions: {
+    setVisible(visible: boolean) {
+      this.settings.visible = visible;
+      localStorageVisible.set(this.settings.visible);
+    },
+    toggleMode() {
+      switch (this.settings.mode) {
+        case "staticImage":
+          this.settings.mode = "vinyl";
+          break;
+        case "vinyl":
+          this.settings.mode = "cassetteTape";
+          break;
+        case "cassetteTape":
+          this.settings.mode = "staticImage";
+          break;
+      }
+      localStorageMode.set(this.settings.mode);
+    },
+    togglePlaybackAnimation() {
+      this.settings.playBackAnimation = !this.settings.playBackAnimation;
+      localStoragePlaybackAnimation.set(this.settings.playBackAnimation);
+    }
+  }
+});
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useSidebarAlbumCoverSettingsStore, import.meta.hot));
+}
