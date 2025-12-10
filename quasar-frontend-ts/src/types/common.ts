d6358c843dfe9c463576fc8d10467cdc4c961c1d
alex <766f6964+github@gmail.com>
2025-12-11 00:34:48 +0100

    added Player interface

--- quasar-frontend-ts/src/types/common.ts
+++ quasar-frontend-ts/src/types/common.ts
@@ -1,81 +1,101 @@
-import { type SortOrder } from "./sort";
+import { type SortOrder } from './sort';
 
-type EnvironmentType = "development" | "production";
+type EnvironmentType = 'development' | 'production';
 
-type ValidAuthTypes = "Bearer";
+type ValidAuthTypes = 'Bearer';
 
-type VinylAnimation = "vinyl" | "cassete" | null;
+type VinylAnimation = 'vinyl' | 'cassete' | null;
 
-type SpectrumAnalyzerChannelLayout = "single" | "dual-combined" | "dual-horizontal" | "dual-vertical";
+type SpectrumAnalyzerChannelLayout =
+  | 'single'
+  | 'dual-combined'
+  | 'dual-horizontal'
+  | 'dual-vertical';
 
-type PlayerStatus = "stopped" | "playing" | "paused";
-type PlayerRepeatMode = "none" | "track" | "playList";
+interface Player {
+  userInteracted: boolean;
+  status: PlayerStatus;
+  repeatMode: PlayerRepeatMode;
+  shuffle: boolean;
+}
 
-type PlayListTableColumnName = "index" | "image" | "trackTitle" | "trackArtist" | "trackAlbumTitle" | "trackAlbumNumber" | "trackAlbumArtist" | "year" | "actions";
+type PlayerStatus = 'stopped' | 'playing' | 'paused';
+type PlayerRepeatMode = 'none' | 'track' | 'playList';
+
+type PlayListTableColumnName =
+  | 'index'
+  | 'image'
+  | 'trackTitle'
+  | 'trackArtist'
+  | 'trackAlbumTitle'
+  | 'trackAlbumNumber'
+  | 'trackAlbumArtist'
+  | 'year'
+  | 'actions';
 
 interface PlayListTableColumn {
   name: PlayListTableColumnName;
   label: string;
   index: number;
   visible: boolean;
-};
+}
 
 const availablePlayListTableColumns: PlayListTableColumn[] = [
   {
-    name: "index",
-    label: "Index",
+    name: 'index',
+    label: 'Index',
     index: 0,
     visible: true,
   },
   {
-    name: "image",
-    label: "Image",
+    name: 'image',
+    label: 'Image',
     index: 1,
     visible: false,
   },
   {
-    name: "trackTitle",
-    label: "Title",
+    name: 'trackTitle',
+    label: 'Title',
     index: 2,
     visible: true,
   },
   {
-    name: "trackArtist",
-    label: "Artist",
+    name: 'trackArtist',
+    label: 'Artist',
     index: 3,
     visible: true,
   },
   {
-    name: "trackAlbumTitle",
-    label: "Album",
+    name: 'trackAlbumTitle',
+    label: 'Album',
     index: 4,
     visible: true,
   },
   {
-    name: "trackAlbumNumber",
-    label: "Track album number",
+    name: 'trackAlbumNumber',
+    label: 'Track album number',
     index: 5,
     visible: true,
   },
   {
-    name: "trackAlbumArtist",
-    label: "Album artist",
+    name: 'trackAlbumArtist',
+    label: 'Album artist',
     index: 6,
     visible: true,
   },
 
   {
-    name: "year",
-    label: "Year",
+    name: 'year',
+    label: 'Year',
     index: 7,
     visible: true,
   },
   {
-    name: "actions",
-    label: "Actions",
+    name: 'actions',
+    label: 'Actions',
     index: 8,
     visible: true,
-  }
+  },
 ];
 
 interface SelectorOption {
@@ -85,17 +105,28 @@ interface SelectorOption {
 
 const sortOrderSelectorOptions: SelectorOption[] = [
   {
-    label: "Ascending",
-    value: "ASC",
+    label: 'Ascending',
+    value: 'ASC',
   },
   {
-    label: "Descending",
-    value: "DESC",
-  }
+    label: 'Descending',
+    value: 'DESC',
+  },
 ];
 
-type AudioMotionAnalyzerOptionColorMode = "gradient" | "bar-index" | "bar-level";
+type AudioMotionAnalyzerOptionColorMode = 'gradient' | 'bar-index' | 'bar-level';
 
 export {
-  type EnvironmentType, type ValidAuthTypes, type VinylAnimation, type SpectrumAnalyzerChannelLayout, type PlayerStatus, type PlayerRepeatMode, type PlayListTableColumn, availablePlayListTableColumns, type SelectorOption, sortOrderSelectorOptions, type AudioMotionAnalyzerOptionColorMode
+  type EnvironmentType,
+  type ValidAuthTypes,
+  type VinylAnimation,
+  type SpectrumAnalyzerChannelLayout,
+  type Player,
+  type PlayerStatus,
+  type PlayerRepeatMode,
+  type PlayListTableColumn,
+  availablePlayListTableColumns,
+  type SelectorOption,
+  sortOrderSelectorOptions,
+  type AudioMotionAnalyzerOptionColorMode,
 };
