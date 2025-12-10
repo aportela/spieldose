46af5c0444d22a0f7bcbc912222ed3be39635a70
alex <766f6964+github@gmail.com>
2025-12-10 21:37:02 +0100

    added support for failback image && new "no image" placeholder

--- quasar-frontend-ts/src/components/Widgets/Visualizations/StaticAlbumCoverImage.vue
+++ quasar-frontend-ts/src/components/Widgets/Visualizations/StaticAlbumCoverImage.vue
@@ -1,34 +1,42 @@
 <template>
   <div :class="{ 'cursor-pointer': clickable }" @click="onClick">
-    <q-img v-if="currentImage" :src="currentImage" @error="currentImage = null" alt="Album cover" :ratio="1"
-      width="100%" spinner-color="pink" />
-    <q-img v-else src="vectors/Vinyl_record.svg" alt="Vinyl" :ratio="1" width="100%" spinner-color="pink" />
+    <q-img v-if="currentImage" :src="currentImage" @error="currentImage = null" :ratio="1" width="100%"
+      spinner-color="pink" />
+    <div v-else class="no-image shadow-6">
+      <p class="q-mt-xl no-image-label no-image-top-label text-grey-7">Spieldose</p>
+      <p class="q-mt-xl no-image-label no-image-bottom-label text-pink">Awesome Mix Vol. {{ currentDayOfYear }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { nextTick, ref, watch } from "vue";
+import { nextTick, ref, watch, computed } from "vue";
+import { date } from "quasar";
 
 interface StaticAlbumCoverImageProps {
   image?: string | null;
+  failbackImage?: string | null;
   clickable?: boolean;
 };
 
 const props = withDefaults(defineProps<StaticAlbumCoverImageProps>(), {
   image: null,
+  failbackImage: null,
   clickable: false,
 });
 
 const emit = defineEmits(['onClick']);
 
-const currentImage = ref<string | null>(props.image);
+const currentImage = ref<string | null>(props.image ?? props.failbackImage ?? null);
+
+const currentDayOfYear = computed(() => date.formatDate(new Date(), "DDD"));
 
 watch(() => props.image, (newValue: string | null) => {
   currentImage.value = null;
-  if (newValue) {
+  if (newValue || props.failbackImage) {
     nextTick()
       .then(() => {
-        currentImage.value = newValue;
+        currentImage.value = newValue ?? props.failbackImage;
       }).catch((e) => {
         console.error(e);
       });
@@ -43,4 +51,36 @@ const onClick = () => {
 
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
+
+.no-image {
+  width: 98%;
+  margin: 1% auto;
+  height: auto;
+  aspect-ratio: 1;
+}
+
+.no-image-label {
+  font-family: "Gochi Hand", cursive;
+  font-weight: 400;
+  font-style: normal;
+  width: 100%;
+  white-space: nowrap;
+  text-align: center;
+  display: inline-block;
+  transform-origin: center;
+}
+
+.no-image-top-label {
+  color: hsl(46, 93%, 45%);
+  font-size: 6em;
+  transform: rotate(-5deg);
+}
+
+
+.no-image-bottom-label {
+  font-size: 3em;
+  transform: rotate(-3deg);
+}
+</style>
