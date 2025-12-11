24ad53338a63c470dcc2a9bccbaef7031d59fdf7
alex <766f6964+github@gmail.com>
2025-12-11 01:52:12 +0100

    shadow removed

--- quasar-frontend-ts/src/components/Widgets/Visualizations/StaticAlbumCoverImage.vue
+++ quasar-frontend-ts/src/components/Widgets/Visualizations/StaticAlbumCoverImage.vue
@@ -2,7 +2,7 @@
   <div :class="{ 'cursor-pointer': clickable }" @click="onClick">
     <q-img v-if="currentImage" :src="currentImage" @error="currentImage = failbackImage ?? null" :ratio="1" width="100%"
       spinner-color="pink" />
-    <div v-else class="no-image shadow-6 overflow-hidden">
+    <div v-else class="no-image overflow-hidden">
       <p class="q-mt-xl no-image-label no-image-top-label text-grey-7">Spieldose</p>
       <p class="q-mt-xl no-image-label no-image-bottom-label text-pink">Awesome Mix Vol. {{ currentDayOfYear }}</p>
     </div>
