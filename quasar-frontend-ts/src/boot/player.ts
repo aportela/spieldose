88b362f250d320d024147576238104eca21edeb8
alex <766f6964+github@gmail.com>
2025-12-11 01:33:04 +0100

    replaced playerStore with currentPlayListsStore

--- quasar-frontend-ts/src/boot/player.ts
+++ quasar-frontend-ts/src/boot/player.ts
@@ -1,45 +1,21 @@
-import { defineBoot } from '#q-app/wrappers'
+import { defineBoot } from '#q-app/wrappers';
 import { watch } from 'vue';
-import { usePlayerStore } from 'src/stores/player';
-//import { useCurrentPlaylistItemStore } from 'src/stores/currentPlaylistItem';
 import { useCurrentPlayListsStore } from 'src/stores/currentPlayLists';
 
-const playerStore = usePlayerStore();
-//const currentPlaylistItemStore = useCurrentPlaylistItemStore();
 const currentPlayListsStore = useCurrentPlayListsStore();
-
-/*
-watch(
-  () => currentPlaylistItemStore.t,
-  (newValue: number) => {
-    console.debug(`Last change: ${newValue}`);
-    if (currentPlaylistItemStore.isTrack) {
-      playerStore.setAudioSource(
-        "/api2/file/raw/" + currentPlaylistItemStore.trackFileId,
-      );
-      if (playerStore.hasPreviousUserInteractions) {
-        playerStore.play(true);
-      }
-    }
-  },
-);
-*/
 
 watch(
   () => currentPlayListsStore.currentFileId,
   (newValue: string | null) => {
     if (newValue) {
-      playerStore.setAudioSource(
-        "/api2/file/raw/" + newValue,
-      );
-      if (playerStore.hasPreviousUserInteractions) {
-        playerStore.play(true);
+      currentPlayListsStore.setAudioSource('/api2/file/raw/' + newValue);
+      if (currentPlayListsStore.playerHasPreviousUserInteractions) {
+        currentPlayListsStore.playerActionPlay(true);
       }
     }
   },
 );
 
-
-export default defineBoot((/* { app, router, ... } */) => {
-  playerStore.create();
-})
+export default defineBoot(() => {
+  currentPlayListsStore.create();
+});
