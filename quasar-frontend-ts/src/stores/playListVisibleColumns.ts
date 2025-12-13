9f752830ec55f67e3927cb5702eb9a6bc706ab31
alex <766f6964+github@gmail.com>
2025-12-13 16:02:19 +0100

    columns added

--- quasar-frontend-ts/src/stores/playListVisibleColumns.ts
+++ quasar-frontend-ts/src/stores/playListVisibleColumns.ts
@@ -1,7 +1,78 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
+import { createStorageEntry } from 'src/composables/localStorage';
 
+const localStoragePlaylistIndexColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.index',
+  true,
+);
+
+const localStoragePlaylistImageColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.image',
+  false,
+);
+
+const localStoragePlaylistTrackTitleColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.trackTitle',
+  true,
+);
+
+const localStoragePlaylistTrackArtistColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.trackArtist',
+  true,
+);
+
+const localStoragePlaylistTrackAlbumTitleColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.trackAlbumTitle',
+  true,
+);
+
+const localStoragePlaylistTrackAlbumNumberColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.trackAlbumNumber',
+  true,
+);
+
+const localStoragePlaylistTrackAlbumArtistColumnVisibility = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.trackAlbumArtist',
+  true,
+);
+
+const localStoragePlaylistTrackYear = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.year',
+  true,
+);
+
+const localStoragePlaylistActions = createStorageEntry<boolean>(
+  'interface.playlist.columnVisibility.actions',
+  true,
+);
+
+interface State {
+  columns: {
+    index: boolean;
+    image: boolean;
+    trackTitle: boolean;
+    trackArtist: boolean;
+    trackAlbumTitle: boolean;
+    trackAlbumNumber: boolean;
+    trackAlbumArtist: boolean;
+    year: boolean;
+    actions: boolean;
+  };
+}
 export const usePlayListVisibleColumnsStore = defineStore('playListVisibleColumnsStore', {
-  state: () => ({}),
+  state: (): State => ({
+    columns: {
+      index: localStoragePlaylistIndexColumnVisibility.get(),
+      image: localStoragePlaylistImageColumnVisibility.get(),
+      trackTitle: localStoragePlaylistTrackTitleColumnVisibility.get(),
+      trackArtist: localStoragePlaylistTrackArtistColumnVisibility.get(),
+      trackAlbumTitle: localStoragePlaylistTrackAlbumTitleColumnVisibility.get(),
+      trackAlbumNumber: localStoragePlaylistTrackAlbumNumberColumnVisibility.get(),
+      trackAlbumArtist: localStoragePlaylistTrackAlbumArtistColumnVisibility.get(),
+      year: localStoragePlaylistTrackYear.get(),
+      actions: localStoragePlaylistActions.get(),
+    },
+  }),
   getters: {},
   actions: {},
 });
