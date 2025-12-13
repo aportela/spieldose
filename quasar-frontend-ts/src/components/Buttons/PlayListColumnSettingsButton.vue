1f033209045e8a9dae1d3ae86f0eaf1a8f8c3ce0
alex <766f6964+github@gmail.com>
2025-12-13 17:04:43 +0100

    use columns from store

--- quasar-frontend-ts/src/components/Buttons/PlayListColumnSettingsButton.vue
+++ quasar-frontend-ts/src/components/Buttons/PlayListColumnSettingsButton.vue
@@ -1,10 +1,12 @@
 <template>
   <q-btn-dropdown outline no-caps label="Columns" icon="settings">
     <q-list>
-      <q-item dense v-for="column in availablePlayListTableColumns" :key="column.name" v-show="column.name !== 'index'"
-        clickable @click="onToggleColumnVisibility(column)">
-        <q-icon :name="column.visible ? 'visibility' : 'visibility_off'" size="xs" class="q-mr-sm"
-          :class="{ 'text-pink': column.visible, 'text-grey-6': !column.visible }" />
+      <q-item dense v-for="column in playListVisibleColumnsStore.availableColumnDefinitions" :key="column.name"
+        v-show="column.name !== 'index'" clickable @click="onToggleColumnVisibility(column.name)">
+        <q-icon
+          :name="playListVisibleColumnsStore.visibleColumnNames.includes(column.name) ? 'visibility' : 'visibility_off'"
+          size="xs" class="q-mr-sm"
+          :class="{ 'text-pink': playListVisibleColumnsStore.visibleColumnNames.includes(column.name), 'text-grey-6': !playListVisibleColumnsStore.visibleColumnNames.includes(column.name) }" />
         {{ column.label }}
       </q-item>
     </q-list>
@@ -12,13 +14,12 @@
 </template>
 
 <script setup lang="ts">
-  import { type PlayListTableColumn, availablePlayListTableColumns } from "src/types/common";
-  import { usePlayListVisibleColumnsStore } from "src/stores/playListVisibleColumns";
+  import { type PlayListTableColumnName, usePlayListVisibleColumnsStore } from "src/stores/playListVisibleColumns";
 
   const playListVisibleColumnsStore = usePlayListVisibleColumnsStore();
 
-  const onToggleColumnVisibility = (column: PlayListTableColumn): void => {
-    column.visible = !column.visible;
+  const onToggleColumnVisibility = (columnName: PlayListTableColumnName): void => {
+    playListVisibleColumnsStore.toggleColumnVisibility(columnName);
   };
 
 </script>
