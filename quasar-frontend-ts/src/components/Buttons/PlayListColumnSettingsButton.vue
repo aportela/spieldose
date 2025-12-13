637599a4d6034512a1cf15b28cd836ce33fc9251
alex <766f6964+github@gmail.com>
2025-12-13 16:36:12 +0100

    added new button component for playlist columns setttings

---
+++ quasar-frontend-ts/src/components/Buttons/PlayListColumnSettingsButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-btn-dropdown outline no-caps label="Columns" icon="settings">
+    <q-list>
+      <q-item dense v-for="column in availablePlayListTableColumns" :key="column.name" v-show="column.name !== 'index'"
+        clickable @click="onToggleColumnVisibility(column)">
+        <q-icon :name="column.visible ? 'visibility' : 'visibility_off'" size="xs" class="q-mr-sm"
+          :class="{ 'text-pink': column.visible, 'text-grey-6': !column.visible }" />
+        {{ column.label }}
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
+</template>
+
+<script setup lang="ts">
+  import { type PlayListTableColumn, availablePlayListTableColumns } from "src/types/common";
+  import { usePlayListVisibleColumnsStore } from "src/stores/playListVisibleColumns";
+
+  const playListVisibleColumnsStore = usePlayListVisibleColumnsStore();
+
+  const onToggleColumnVisibility = (column: PlayListTableColumn): void => {
+    column.visible = !column.visible;
+  };
+
+</script>
