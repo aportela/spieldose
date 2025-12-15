cf307814026daccef49b09c3fc5e348f1bfe3ada
alex <766f6964+github@gmail.com>
2025-12-15 17:32:14 +0100

    added auto-refresh access token interval

--- quasar-frontend-ts/src/boot/router.ts
+++ quasar-frontend-ts/src/boot/router.ts
@@ -1,46 +1,50 @@
-import { defineBoot } from '#q-app/wrappers'
-import { type GetNewAccessTokenResponse as GetNewAccessTokenResponseInterface } from "src/types/apiResponses";
-import { useSessionStore } from "src/stores/session";
-import { api } from "src/composables/api";
-import { AxiosError } from "axios";
+import { defineBoot } from '#q-app/wrappers';
+import { useSessionStore } from 'src/stores/session';
 
 const sessionStore = useSessionStore();
+const accessTokenCheckInterval = 300; // check every 5 min (300 seconds)
 
 export default defineBoot(async ({ router }) => {
-
+  // at startup, if a refresh token is available, we generate a new access token
   if (!sessionStore.hasAccessToken) {
-    try {
-      const successResponse: GetNewAccessTokenResponseInterface = await api.auth.renewAccessToken();
-      sessionStore.setAccessToken(successResponse.data.accessToken);
-    } catch (e: unknown) {
-      if (e instanceof AxiosError) {
-        if (e.status !== 401) { // 401 is the "normal" error response if we do not have a previous refresh token
-          console.error("Invalid error http response code", e.status);
-        }
-      } else {
-        console.error("Invalid error response", e);
-      }
-    }
+    await sessionStore.refreshAccessToken();
   }
+
+  const refreshAccessTokenIfNeeded = async (): Promise<boolean> => {
+    if (
+      sessionStore.hasAccessToken &&
+      sessionStore.accessTokenExpiresBeforeInterval(accessTokenCheckInterval)
+    ) {
+      return await sessionStore.refreshAccessToken();
+    } else {
+      return false;
+    }
+  };
+
+  // create interval for checking && auto-refresh access token
+  setInterval(() => {
+    refreshAccessTokenIfNeeded()
+      .then(() => {})
+      .catch(() => {});
+  }, accessTokenCheckInterval * 1000);
 
   router.beforeEach((to, _from, next) => {
     if (!to.matched.length) {
-      next({ name: "notFound" });
+      next({ name: 'notFound' });
       return;
     }
     if (sessionStore.hasAccessToken) {
-      if (to.name === "login" || to.name === "register") {
-        next({ name: "index" });
+      if (to.name === 'login' || to.name === 'register') {
+        next({ name: 'index' });
       } else {
         next();
       }
     } else {
-      // TODO: session.hasRefreshToken
-      if (to.name === "login" || to.name === "register") {
+      if (to.name === 'login' || to.name === 'register') {
         next();
       } else {
-        next({ name: "login" });
+        next({ name: 'login' });
       }
     }
   });
-})
+});
