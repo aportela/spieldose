574b6a269bbe754c6c204ddd086a2dc0b35be00e
alex <766f6964+github@gmail.com>
2025-12-18 14:45:33 +0100

    added missing scripts

---
+++ quasar-frontend-ts/public/cliserver.php
@@ -0,0 +1,22 @@
+<?php
+
+// https://gist.github.com/ckressibucher/e37520cf2f1d08ec56d250c54d96ed72
+
+// public/cliserver.php (router script)
+
+if (PHP_SAPI !== 'cli-server') {
+    die('this is only for the php development server');
+}
+
+$file = (is_string($_SERVER['DOCUMENT_ROOT']) && is_string($_SERVER['SCRIPT_NAME'])) ? $_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['SCRIPT_NAME'] : null;
+
+if ($file !== null && is_file($file)) {
+    // probably a static file...
+    return false;
+}
+
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+// if needed, fix also 'PATH_INFO' and 'PHP_SELF' variables here...
+
+// require the entry point
+require __DIR__ . '/index.php';
