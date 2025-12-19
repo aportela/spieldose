380ca6ebf7e67c6eb298dc2f9de1b45432e50034
alex <766f6964+github@gmail.com>
2025-12-19 01:38:11 +0100

    new consts for sizes, added new methods for setting local & remote images from path id / url

--- phpslim-api/Spieldose/Entities/EntityImages.php
+++ phpslim-api/Spieldose/Entities/EntityImages.php
@@ -6,6 +6,19 @@ namespace Spieldose\Entities;
 
 final class EntityImages
 {
+    public const string LOCAL_URL_MASK = "api2/local_thumbnail?width=%d&height=%d&quality=90&pathId=%s";
+    public const string REMOTE_URL_MASK = "api2/remote_thumbnail?width=%d&height=%d&quality=90&url=%s";
+
+    public const int SMALL_WIDTH = 100;
+    public const int SMALL_HEIGTH = 100;
+
+    public const int MEDIUM_WIDTH = 400;
+    public const int MEDIUM_HEIGTH = 400;
+
+    public const int BIG_WIDTH = 800;
+    public const int BIG_HEIGTH = 800;
+
+
     public string|null $small;
     public string|null $medium;
     public string|null $big;
@@ -37,5 +50,19 @@ final class EntityImages
         $this->small = $this->parseAndValidate($small);
         $this->medium = $this->parseAndValidate($medium);
         $this->big = $this->parseAndValidate($big);
+    }
+
+    public function setLocal(string $pathId): void
+    {
+        $this->small = $this->parseAndValidate(sprintf(self::LOCAL_URL_MASK, self::SMALL_WIDTH, self::SMALL_HEIGTH, $pathId));
+        $this->medium = $this->parseAndValidate(sprintf(self::LOCAL_URL_MASK, self::SMALL_WIDTH, self::SMALL_HEIGTH, $pathId));
+        $this->big = $this->parseAndValidate(sprintf(self::LOCAL_URL_MASK, self::SMALL_WIDTH, self::SMALL_HEIGTH, $pathId));
+    }
+
+    public function setRemote(string $url): void
+    {
+        $this->small = $this->parseAndValidate(sprintf(self::REMOTE_URL_MASK, self::SMALL_WIDTH, self::SMALL_HEIGTH, urlencode($url)));
+        $this->medium = $this->parseAndValidate(sprintf(self::REMOTE_URL_MASK, self::SMALL_WIDTH, self::SMALL_HEIGTH, urlencode($url)));
+        $this->big = $this->parseAndValidate(sprintf(self::REMOTE_URL_MASK, self::SMALL_WIDTH, self::SMALL_HEIGTH, urlencode($url)));
     }
 }
