ae5091b9941496bd45b78a619b1ee46e8238a080
alex <766f6964+github@gmail.com>
2025-12-20 22:12:54 +0100

    only show spieldose  awesome mix labels if no image prop found, on image errors show "no image" label

--- quasar-frontend-ts/src/components/Widgets/Visualizations/StaticAlbumCoverImage.vue
+++ quasar-frontend-ts/src/components/Widgets/Visualizations/StaticAlbumCoverImage.vue
@@ -2,85 +2,96 @@
   <div :class="{ 'cursor-pointer': clickable }" @click="onClick">
     <q-img v-if="currentImage" :src="currentImage" @error="currentImage = failbackImage ?? null" :ratio="1" width="100%"
       spinner-color="pink" />
-    <div v-else class="no-image overflow-hidden">
+    <div v-else-if="!props.image" class="no-image overflow-hidden">
       <p class="q-mt-xl no-image-label no-image-top-label text-grey-7">Spieldose</p>
       <p class="q-mt-xl no-image-label no-image-bottom-label text-pink">Awesome Mix Vol. {{ currentDayOfYear }}</p>
+    </div>
+    <div v-else class="no-image-or-error row items-center justify-center">
+      [ no image ]
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { nextTick, ref, watch, computed } from "vue";
-import { date } from "quasar";
+  import { nextTick, ref, watch, computed } from "vue";
+  import { date } from "quasar";
 
-interface StaticAlbumCoverImageProps {
-  image?: string | null;
-  failbackImage?: string | null;
-  clickable?: boolean;
-};
+  interface StaticAlbumCoverImageProps {
+    image?: string | null;
+    failbackImage?: string | null;
+    clickable?: boolean;
+  };
 
-const props = withDefaults(defineProps<StaticAlbumCoverImageProps>(), {
-  image: null,
-  failbackImage: null,
-  clickable: false,
-});
+  const props = withDefaults(defineProps<StaticAlbumCoverImageProps>(), {
+    image: null,
+    failbackImage: null,
+    clickable: false,
+  });
 
-const emit = defineEmits(['onClick']);
+  const emit = defineEmits(['onClick']);
 
-const currentImage = ref<string | null>(props.image ?? props.failbackImage ?? null);
+  const currentImage = ref<string | null>(props.image ?? props.failbackImage ?? null);
 
-const currentDayOfYear = computed(() => date.formatDate(new Date(), "DDD"));
+  const currentDayOfYear = computed(() => date.formatDate(new Date(), "DDD"));
 
-watch(() => props.image, (newValue: string | null) => {
-  currentImage.value = null;
-  if (newValue || props.failbackImage) {
-    nextTick()
-      .then(() => {
-        currentImage.value = newValue ?? props.failbackImage;
-      }).catch((e) => {
-        console.error(e);
-      });
-  }
-});
+  watch(() => props.image, (newValue: string | null) => {
+    currentImage.value = null;
+    if (newValue || props.failbackImage) {
+      nextTick()
+        .then(() => {
+          currentImage.value = newValue ?? props.failbackImage;
+        }).catch((e) => {
+          console.error(e);
+        });
+    }
+  });
 
-const onClick = () => {
-  if (props.clickable) {
-    emit('onClick');
-  }
-};
+  const onClick = () => {
+    if (props.clickable) {
+      emit('onClick');
+    }
+  };
 
 </script>
 
 <style lang="css">
-@import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
 
-.no-image {
-  width: 98%;
-  margin: 1% auto;
-  height: auto;
-  aspect-ratio: 1;
-}
+  .no-image-or-error {
+    width: 98%;
+    height: auto;
+    aspect-ratio: 1;
+    font-family: "Gochi Hand", cursive;
+    font-size: 3em;
+  }
 
-.no-image-label {
-  font-family: "Gochi Hand", cursive;
-  font-weight: 400;
-  font-style: normal;
-  width: 100%;
-  white-space: nowrap;
-  text-align: center;
-  display: inline-block;
-  transform-origin: center;
-}
+  .no-image {
+    width: 98%;
+    margin: 1% auto;
+    height: auto;
+    aspect-ratio: 1;
+  }
 
-.no-image-top-label {
-  color: hsl(46, 93%, 45%);
-  font-size: 6em;
-  transform: rotate(-5deg);
-}
+  .no-image-label {
+    font-family: "Gochi Hand", cursive;
+    font-weight: 400;
+    font-style: normal;
+    width: 100%;
+    white-space: nowrap;
+    text-align: center;
+    display: inline-block;
+    transform-origin: center;
+  }
+
+  .no-image-top-label {
+    color: hsl(46, 93%, 45%);
+    font-size: 6em;
+    transform: rotate(-5deg);
+  }
 
 
-.no-image-bottom-label {
-  font-size: 3em;
-  transform: rotate(-3deg);
-}
+  .no-image-bottom-label {
+    font-size: 3em;
+    transform: rotate(-3deg);
+  }
 </style>
