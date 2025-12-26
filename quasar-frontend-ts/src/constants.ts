54ff6eb650016a7a0312dbcae007bc674ea83a6b
alex <766f6964+github@gmail.com>
2025-12-26 12:09:58 +0100

    fixed thumbnail api url

--- quasar-frontend-ts/src/constants.ts
+++ quasar-frontend-ts/src/constants.ts
@@ -1,12 +1,12 @@
-export const GITHUB_PROJECT_URL = "http://github.com/aportela/spieldose";
+export const GITHUB_PROJECT_URL = 'http://github.com/aportela/spieldose';
 // this (prefix/namespace) is for preventing sharing keys on web sites hosted on same url
-export const LOCAL_STORAGE_NAMESPACE = "spieldose#";
+export const LOCAL_STORAGE_NAMESPACE = 'spieldose#';
 
-export const SERVER_API_BASE_PATH = "api2";
+export const SERVER_API_BASE_PATH = 'api2';
 
-export const DEFAULT_LOCALE = "en-US";
+export const DEFAULT_LOCALE = 'en-US';
 
-export const THUMBNAIL_API_URL = `/${SERVER_API_BASE_PATH}/remote_thumbnail`;
+export const THUMBNAIL_API_URL = `/${SERVER_API_BASE_PATH}/thumbnail/remote`;
 
 export const SMALL_THUMBNAIL_WIDTH = 200;
 export const SMALL_THUMBNAIL_HEIGHT = 200;
